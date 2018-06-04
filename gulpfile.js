6a9aed23b5c40df804223abbddb5cfae96b6762e
Simon <Simon_Freelance@hotmail.com>
2018-06-04 15:44:27 -0400

    Error handling completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,29 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+function customPlumber(errTitle) {
+    return plumber({
+        errorHandler: notify.onError({
+            // Customizing error title
+            title: errTitle || "Error running Gulp",
+            message: "Error: <%= error.message %>",
+        })
+    });
+}
 
-gulp.task('less', function() {
-    return gulp.src('./less/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('./css'));
-});
+function errorHandler(err) {
+            console.log(err.toString());
+
+            this.emit('end');
+        }
+gulp.task('less', function () {
+            return gulp.src('./less/*.less')
+                .pipe(customPlumber('Error Running Less'))
+                .pipe(less())
+                .pipe(gulp.dest('./css'));
+        });
+
+    gulp.task('watch', function () {
+        gulp.watch('./less/**/*.less', ['less']);
+    })
