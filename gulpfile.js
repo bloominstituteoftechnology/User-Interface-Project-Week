3a6b2fb0269402ac0283962b0b4a09e8846df089
Simon <Simon_Freelance@hotmail.com>
2018-06-04 15:50:40 -0400

    Browser sync setup completed

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
+const browserSync = require('browser-sync');
 function customPlumber(errTitle) {
     return plumber({
         errorHandler: notify.onError({
@@ -12,18 +13,31 @@ function customPlumber(errTitle) {
     });
 }
 
-function errorHandler(err) {
-            console.log(err.toString());
-
-            this.emit('end');
-        }
-gulp.task('less', function () {
-            return gulp.src('./less/*.less')
-                .pipe(customPlumber('Error Running Less'))
-                .pipe(less())
-                .pipe(gulp.dest('./css'));
-        });
-
-    gulp.task('watch', function () {
-        gulp.watch('./less/**/*.less', ['less']);
+gulp.task('browserSync', function () {
+    browserSync({
+        server: {
+            baseDir: '.'
+        },
     })
+})
+
+function errorHandler(err) {
+    console.log(err.toString());
+
+    this.emit('end');
+}
+gulp.task('less', function () {
+    return gulp.src('./less/*.less')
+        .pipe(customPlumber('Error Running Less'))
+        .pipe(less())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.reload({
+            stream: true,
+        }))
+});
+
+
+
+gulp.task('watch', ['browserSync', 'less'], function () {
+    gulp.watch('./less/**/*.less', ['less']);
+});
