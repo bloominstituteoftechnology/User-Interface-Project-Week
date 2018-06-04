ec84b6b116b6feee644860ee910e3d0deeed6fc2
JJ Ashcraft <jj@jjashcraft.com>
2018-06-04 19:00:08 -0400

    header updated, nav in progress

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// JS goes here
+const menuIcon = document.querySelector('#navhamburger');
+const openMenu = document.querySelector('.nav-fullscreen');
+
+menuIcon.addEventListener('click', () => {
+    toggleMenu()
+});
+
+const toggleMenu = function () {
+ openMenu.classList.toggle('open')
+ if (menuIcon.getAttribute.src === '/img/nav-hamburger.png') {
+menuIcon.setAttribute.src = '/img/nav-hamburger-close.png';
+ } else {
+     menuIcon.setAttribute.src = '/img/nav-hamburger.png';
+ }
+ menuIcon.classList.toggle('index-top')
+
+}
+
