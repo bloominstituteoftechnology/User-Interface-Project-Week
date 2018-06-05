bc87844fc6e64c8eb507de6cb0b811a106bc629b
sibhat <sibhat.temelso@gmail.com>
2018-06-05 14:28:59 -0700

    navigation link animated

--- js/index.js
+++ js/index.js
@@ -4,14 +4,13 @@ let scroll = document.querySelector('.header__menu');
 window.addEventListener('scroll', function(e) {
     // console.log(scroll.style.backgroundColor);
     if(e.path[1].scrollY > 210){
-        scroll.style.backgroundColor = '#808080ad'
-        scroll.style.marginTop = '0'
-        scroll.style.opacity = '0.8'
+        scroll.style.backgroundColor = '#5c788f';
+        scroll.style.marginTop = '0';
+        scroll.style.opacity = '0.9';
 
     }else{
-        scroll.style.backgroundColor = 'transparent'
-        scroll.style.opacity = '1'
-        scroll.style.marginTop = '3rem'        
-        
+        scroll.style.backgroundColor = 'transparent';
+        scroll.style.opacity = '1';
+        scroll.style.marginTop = '3rem';
     }
 });
