ba7d5a468cca821198cdc2f6c3f910079f7acd45
sibhat <sibhat.temelso@gmail.com>
2018-06-05 14:44:39 -0700

    greensock added to navigation menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,15 @@
 let menu = document.querySelector("#menu");
 let headrMenu = document.querySelector('#headrMenu');
+let isOpen = false;
+let time = 1;
 headrMenu.addEventListener('click', (e) =>{
-    menu.classList.toggle('menu--display');
+    // menu.classList.toggle('menu--display');
     e.target.classList.toggle('header__menu--photo--close');
+    if(isOpen){
+        TweenMax.to(menu, time, {display: 'none', opacity: 0});
+    }else if(!isOpen){
+        TweenMax.to(menu, time, {display: 'block', opacity: 1});
+    }
+    isOpen = !isOpen;
 });
+
