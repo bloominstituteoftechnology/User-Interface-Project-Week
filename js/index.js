3ea4a89c7874b1a77efdf0c0ec021c5541102867
Sukhada Gholba <sukhadagholba@gmail.com>
2018-06-05 12:36:01 -0700

    added js for service page to select content when a specific button is clicked

--- js/index.js
+++ js/index.js
@@ -1,1 +1,69 @@
 // JS goes here
+
+class ServiceButtonRow {
+  constructor(element) {
+    this.element = element;
+    this.buttons = this.element.querySelectorAll('.service-btn');
+    this.buttons = Array.from(this.buttons).map(eachbutton => {
+      return new ButtonService(eachbutton, this);
+    });
+    this.activeLink = this.buttons[0];
+    this.init();
+  }
+
+  init() {
+    this.activeLink.select();
+  }
+
+  updateActive(newActive) {
+    this.activeLink.deselect();
+    this.activeLink = newActive;
+  }
+
+  getTab(data) {
+    return document.querySelector(`.button-content[data-tab="${data}"]`);
+
+  }
+
+}
+
+class ButtonService {
+  constructor(element, parent) {
+    this.element = element;
+    this.serviceButtonRow = parent;
+    this.buttonContent = parent.getTab(this.element.dataset.tab);
+    this.buttonContent = new ButtonContent(this.buttonContent);
+    this.element.addEventListener('click', () => {
+      this.serviceButtonRow.updateActive(this);
+      this.select();
+    });
+  };
+
+  select() {
+    this.element.classList.add('button-selected')
+    this.buttonContent.select();
+  }
+
+  deselect() {
+    this.element.classList.remove('button-selected');
+    this.buttonContent.deselect();
+  }
+}
+
+class ButtonContent {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    this.element.classList.add('button-content-selected');
+  }
+
+  deselect() {
+    this.element.classList.remove('button-content-selected');
+  }
+}
+
+
+let serviceButtonsRow= document.querySelectorAll('.service-btn-row');
+serviceButtonsRow  = Array.from(serviceButtonsRow).map( item => new ServiceButtonRow(item));
