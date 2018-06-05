dfaba602cda0d3d91f6bc5cd5970abeb4f4deaeb
JJ Ashcraft <jj@jjashcraft.com>
2018-06-05 15:54:41 -0400

    variables added, bootstrap converted from cdn to LESS file

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+// scrollMagic (with gsap tween animation for alpha channel on bg color)
+
+const controller = new ScrollMagic.Controller();
+const navMenu = document.querySelector('.nav-container');
+let scrollMenuMod = new TweenMax.to(navMenu, 1, {
+    backgroundColor: 'rgba(157, 167, 177, 0.95)',
+});
+
+const scene = new ScrollMagic.Scene({
+        offset: 10,
+    })
+    .setTween(scrollMenuMod)
+    .addTo(controller);
+
+    // navigation open/close script
+
 const openIcon = document.querySelector('#navhamburger');
 const closeIcon = document.querySelector('#navhamburgerclose');
 const openMenu = document.querySelector('.nav-fullscreen');
@@ -13,14 +29,4 @@ const toggleMenu = function () {
  openMenu.classList.toggle('open');
 
 }
-const controller = new ScrollMagic.Controller();
-const navMenu = document.querySelector('.nav-container');
-let scrollMenuMod = new TweenMax.to(navMenu, 1, {
-    backgroundColor: 'rgba(157, 167, 177, 0.95)',
-});
 
-const scene = new ScrollMagic.Scene({
-    offset: 10, 
-})
-.setTween(scrollMenuMod)
-.addTo(controller);
