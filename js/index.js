12ce4a973e99464e0bc78d4a8a23c8b1b0bb3d5f
JJ Ashcraft <jj@jjashcraft.com>
2018-06-05 15:12:57 -0400

    scrollMagic added
    -event added for scroll after 10px.
    -fixed fullscreen Nav menu (changed to position:fixed).

--- js/index.js
+++ js/index.js
@@ -12,6 +12,15 @@ closeIcon.addEventListener('click', () => {
 const toggleMenu = function () {
  openMenu.classList.toggle('open');
 
-
 }
+const controller = new ScrollMagic.Controller();
+const navMenu = document.querySelector('.nav-container');
+let scrollMenuMod = new TweenMax.to(navMenu, 1, {
+    backgroundColor: 'rgba(157, 167, 177, 0.95)',
+});
 
+const scene = new ScrollMagic.Scene({
+    offset: 10, 
+})
+.setTween(scrollMenuMod)
+.addTo(controller);
