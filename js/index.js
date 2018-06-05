c669b544769e1da6e52c091388f7f077ba734fe6
Alec <afroenygam@gmail.com>
2018-06-04 23:42:37 -0400

    Created popout menu began adding JS functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+function menuToggle() {
+  document.querySelector('.cta').toggle('.nav-expanded');
+  document.querySelector('.menu').toggle('.menu-toggle');
+}
+
+document.querySelector.('.hamburger').addEventListener('click', (e) => {
+  e.target.setAttribute('src', './img/nav-hamburger-close.png');
+  menuToggle();
+})
+
+// document.body.addEventListener('wheel', function() {
+//   console.log('Is listening');
+//   document.querySelector('.custom-nav').classList.add('nav-scroll');
+// });
