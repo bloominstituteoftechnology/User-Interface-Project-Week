188f0038ef4cad2f0945dda6087430a34c8e1b62
Jasper <joshtietz11@gmail.com>
2018-06-05 18:18:31 -0500

    html and css all done working on the nav menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// JS goes here
+let header = document.querySelector(".header")
+document.addEventListener("scroll", () => {
+  header.classList.add("opacity")
+})
+
+let menu = document.querySelector(".menu")
+let navbtn = document.querySelector(".header img")
+navbtn.addEventListener("click", () => {
+  header.classList.toggle("hidden")
+})
+navbtn.addEventListener("click", () => {
+  menu.classList.toggle("hidden")
+})
