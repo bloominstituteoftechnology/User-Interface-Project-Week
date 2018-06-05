6e4f20373552ee95a2972e1422584cd2c81660f4
sibhat <sibhat.temelso@gmail.com>
2018-06-05 13:44:39 -0700

    service tab and navigation animation completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // JS goes here
+let scroll = document.querySelector('.header__menu');
+
+window.addEventListener('scroll', function(e) {
+    // console.log(scroll.style.backgroundColor);
+    if(e.path[1].scrollY > 210){
+        scroll.style.backgroundColor = '#808080ad'
+        scroll.style.marginTop = '0'
+        scroll.style.opacity = '0.8'
+
+    }else{
+        scroll.style.backgroundColor = 'transparent'
+        scroll.style.opacity = '1'
+        scroll.style.marginTop = '3rem'        
+        
+    }
+});
