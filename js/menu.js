8173ef4a1aaa6b6f29c904c8a44e9f8bd89bac95
LBWright <lwright.js@gmail.com>
2018-06-05 17:58:20 -0500

    Completed javascript menu functionality

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,18 @@
 let navbar = document.querySelector('.navbar');
+let hamburger = document.querySelector('.hamburger');
+let hamburgerImage = document.querySelector('.navbar img')
 
 window.addEventListener('scroll', ()=>{
-	console.log('scrolling')
 	navbar.style.background = '#777';
 	navbar.style.opacity = '.95';
 })
 
-navbar.addEventListener('click', ()=> {
-	console.log(`i've been clicked`)
+hamburger.addEventListener('click', ()=> {
+		navbar.classList.toggle('navbar-active')
+		if (hamburger.src.indexOf('nav-hamburger.png')!=-1) {
+            hamburger.src  = 'img/nav-hamburger-close.png';
+        }
+         else {
+          hamburger.src = 'img/nav-hamburger.png';
+       }
 })
