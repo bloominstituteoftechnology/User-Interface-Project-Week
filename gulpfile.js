a231c1a4cb41057f7fa9646f26cf79f35205cd13
Tyson Gushiken <tysonism@gmail.com>
2018-06-05 00:13:42 -0700

    Install gulp-plumber to handle gulp-less errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,32 @@
 'use strict';
 
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var less = require('gulp-less');
+let gulp = require('gulp');
+let browserSync = require('browser-sync').create();
+let less = require('gulp-less');
+let plumber = require('gulp-plumber');
+
+gulp.task('less', function() {
+  return gulp.src('less/*.less')
+    .pipe(plumber(function (error) {
+      console.log(error);
+      this.emit('end');
+      this.destroy();
+    }))
+    .pipe(less())
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
+});
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['less'], function() {
 
-    browserSync.init({
-        server: './'
-    });
+  browserSync.init({
+      server: './'
+  });
 
-    gulp.watch('less/**/*.less', ['less']);
-    gulp.watch('*.html').on('change', browserSync.reload);
-    gulp.watch('js/**/*.js').on('change', browserSync.reload);
-});
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('less', function() {
-    return gulp.src('less/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('css/'))
-        .pipe(browserSync.stream());
+  gulp.watch('less/**/*.less', ['less']);
+  gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch('js/**/*.js').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
