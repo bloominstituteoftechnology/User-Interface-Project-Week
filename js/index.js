511b62d0fa34bfbc3b259ca637baa0573ea3b265
DirupT <iamcooled@gmail.com>
2018-06-04 21:03:32 -0500

    updated navbar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // JS goes here
+let nav = document.querySelector('.navigation');
+
+document.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+        nav.setAttribute('style', 'background: gray; opacity: 0.95');
+    } else {
+        nav.removeAttribute('style', 'background');
+    }
+});
