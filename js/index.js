517e6b2a3e4133df137bbb1a55968d789cc2c962
Jasper <joshtietz11@gmail.com>
2018-06-06 16:14:09 -0500

    MVP completed

--- js/index.js
+++ js/index.js
@@ -14,3 +14,61 @@ menubtn.addEventListener("click", () => {
   header.classList.toggle("hidden")
     menu.classList.toggle("hidden")
 })
+
+class Sections {
+  constructor(element) {
+    this.element = element;
+    this.buttons = this.element.querySelectorAll(".button");
+    this.buttons = Array.from(this.buttons).map(button => {
+      return new Button(button, this);
+    });
+    this.activebtn = this.buttons[0];
+    this.init();
+  }
+  init() {
+    this.activebtn.select();
+  }
+  updateActive(newActive) {
+    this.activebtn.deselect();
+    this.activebtn = newActive;
+  }
+  getTopic(data) {
+    return this.element.querySelector(`.tab[data-topic="${data}"]`);
+  }
+}
+
+class Button {
+  constructor(element, parent) {
+    this.element = element;
+    this.sections = parent
+    this.tab = parent.getTopic(this.element.dataset.topic);
+    this.tab = new Tab(this.tab);
+    this.element.addEventListener("click", () => {
+      this.sections.updateActive(this);
+      this.select();
+    });
+  };
+  select() {
+    this.element.classList.add("button-active");
+    this.tab.select();
+  }
+  deselect() {
+    this.element.classList.remove("button-active");
+    this.tab.deselect();
+  }
+}
+
+class Tab {
+  constructor(element) {
+    this.element = element;
+  }
+  select() {
+    this.element.classList.add("tab-active")
+  }
+  deselect() {
+    this.element.classList.remove("tab-active")
+  }
+}
+
+let sections = document.querySelectorAll(".sections")
+sections = Array.from(sections).map(element => new Sections(element));
