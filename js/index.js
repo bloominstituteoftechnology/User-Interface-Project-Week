945b264701bdb1ff6514bf9ab864d89520862332
Erin Meredith <ekmeredith@att.net>
2018-06-06 18:46:37 -0400

    almost finished services mobile layout, but somehow lost my tab functions

--- js/index.js
+++ js/index.js
@@ -1,37 +1,5 @@
 // JS goes here
 
-//Site-wide//
-const overlay = document.querySelector(".overlay");
-const btn = document.querySelector(".hamburger");
-console.log(btn);
-btn.addEventListener("click", function() {
-  overlay.classList.toggle("hidden");
-});
-
-/*class Expand {
-  constructor(element) {
-    this.element = element;
-    this.button = this.element.querySelectorAll(".hamburger");
-    this.content = this.element.querySelectorAll(".expand-content");
-    this.button.addEventListener("click", function() {
-      this.toggleContent();
-    });
-  }
-
-  toggleContent() {
-    this.content.classList.toggle("hidden");
-  }
-}
-let expand = document.querySelectorAll('.menu');
-expand = Array.from(expand).map( element => new Expand(element));*/
-
-
-let navbar = document.querySelector(".navbar");
-window.addEventListener("scroll", () => {
-  navbar.classList.add("navbar-scroll");
-});
-
-
 
 //Services page//
 class Tabs {
@@ -101,4 +69,36 @@ class TabsItem {
 
 let tabs = document.querySelectorAll(".tabs");
 tabs = Array.from(tabs).map( element => new Tabs(element));
+
+
+//Site-wide//
+const overlay = document.querySelector(".overlay");
+const btn = document.querySelector(".hamburger");
+console.log(btn);
+btn.addEventListener("click", function() {
+  overlay.classList.toggle("hidden");
+});
+
+/*class Expand {
+  constructor(element) {
+    this.element = element;
+    this.button = this.element.querySelectorAll(".hamburger");
+    this.content = this.element.querySelectorAll(".expand-content");
+    this.button.addEventListener("click", function() {
+      this.toggleContent();
+    });
+  }
+
+  toggleContent() {
+    this.content.classList.toggle("hidden");
+  }
+}
+let expand = document.querySelectorAll('.menu');
+expand = Array.from(expand).map( element => new Expand(element));*/
+
+
+let navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  navbar.classList.add("navbar-scroll");
+});
 
