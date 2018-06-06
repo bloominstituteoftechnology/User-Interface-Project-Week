34d7d5b015510d116904a0d4213f48ee14f4dae9
snowcoding <vimalshah77@gmail.com>
2018-06-06 14:45:23 -0700

    added overlay navigation menu to all pages

--- js/index.js
+++ js/index.js
@@ -12,3 +12,24 @@ window.addEventListener('scroll',function(e){
     nav.classList.remove('scroll')
   }
 })
+
+
+let overlay = document.querySelector(".overlay");
+let menuBtn = document.querySelector(".menu-btn img");
+let closeBtn = document.querySelector(".close-btn");
+
+console.log(menuBtn)
+
+menuBtn.addEventListener('click', function() {
+  // I want overlay to display block now
+
+  if (menuBtn.classList.contains('open')){
+    overlay.style.display = "none";
+    menuBtn.src="../img/nav-hamburger.png"
+    menuBtn.classList.remove('open')  
+  }else {
+    overlay.style.display = "block";
+    menuBtn.src="../img/nav-hamburger-close.png"
+    menuBtn.classList.add('open')
+  }
+});
