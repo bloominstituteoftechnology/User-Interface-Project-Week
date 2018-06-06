efe8ec4877ab374eae7edb383636198a57840f15
TangledTessellations <mcevoya@oregonstate.edu>
2018-06-06 12:49:16 -0700

    Finished popout menu with changing menu icon

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // JS goes here
+
+// Header menu button
+let menuButton = document.querySelector(".menu-button");
+let expandedMenu = document.querySelector(".expanded-menu");
+let hamburger = document.querySelector(".hamburger")
+
+menuButton.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-close");
+  expandedMenu.classList.toggle("display_none");
+});
