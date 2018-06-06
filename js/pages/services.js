4a4ac76dafd4ad62bedb92b424ca1127a017b6e7
Tyson Gushiken <tysonism@gmail.com>
2018-06-06 11:36:22 -0700

    Create services html for desktop and phone

--- js/pages/services.js
+++ js/pages/services.js
@@ -1,2 +1,17 @@
-// JS goes here
-console.log('services');
+(function (library) {
+
+    library(window, document);
+
+}
+
+(function (window, document) {
+
+  init();
+
+  function init() {
+    const navElement = document.querySelector('nav');
+    const nav = new Navigation(navElement, window);
+    nav.init();
+  }
+
+}));
