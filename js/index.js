b78f429bf4d6585e63c52f84faa813843ac972a3
Dil Sim <dilyasom@gmail.com>
2018-06-06 12:13:21 -0700

    eventListener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
 // JS goes here
+
+// function openNav() {
+//     let element = document.getElementById("myNav");
+//     element.classList.remove("hidden");
+//     // element.classList.add("overlay");
+
+// }
+
+// function closeNav() {
+//     var element = document.getElementById("myNav")
+//     element.classList.add("hidden");
+// }
+// let overlay = document.querySelector(".overlay");
+// let menuBtn = document.querySelector(".menuBtn");
+// let closeBtn = document.querySelector('.close-btn');
+
+// menuBtn.addEventListener('click', function () {
+//     // I want overlay to display block now
+//     console.log('clicked');
+//     overlay.style.display = "block";
+// });
+// closeBtn.addEventListener('click', function () {
+//     overlay.style.display = 'none';
+// })
+
+
+let overlay = document.querySelector(".overlay");
+let menuBtn = document.querySelector(".nav-btn");
+let closeBtn = document.querySelector(".close-btn");
+
+console.log(menuBtn)
+
+menuBtn.addEventListener('click', function () {
+    // I want overlay to display block now
+    overlay.style.display = "block";
+});
+
+closeBtn.addEventListener('click', function () {
+    console.log('clicked');
+    // I want overlay to display none now
+    overlay.style.display = "none";
+
+});
