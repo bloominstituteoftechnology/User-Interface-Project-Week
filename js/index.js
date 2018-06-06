ca414f0043b6e954780ed6f63beff2037e664e36
M.Real <mreal126@protonmail.com>
2018-06-06 16:39:24 -0400

    home mobile done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // JS goes here
+
+/* let overlay = document.querySelector(".overlay");
+let hamburger = document.querySelector(".hamburger");
+let closeBtn = document.querySelector(".close-btn");
+console.log(hamburger);
+
+hamburger.addEventListener('click', function() {
+overlay.style.display = "block";
+});
+
+closeBtn.addEventListener('click', function() {
+overlay.style.display = "none";
+});
+
+map through buttons on services page, giving each an eventListener
+Lambda page with 4 tabs is identical...
+*/
