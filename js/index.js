20ce47cbddbf0257890c6e89396a7f2ce0ba9f78
raymondgrc <raymond_grc@yahoo.com>
2018-06-06 16:42:36 -0500

    added overlay, weird bug with close icon

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@
 var menuItems = document.querySelectorAll(".services-content-menu li");
 var innerContent = document.querySelectorAll(".services-inner-content");
 var navBar = document.querySelector(".mynav");
+var overlayButton = document.querySelector(".mynav img");
+var overlay = document.querySelector(".overlay");
+console.log(overlayButton.getAttribute("src"))
 window.addEventListener("scroll", function(){
   navBar.style.backgroundColor = "#9DA7B1";
   navBar.style.opacity = ".95";
@@ -18,4 +21,13 @@ menuItems.forEach(function (button,index){
        innerContent[index].removeAttribute("hidden");
     }
   })
+});
+overlayButton.addEventListener("click", function() {
+if (overlay.style.display == "flex"){
+overlayButton.setAttribute("src", "img\\nav-hamburger.png");
+console.log("hi")
+overlay.style.display = "none";
+}
+else overlay.style.display = "flex";
+overlayButton.setAttribute("src", "img\\nav-hamburger-close.png");
 });
