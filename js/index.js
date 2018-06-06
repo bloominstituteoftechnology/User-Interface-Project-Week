7e90d8a48c63d8eeb86dc3c6375c5bbef3701ac2
Katie Gorbell <katiegorbell@gmail.com>
2018-06-06 17:44:46 -0500

    working on any JS component

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-// JS goes here
+// GLOBAL FUNCTIONALITY
+
+//(in-progress) navigation overlays screeen when icon is clicked
+
+
+// (in-progress) turns scrollbar to opaque grey when user scrolls page 
+let navBar = () => document.querySelector('.navigation');
+window.addEventListener('scroll', () => {
+    navBar.setAttribute('style', 'background-color: #9DA7B1; opacity: .95' )
+});
+
+
+
+
+
+
+// SERVICES PAGE
+
+// class serviceButtons {
+//     constructor(service) {
+//         this.tab = service;
+//         this.button = this.service.querySelectorAll('.tab-button');
+
+//     }
+// }
+
+// let tabs = document.querySelectorAll('.tab-button')
