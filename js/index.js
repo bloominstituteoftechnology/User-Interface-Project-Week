372fe92b8dfe2c870218997b1f69dcc27663f942
Terrance Danckert <tbarnett2020@gmail.com>
2018-06-06 18:10:52 -0400

    Working Services**

--- js/index.js
+++ js/index.js
@@ -18,9 +18,3 @@ window.onclick = function(event) {
     }
   }
 };
-
-// $(".menu-toggle").on("click", function() {
-//   $(this).toggleClass("on");
-//   $(".menu-section").toggleClass("on");
-//   $("nav ul").toggleClass("hidden");
-// });
