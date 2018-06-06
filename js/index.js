469930a5f65d2c76df04283a0121e3cdd20a785e
sibhat <sibhat.temelso@gmail.com>
2018-06-05 21:25:31 -0700

    aos js added, with some on scorll effect on the pages

--- js/index.js
+++ js/index.js
@@ -14,3 +14,9 @@ window.addEventListener('scroll', function(e) {
         scroll.style.marginTop = '3rem';
     }
 });
+
+var tl = new TimelineLite({paused:true});
+tl.from('.header__menu--logo', 0.5, {left:"-=60px", ease:Back.easeOut})
+  .from('.header__text', 0.5, {width:"0px", alpha:0}, "-=0.2")
+//   .staggerFrom('.header__text', 0.5, {top:"-=30px", rotation:"-40deg", alpha:0, scale:1.8, ease:Back.easeOut}, 0.2);
+tl.restart();
