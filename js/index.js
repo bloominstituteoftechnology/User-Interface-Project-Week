89e4a316901abbf68bdc17bcf43db2b6862acbe9
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-06-06 18:18:11 -0400

    Fourth commit
    Attempted nav buttons
    Built overlay
    Can close overlay when visible but can't open when hidden
    Refactored homepage project images for desktop and mobile

--- js/index.js
+++ js/index.js
@@ -66,3 +66,20 @@ class Tabs {
   
   let tabs = document.querySelectorAll('.tabs');
   tabs = Array.from(tabs).map( tab => new Tabs(tab));
+
+let overlay = document.querySelector(".overlay");
+let menuBtn = document.querySelector(".nav-btn");
+let closeBtn = document.querySelector(".close-btn");
+
+console.log(menuBtn)
+
+menuBtn.addEventListener('click', function() {
+  // I want overlay to display block now
+  overlay.style.display = "block";
+  console.log()
+});
+
+closeBtn.addEventListener('click', function() {
+  // I want overlay to display none now
+  overlay.style.display = "none";
+});
