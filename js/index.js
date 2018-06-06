e1efa96a281f097d5c4bcf3d5056d5645a5b5082
Wdifulvio523 <w.difulvio523@gmail.com>
2018-06-06 13:18:30 -0400

    working on dropdown menu

--- js/index.js
+++ js/index.js
@@ -4,15 +4,44 @@ class Dropdown {
     constructor(element) {
 
       this.element = element;
+
       this.button = this.element.querySelector('.dropdown-button');
       this.content = this.element.querySelector('.dropdown-content');
+      // this.close = this.element.querySelector('close-btn');
       this.button.addEventListener("click", () => { this.toggleContent() });
+      // this.close.addEventListener("click", () => { this.toggleClose() });
     }
   
     toggleContent() {
       this.content.classList.toggle("dropdown-hidden");
+      this.button.classList.toggle("dropdown-hidden");
+      console.log("does this work")
     }
+
+    // toggleClose() {
+    //   this.content.classList.toggle("dropdown-hidden");
+    //   console.log("does this work")
+    // }
   }
   
   let dropdowns = document.querySelectorAll('.dropdown');
   dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
+// JOSH CODE
+
+
+// let overlay = document.querySelector(".overlay");
+// let menuBtn = document.querySelector(".nav-btn");
+// let closeBtn = document.querySelector(".close-btn");
+
+// console.log(menuBtn)
+
+// menuBtn.addEventListener('click', function() {
+//   // I want overlay to display block now
+//   overlay.style.display = "block";
+// });
+
+// closeBtn.addEventListener('click', function() {
+//   // I want overlay to display none now
+//   overlay.style.display = "none";
+// });
