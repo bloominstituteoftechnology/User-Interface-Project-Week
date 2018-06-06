ffce16d2522a4947b08a4c73d7ae14bf9474b49a
chainchompa <chainchompa@gmail.com>
2018-06-06 15:36:22 -0400

    finished navigation bar, now changes color when scrolled down

--- js/index.js
+++ js/index.js
@@ -1,24 +1,3 @@
-// class Dropdown {
-//   constructor(element) {
-//     this.element = element;
-//     this.button = dropdownbtn;
-//     this.content = dropdownContent;
-//     this.button.addEventListener("click", () => {
-//       this.toggleContent();
-//     })
-//   }
-//
-//   toggleContent() {
-//     alert("yay!");
-//     //this.content.classList.toggle("dropdown-hidden");
-//   }
-// }
-// let dropdownContent = document.querySelector('.dropdown-content')
-// let dropdownbtn = document.querySelector('.dropdown-button')
-// let dropdowns = document.querySelectorAll('.dropdown');
-// dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
-
-//
 let overlay = document.querySelector(".overlay");
 let menuBtn = document.querySelector(".dropdown-button");
 let closeBtn = document.querySelector(".close-btn");
@@ -26,11 +5,21 @@ let closeBtn = document.querySelector(".close-btn");
 console.log(menuBtn)
 
 menuBtn.addEventListener('click', function() {
-  // I want overlay to display block now
   overlay.style.display = "block";
 });
 
 closeBtn.addEventListener('click', function() {
-  // I want overlay to display none now
   overlay.style.display = "none";
 });
+
+
+window.addEventListener('scroll', function () {
+        let nav = document.querySelector('.navigation');
+        if (document.documentElement.scrollTop > 50 || document.body.scrollTop > window.innerHeight) {
+                nav.classList.add('navigation-bg');
+                nav.classList.remove('navigation-clear');
+            } else {
+                nav.classList.add('navigation-clear');
+                nav.classList.remove('navigation-bg');
+            }
+    });
