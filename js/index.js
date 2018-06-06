d4a0e025c819b75202c7a102951f20b592a35dfd
emaquino44 <prexy44@yahoo.com>
2018-06-05 22:48:27 -0700

    finished navbarscroll and toggle

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // JS goes here
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+
+const toggleMenu = () => {
+  menu.classList.toggle('menu--open')
+}
+
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', toggleMenu);
