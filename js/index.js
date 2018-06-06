f5b364bfb73bd4818b93716b94f56b289135e073
Kevin <brackkevin@gmail.com>
2018-06-06 10:42:49 -0500

    Added events and toggles for nav overlay and hamburger

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-let placeholder = "placeholder"
+let hamburgerButton = document.querySelector('.hamburger-button');
+let navOverlay = document.querySelector('.nav-overlay');
+let navVisibility = false;
+
+//////////////////////////////////
+// TOGGLE NAVIGATION VISIBILITY //
+////////////////////////////////// 
+hamburgerButton.addEventListener('click', () => {
+	console.log(navOverlay.classList);
+	navOverlay.classList.toggle("nav-overlay-hidden");
+	hamburgerButton.classList.toggle("hamburger-is-x")
+	navVisibility = !navVisibility;
+})
+
+//////////////////////////////////////////////////////
+// CHANGE HAMBURGER ICON TO X WHILE OVERLAY VISIBLE //
+//////////////////////////////////////////////////////
