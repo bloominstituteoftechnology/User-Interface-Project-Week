2399ed134639bb6cd317c7b907f25b81f188406b
Simon <Simon_Freelance@hotmail.com>
2018-06-06 09:28:52 -0400

    Starting code for the tab navigator

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-// JS goes here
+
+
+
+class TabNavigator {
+    constructor(element) {
+        this.element = element;
+        this.tabLinks = this.element.querySelectorAll('tab-link');
+        this.tabLinks = Array.from(this.tabLinks).map(tabLink => new TabLink(tabLink, this));
+    }
+    
+}
+
+class TabLink {
+    constructor(element, parent) {
+
+    }
+}
+let tabNav = document.querySelectorAll('.tab-navigator');
+
+tabNav = Array.from(tabNav).map(tab => new TabNavigator(tabNav));
