f704d8aa7cdb3c66fb962b0e34367dcc7a954c0f
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-06 11:34:51 -0600

    Hamburger button updates working

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,27 @@
-let hamburger = document.querySelector(".nav-main-button");
+let navToggleButton = {
+  "collapsed": true,
+  "element": document.querySelector(".nav-main-button"),
+  "openImage": "img/nav-hamburger.png",
+  "closeImage": "img/nav-hamburger-close.png"
+}
+
 let pageContents = document.querySelector('.container');
 let nav = document.querySelector('nav');
 
-
-hamburger.addEventListener("click", () => {
+//handles hamburger
+navToggleButton.element.addEventListener("click", () => {
     nav.classList.toggle("collapsed");
+    if (navToggleButton.collapsed === true) {
+      navToggleButton.element.innerHTML = `<img src="${navToggleButton.closeImage}" alt="close button">`;
+      navToggleButton.collapsed = false;
+    } else {
+      navToggleButton.element.innerHTML = `<img src="${navToggleButton.openImage}" alt="close button">`;
+      navToggleButton.collapsed = true;
+    }
 });
 
+
+//Handles Scroll Logic
 (function scrollLogic () {
     var last_known_scroll_position = 0;
     var ticking = false;
