6fecf0f414207c9154636c2b006d3617b6e6e308
nickolaussmith <nickolaussmith721@gmail.com>
2018-06-06 17:05:21 -0600

    NAVVVVVIGATION DONEZO

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // JS goes here
+
+let overlay = document.querySelector(".overlay");
+let menuBtn = document.querySelector(".close-btn");
+
+
+
+
+let show = document.querySelector(".showbtn");
+
+show.addEventListener('click', function() {
+overlay.style.display = "block";
+})
+
+menuBtn.addEventListener('click', function(){
+overlay.style.display = "none";
+
+
+})
