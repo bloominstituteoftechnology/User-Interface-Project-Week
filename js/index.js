4a812ef68afba9b74869596fcc6a6ce22f09151d
Daniel Chamorro <dan@dcitllc.com>
2018-06-06 17:05:51 -0400

    Click on Hamburger is Working

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // JS goes here
-// On scroll enlarge image the click image to revert back
+
 window.addEventListener("scroll", () => {
-  // middleImage.style.transform = "scale(1.5)";
   document.querySelector(".header-row").classList.add("scroll-bar");
+});
+
+let hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function() {
+  console.log("I is working");
 });
