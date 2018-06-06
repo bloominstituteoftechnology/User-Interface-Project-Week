39af33139c2fc5f71318104cdd2abdf65d27a2c4
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-06 09:44:07 -0600

    Scroll logic implemented on home page

--- scripts/index.js
+++ scripts/index.js
@@ -9,4 +9,35 @@ hamburger.addEventListener("click", () => {
 });
 
 
-console.log("loaded");
+function scrollLogic () {
+    var last_known_scroll_position = 0;
+    var ticking = false;
+
+function updateNavColor(scroll_pos) {
+  console.log(scroll_pos);
+  if (scroll_pos === 0) {
+      nav.classList.remove('scrolled');
+  } else {
+      nav.classList.add('scrolled');
+  }
+}
+
+window.addEventListener('scroll', function handleScroll (e) {
+
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+
+    window.requestAnimationFrame(function() {
+      updateNavColor(last_known_scroll_position);
+      ticking = false;
+    });
+     
+    ticking = true;
+
+  }
+  
+});
+}
+
+scrollLogic();
