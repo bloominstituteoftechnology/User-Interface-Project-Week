f1d0d0e66bde9794104a02dd663d9bf01365212a
Kelley Henican <kelleyhenican2@gmail.com>
2018-06-06 14:11:16 -0500

    got naviation system part 1 working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // JS goes here
+
+// navbar scroll
+
+let navbar = document.querySelector(".navigation")
+
+window.addEventListener("scroll", function() {
+    navbar.style.background = "#9DA7B1";
+    navbar.style.opacity = ".95";
+   
+})
+
+
