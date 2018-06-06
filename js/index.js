f28493951340ec4a44aee8d5aa7481c6081f840a
Jameson <jamesonbrown@neolivity.com>
2018-06-06 14:40:53 -0600

    Working on responsive to desktop @media. Partial.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS goes here
+var last_known_scroll_position = 0;
+let myNav = document.querySelector(".navigation");
+
+window.addEventListener('scroll', function() {
+    last_known_scroll_position = window.scrollY;
+    if (last_known_scroll_position >= 300) {
+        myNav.classList.add("nav-bottom")
+    } else {
+        myNav.classList.remove("nav-bottom")
+    }
+})
