cdda9755e88575950a5f8fc2f9ad5fc6e6c30f8e
chainchompa <chainchompa@gmail.com>
2018-06-06 13:42:08 -0400

    added dropdown menu for desktop views

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-// JS goes here
+// class Dropdown {
+//   constructor(element) {
+//     this.element = element;
+//     this.button = dropdownbtn;
+//     this.content = dropdownContent;
+//     this.button.addEventListener("click", () => {
+//       this.toggleContent();
+//     })
+//   }
+//
+//   toggleContent() {
+//     alert("yay!");
+//     //this.content.classList.toggle("dropdown-hidden");
+//   }
+// }
+// let dropdownContent = document.querySelector('.dropdown-content')
+// let dropdownbtn = document.querySelector('.dropdown-button')
+// let dropdowns = document.querySelectorAll('.dropdown');
+// dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
+//
+let overlay = document.querySelector(".overlay");
+let menuBtn = document.querySelector(".dropdown-button");
+let closeBtn = document.querySelector(".close-btn");
+
+console.log(menuBtn)
+
+menuBtn.addEventListener('click', function() {
+  // I want overlay to display block now
+  overlay.style.display = "block";
+});
+
+closeBtn.addEventListener('click', function() {
+  // I want overlay to display none now
+  overlay.style.display = "none";
+});
