fc49faceeec2895e49c475206ec1c5d3097defd2
Daniel Chamorro <dan@dcitllc.com>
2018-06-06 18:10:38 -0400

    Overlay Working with some Issues

--- js/index.js
+++ js/index.js
@@ -4,7 +4,15 @@ window.addEventListener("scroll", () => {
   document.querySelector(".header-row").classList.add("scroll-bar");
 });
 
+// Open Nav
 let hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function() {
-  console.log("I is working");
+  //   console.log("I is working");
+  document.getElementById("myNav").style.display = "block";
+});
+
+// Close Nav
+let closeNav = document.getElementById("myNav");
+closeNav.addEventListener("click", function() {
+  closeNav.style.display = "none";
 });
