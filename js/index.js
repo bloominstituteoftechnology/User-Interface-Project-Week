48a1c515220c1a732ec86c41207ad92a9bd4a268
Adrian Adames <adrianadames@gmail.com>
2018-06-06 01:46:53 -0400

    Draft of navbar html element and styling done. Limited functionality, but it's a start.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // JS goes here 
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () {
+    customFunc()
+}; 
+
+// Get the navbar
+let navbar = document.getElementById("navbar");
+
+
+// Get the offset position of the navbar
+let sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function customFunc() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
