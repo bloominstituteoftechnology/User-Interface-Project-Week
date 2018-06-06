d3c793e46f7f87f81d0d788d2ca800083281f297
Dil Sim <dilyasom@gmail.com>
2018-06-06 15:36:24 -0700

    navbar srolls

--- js/index.js
+++ js/index.js
@@ -1,35 +1,8 @@
 // JS goes here
 
-// function openNav() {
-//     let element = document.getElementById("myNav");
-//     element.classList.remove("hidden");
-//     // element.classList.add("overlay");
-
-// }
-
-// function closeNav() {
-//     var element = document.getElementById("myNav")
-//     element.classList.add("hidden");
-// }
-// let overlay = document.querySelector(".overlay");
-// let menuBtn = document.querySelector(".menuBtn");
-// let closeBtn = document.querySelector('.close-btn');
-
-// menuBtn.addEventListener('click', function () {
-//     // I want overlay to display block now
-//     console.log('clicked');
-//     overlay.style.display = "block";
-// });
-// closeBtn.addEventListener('click', function () {
-//     overlay.style.display = 'none';
-// })
-
-
 let overlay = document.querySelector(".overlay");
 let menuBtn = document.querySelector(".nav-btn");
 let closeBtn = document.querySelector(".close-btn");
-
-console.log(menuBtn)
 
 menuBtn.addEventListener('click', function () {
     // I want overlay to display block now
@@ -114,3 +87,18 @@ class TabsItem {
 let tabs = document.querySelectorAll('.tabs');
 tabs = Array.from(tabs).map(tab => new Tabs(tab));
 // tabs end
+
+// navigation
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 300) {
+            $(".navigation").css("backgroundColor", "#9DA7B1");
+            $(".navigation").css("opacity", "0.95");
+        }
+
+        else {
+            $(".navigation").css("backgroundColor", "none");
+        }
+    })
+})
