b6c593cabf9615202f0b5bdb6bda09d789308ff6
Daniel Chamorro <dan@dcitllc.com>
2018-06-06 15:51:35 -0400

    Nav Scroll with Background

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // JS goes here
+// On scroll enlarge image the click image to revert back
+window.addEventListener("scroll", () => {
+  // middleImage.style.transform = "scale(1.5)";
+  document.querySelector(".header-row").classList.add("scroll-bar");
+});
