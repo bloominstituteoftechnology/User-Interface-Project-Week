80d769137b6183ee0713d4ccf84fd71c7d540229
sibhat <sibhat.temelso@gmail.com>
2018-06-05 17:58:16 -0700

    navigation animation, height width property added

--- js/menu.js
+++ js/menu.js
@@ -6,9 +6,9 @@ headrMenu.addEventListener('click', (e) =>{
     // menu.classList.toggle('menu--display');
     e.target.classList.toggle('header__menu--photo--close');
     if(isOpen){
-        TweenMax.to(menu, time, {display: 'none', opacity: 0});
+        TweenMax.to(menu, time, {display: 'none', opacity: 0, height: '0vh', width: '0vh', borderRadius: '50%'});
     }else if(!isOpen){
-        TweenMax.to(menu, time, {display: 'block', opacity: 1});
+        TweenMax.to(menu, time, {display: 'block', opacity: 1, height: '100vh', width: '110vw', borderRadius: '5%'});
     }
     isOpen = !isOpen;
 });
