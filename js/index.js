6fef6617cdfc99d6d7c933f4bec6f11db7e67f82
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-06 23:09:32 -0500

    completed tab select program

--- js/index.js
+++ js/index.js
@@ -14,3 +14,70 @@ class MenuItems {
 
 let menu = document.querySelectorAll(".popup ul li a");
 menu= Array.from(menu).map(item => new MenuItems(item));
+
+class Tabs {
+    constructor(element) {
+        this.element = element;
+        this.links = this.element.querySelectorAll("ul li");
+        this.links = Array.from(this.links).map(link => {
+            return new TabsLink(link, this);
+        });
+        this.activeLink = this.links[0];
+        this.init();
+    }
+
+    init(){
+        this.activeLink.select();
+    }
+
+    updateActive(newActive) {
+        this.activeLink.deselect();
+        this.activeLink = newActive;
+    }
+
+    getTab(data) {
+        this.element = document.querySelector(`.content[data-tab="${data}"]`);
+        return this.element;
+    }
+}
+
+class TabsLink {
+    constructor(element, parent) {
+        this.element = element;
+        this.tabs = parent;
+        this.tabsItem = parent.getTab(this.element.dataset.tab);
+        this.tabsItem = new TabsItem(this.tabsItem);
+        this.element.addEventListener("click", () => {
+            this.tabs.updateActive(this);
+            this.select();
+        });
+    }
+
+    select() {
+        this.element.classList.add("highlight");
+        this.tabsItem.select();
+    }
+
+    deselect() {
+        this.element.classList.remove("highlight");
+        this.tabsItem.deselect();
+    }
+}
+
+class TabsItem {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select() {
+        this.element.classList.add("active");
+    }
+
+    deselect() {
+        this.element.classList.remove("active");
+    }
+}
+
+
+let tabs = document.querySelectorAll(".tab-links");
+tabs= Array.from(tabs).map(tab => new Tabs(tab)); 
