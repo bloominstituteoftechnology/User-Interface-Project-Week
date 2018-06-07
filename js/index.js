9d07c21e8dde8e8f81eeb2b8f7d84dfa40ee62e7
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-06 21:36:07 -0500

    completed menu expand program

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// JS goes here
+class MenuItems {
+    constructor(item) {
+        this.item = item;
+        this.button = document.querySelector(".ham-button");
+        this.content = document.querySelector(".toggle");
+        this.button.addEventListener("click", () => {this.expand()});
+    }
+
+    expand() {
+        this.content.classList.toggle("hide");
+        this.button.classList.toggle("close");
+    }
+}
+
+let menu = document.querySelectorAll(".popup ul li a");
+menu= Array.from(menu).map(item => new MenuItems(item));
