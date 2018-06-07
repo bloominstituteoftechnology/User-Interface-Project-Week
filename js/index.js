c5e0d3b02ce563d303ee092df58075bd37d1ee17
Gabriel Duquette <gabrielduquette@gmail.com>
2018-06-07 02:05:09 -0400

    finishnav scrolling

--- js/index.js
+++ js/index.js
@@ -1,1 +1,75 @@
-// JS goes here
+class Tabs {
+  constructor(element) {
+    this.element = element;
+    this.links = this.element.querySelectorAll(".tabs-link");
+    this.links = Array.from(this.links).map( link => {
+      return new TabsLink(link, this);
+    });
+    this.activeLink = this.links[0];
+    this.init();
+  }
+
+  init() {
+    this.activeLink.select();
+  }
+
+  updateActive(newActive) {
+    this.activeLink.deselect();
+    this.activeLink = newActive;
+  }
+
+  getTab(data) {
+    return this.element.querySelector(`.tabs-item[data-tab="${data}"]`);
+  }
+}
+
+class TabsLink {
+  constructor(element, parent) {
+    this.element = element;
+    this.tabs = parent;
+    this.tabsItem = parent.getTab(element.dataset.tab);
+    this.tabsItem = new TabsItem(this.tabsItem);
+    this.element.addEventListener('click', () => {
+      this.tabs.updateActive(this);
+      this.select();
+    });
+  };
+
+  select() {
+    this.element.classList.add("tabs-link-selected");
+    this.tabsItem.select();
+  }
+
+  deselect() {
+    this.element.classList.remove("tabs-link-selected");
+    this.tabsItem.deselect();
+  }
+}
+
+class TabsItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    this.element.classList.add("tabs-item-selected");
+  }
+
+  deselect() {
+    this.element.classList.remove("tabs-item-selected");
+  }
+}
+
+let tabs = document.querySelectorAll(".services");
+tabs = Array.from(tabs).map(tab => {
+  return new Tabs(tab);
+});
+
+document.addEventListener("scroll", () => {
+  let nav = document.querySelector('.nav-container');
+  if (document.documentElement.scrollTop > 1) {
+    nav.setAttribute("style", "background: #9DA7B1; opacity: 0.95; z-index: 100;");
+  } else {
+    nav.setAttribute("style", "background: none; opacity: 1;");
+  }
+});
