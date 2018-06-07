e07280cc921610d4013ccfe2d65481a8a3743012
Kairi <kairijb@gmail.com>
2018-06-07 02:06:01 -0500

    rebuilding home page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // JS goes here
+
+class Tabs {
+    constructor(element) {
+        this.element = element;
+        console.log(this.element)
+    }
+    select() {
+        let selected = document.querySelectorAll(".one");
+        console.log(selected);
+        selected.addEventListener("mouseover", () => {
+            console.log(this.selected);
+        });
+    }
+
+}
+
+let tabs = document.querySelectorAll('.view');
+tabs = Array.from(tabs).map( tab => new Tabs(tab));
