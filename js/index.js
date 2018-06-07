a77b977dd5fe95abe038d0840c002b1840b8c6a1
Allen Hai <allenhai03@gmail.com>
2018-06-07 10:01:01 -0500

    finished services tabs

--- js/index.js
+++ js/index.js
@@ -1,1 +1,68 @@
-// JS goes here
+class TabsContainer {
+
+  constructor(el) {
+    this.el = el
+    console.log(this.el)
+    this.links = this.el.querySelectorAll('.tab')
+    this.links = Array.from(this.links)
+                      .map(link => new TabLink(link, this)) 
+    this.activeLink = this.links[0]
+    this.init()
+  }
+  
+  init() {
+    this.activeLink.select()
+  }
+
+  updateActive(newActive) {
+    this.activeLink.deselect()
+    this.activeLink = newActive
+  }
+
+  getTabContent(data) {
+    return document.querySelector(`.tab-content[data-tab="${data}"]`)
+  }
+}
+
+class TabLink {
+  constructor(el, parent) {
+    this.el = el
+    this.tabContainer = parent
+    this.tabContent = this.tabContainer.getTabContent(this.el.dataset.tab)
+    console.log(this.tabContent)
+    this.tabContent = new TabContent(this.tabContent)
+    this.el.addEventListener('click', () => {
+      this.tabContainer.updateActive(this)
+      this.select()
+    })
+  }
+
+  select() {
+    this.el.classList.add('tab-selected')
+    this.tabContent.select()
+  }
+
+  deselect() {
+    this.el.classList.remove('tab-selected')
+    this.tabContent.deselect()
+  }
+}
+
+class TabContent {
+  constructor(el) {
+    this.el = el
+  }
+
+  select() {
+    this.el.classList.add('tab-content-selected')
+  }
+
+  deselect() {
+    this.el.classList.remove('tab-content-selected')
+  }
+}
+
+let tc = document.querySelectorAll('.tabs-container')
+tc = Array.from(tc).map(el => new TabsContainer(el))
+
+
