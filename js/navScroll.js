e13145fc4217796ccceff2900b19fa2b595c0f78
Erin Meredith <ekmeredith@att.net>
2018-06-07 13:23:05 -0400

    scroll changed back at top of page

--- js/navScroll.js
+++ js/navScroll.js
@@ -1,4 +1,8 @@
 let stickyNav = document.querySelector(".nav-static");
 window.addEventListener("scroll", function() {
     stickyNav.classList.add("navbar-scroll");
+    if (Math.round(window.scrollY) === 0) {
+    stickyNav.classList.remove("navbar-scroll");
+}
 });
+
