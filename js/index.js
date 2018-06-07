7f73ec6cd4fb41413dc7ba34263dc25463f5362d
snowcoding <vimalshah77@gmail.com>
2018-06-06 20:44:11 -0700

    Corrected menu X button issue and moved all jumbo's up 1 px to be flush

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 //Main JS file
 
+// Grab Navigation Parent
 let nav = document.querySelector('.nav');
-console.log(nav);
 
-//
+//Add a scroll event listener that adds a class if scroll.Y > 0
 window.addEventListener('scroll',function(e){
   console.log(window.scrollY, 'y coord');
   if (window.scrollY > 0){
@@ -14,22 +14,23 @@ window.addEventListener('scroll',function(e){
 })
 
 
+//Grab overlay and menu btn
 let overlay = document.querySelector(".overlay");
 let menuBtn = document.querySelector(".menu-btn img");
-let closeBtn = document.querySelector(".close-btn");
 
-console.log(menuBtn)
-
+//Add a listener to toggle between the menu open and close state
 menuBtn.addEventListener('click', function() {
-  // I want overlay to display block now
 
   if (menuBtn.classList.contains('open')){
     overlay.style.display = "none";
-    menuBtn.src="../img/nav-hamburger.png"
+    menuBtn.src= "./img/nav-hamburger.png"
     menuBtn.classList.remove('open')  
+
   }else {
+    console.log('menu open')
     overlay.style.display = "block";
-    menuBtn.src="../img/nav-hamburger-close.png"
+    menuBtn.src= "./img/nav-hamburger-close.png"
     menuBtn.classList.add('open')
   }
+
 });
