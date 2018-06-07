058d7f210d90d030587684cb4fbecef0d400c686
tommycam <tommycam67@gmail.com>
2018-06-07 14:34:14 -0500

    working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // JS goes here
+let menuButton = document.querySelector(".nav-icon");
+let nav = document.querySelector(".nav-bar")
+menuButton.addEventListener('click', () => {
+  nav.classList.toggle("nav-bar-active");
+  //nav.classList.add("nav-bar-active");
+});
