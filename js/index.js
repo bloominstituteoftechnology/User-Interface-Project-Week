b83444c3e310c36648a2520306a1981fc0f9c9af
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-06-07 12:35:32 -0700

    adds hamburger nav button functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// JS goes here
+let overlay = document.querySelector('.overlay');
+let closeBtn = document.querySelector('.close-btn');
+let menuBtn = document.querySelector('.hambutton');
+
+closeBtn.addEventListener('click', () => {
+  overlay.style.display = 'none';
+});
+
+menuBtn.addEventListener('click', () => {
+  overlay.style.display = 'block';
+});
