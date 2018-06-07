3a5f1c5121a5c497aabbdb77023205dcde45a2cf
Erin Meredith <ekmeredith@att.net>
2018-06-07 00:22:20 -0400

    Nav-scroll working

---
+++ js/navExpand.js
@@ -0,0 +1,5 @@
+let overlay = document.querySelector(".overlay");
+let btn = document.querySelector(".hamburger");
+btn.addEventListener("click", function() {
+  navbar.classList.toggle("hidden");
+});
---
+++ js/navScroll.js
@@ -0,0 +1,4 @@
+let stickyNav = document.querySelector(".nav-static");
+window.addEventListener("scroll", function() {
+    stickyNav.classList.toggle("navbar-scroll");
+});
