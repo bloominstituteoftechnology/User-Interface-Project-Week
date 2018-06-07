c35a9c770e7c128f41bedc303308b2ad3e19d7bf
Adrian Adames <adrianadames@gmail.com>
2018-06-07 05:53:27 -0400

    Finished with services page html/css and components except for the form in the footer.

--- js/services.js
+++ js/services.js
@@ -1,33 +1,3 @@
-// JS goes here 
-
-
-//**************SERVICES TAB COMPONENT*******************//
-
-// // When the user scrolls the page, execute myFunction 
-// window.onscroll = function () {
-//     customFunc()
-// }; 
-
-// // Get the navbar
-// let navbar = document.getElementById("navbar");
-
-
-// // Get the offset position of the navbar
-// let sticky = navbar.offsetTop;
-
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function customFunc() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-
-// //**************NAV SYSTEM PART 2*******************//
-
-// let burgerIcon = document.getElementsByClassName("nav-hamburger2");
-
 
 
 
