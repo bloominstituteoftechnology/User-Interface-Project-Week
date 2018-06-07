a4bf19015b7acda0289f5a651284c570dcd09b58
unknown <may.alex43@gmail.com>
2018-06-07 03:30:09 -0400

    started menu button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // JS goes here
+let overlay = document.querySelector(".overlay");
+let menuBtn = document.querySelector(".ham");
+let closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener('click', function() {
+  // I want overlay to display block now
+  overlay.style.display = "block";
+});
+
+closeBtn.addEventListener('click', function() {
+  // I want overlay to display none now
+  overlay.style.display = "none";
+});
