c6512ee5032be6090681de428018af8447100d5f
Katie Gorbell <katiegorbell@gmail.com>
2018-06-12 12:52:25 -0500

    working on overlaying nav bar on home screen

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-// JS goes here
-
 // HEADER TURNS GREY WHEN SCROLLED
 
 greyScrollbar = function() {
@@ -9,3 +7,15 @@ greyScrollbar = function() {
 
 let header = document.querySelector('.header');
 window.addEventListener('scroll', greyScrollbar);
+
+// NAVIGATION OVERLAYS SCREEN WHEN CLICKED
+
+let overlay = document.querySelector('.navigation-overlay');
+let navBtn = document.querySelector('.header-icon');
+let navCloseBtn = document.querySelector('.header-close-icon');
+
+navBtn.addEventListener('click', () => {
+    overlay.setAttribute('style', 'display: flex;');
+    navCloseBtn.setAttribute('style', 'display: block');
+    navBtn.setAttribute('style', 'display: none');
+})
