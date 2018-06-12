a7c58329748e435335cb98e8697cd6eb88678b5b
Katie Gorbell <katiegorbell@gmail.com>
2018-06-12 11:00:32 -0500

    working on scroll function for header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // JS goes here
+
+// HEADER TURNS GREY WHEN SCROLLED
+
+let header = document.getElementsByClassName('header');
+window.addEventListener('scroll', () => {
+    header.setAttribute('style', 'background: #9DA7B1, opacity:.95');
+});
