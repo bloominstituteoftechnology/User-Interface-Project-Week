d5237f0540743287a1dc98966bf6d16a2164f810
Kelley Henican <kelleyhenican2@gmail.com>
2018-06-13 15:47:38 -0500

    fixed some of navbar overlay. Need to fix the styling

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@
 let nav = document.querySelector(".nav");
 
 window.addEventListener("scroll", () => {
-    nav.style.background = "#9DA7B1"; 
+    nav.style.background = "#9DA7B1";
     nav.style.opacity = "0.95";
     nav.style.zIndex = "1";
 })
@@ -15,24 +15,16 @@ window.addEventListener("scroll", () => {
 
 let hamburger = document.querySelector(".hamburger");
 let overlay = document.querySelector("#overlay");
-
+let anchor = document.querySelector(".home");
+let closeHamburger = document.querySelector(".hamburger-close");
 
 
 hamburger.addEventListener("click", () => {
-   if (hamburger.src == "file:///Users/kelleyhenican/LambdaSchool/User-Interface-Project-Week/img/nav-hamburger.png") {
-      overlay.style.display = "block";
-      hamburger.src = "file:///Users/kelleyhenican/LambdaSchool/User-Interface-Project-Week/img/nav-hamburger-close.png"
-      nav.style.zIndex = "2";
-
-
-   }
-   else {
-       overlay.style.display = "none";
-       hamburger.src = "file:///Users/kelleyhenican/LambdaSchool/User-Interface-Project-Week/img/nav-hamburger.png"
-       nav.style.zIndex = "2";
-   }
-  
+    overlay.style.display = "block";
 })
 
+closeHamburger.addEventListener("click", () => {
+    overlay.style.display = "none";
+})
 
 
