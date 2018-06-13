8c1bcb7dd37a6a456d6f9eec13b09514e01dbba2
Katie Gorbell <katiegorbell@gmail.com>
2018-06-12 19:02:35 -0500

    working on JS for tab componenets

--- js/index.js
+++ js/index.js
@@ -26,3 +26,72 @@ navCloseBtn.addEventListener('click', () => {
     navCloseBtn.setAttribute('style', 'display: none');
     navBtn.setAttribute('style', 'display: block');
 });
+
+
+// SERVICES PAGE: CLICKABLE TABS
+
+class Tabs {
+    constructor(element) {
+        this.element = element;
+        this.btn = this.element.querySelectorAll('.tab-btn');
+        this.btn = Array.from(this.btn).map( button => {
+            return new TabsBtn(button, this);
+        });
+        this.activeLink = this.btn[0];
+        this.init();
+    }
+
+    init() {
+        this.activeLink.select();
+    }
+
+    updateActive(newActive) {
+        this.activeLink.deselect();
+        this.activeLink = newActive;
+    }
+
+    getTab(data) {
+        return this.element.querySelector(`.tab-content[data-tab='${data}']`)
+    }
+}
+
+class TabsBtn {
+    constructor(element, parent) {
+        this.element = element;
+        this.tabs = parent;
+        this.tabsContent = parent.getTab(this.element.dataset.tab);
+        this.tabsContent = new TabsContent(this.tabsContent);
+        this.element.addEventListener('click', () => {
+            this.tabs.updateActive(this);
+            this.select();
+        });
+    }
+
+    select() {
+        this.element.classList.add('tab-btn-selected');
+        this.tabsContent.select();
+    }
+
+    deselect() {
+        this.element.classList.add('tab-btn-selected')
+        this.tabsContent.deselect();
+    }
+}
+
+class TabsContent {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select() {
+        this.element.classList.add('tab-btn-selected');
+    }
+
+    deselect() {
+        this.element.classList.remove('tab-btn-selected');
+    }
+}
+
+
+let tabs = document.querySelectorAll('.tabs')
+tabs = Array.from(tabs).map(tab = new tabs(tab));
