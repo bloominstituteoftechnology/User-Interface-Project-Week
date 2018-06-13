0d84287fd9af33ab2404aa98b7fee080ca15a31d
Kelley Henican <kelleyhenican2@gmail.com>
2018-06-13 13:00:43 -0500

    some work on navigation overlay. Trying to figure out how to make the anchor tags work

--- js/index.js
+++ js/index.js
@@ -9,3 +9,30 @@ window.addEventListener("scroll", () => {
     nav.style.opacity = "0.95";
     nav.style.zIndex = "1";
 })
+
+
+// navbar overlay 
+
+let hamburger = document.querySelector(".hamburger");
+let overlay = document.querySelector("#overlay");
+
+
+
+hamburger.addEventListener("click", () => {
+   if (hamburger.src == "file:///Users/kelleyhenican/LambdaSchool/User-Interface-Project-Week/img/nav-hamburger.png") {
+      overlay.style.display = "block";
+      hamburger.src = "file:///Users/kelleyhenican/LambdaSchool/User-Interface-Project-Week/img/nav-hamburger-close.png"
+      nav.style.zIndex = "2";
+
+
+   }
+   else {
+       overlay.style.display = "none";
+       hamburger.src = "file:///Users/kelleyhenican/LambdaSchool/User-Interface-Project-Week/img/nav-hamburger.png"
+       nav.style.zIndex = "2";
+   }
+  
+})
+
+
+
