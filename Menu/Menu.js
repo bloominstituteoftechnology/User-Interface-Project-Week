8e793e8f234de3dbc98bd194db00ea9e0e4697c3
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-06-14 13:25:16 -0600

    Working on layout positioning

---
+++ Menu/Menu.js
@@ -0,0 +1,7 @@
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open')
+  }
+  
+  const menu = document.querySelector('.menu');
+  const menuButton = document.querySelector('.menu-button');
+  menuButton.addEventListener('click', toggleMenu);
