b875351f99f85c6127a2bace345f59fd6315aaf8
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-06-15 17:54:58 -0600

    added viewport

--- Menu/Menu.js
+++
@@ -1,7 +0,0 @@
-const toggleMenu = () => {
-    menu.classList.toggle('menu--open')
-  }
-  
-  const menu = document.querySelector('.menu');
-  const menuButton = document.querySelector('.menu-button');
-  menuButton.addEventListener('click', toggleMenu);
