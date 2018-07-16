2ec2d37e12a78465a9f3271f32c70a810075f963
Brandon White <bwhite06@stac.edu>
2018-07-16 18:35:28 -0400

    nav bar complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-// JS goes here
+//Navigation
+let image_tracker="open";
+function Navi(){
+  let Navi=document.querySelectorAll(".navi")
+  for (var i = 0; i < Navi.length; i++) {
+    Navi[i].classList.toggle('Navigation');
+  }
+
+
+  }
+
+  var customMenu = document.querySelector("#menu-button");
+
+  customMenu.src='img/nav-hamburger.png'
+  customMenu.addEventListener('click',(event) => {
+    Navi();
+menu();
+
+
+
+  if (image_tracker=='open') {
+    event.target.src='img/nav-hamburger-close.png';
+    image_tracker='closed';
+  }
+  else if (image_tracker==='closed') {
+    event.target.src='img/nav-hamburger.png'
+    image_tracker='open';
+  }
+
+  });
+
+
+
+
+function menu(){
+let menuOpen = document.querySelector('.menu-closed');
+menuOpen.classList.toggle('custom-col')
+}
