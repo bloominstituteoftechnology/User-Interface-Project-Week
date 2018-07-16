97e902424be099f95887188b3f3be53e63064ef6
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-16 13:30:12 -0700

    add tab functionality to services.html

--- js/index.js
+++ js/index.js
@@ -15,3 +15,69 @@ hamButtonClose.addEventListener('click', ()=> {
 	hamButton.style.display = 'block';
 	hamButtonClose.style.display = 'none';
 });
+
+class Tabs {
+  constructor(element) {
+    this.element = element;
+    this.links = this.element.querySelectorAll('.tab');
+    this.links = Array.from(this.links).map( link => {
+      return new TabsLink(link, this);
+    });
+    this.activeLink = this.links[0];
+    this.init();
+  }
+
+  init() { 
+    this.activeLink.select();
+  }
+
+  updateActive(newActive) {
+    this.activeLink.deselect();
+    this.activeLink = newActive;
+  }
+
+  getTab(data) {
+    return document.querySelector(`.content[data-tab="${data}"]`);
+  }
+
+}
+
+class TabsLink {
+  constructor(link, parent) {
+    this.link = link;
+    this.tabs = parent;
+    this.tabsItem = parent.getTab(this.link.dataset.tab);
+    this.tabsItem = new TabsItem(this.tabsItem);
+    this.link.addEventListener('click', () => {
+      this.tabs.updateActive(this);
+      this.select();
+    });
+  };
+
+  select() {
+    this.link.classList.add('tab-active');
+    this.tabsItem.select();
+  }
+
+  deselect() {
+    this.link.classList.remove('tab-active'); 
+    this.tabsItem.deselect();
+  }
+}
+
+class TabsItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    this.element.classList.add('content-active');
+  }
+
+  deselect() {
+    this.element.classList.remove('content-active');
+  }
+}
+
+let tabs = document.querySelectorAll('.services-tabs');
+tabs = Array.from(tabs).map(tab => new Tabs(tab));
