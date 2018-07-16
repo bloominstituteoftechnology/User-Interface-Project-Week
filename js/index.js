73058af8badb016d9e7537a3bce1f9363c5e8b77
sliw <slioweng@gmail.com>
2018-07-17 06:44:37 +0800

    done navigation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// JS goes here
+let onHumburger = document.querySelector('.on-humburger');
+let offHumburger = document.querySelector('.off-humburger');
+let hiddenNavigation = document.querySelector('.hidden-navigation');
+
+onHumburger.addEventListener('click', function(e) {
+  onHumburger.classList.add('hidden');
+  offHumburger.classList.remove('hidden');
+
+  hiddenNavigation.classList.remove('hidden');
+  hiddenNavigation.classList.add('expanded');
+});
+
+offHumburger.addEventListener('click', function(e) {
+  onHumburger.classList.remove('hidden');
+  offHumburger.classList.add('hidden');
+
+  hiddenNavigation.classList.add('hidden');
+  hiddenNavigation.classList.remove('expanded');
+});
