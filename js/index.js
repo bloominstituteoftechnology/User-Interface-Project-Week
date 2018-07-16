eec32d9d15d32d8007f8372905a5e3e10a559097
sca4cs <shannon.atkinson.ca@gmail.com>
2018-07-16 17:53:33 -0400

    home page desktop view

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // JS goes here
+
+// Toggle menu button
+const toggleMenu = () => {
+    menu.classList.toggle("menu--open");
+  }
+  
+  const menu = document.querySelector(".menu");
+  const menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", (toggleMenu));
