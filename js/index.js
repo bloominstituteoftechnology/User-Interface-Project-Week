cd1ea7cdc7d6cba028ca006286fe8daaf4c6443d
johnoro <jjcummins102@gmail.com>
2018-07-16 18:35:55 -0400

    Started on home mobile view.
    Also implemented slight fixes here and there.

--- js/index.js
+++ js/index.js
@@ -4,5 +4,5 @@ let navBtn = document.querySelector('.nav-btn');
 
 navBtn.addEventListener('click', e => {
     nav.style.display = (nav.style.display == 'none' ? 'flex' : 'none');
-    navBtn.src = (nav.style.display == 'none'? './img/nav-hamburger.png' : './img/nav-hamburger-close.png');
+    navBtn.src = (nav.style.display == 'none' ? './img/nav-hamburger.png' : './img/nav-hamburger-close.png');
 });
