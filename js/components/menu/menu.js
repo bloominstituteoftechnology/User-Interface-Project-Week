e1596bba56ddc0939bbac15da545339c5e1924d1
keegn <keegan.t.burkett@gmail.com>
2018-07-16 12:17:18 -0500

    initial commit

---
+++ js/components/menu/menu.js
@@ -0,0 +1,27 @@
+class Menu {
+    constructor(element) {
+        this.element = element;
+        this.button = this.element.querySelector('.menu-button');
+        this.content = this.element.querySelector('.menu-content');
+        this.button.addEventListener('click', () => { this.toggleContent() });
+    }
+    toggleContent() {
+        this.content.classList.toggle('menu-hidden');
+    }
+}
+
+let menus = document.querySelectorAll('.menu');
+console.log(menus)
+menus = Array.from(menus).map( menu => new Menu(menu));
+console.log(menus)
+
+
+
+
+let tests = document.querySelectorAll('p');
+console.log(tests)
+tests.forEach(function(item) {
+    item.addEventListener("click", function() {
+        item.style.color = 'green'
+    });
+});
