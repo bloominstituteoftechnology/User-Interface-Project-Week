3a11d34795fdbbe301532bf2ae0cac1574f3799e
tulf <timzulf@gmail.com>
2018-07-16 17:21:58 -0400

    Tim Zulf
    still setting up html. Commiting for posterity.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // JS goes here
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
