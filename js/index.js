b03739a95518c321fbcf373307433ad027c62e20
johnoro <jjcummins102@gmail.com>
2018-07-16 17:42:04 -0400

    Added functional expanded nav.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-// JS goes here
+// Nav
+let nav = document.querySelector('nav');
+let navBtn = document.querySelector('.nav-btn');
+
+navBtn.addEventListener('click', e => {
+    nav.style.display = (nav.style.display == 'none' ? 'flex' : 'none');
+    navBtn.src = (nav.style.display == 'none'? './img/nav-hamburger.png' : './img/nav-hamburger-close.png');
+});
