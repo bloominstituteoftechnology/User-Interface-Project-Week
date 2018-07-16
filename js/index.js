049f21ed34da4f8272ee3ae0aa5a31919d33e097
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-16 13:13:39 -0700

    add functionality to the nav bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // JS goes here
+
+const hamButton = document.querySelector('.ham-button');
+const hamButtonClose = document.querySelector('.ham-button-close');
+const mainNav = document.querySelector('.main-nav');
+
+hamButton.addEventListener('click', () => {
+	mainNav.classList.add('main-nav-show');
+	hamButton.style.display = 'none';
+	hamButtonClose.style.display = 'block';
+});
+
+hamButtonClose.addEventListener('click', ()=> {
+	mainNav.classList.remove('main-nav-show');
+	hamButton.style.display = 'block';
+	hamButtonClose.style.display = 'none';
+});
