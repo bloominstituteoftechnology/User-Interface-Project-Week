fa5e3c0c2e3b558e260399ee68c6dcdca48894ff
Trevor <trevorfehrman@gmail.com>
2018-07-16 13:44:30 -0700

    Menu overlay working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // JS goes here
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open');
+}
+
+const menu = document.querySelector('.menu-overlay');
+console.log(menu);
+const menuButton = document.querySelector('.menu-button');
+console.log(menuButton);
+menuButton.addEventListener('click', toggleMenu);
