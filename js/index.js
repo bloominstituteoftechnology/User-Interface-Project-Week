c60c616a4ca74c08a1ad749cd16ae427d89a4bca
dasgma <dasgrigoma@gmail.com>
2018-07-16 11:03:59 -0700

    Rough header/nav done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // JS goes here
+
+let menuButton = document.querySelector(".menu-button");
+let closeButton = document.querySelector(".close-button");
+
+menuButton.addEventListener("click", () => {
+    document.querySelector(".navigation-links").style.display = "flex";
+    menuButton.style.display = "none";
+    closeButton.style.display = "block";
+})
+
+closeButton.addEventListener("click", () => {
+    document.querySelector(".navigation-links").style.display = "none";
+    menuButton.style.display = "block";
+    closeButton.style.display = "none";
+})
