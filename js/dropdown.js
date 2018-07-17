ad0d6fd8ccbcb97e317eb8575e8feae390b7f187
Erik Kimsey <erik@erikkimsey.com>
2018-07-17 17:52:19 -0400

    Progress on scratch dropdown component

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,8 +2,25 @@
 class DropDown {
     constructor ([...names]){
         this.itemNames = [...names];
-        console.log(this.itemNames);   
+        // this.dropdown = document.querySelector('.beat-drop');
+        // this.listItemContainer = `<div class="menu-items"></div>`;  
+        // this.dropdown.append(this.listItemContainer);      
+        this.items = this.createItems(this.itemNames);
+        console.log(this.items);
         
+        
+        // this.appendedList = this.appendList(this.listItemContainer, this.items);
+    }
+
+    createItems(items){
+        let temp = items.map((elem) => {
+            return new DropDownItem(elem);
+        });
+        console.log(typeof temp[0].name);
+        temp = temp.map((elem)=>{
+            return elem.createItem(elem.name);
+        });
+        return temp;
     }
 
 
@@ -11,7 +28,7 @@ class DropDown {
 
 class DropDownItem {
     constructor(name){
-        this.name = name;
+        this.name = name;   
     }
     createItem(name){
         return `<div class="drop-down-item data-name="${name}">${name}</div>`;
