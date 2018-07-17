ac927cb19b8041fef40bf1f476a14506cee3e40d
codejoncode <jonathanjholloway@gmail.com>
2018-07-17 00:47:34 -0500

    full page navagation works spacing is off for the text included on the hidden navigation though

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // JS goes here
+
+// Drop down 
+
+function menuOpen() {
+    document.getElementById("hidden-menu").style.width = "100%";
+}
+
+function menuClose() {
+    document.getElementById("hidden-menu").style.width = "0%";
+}
