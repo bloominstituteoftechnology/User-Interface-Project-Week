a1ac29aadee94080e8b2c7c3bd145aee44415c87
sliw <slioweng@gmail.com>
2018-07-18 00:12:58 +0800

    done tab

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// Navigation
+
 let onHumburger = document.querySelector('.on-humburger');
 let offHumburger = document.querySelector('.off-humburger');
 let hiddenNavigation = document.querySelector('.hidden-navigation');
@@ -17,3 +19,49 @@ offHumburger.addEventListener('click', function(e) {
   hiddenNavigation.classList.add('hidden');
   hiddenNavigation.classList.remove('expanded');
 });
+
+// Service
+// Tab
+class Tabs {
+  constructor(tabElement) {
+    this.element = tabElement;
+    this.tabContent = topContent.map(content => new Content(content));
+    this.activeTab = this.tabContent[1];
+
+    this.element.addEventListener('click', this.select.bind(this));
+    this.init();
+  }
+
+  init() {
+    this.tabContent.forEach(content => {
+      if (content.dataTab === this.activeTab.dataTab) {
+        content.element.style.display = 'flex';
+      } else {
+        content.element.style.display = 'none';
+      }
+    })
+  };
+
+  select(e) {
+    this.tabContent.forEach(content => {
+      if (content.dataTab === e.target.dataset.tab) {
+        content.element.style.display = 'flex';
+        this.activeTab = content;
+        console.log(this.activeTab);
+      } else {
+        content.element.style.display = 'none';
+      }
+    })
+  }
+}
+
+class Content {
+  constructor(contentElement) {
+    this.element = contentElement;
+    this.dataTab = this.element.dataset.tab;
+  }
+}
+
+let topContent = Array.from(document.querySelectorAll('.top-content'));
+let serviceTabs = Array.from(document.querySelectorAll('.service-tab'));
+serviceTabs = serviceTabs.map(tab => new Tabs(tab));
