b95a5768ebe74ee6a5313dc0d423eb298fd23d38
sca4cs <shannon.atkinson.ca@gmail.com>
2018-07-17 08:21:05 -0400

    services page

--- js/index.js
+++ js/index.js
@@ -8,3 +8,75 @@ const toggleMenu = () => {
   const menu = document.querySelector(".menu");
   const menuButton = document.querySelector(".menu-button");
   menuButton.addEventListener("click", (toggleMenu));
+
+
+
+
+
+
+  //Services Tabs Component
+  class Tabs {
+    constructor(element) {
+      this.element = element;
+      this.links = this.element.querySelectorAll('.tabs-link');
+      this.links = Array.from(this.links).map(link => {
+        return new TabsLink(link, this);
+      });
+      this.activeLink = this.links[0];
+      this.init();
+    }
+  
+    init() {
+      this.activeLink.select()
+    }
+  
+    updateActive(newActive) {
+      this.activeLink.deselect();
+      this.activeLink = newActive;
+    }
+  
+    getTab(data) {
+      return this.element.querySelector(`.tabs-item[data-tab="${data}"]`)
+    }
+  
+  }
+  
+  class TabsLink {
+    constructor(link, parent) {
+      this.link = link;
+      this.tabs = parent;
+      this.tabsItem = parent.getTab(this.link.dataset.tab);
+      this.tabsItem = new TabsItem(this.tabsItem);
+      this.link.addEventListener('click', () => {
+        this.tabs.updateActive(this);
+        this.select()
+      });
+    };
+  
+    select() {
+      this.link.classList.add('tabs-link-selected')
+      this.tabsItem.select();
+    }
+  
+    deselect() {
+      this.link.classList.remove('tabs-link-selected');
+      this.tabsItem.deselect();
+    }
+  }
+  
+  class TabsItem {
+    constructor(element) {
+      this.element = element;
+    }
+  
+    select() {
+      this.element.classList.add('tabs-item-selected');
+    }
+  
+    deselect() {
+      this.element.classList.remove('tabs-item-selected');
+    }
+  }
+  
+  let tabs = document.querySelectorAll('.tabs');
+  tabs = Array.from(tabs).map( tab => new Tabs(tab));
