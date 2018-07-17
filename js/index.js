f9961b2605a674130a9532cc4f8e029f2fa833fd
Erica Thompson <EricaThompson@iMac.lan>
2018-07-17 15:47:00 -0700

    1/4

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // JS goes here
+
+
+// function mobileImages(width) {
+//     let mobile = document.querySelector(".mobile");
+//     if (width.matches){
+//         mobile.src = "/img/home/home-mobile-jumbotron.png";
+
+//     }  else {
+//         mobile.src = "";
+//     }  
+// }
+
+// let width = window.matchMedia("(max-width: 500px)")
+// let mobile = document.querySelector(".mobile");
+// mobileImages(width);
+// width.addListener(mobileImages);
+
+
+// let mobile = document.querySelector(".mobile");
+// console.log(mobile);
+
+// mobile.src = "/img/home/home-mobile-jumbotron.png";
