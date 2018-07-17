db25e7e434a667116ad7a54f4558bd8203625daf
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-07-17 11:35:15 -0600

    Added the js fullscreen menu navigation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-// JS goes here
+const menuButton = document.querySelector('.open-hamburger'); 
+const menuOpenContent = document.querySelector('.menu-content'); 
+const closeMenuButton = document.querySelector('.closed-hamburger'); 
+
+menuButton.addEventListener('click', function(){
+    menuOpenContent.classList.remove('menu-hidden'); 
+    menuButton.style.display = 'none'; 
+    closeMenuButton.style.display = "flex"; 
+})
+
+closeMenuButton.addEventListener('click', function(){
+    menuOpenContent.classList.add('menu-hidden'); 
+    menuButton.style.display = "flex"; 
+    closeMenuButton.style.display = "none";
+})
+
+
+
+
+
+
+
