140a77913ccc4108e430b8038c9e1da37997e73c
Erik Kimsey <erik@erikkimsey.com>
2018-07-17 18:27:09 -0400

    Homemade dropdown component complete.  Beginning binding of dropdown to index.html

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,42 +1,67 @@
 
 class DropDown {
-    constructor ([...names]){
-        this.itemNames = [...names];
-        // this.dropdown = document.querySelector('.beat-drop');
+    constructor ([...elems]){
+        this.itemArray = [...elems];
+        this.dropdown = document.querySelector('.beat-drop');
         // this.listItemContainer = `<div class="menu-items"></div>`;  
         // this.dropdown.append(this.listItemContainer);      
-        this.items = this.createItems(this.itemNames);
+        this.items = this.createItems(this.itemArray);
         console.log(this.items);
-        
-        
-        // this.appendedList = this.appendList(this.listItemContainer, this.items);
+        this.appendList(this.dropdown, this.items);
     }
 
     createItems(items){
         let temp = items.map((elem) => {
             return new DropDownItem(elem);
         });
-        console.log(typeof temp[0].name);
         temp = temp.map((elem)=>{
-            return elem.createItem(elem.name);
+            console.log(elem);
+            
+            return elem.createItem();
         });
         return temp;
+    }
+
+    appendList(duper, list){
+        list.forEach((elem)=>{
+            duper.appendChild(elem);
+        });
     }
 
 
 }
 
 class DropDownItem {
-    constructor(name){
-        this.name = name;   
+    constructor(elem){
+        this.title = elem.title;   
+        this.url = elem.url;
     }
-    createItem(name){
-        return `<div class="drop-down-item data-name="${name}">${name}</div>`;
+    createItem(){
+        return `<a href="${this.url}" class="drop-down-item data-name="${this.title}">${this.title}</div>`;
     }
+
 }
 
+const tags = [
+    {
+        title: "silly",
+        url: "https://en.wikipedia.org/wiki/Cybernetic_Culture_Research_Unit"
+    },
+    {
+        title: "billy",
+        url: "https://en.wikipedia.org/wiki/Cybernetic_Culture_Research_Unit"
+    },
+    {
+        title: "fo",
+        url: "https://en.wikipedia.org/wiki/Cybernetic_Culture_Research_Unit"
+    },
+    {
+        title: "rilly",
+        url: "https://en.wikipedia.org/wiki/Cybernetic_Culture_Research_Unit"
+    }
+];
 const names = ['silly', 'billy', 'fo', 'rilly'];
-const drop = new DropDown(names);
+const drop = new DropDown(tags);
 
 
 
