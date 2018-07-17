4181c6298fbcf70057377377f215bf93a51bae20
Trevor <trevorfehrman@gmail.com>
2018-07-17 13:35:15 -0700

    Menu Toggling Working, minor debugging in progress.

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,17 @@
 const toggleMenu = () => {
     menu.classList.toggle('menu--open');
-    menuButton.classList.toggle('hide-hamburger');
+    // menuButton.classList.toggle('hide-hamburger');
+    if (imgTracker === 'open') {
+        menuButton.src='img/nav-hamburger-close.png';
+        imgTracker = 'closed';
+    } else {
+        menuButton.src='img/nav-hamburger'
+        imgTracker = 'open';
+    }
 }
 
 
 const menu = document.querySelector('.menu-overlay');
 const menuButton = document.querySelector('.menu-button');
+const imgTracker = 'open';
 menuButton.addEventListener('click', toggleMenu);
