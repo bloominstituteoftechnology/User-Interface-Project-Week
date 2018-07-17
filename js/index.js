fa7f249b22b11bff31652861501db79dd672b4fd
mkerbleski <mkerbles@gmail.com>
2018-07-16 20:28:01 -0600

    Started work on nav functions.
    Currently logs that an object was clicked.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 // JS goes here
-let navBTN = document.querySelectorAll()
+
+const showNavPanel = (e) => {
+  console.log(`${e} was clicked`);
+  // navPage.classlist.toggle('.show');
+}
+
+const navBTN = document.querySelector('#nav-icon');
+const navPage = document.querySelector("#nav-page");
+
+console.log(navBTN);
+console.log(navPage);
+
+navBTN.addEventListener("click", showNavPanel);
