11320073e292be05020f8d344ba9037b279ee6e9
Erica Thompson <EricaThompson@iMac.lan>
2018-07-18 12:13:10 -0700

    Nav Animation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
+
+// Nav Bar
 let navItems = document.querySelector('.navItems');
 let test = document.querySelector('.queryTest');
-navItems.style.display = "none";
+let navHome = document.querySelector('.home');
+let navServices = document.querySelector('.services');
+let navContact = document.querySelector('.contact');
+navHome.style.display = "none";
+navServices.style.display = "none";
+navContact.style.display = "none";
 
 console.log(test);
 
@@ -8,13 +15,20 @@ $(document).ready(function() {
     $(".hamburger").click(function() {
         if (this.src.length === 43){
       this.src="/img/nav-hamburger-close.png";
-      navItems.style.display = "flex";}
+    //   navItems.style.display = "flex";
+      $(".contact").fadeIn();
+      $(".services").fadeIn(1000);
+      $(".home").fadeIn(2000);
+      $(".test").style.color = "copper";
+    }
 
 
         else if (this.src.length === 49){
-      this.src="/img/nav-hamburger.png";
-      navItems.style.display = "none";
-        }
+        this.src="/img/nav-hamburger.png";
+        navHome.style.display = "none";
+        navServices.style.display = "none";
+        navContact.style.display = "none";
+     }
     });
   });
   
