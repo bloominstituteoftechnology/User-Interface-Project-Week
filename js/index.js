e7c73735b437cca082a4b2dbb8e79fc32e1dc5fa
Rob <rob@users-MacBook-Air.local>
2018-07-17 21:22:17 -0500

    attempts to change logo positoning and size

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // JS goes here
+class Dropdown {
+    constructor(element){
+        this.element=element;
+        this.button=this.element.querySelector('.dropdown-button');
+        this.button.addEventListener("click", ()=> {this.toggleContent()});
+    }
+    toggleContent() {
+        this.toggleContent.classList.toggle("dropdown-hidden");
+
+    }
+}
+let dropdowns = document.querySelectorAll('.dropdown');
+dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
+
+
+
+  
+  
