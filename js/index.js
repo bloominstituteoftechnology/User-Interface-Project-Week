05d945f06abaccb2c1c1423447308d1351b4857f
jordan-massingill <jordan_massingill@hotmail.com>
2018-07-18 16:14:21 -0500

    Working on Task 4
    Completed some of the JS for the navbar
    Currently troubleshooting navbar functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // JS goes here
+class NaviBar = {
+  constructor (element) {
+    this.element = element;
+    this.links = this.element.querySelector(".links");
+    this.burger = this.element.querySelector(".navBurger");
+    this.burger.addEventListener("click", () => {this.toggleContent()});
+  }
+
+  toggleContent() {
+    this.classList.toggle("burger-select-nav");
+    this.links.classList.toggle("links-hidden");
+  }
+
+}
+
+let naviBar = document.querySelectorAll(".nav");
+naviBar = Array.from(naviBar).map(x => new NaviBar (x));
