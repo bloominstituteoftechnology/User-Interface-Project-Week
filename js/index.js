9b01f73cc9fa049faca93ddf014d64e85c55fce3
jordan-massingill <jordan_massingill@hotmail.com>
2018-07-18 18:20:25 -0500

    Changes- step 4
    Almost finished with the navbar
    Working on the hamburger/Exit image change
    Current code is not less-watch-compiler compatible

--- js/index.js
+++ js/index.js
@@ -3,13 +3,16 @@ class NaviBar {
   constructor (element) {
     this.element = element;
     this.links = this.element.querySelector(".links");
-    this.burger = this.element.querySelector(".navBurger");
+    this.burger = this.element.querySelector(".burger-div");
+    // this.burgers = this.element.querySelectorAll(".navBurger");
+    this.img = this.element.querySelector(".burger-img")
     this.burger.addEventListener('click', () => {this.toggleContent()});
   }
 
   toggleContent() {
     this.element.classList.toggle("burger-select-nav");
     this.links.classList.toggle("links-hidden");
+    this.burger.classList.toggle("burger-on");
   }
 
 }
