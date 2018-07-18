9de446453c5f84f56a1081b3f433d389017aea03
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-07-18 10:48:28 -0500

    Working on Java for menu
    The menu is working on the home page, got to change some things on the service page so it works there as well.

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+console.log("menu connected")
+
+const toggleMenu = () => {
+    // Toggle the "menu--open" class on your menu refence. 
+  }
+  
+  // Start Here: Create a reference to the ".menu" class
+  const menu = document.querySelector(".menu");
+  // create a reference to the ".menu-button" class
+  const menuButton = document.querySelector(".menu-button");
+  // Using your menuButton reference, add a click handler that calls toggleMenu
+  
+  menuButton.addEventListener("click", (event)=>{
+    console.log("toggleMenu")
+    menu.classList.toggle("menu--open");
+  })
