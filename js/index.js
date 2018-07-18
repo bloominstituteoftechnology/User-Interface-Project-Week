e7692b496b83ef604c1f4f090d2103c2e49af528
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-07-18 18:34:37 -0400

    Minor overlooked styling issue from removing bootstrap -added some padding to homepage and navbar.

--- js/index.js
+++ js/index.js
@@ -11,9 +11,9 @@ let headerInner =
 
 //the toggle box which overlays the screen when hamburger is clicked
 let toggleInner =
-  `<a href="index.html" class="nav-link">Home</a>
-  <a href="services.html" class="nav-link">Services</a>
-  <a href="#" class="nav-link">Contact</a>`;
+   `<a href="index.html" class="nav-link">Home</a>
+    <a href="services.html" class="nav-link">Services</a>
+    <a href="#" class="nav-link">Contact</a>`;
 
 let header = document.querySelector('header');
 header.classList.add('myHeader', 'myModal');//add classes here
