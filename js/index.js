311d201732f6081bda4883a080695646d3bda914
Erica Thompson <EricaThompson@iMac.lan>
2018-07-18 11:46:02 -0700

    navItems Toggling Properly

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 let navItems = document.querySelector('.navItems');
 let test = document.querySelector('.queryTest');
+navItems.style.display = "none";
+
 console.log(test);
 
 $(document).ready(function() {
     $(".hamburger").click(function() {
         if (this.src.length === 43){
       this.src="/img/nav-hamburger-close.png";
-      test.innerHTML = "hello";}
+      navItems.style.display = "flex";}
 
 
-        else if (this.src.length === 49)
+        else if (this.src.length === 49){
       this.src="/img/nav-hamburger.png";
+      navItems.style.display = "none";
+        }
     });
   });
   
