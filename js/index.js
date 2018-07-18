6269e4c6537b79b59359e73ad73308076ec78ad2
Charles Cortinas <crcortinas@gmail.com>
2018-07-18 18:47:26 -0500

    navigation
    Javascript completed for navigation menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// JS goes here
+let navBtn = document.querySelector('.nav-btn');
+let nav = document.querySelector('.hamburger-content');
+let navImg = document.querySelector('.nav-img');
+let close = document.querySelector('.close-btn');
+navBtn.addEventListener('click', () => {
+  nav.classList.toggle('hidden');
+  close.classList.toggle('hidden');
+  navImg.classList.toggle('hidden');
+});
