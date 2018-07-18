433d79e5f17cc667eb76268d1c6dd69a72a2d699
Rob <rob@users-MacBook-Air.local>
2018-07-18 15:37:45 -0500

    fixed split pictures

--- js/index.js
+++ js/index.js
@@ -1,17 +1,5 @@
 // JS goes here
-class Dropdown {
-    constructor(element){
-        this.element=element;
-        this.button=this.element.querySelector('.dropdown-button');
-        this.button.addEventListener("click", ()=> {this.toggleContent()});
-    }
-    toggleContent() {
-        this.toggleContent.classList.toggle("dropdown-hidden");
 
-    }
-}
-let dropdowns = document.querySelectorAll('.dropdown');
-dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
 
 
 
