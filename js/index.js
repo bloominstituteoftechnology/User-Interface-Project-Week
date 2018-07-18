399c6ccb8f67eed67c03633d8b45ef075ba53b5f
Kelley Henican <kelleyhenican2@gmail.com>
2018-07-18 15:33:40 -0500

    added scroll event listener for nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS goes here
+
+// navbar scroll 
+
+let nav = document.querySelector(".nav");
+
+window.addEventListener("scroll", () => {
+    nav.style.background = "#9DA7B1";
+    nav.style.opacity = "0.95";
+    nav.style.zIndex = "1";
+})
+
