e61caae134e66f9250551ff1b5d0ffdfb1f089f4
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-07-18 14:21:40 -0700

    Major Changes - Navigation System
    Added functionality to the menu button. When clicked, the menu opens. Minor changes needed to be done (the hamburger image needs to change when clicked. Also the navbar has a max-width, so when the menu opens up, it doesn't look good).

--- NavigationSystem/navigation.js
+++ NavigationSystem/navigation.js
@@ -1,5 +1,13 @@
+const toggleMenu = () => {
+    menu.classList.toggle("menu--open");
+}
+
+
+
 // Create a reference to the ".custom-menu" class
 const menu = document.querySelector(".custom-menu");
-
-// Create a reference to the ""
+// Create a reference to the  ".menu-button" class (hamburger image)
+const menuButton = document.querySelector(".menu-button");
+//Using my menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", toggleMenu);
 
