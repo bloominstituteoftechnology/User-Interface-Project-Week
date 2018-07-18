140bac6d73c1bc64dea15d5304588d7b6a5ac228
Grant Reighard <grantreighard@Grants-iMac.local>
2018-07-18 13:59:08 -0400

    Working on carousel on projects.html.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Tab Navigation
 class Tabs {
     constructor(element) {
         this.element = element;
@@ -67,7 +68,7 @@ tabs = Array.from(tabs).map(tab => new Tabs(tab));
 
 
 
-let open = false;
+// Expand Navigation
 class ExpandedNav {
     constructor(element) {
         this.element = element;
@@ -103,3 +104,90 @@ let expandedNav = document.querySelectorAll(".nav");
 console.log(expandedNav);
 expandedNav = Array.from(expandedNav).map(expanded => new ExpandedNav(expanded));
 console.log(expandedNav);
+
+
+// Carousel
+
+
+class Carousel {
+    constructor(element) {
+        this.carousel = element;
+        this.images = document.querySelectorAll(".carousel img");
+
+        this.images = Array.from(this.images);
+
+        this.prevButton = document.querySelector(".left-button");
+        this.nextButton = document.querySelector(".right-button");
+
+        this.activeImg = this.images[0];
+        this.activeImg.style.display = "block";
+
+        this.prevImg = this.images[this.images.length - 1];
+        this.nextImg = this.images[1];
+
+        this.prevButton.addEventListener('click', () => {
+            this.prevImg.style.display = "block";
+            this.goToPrevious();
+        });
+
+        this.nextButton.addEventListener('click', () => {
+            this.nextImg.style.display = "block";
+            this.goToNext();
+        });
+    }
+
+    goToPrevious() {
+        let toDisappear = this.activeImg;
+        toDisappear.style.position = "absolute";
+
+        TweenMax.fromTo(this.prevImg, 2, {xPercent:-100}, {xPercent:0});
+        TweenMax.fromTo(this.activeImg, 2, {xPercent:0}, {xPercent:100, onComplete: () => {
+            toDisappear.style.display = "none";
+            toDisappear.style.position = "relative";
+        }});
+
+        if((parseInt(this.activeImg.dataset.img)) === 0) {
+            this.activeImg = this.images[this.images.length - 1];
+        } else {
+            this.activeImg = this.images[parseInt(this.activeImg.dataset.img) - 1];
+        }
+
+        this.setPrevNext();
+    }
+
+    goToNext() {
+        let toDisappear = this.activeImg;
+        toDisappear.style.position = "absolute";
+
+        TweenMax.fromTo(this.nextImg, 2, {xPercent:100}, {xPercent:0});
+        TweenMax.fromTo(this.activeImg, 2, {xPercent:0}, {xPercent:-100, onComplete: () => {
+            toDisappear.style.display = "none";
+            toDisappear.style.position = "relative"; 
+        }});
+
+        if((parseInt(this.activeImg.dataset.img)) === (this.images.length - 1)) {
+            this.activeImg = this.images[0];
+        } else {
+            this.activeImg = this.images[parseInt(this.activeImg.dataset.img) + 1];
+        }
+
+        this.setPrevNext();
+    }
+
+    setPrevNext() {
+        if (parseInt(this.activeImg.dataset.img) === 0) {
+            this.prevImg = this.images[this.images.length - 1];
+        } else {
+            this.prevImg = this.images[parseInt(this.activeImg.dataset.img) - 1];
+        }
+        if ((parseInt(this.activeImg.dataset.img)) === (this.images.length - 1)) {
+            this.nextImg = this.images[0];
+        } else {
+            this.nextImg = this.images[parseInt(this.activeImg.dataset.img) + 1];
+        }
+    }
+}
+
+
+let carousel = document.querySelectorAll(".carousel");
+carousel = Array.from(carousel).map(carousel => new Carousel(carousel));
