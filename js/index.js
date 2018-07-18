9a331f0a5eeeebe097218b3cc53bcd062368db05
codejoncode <jonathanjholloway@gmail.com>
2018-07-18 15:30:43 -0500

    i have a working carousel on the projects page but I don't like the sizing bigger than i thought it would be and i need to position the stop button better

--- js/index.js
+++ js/index.js
@@ -103,3 +103,62 @@ class Tabs {
   let tabs = document.querySelectorAll(".tabs");
   // map through each tabs element and create a new Tabs object.  pass in a reference to the tab when creating the Tabs object.
   tabs = Array.from(tabs).map( tab => new Tabs(tab));
+
+
+
+  //Carousel  funtionality 
+
+  class Carousel{
+    constructor(containerID){
+    this.container = document.getElementById(containerID) || document.body; 
+    this.slides = this.container.querySelectorAll('.carousel');
+    this.total = this.slides.length - 1; 
+    this.current = 0; 
+    this.slide(this.current); //This will start on slide 1. 
+    }
+    next (interval){
+      (this.current === this.total) ? this.current = 0 : this.current += 1; 
+  
+      this.stop(); 
+      this.slide(this.current); 
+  
+      if(typeof interval === 'number' && (interval % 1) === 0){
+        let context = this; 
+        this.run = setTimeout(function(){
+          context.previous(interval);
+        }, interval); 
+      }
+    }
+    //previous 
+    previous(interval){
+      (this.current === this.total) ? this.current = 0 : this.current += 1; 
+  
+      this.stop(); 
+      this.slide(this.current); 
+  
+      if(typeof interval === 'number' && (interval % 1) === 0){
+        let context = this; //allows us to access current this inside of another function. 
+        this.run = setTimeout(function(){
+          context.previous(interval); 
+        }, interval); 
+      }
+    }
+    stop() {
+      clearTimeout(this.run); 
+    }
+    slide (index){
+      if(index >= 0 && index <= this.total) {
+        this.stop();
+        for (let i = 0; i <= this.total; i++){
+          if (i === index) {
+            this.slides[i].style.display = "inline-block";
+          } else {
+            this.slides[i].style.display = 'none'; 
+          }
+        }
+      } else {
+        alert("Index " + index + " doesn't exist. Available : 0 - " + this.total); 
+      }
+    }
+}
+const carouselImages = new Carousel("images"); 
