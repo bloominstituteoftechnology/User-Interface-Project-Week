0c4a712a79f2b6bccbd321c877c46ac619a72664
Kelley Henican <kelleyhenican2@gmail.com>
2018-07-18 15:36:58 -0500

    added navbar overlay

--- js/index.js
+++ js/index.js
@@ -10,3 +10,19 @@ window.addEventListener("scroll", () => {
     nav.style.zIndex = "1";
 })
 
+// navbar overlay 
+
+let hamburger = document.querySelector(".hamburger");
+let overlay = document.querySelector("#overlay");
+let anchor = document.querySelector(".home");
+let closeHamburger = document.querySelector(".hamburger-close");
+
+
+hamburger.addEventListener("click", () => {
+    overlay.style.display = "block";
+})
+
+closeHamburger.addEventListener("click", () => {
+    overlay.style.display = "none";
+})
+
