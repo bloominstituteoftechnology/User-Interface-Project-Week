3f9280b6e589af143af626ec296011796377c1a1
jordan-massingill <jordan_massingill@hotmail.com>
2018-07-18 17:19:18 -0500

    Navigation Bar Functionality
    Updated less and js for navbar functionality

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 // JS goes here
-class NaviBar = {
+class NaviBar {
   constructor (element) {
     this.element = element;
     this.links = this.element.querySelector(".links");
     this.burger = this.element.querySelector(".navBurger");
-    this.burger.addEventListener("click", () => {this.toggleContent()});
+    this.burger.addEventListener('click', () => {this.toggleContent()});
   }
 
   toggleContent() {
-    this.classList.toggle("burger-select-nav");
+    this.element.classList.toggle("burger-select-nav");
     this.links.classList.toggle("links-hidden");
   }
 
 }
 
 let naviBar = document.querySelectorAll(".nav");
-naviBar = Array.from(naviBar).map(x => new NaviBar (x));
+naviBar = Array.from(naviBar).map(nav => new NaviBar (nav));
