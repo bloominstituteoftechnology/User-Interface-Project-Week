b5e7f61ddd5e2c2b44cb51775805d7a647475634
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-07-18 18:18:16 -0500

    added individual less files, added js for navigation, is sort of working..

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // JS goes here
+
+//navigation overlay open/close
+let overlay = document.querySelector(".overlay");
+let menuBtn = document.querySelector(".nav-btn");
+let closeBtn = document.querySelector(".close-btn");
+
+console.log(menuBtn)
+
+menuBtn.addEventListener('click', function() {
+  // I want overlay to display block now
+  overlay.style.display = "block";
+});
+
+closeBtn.addEventListener('click', function() {
+  // I want overlay to display none now
+  overlay.style.display = "none";
+});
