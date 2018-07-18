eb67d7241c938b7d7e8a105a7717b90bdf2b1cd7
Ashwin <aswinjeganath@gmail.com>
2018-07-19 03:03:33 +0530

    Add tabs to projects.html, sync tabs with carousel

--- js/Carousel.js
+++ js/Carousel.js
@@ -26,36 +26,48 @@ class Carousel {
       this.currentImage - 1 < 0
         ? this.images.length - 1
         : this.currentImage - 1;
+
+    store.dispatch({
+      type: 'UPDATE_ACTIVE',
+      activeTabNumber: previous
+    });
+
     const previousSlide = this.images[previous];
     const currentSlide = this.images[this.currentImage];
     previousSlide.classList.add(this.activeClassName);
     currentSlide.style.position = 'absolute';
-    TweenMax.to(currentSlide, 1, {
+    TweenMax.to(currentSlide, 0.5, {
       xPercent: 100,
       onComplete: () => {
         currentSlide.classList.remove(this.activeClassName);
         currentSlide.style.position = 'relative';
       }
     });
-    TweenMax.fromTo(previousSlide, 1, { xPercent: -100 }, { xPercent: 0 });
+    TweenMax.fromTo(previousSlide, 0.5, { xPercent: -100 }, { xPercent: 0 });
 
     this.currentImage = previous;
   }
 
   nextSlide() {
     const next = (this.currentImage + 1) % this.images.length;
+
+    store.dispatch({
+      type: 'UPDATE_ACTIVE',
+      activeTabNumber: next
+    });
+
     const nextSlide = this.images[next];
     const currentSlide = this.images[this.currentImage];
     nextSlide.classList.add(this.activeClassName);
     currentSlide.style.position = 'absolute';
-    TweenMax.to(currentSlide, 1, {
+    TweenMax.to(currentSlide, 0.5, {
       xPercent: -100,
       onComplete: () => {
         currentSlide.classList.remove(this.activeClassName);
         currentSlide.style.position = 'relative';
       }
     });
-    TweenMax.fromTo(nextSlide, 1, { xPercent: 100 }, { xPercent: 0 });
+    TweenMax.fromTo(nextSlide, 0.5, { xPercent: 100 }, { xPercent: 0 });
 
     this.currentImage = next;
   }
