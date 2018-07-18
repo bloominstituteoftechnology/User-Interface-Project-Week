59e3391356b3253ec0b29a3238470b867dd4da06
William <kwontaewoongah@gmail.com>
2018-07-18 15:42:16 -0700

    started working on tabs component

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS goes here
+class Tabs {
+    constructor (element) {
+        this.element=element;
+        console.log(this.element);
+        
+    }
+}
+let tabs=document.querySelectorAll('.tabs');
+tabs=Array.from(tabs).map((tab)=>new Tabs(tab));
+
+console.log(tabs)
