d4c6dca64a0a6f34c3ab6fec58e5466e6b04cc5c
jaredrcooper <jaredrcoopet@gmail.com>
2018-07-17 22:41:32 -0400

    parallexic effect added, jumbotron centered, not stretching to container width

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// JS goes here
+let jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    let scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
