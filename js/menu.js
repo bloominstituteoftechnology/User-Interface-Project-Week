b0286b1368c5031bb0d73ee59492871ec3bb6ea7
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-07-18 12:27:22 -0600

    Navigation menu figured out

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,27 @@
 // JS goes here
 // Menu component
+const openMenu = () => {
+    menu.classList.add("menu-open");
+    menuClose.classList.remove("hidden");
+    menuButton.classList.add("hidden");
+}
+
+const closeMenu = () => {
+    menu.classList.remove("menu-open");
+    menuClose.classList.add("hidden");
+    menuButton.classList.remove("hidden");
+   
+}
+
+
+const menu = document.querySelector(".menu");
+const menuButton = document.querySelector(".menu-button");
+const menuClose = document.querySelector(".close-button");
+
+menuButton.addEventListener("click", openMenu);
+menuClose.addEventListener("click", closeMenu);
+
+console.log(menu);
+console.log(menuButton);
+console.log(menuClose);
 
