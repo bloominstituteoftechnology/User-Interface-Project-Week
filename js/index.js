fe665d02fccef488365e295e6bf036e313b93b6a
Arthur Bates <aabatesj@gmail.com>
2018-07-19 11:47:47 -0400

    Navigation functionality
    working, but not full screen

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // JS goes here
+const mainNav = document.querySelector(".main-nav");
+const menuBtn = document.querySelector(".menu-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click", (event) => {
+    menuBtn.classList.toggle("hidden");
+    mainNav.classList.toggle("display-flex");
+    closeBtn.classList.add("visibility");
+})
+
+closeBtn.addEventListener("click", (event) => {
+    menuBtn.classList.toggle("hidden");
+    mainNav.classList.toggle("display-flex");
+    closeBtn.classList.remove("visibility");
+})
