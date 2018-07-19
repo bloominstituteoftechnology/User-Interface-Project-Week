4d405346798f753073066bea7de04d43274c1327
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-07-19 11:29:45 -0700

    Menu button working properly.
    Menu button changes to X when clicked and changes back to hamburger button when clicked again.
    
    Restructured the navigation.less.

--- NavigationSystem/navigation.js
+++ NavigationSystem/navigation.js
@@ -1,25 +1,45 @@
-const toggleMenu = () => {
-    menu.classList.toggle("menu--open");  
-}
+// const menuOpen = () => {
+//     menu.classList.toggle("menu--open");  
+// }
 
-const openMenu = function () {
-    if(menuButton.src = "img/nav-hamburger.png") {
-        return menuButton.src = "img/nav-hamburger-close.png";
-    } 
-    else if(menuButton.src = "img/nav-hamburger-close.png") {
-        return menuButton.src = "img/nav-hamburger.png"; /* === This isnt working ====*/
-    }
-}
+// const menuClose = () => {
+//     menu.classList.toggle("menu-");  
+// }
+
+
+// // swapImage changes the hamburger image
+// let swapImage =  () => {
+//     closeHamburger.classList.toggle("appear--menu");
+//     hamburgerButton.classList.toggle("close-button");
+// }
+    
 
 
     
 
 
-// Create a reference to the ".custom-menu" class
+// // Create a reference to the ".custom-menu" class
 const menu = document.querySelector(".custom-menu");
-// Create a reference to the  ".menu-button" class (hamburger image)
-const menuButton = document.querySelector(".menu-button");
-//Using my menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", toggleMenu);
-menuButton.addEventListener("click", openMenu);
+// // Create a reference to the  ".menu-button" class (hamburger image)
+// const menuButton = document.querySelector(".menu-button");
+// //Using my menuButton reference, add a click handler that calls toggleMenu
+// menuButton.addEventListener("click", toggleMenu);
+
+
+
+let Button = document.querySelector(".open-button");
+let ButtonClose = document.querySelector(".close-button");
+
+// hamburgerButton.addEventListener("click", swapImage);
+Button.addEventListener("click", () => {
+    menu.classList.toggle("menu--open");
+    Button.style.display = "none";
+    ButtonClose.style.display = "block";
+})
+
+ButtonClose.addEventListener("click", () => {
+    menu.classList.toggle("menu--open");
+    Button.style.display = "block";
+    ButtonClose.style.display = "none";
+})
 
