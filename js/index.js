21d6b673e9ca56f5d03eb2e974ddf159254b14ac
codejoncode <jonathanjholloway@gmail.com>
2018-07-19 12:36:15 -0500

    added a spinning icon next to the link in the menu on hover and goes away on mouseout

--- js/index.js
+++ js/index.js
@@ -15,10 +15,10 @@ function menuClose() {
 //Below is components being added to each page. The body etc. 
 
 let body = document.querySelector("body");
-body.addEventListener("contextmenu", (element)=> {
-    element.preventDefault(); 
-    alert("Right clicking has been disabled!"); //Question would this be a secure method for protecting user profiles from password hacks? 
-}); //works now
+// body.addEventListener("contextmenu", (element)=> {
+//     element.preventDefault(); 
+//     alert("Right clicking has been disabled!"); //Question would this be a secure method for protecting user profiles from password hacks? 
+// }); //works now
 
 // body.addEventListener("keypress", (element) => {
 //     alert("We can't see what you type here. Please contact us by phone at 1(8")
@@ -37,6 +37,33 @@ body.addEventListener("select", (e) => {
         alert("Please reference www.greatidea.io"); 
 });
 
+
+//Menu to add a font awesome spinner on hover and remove it once the hover stops 
+
+//get the actual links 
+
+const dropDownLinks = document.querySelectorAll('.dropdown-link');
+const icon = document.createElement('i');
+icon.classList.add("fa");
+icon.classList.add("fa-cog");
+icon.classList.add("fa-spin");
+icon.classList.add("fa-fw");
+dropDownLinks.forEach(function(dropDownLink){
+    dropDownLink.addEventListener("mouseover", () => {
+        dropDownLink.appendChild(icon);
+        
+    });
+});
+
+dropDownLinks.forEach(function(dropDownLink){
+    dropDownLink.addEventListener("mouseout", () => {
+        dropDownLink.removeChild(icon); 
+    });
+});
+
+
+
+{/* <i class="fa fa-cog fa-spin fa-fw "></i> */}
 
 
 
