aef4ddd8fde28e4ea6323767286a1ff6e9bd5bed
Vance Leon <vanceleon44@gmail.com>
2018-07-18 21:48:38 -0400

    navigation bar completed with links to home and services pages styling toggle view on and off set margin for user friendliness on the a tags

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
 // JS goes here
 console.log("is it working?")
+
+console.log("vance is awesome and he totaly can do this")
+
+const hamburgerMenu = document.querySelector(".hamburger");
+const menuNav = document.querySelector(".menu-navigation");
+
+
+hamburgerMenu.addEventListener("click", (event)=> {
+    // console.log("I work when clicked!");
+    menuNav.classList.toggle("full-display");
+})
+
+
+
+
+
+
+// const mainNav = document.querySelector(".main-nav");
+// menuBtn.addEventListener("click", (event) => {
+//     //mainNav.style.display = "flex"; 
+//     //console.log(mainNav.classList);
+//     mainNav.classList.toggle("display-flex");
+//   })
+  
