138b54e770287cc138271da82cb1bd973ee3914d
AlexanderCaves <ahcaves@gmail.com>
2018-07-19 15:12:37 -0700

    done for now

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+class Menu {
+    constructor(element) {
+      this.element = element;
+      this.button = this.element.querySelector('.menu-icon');
+      this.content = this.element.querySelector('.menu-content');
+      this.button.addEventListener('click', () => {this.toggleContent()});
+    }
+    toggleContent() {
+      this.content.classList.toggle('menu-hidden');
+    }
+  }
+ 
+  let hamburgers = document.querySelectorAll('.hamburger');
+  hamburgers = Array.from(hamburgers).map( hamburger => new Menu(hamburgers));
