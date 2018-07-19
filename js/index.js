dc07eaae529229f932b769cdfee6e200527cbf87
bri79319 <bri79319@gmail.com>
2018-07-19 02:39:23 -0400

    adjustments to code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,66 @@
 // JS goes here
+class Tabs {
+    constructor(element) {
+        this.element = element;
+        this.links = this.element.querySelectorAll(".tab-link");
+        this.links = Array.from(this.links).map(link => {
+            return new TabsLink(link, this);
+        });
+        this.activeLink = this.links[0];
+        this.init();
+    }
+
+    init() {
+        this.activeLink.select();
+    }
+
+    updateActive(newActive) {
+        this.activeLink.deselect();
+        this.activeLink = newActive;
+    }
+
+    getTab(data) {
+        return this.element.querySelector(`.tabs-item[data-tab="${data}"]`);
+    }
+}
+
+class TabsLink {
+    constructor(link, parent) {
+        this.link = link;
+        this.tabs = parent;
+        this.tabsItem = parent.getTab(this.link.dataset.tab);
+        this.tabsItem = new TabsItem(this.tabsItem);
+        this.link.addEventListener('click', () => {
+            this.tabs.updateActive(this);
+            this.select();
+        });
+    };
+
+    select() {
+        this.link.classList.add("tabs-link-selected");
+        this.tabsItem.select();
+    }
+
+    deselect() {
+        this.link.classList.remove("tabs-link-selected");
+        this.tabsItem.deselect();
+    }
+}
+
+class TabsItem {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select() {
+        this.element.classList.add("tabs-item-selected");
+    }
+
+    deselect() {
+        this.element.classList.remove("tabs-item-selected");
+    }
+}
+
+
+let tabs = document.querySelectorAll(".tbs");
+tabs = Array.from(tabs).map(each => new Tabs(each));
