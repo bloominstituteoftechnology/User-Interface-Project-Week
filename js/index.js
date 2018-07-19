98b1ba7258ea237369601f4b07d3c7087190d761
tulf <timzulf@gmail.com>
2018-07-19 14:16:00 -0400

    active nav tracker

--- js/index.js
+++ js/index.js
@@ -6,3 +6,18 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+// Keep active
+let btnContainer = document.getElementById("myNav");
+
+// Get all buttons with class="btn" inside the container
+let btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
