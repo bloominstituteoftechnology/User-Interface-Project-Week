dccfe4435b9952fcd1f3d570bf4306c70174b916
William <kwontaewoongah@gmail.com>
2018-07-18 17:18:08 -0700

    finished tabs component

--- js/index.js
+++ js/index.js
@@ -2,11 +2,51 @@
 class Tabs {
     constructor (element) {
         this.element=element;
-        console.log(this.element);
-        
+        this.links=document.querySelectorAll('.tab-link');
+        this.links=Array.from(this.links).map(link=>{return new TabLink(link,this);});
+        this.activeLink=this.links[0];
+        this.init();
+    }
+    init() {
+        this.activeLink.select();
+    }
+    updateActive(newActiveLink) {
+        this.activeLink.deselect();
+        this.activeLink=newActiveLink;
+    }
+    getTab(data) {
+        return this.element.querySelector(`.tab-item[data-tab="${data}"]`);
+    }
+}
+class TabLink{
+    constructor(link,parent) {
+        this.link=link;
+        this.tab=parent;
+        this.tabItem=parent.getTab(this.link.dataset.tab);
+        this.tabItem=new TabItem(this.tabItem);
+        this.link.addEventListener('click',()=>{this.tab.updateActive(this);this.select(this)});
+    }
+    select(){
+        this.link.classList.add('selected');
+        this.tabItem.select();
+    }
+    deselect(){
+        this.link.classList.remove('selected');
+        this.tabItem.deselect();
+    }
+}
+class TabItem{
+    constructor(element){
+        this.element=element;
+    }
+    select(){
+        console.log(this.element.classList)
+        this.element.classList.add('selected');
+    }
+    deselect(){
+        this.element.classList.remove('selected');
     }
 }
 let tabs=document.querySelectorAll('.tabs');
 tabs=Array.from(tabs).map((tab)=>new Tabs(tab));
 
-console.log(tabs)
