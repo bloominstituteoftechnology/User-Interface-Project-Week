86a178f2bd2443eae2721724f7463cf15a7deb28
codejoncode <jonathanjholloway@gmail.com>
2018-07-19 10:32:27 -0500

    added pictures modified brand name a tag on hover and added event listeners to make it so that you cannot copy material from the website with drag copy from mouse or keyboard

--- js/index.js
+++ js/index.js
@@ -10,6 +10,33 @@ function menuClose() {
     document.getElementById("hidden-menu").style.width = "0%";
 }
 
+//Above is the menu for each page
+
+//Below is components being added to each page. The body etc. 
+
+let body = document.querySelector("body");
+body.addEventListener("contextmenu", (element)=> {
+    element.preventDefault(); 
+    alert("Right clicking has been disabled!"); //Question would this be a secure method for protecting user profiles from password hacks? 
+}); //works now
+
+body.addEventListener("keypress", (element) => {
+    alert("We can't see what you type here. Please contact us by phone at 1(888) 888-8888 or Email : sales@greatidea.io")
+});
+
+body.addEventListener("copy", function(e){
+    e.clipboardData.setData('text/plain', 'Not so fast this is copyrighted material.');
+    e.preventDefault();        
+});
+
+body.addEventListener("dragstart", (e) => {
+    e.preventDefault();
+});
+
+body.addEventListener("select", (e) => {
+        alert("Please reference www.greatidea.io"); 
+    });
+
 
 
 
