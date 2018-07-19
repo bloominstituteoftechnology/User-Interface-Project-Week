2d032cd8654e9a6fc45c93c4d8be85552d2a97dc
Vance Leon <vanceleon44@gmail.com>
2018-07-18 23:58:39 -0400

    commented changes in index.js file will look at later

--- js/index.js
+++ js/index.js
@@ -3,16 +3,39 @@ console.log("is it working?")
 
 console.log("vance is awesome and he totaly can do this")
 
+
+
 const hamburgerMenu = document.querySelector(".hamburger");
+const closeButton = document.querySelector(".close-button");
 const menuNav = document.querySelector(".menu-navigation");
 
 
 hamburgerMenu.addEventListener("click", (event)=> {
     // console.log("I work when clicked!");
     menuNav.classList.toggle("full-display");
-})
 
 
+    // const x = closeButton.classList.toggle("close-button");
+    // if (x){
+    //     hamburgerMenu.classList.toggle("hamburger-none")
+    //     closeButton.classList.toggle("display-close");
+    //     console.log("hamburger off");
+    // }else{
+    //     hamburgerMenu.classList.toggle("hamburger");
+    //     console.log("hamburger on");
+    // }
+    
+    // hamburgerMenu.classList.toggle("hamburger-none");
+    
+});
+
+// closeButton.addEventListener("click", (event) =>{
+    
+    
+
+
+
+// });
 
 
 
@@ -22,5 +45,4 @@ hamburgerMenu.addEventListener("click", (event)=> {
 //     //mainNav.style.display = "flex"; 
 //     //console.log(mainNav.classList);
 //     mainNav.classList.toggle("display-flex");
-//   })
-  
+
