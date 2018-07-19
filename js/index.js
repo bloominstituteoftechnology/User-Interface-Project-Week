62869e7f7a6cd0cc802fe0ae3a2eeb52c37af868
Sean <sean47900@gmail.com>
2018-07-19 10:20:27 -0500

    minor changes to Javascript

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ class Tabs {
     constructor(element) {
       this.element = element;
       // create a reference to the ".tabs-link" class nested in your tab object
-      this.links = this.element.querySelectorAll('.tab');
+      this.links = this.element.querySelectorAll(".tab");
       // This step will map over the array creating new TabsLink class instances of each link.  No need to update anything here, just study what is going on.  Notice that we are creating another new object using the TabsLink class.
       this.links = Array.from(this.links).map( link => {
         return new TabsLink(link, this);
@@ -51,14 +51,14 @@ class Tabs {
   
     select() {
       // Add a class named "tabs-link-selected" to the link
-      this.link.classList.add('tabs-link-selected');
+      this.link.classList.add("tabs-link-selected");
       // Notice that we are using the select method on tabsItem
       this.tabsItem.select();
     }
   
     deselect() {
       // Remove a class named "tabs-link-selected" from the link
-      this.link.classList.remove('tabs-link-selected'); 
+      this.link.classList.remove("tabs-link-selected"); 
       // Notice that we are using the deselect method on tabsItem
       this.tabsItem.deselect();
     }
@@ -71,19 +71,19 @@ class Tabs {
   
     select() {
       // Add a class named "tabs-item-selected" to the element 
-      this.element.classList.add('tabs-item-selected'); console.log('hi')
+      this.element.classList.add("tabs-item-selected"); console.log('hi')
     }
   
     deselect() {
       // Remove a class named "tabs-item-selected" from the element 
-      this.element.classList.remove('tabs-item-selected');
+      this.element.classList.remove("tabs-item-selected");
       // Congrats, you finished all the instruction, check out your tab navigator!
     }
   }
   
   
   // START HERE: create a reference to the ".tabs" classes
-  let tabs = document.querySelectorAll('.mid-content');
+  let tabs = document.querySelectorAll(".mid-content");
   // map through each tabs element and create a new Tabs object.  Be sure to pass in a reference to the tab when creating the Tabs object.
   tabs = Array.from(tabs).map(tabsContainer => new Tabs(tabsContainer));
   console.log(Tabs)
