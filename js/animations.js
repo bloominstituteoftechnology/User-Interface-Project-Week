ea587b6a532612deb98c9f0a886831b578daff36
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-07-18 23:54:57 -0600

    added one animation when you open the menu

---
+++ js/animations.js
@@ -0,0 +1,7 @@
+// let menuItem = document.querySelector(".menu-items");
+
+// menuItem.addEventListener("mouseover", (event) => {
+//     TweenMax.to(".menu-items", 1, {rotation:360});
+//     event.stopPropagation();
+// })
+ 
--- js/menu.js
+++ js/menu.js
@@ -4,13 +4,14 @@ const openMenu = () => {
     menu.classList.add("menu-open");
     menuClose.classList.remove("hidden");
     menuButton.classList.add("hidden");
+    TweenMax.from(".menu", .5, {y:-800});
+    TweenMax.from(".menu-items", 1, {opacity:0});
 }
 
 const closeMenu = () => {
     menu.classList.remove("menu-open");
     menuClose.classList.add("hidden");
     menuButton.classList.remove("hidden");
-   
 }
 
 
