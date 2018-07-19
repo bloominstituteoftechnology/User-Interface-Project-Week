d71f868dbd0eb37224028d770098f84b091d9a03
Sean <sean47900@gmail.com>
2018-07-18 23:38:13 -0500

    made imports to clean up code

--- js/index.js
+++ js/index.js
@@ -1,50 +1,65 @@
-// JS goes here
 class Tabs {
-  
     constructor(element) {
       this.element = element;
-      this.links = this.element.querySelectorAll(".link");
+      // create a reference to the ".tabs-link" class nested in your tab object
+      this.links = this.element.querySelectorAll('.tab');
+      // This step will map over the array creating new TabsLink class instances of each link.  No need to update anything here, just study what is going on.  Notice that we are creating another new object using the TabsLink class.
       this.links = Array.from(this.links).map( link => {
         return new TabsLink(link, this);
       });
-      console.log(this.links)
+      // Set the active link to the first item in the array
       this.activeLink = this.links[0];
+      // Nothing to update here, just notice we are invoking the init() method
       this.init();
     }
   
     init() { 
+      // invoke the method select() on activeLink
       this.activeLink.select();
     }
-   
+  
     updateActive(newActive) {
+      // invoke the method deselect() on activeLink
       this.activeLink.deselect();
+      // assign this.activeLink to the new active link (newActive)
       this.activeLink = newActive;
     }
-
+  
     getTab(data) {
-      return this.element.querySelector(`.tabs-item[data-tab="${data}"]`); 
+      // return a reference to the element's data attribute
+      return this.element.querySelector(`.tab-content[data-tab="${data}"]`);
     }
+  
   }
+  
   class TabsLink {
+    // notice that we passed in the link reference as well as a reference to the parent class.
     constructor(link, parent) {
       this.link = link;
+      // assign this.tabs to parent
       this.tabs = parent;
-      this.tabsItem = this.tabs.getTab(this.link.dataset.tab);
-     
+      // Using the method from the parent class above, pass in a reference to the custom data attribute.  
+      this.tabsItem = parent.getTab(this.link.dataset.tab);
+      // Create a new TabsItem object that passes in a tabsItem value that you just created above
       this.tabsItem = new TabsItem(this.tabsItem);
       this.link.addEventListener('click', () => {
         this.tabs.updateActive(this);
+        // invoke the select() method on this
         this.select();
       });
     };
-
+  
     select() {
-      this.link.classList.add("tabs-link-selected"); 
+      // Add a class named "tabs-link-selected" to the link
+      this.link.classList.add('tabs-link-selected');
+      // Notice that we are using the select method on tabsItem
       this.tabsItem.select();
     }
   
     deselect() {
-      this.link.classList.remove("tabs-link-selected"); 
+      // Remove a class named "tabs-link-selected" from the link
+      this.link.classList.remove('tabs-link-selected'); 
+      // Notice that we are using the deselect method on tabsItem
       this.tabsItem.deselect();
     }
   }
@@ -55,15 +70,21 @@ class Tabs {
     }
   
     select() {
-      this.element.classList.add("tabs-item-selected"); console.log("hey")
+      // Add a class named "tabs-item-selected" to the element 
+      this.element.classList.add('tabs-item-selected'); console.log('hi')
     }
   
     deselect() {
-      this.element.classList.remove("tabs-item-selected");
+      // Remove a class named "tabs-item-selected" from the element 
+      this.element.classList.remove('tabs-item-selected');
+      // Congrats, you finished all the instruction, check out your tab navigator!
     }
   }
   
-  let tabs = document.querySelectorAll(".tabs");
   
-  tabs = Array.from(tabs).map(tab => new Tabs(tab));
-  console.log(Tabs);
+  // START HERE: create a reference to the ".tabs" classes
+  let tabs = document.querySelectorAll('.mid-content');
+  // map through each tabs element and create a new Tabs object.  Be sure to pass in a reference to the tab when creating the Tabs object.
+  tabs = Array.from(tabs).map(tabsContainer => new Tabs(tabsContainer));
+  console.log(Tabs)
+  
