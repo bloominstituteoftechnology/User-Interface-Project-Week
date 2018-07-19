094336c9d07baaca93f578aca0cf7abcb9df4171
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-07-19 12:08:08 -0500

    completed menu outside of minor styling

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 // JS goes here
 
-console.log("Is this thing on?");
+//MENU 
+
+
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.menu-button');
+const menuClose = document.querySelector('.menu-close');
+ 
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('menu--open');
+  menuButton.style.display = 'none';
+  menuClose.style.display = 'block';
+});
+
+menuClose.addEventListener('click', () => {
+  menu.classList.toggle('menu--open');
+  menuButton.style.display = 'block';
+  menuClose.style.display = 'none';
+});  
+
