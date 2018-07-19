da596e03409e41e59dbaaad86cf02f57a98fe32d
mkerbleski <mkerbles@gmail.com>
2018-07-19 12:35:17 -0600

    Completed functionality of carousel.

--- js/contact.js
+++ js/contact.js
@@ -1,42 +1,61 @@
-
+let currentItem = 0;
 
 const startCarousel = function (controls, allBlocks){
   // console.log(x)
-  init(allBlocks);
-  // moveCarousel();
+  init();
 }
 
-const init = function(allBlocks){
+const init = function(){
   allBlocks.forEach( y => {
     y.style.display = "none";
   });
-  currentItem(0);
+
+  activeItem(currentItem);
 }
 
-const moveCarousel = function(allArrows, allBlocks){
-  allArrows[1].addEventListener('click', function (allBlocks){
-    console.log('hi right');
-    for (let i = 0; i < allBlocks.length; i++){
-      activeItem(curentItem + 1)
-      console.log('hi right');
-    }
+const moveCarouselRight = function(){
+  if (currentItem === allBlocks.length-1){
+    currentItem = 0;
+    activeItem(currentItem);
+  } else {
+    currentItem = currentItem + 1;
+    activeItem(currentItem);
+  }
+};
+
+const moveCarouselleft = function(){
+  if (currentItem === 0){
+      currentItem = allBlocks.length-1;
+      activeItem(currentItem);
+    } else if (currentItem > 0) {
+      currentItem = currentItem - 1;
+      activeItem(currentItem);
+  }
+};
+
+
+const activeItem = function (number) {
+  allBlocks.forEach( y => {
+    y.style.display = "none";
   });
+  allBlocks[number].style.display ="flex"
 }
 
-const currentItem = function (number) {
-  allBlocks[number].style.display ="flex";
-}
+let random = Math.random();
+console.log(random)
 
 let allBlocks = document.querySelectorAll('.block');
 let allArrows = document.querySelectorAll('.arrow');
+let currentItmeHTML = document.querySelectorAll('.arrow');
 
-// console.log(allBlocks);
+
+console.log(currentItem);
 
 allBlocks = Array.from(allBlocks);
 
 allArrows = Array.from(allArrows);
 
-// console.log(allBlocks[1]);
-// console.log(allArrows[1]);
-
 startCarousel(allArrows, allBlocks);
+
+allArrows[1].addEventListener('click', moveCarouselRight)
+allArrows[0].addEventListener('click', moveCarouselleft)
