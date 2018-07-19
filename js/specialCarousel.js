97d2e4e5493ef8d470140c07490b0b1b1040042b
codejoncode <jonathanjholloway@gmail.com>
2018-07-19 00:38:25 -0500

    about us page features a carousel that allows for a change in header and paragraph styling of tags and special affects will be done tomorrow

--- js/specialCarousel.js
+++ js/specialCarousel.js
@@ -8,14 +8,18 @@ class Carousel{
     this.current = 0; 
     this.slide(this.current); //This will start on slide 1.
     this.paragraphs = this.container.querySelectorAll('.p-carousel');
-    this.paragraph = (this.current);
+    this.paragraph(this.current); 
     this.headers = this.container.querySelectorAll('.h-carousel'); 
+    this.header(this.current);
+    
     }
     next (interval){
       (this.current === this.total) ? this.current = 0 : this.current += 1; 
   
       this.stop(); 
-      this.slide(this.current); 
+      this.slide(this.current);
+      this.paragraph(this.current);
+      this.header(this.current);
   
       if(typeof interval === 'number' && (interval % 1) === 0){
         let context = this; //allows us to access current this inside of another function.
@@ -29,7 +33,9 @@ class Carousel{
       (this.current !== 0) ? this.current -= 1 : this.current = this.slides.length -1;
   
       this.stop(); 
-      this.slide(this.current); 
+      this.slide(this.current);
+      this.paragraph(this.current);
+      this.header(this.current);
   
       if(typeof interval === 'number' && (interval % 1) === 0){
         let context = this; //allows us to access current this inside of another function. 
@@ -55,18 +61,41 @@ class Carousel{
         alert("Index " + index + " doesn't exist. Available : 0 - " + this.total); 
       }
     }
+    header(index){
+        if(index >= 0 && index <= this.total) {
+          this.stop();
+          for (let i = 0; i <= this.total; i++){
+            if (i === index) {
+              this.headers[i].style.display = "inline-block";
+            } else {
+              this.headers[i].style.display = 'none'; 
+            }
+          }
+        } else {
+          alert("Index " + index + " doesn't exist. Available : 0 - " + this.total); 
+        }
+    }
+    paragraph(index){
+        if(index >= 0 && index <= this.total) {
+          this.stop();
+          for (let i = 0; i <= this.total; i++){
+            if (i === index) {
+              this.paragraphs[i].style.display = "inline-block";
+            } else {
+              this.paragraphs[i].style.display = 'none'; 
+            }
+          }
+        } else {
+          alert("Index " + index + " doesn't exist. Available : 0 - " + this.total); 
+        }
+    }
 }
+
 
 
 //place all variables for Carousels down here
 
-
  
 const carouselImages4 =  new Carousel("images-4");
 
-let something = "something";
-console.log(something); 
-document.querySelector('.right-button').addEventListener("click", carouselImages4.next("2000"));
-document.querySelector('.stop-button').addEventListener("click", carouselImages4.stop());
-document.querySelector('.left-button').addEventListener("click", carouselImages4.previous("2000"));
 console.log(carouselImages4);
