e3c5ce652135086d7d96813bda65cecdcc891740
Arthur Bates <aabatesj@gmail.com>
2018-07-19 14:23:15 -0400

    Services tabs
    functionality working.
    
    trying to get them to show one at a time when button is clicked

--- js/index.js
+++ js/index.js
@@ -20,8 +20,27 @@ let links = document.querySelectorAll(".link");
 
 class Tab {
     constructor(link) {
-        this.link = links;
+        this.link = link;
+        this.link.addEventListener("click", () => { this.linkClick() });
 
+        this.linkData = this.link.dataset.tab;
+        this.tabContent = document.querySelector(`.content[data-tab="${this.linkData}"]`);
+        this.tabContent = new Content(this.tabContent);
+    }
+
+    linkClick() {
+        this.tabContent.toggleContent();
     }
 }
+
+class Content {
+    constructor(tabContent) {
+        this.tabContent = tabContent;
+    }
+
+    toggleContent() {
+        this.tabContent.classList.toggle("tab-show");
+    }
+}
+
 links = Array.from(links).map(link => new Tab(link));
