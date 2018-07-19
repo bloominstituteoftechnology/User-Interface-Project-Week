5b333470e106aa660c3a5f0f801d3cd7b991f32f
Noah-Amar <ne.amar@outlook.com>
2018-07-19 17:04:01 -0500

    changes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // JS goes here
+const Button = document.querySelector('.button');
+const ButtonClose = document.querySelector('.button-close');
+
+
+Button.addEventListener("click", () => {
+    document.querySelector(".main-nav").style.display = "flex";
+    Button.style.display = "none";
+    ButtonClose.style.display = "block";
+})
+
+ButtonClose.addEventListener("click", () => {
+    document.querySelector(".main-nav").style.display = "none";
+    Button.style.display = "block";
+    ButtonClose.style.display = "none";
+})
