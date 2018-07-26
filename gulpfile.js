aaf34e035f9de0ee5e391f3f88fb761bdeaed9d7
Cwright0428 <cwright0428@yahoo.com>
2018-07-26 13:04:42 -0500

    more styling changes

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create(); // create a browser sync instance.
+
+gulp.task('browser-sync', function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
