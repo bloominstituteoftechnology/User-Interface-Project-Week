e6e2dab8f8b95aef3a27cd9d06c4bdb76ee87181
Randy Calderon <randycweb@gmail.com>
2018-08-17 02:32:05 -0700

    Added modal, working on tabs and css

--- js/index.js
+++ js/index.js
@@ -17,27 +17,101 @@ const navScroll = () => {
         nav.style.backgroundColor= "transparent";
         nav.style.opacity = 1;
     }
-    
 }
+
 const nav = document.querySelector(".navigation")
 document.addEventListener("scroll", navScroll);
+
+class Dropdown {
+    constructor(element, parent) {
+        this.element = element;
+
+        this.button = this.element.querySelector(".modal-button");
+        this.close = this.element.querySelector(".close-button");
+        this.content = this.element.querySelector(".modal");
+        this.button.addEventListener("click", () => {
+            this.toggleContent();
+        })
+        this.close.addEventListener("click", () => {
+            this.toggleClose()
+        })
+    }
+
+    toggleContent() {
+        this.content.classList.toggle("modal-hidden");
+        this.button.classList.toggle("modal-hidden");
+    }
+
+    toggleClose() {
+        this.content.classList.toggle("modal-hidden");
+        this.button.classList.toggle("modal-hidden");
+    }
+}
+let dropdowns = document.querySelectorAll(".dropdown");
+dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
 
 
 class Tabs {
     constructor(element) {
         this.element = element;
-        this.tabs = this.element.querySelectorAll('.tab__text');
-        this.activeTab = this.tabs[0];
-        console.log(this.activeTab);
+        this.links = this.element.querySelectorAll(".tabs-link");
+        this.links = Array.from(this.links).map(link => {
+            return new TabsLink(link, this)
+        })
+        this.activeLink = this.links[0];
+        this.init()
     }
-    select() {
 
+    init() {
+        this.activeLink.select()
     }
-    deselect() {
 
+    updateActive(newActive) {
+        this.activeLink.deselect
+        this.actliveLink = newActive;
+    }
+
+    getTab(data) {
+        return this.element.querySelecter(`.tabs-item[data-tab='${data}']`)
     }
 }
 
-let tabs = document.querySelectorAll('.section-container');
-console.log(tabs)
-tabs = Array.from(tabs).map(tabs => new Tabs(tab));
+class TabsLink {
+    constructor(element, parent) {
+        this.element = element;
+        this.tabs = parent;
+        this.tabsItem = parent.getTab(this.element.dataset.tab);
+        this.tabsItem = new this.tabsItem(this.tabsItem);
+        this.element.addEventListener("click", () => {
+            this.tabs.updateActive(this);
+            this.select();
+        })
+    }
+
+    select() {
+        this.element.classList.add("tabs-link-selected");
+        this.tabsItem.select();
+    }
+
+    deselect() {
+        this.element.classList.remove("tabs-link-selected");
+        this.tabsItem.deselect();
+    }
+ }
+
+class TabsItem {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select() {
+        this.element.classList.add("tabs-item-selected");
+    }
+
+    deselect() {
+        this.element.classList.remove("tabs-item-selected")
+    }
+}
+
+let tabs = document.querySelectorAll(".tabs");
+tabs = Array.from(tabs).map(tab => new Tabs(tab));
