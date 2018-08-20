2939a772e9c747dd3e221aa1ecc6d40ceb216982
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-08-20 15:41:27 -0500

    Messed around with JS for navbar.  Will come back to it later

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // JS goes here
+
+function openNav() {
+  document.querySelector('myNav').style.display = 'block';
+}
+
+function closeNav() {
+  document.querySelector('myNav').style.display = 'none';
+}
