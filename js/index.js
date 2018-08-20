e2326b4836cef0aca66ddda7d3c36f7e698250a7
David Benavidez <benavidez_david13@yahoo.com>
2018-08-20 15:15:13 -0700

    done with homepage

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-// JS goes here
+const toggleMenu = () => {
+    // Toggle the "menu--open" class on your menu refence. 
+    dropDown.style.display= 'block'
+    dropButton2.style.display= 'block'
+
+  
+  }
+
+  const toggleOff = () => {
+    // Toggle the "menu--open" class on your menu refence. 
+    dropDown.style.display= 'none'
+    dropButton2.style.display= 'none'
+  }
+  
+  
+  // Start Here: Create a reference to the ".menu" class
+  const dropDown= document.querySelector('.dropdown');
+  
+ 
+  
+  
+  // create a reference to the ".menu-button" class
+  const dropButton = document.querySelector(".drop-button");
+  const dropButton2 = document.querySelector(".drop-button2");
+  
+  // Using your menuButton reference, add a click handler that calls toggleMenu
+  
+  dropButton.addEventListener('click', toggleMenu);
+  dropButton2.addEventListener('click', toggleOff);
