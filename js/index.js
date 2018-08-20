edaa165025ec5a4613bf6b226ddb167431cc1fa5
Your Name <tjkisner@gmail.com>
2018-08-20 18:56:22 -0400

    Created home and service pages, created expanded menu functionallity

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS goes here
+const hamburger = document.querySelector('.hamburger');
+
+const menu = document.querySelector('.menu');
+
+const x = document.querySelector('.x');
+hamburger.addEventListener('click', (event)=>{
+    menu.classList.toggle('menu-hidden');
+    console.log('hi');
+})
+
+ 
