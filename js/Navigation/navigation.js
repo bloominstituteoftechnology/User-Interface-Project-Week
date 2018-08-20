13db9dcbdf9aeec75cae3fd2944dfaabaaa1478a
MikeRGarrity <mikergarrity@gmail.com>
2018-08-20 16:59:02 -0500

    finished with nav and jumbotron on main page, moving on to homepage main content

---
+++ js/Navigation/navigation.js
@@ -0,0 +1,18 @@
+const openMenu = () => {
+  menu.classList.add("menu--open");
+  menuClose.classList.remove("hidden");
+  menuButton.classList.add("hidden");
+}
+
+const closeMenu = () => {
+  menu.classList.remove("menu--open");
+  menuClose.classList.add("hidden");
+  menuButton.classList.remove("hidden");
+}
+
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.menu-button');
+const menuClose = document.querySelector(".close-button");
+
+menuButton.addEventListener("click", openMenu);
+menuClose.addEventListener("click", closeMenu);
