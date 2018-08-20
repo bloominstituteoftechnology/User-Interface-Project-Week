afccf11a9ebe909da64e6a4d5388620fa847bb32
Jeremy Boggs <nicocchilover@gmail.com>
2018-08-20 19:42:16 -0400

    tab page complete

--- js/index.js
+++ js/index.js
@@ -26,3 +26,49 @@ class Dropdown {
 
 let dropdowns = document.querySelectorAll('.dropdown');
 dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
+
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.dataTab = this.element.dataset.tab;
+        this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.dataTab}"]`);
+        this.tabItem = new TabItem(this.itemElement);
+        this.element.addEventListener('click', () => { this.select();});
+
+    }
+
+    select() {
+        const links = document.querySelectorAll('.tabs-link');
+
+        links.forEach(elem => {
+            elem.classList.remove('tabs-link-selected');
+        });
+
+        this.element.classList.add('tabs-link-selected');
+
+        this.tabItem.select();
+    }
+}
+
+class TabItem {
+    constructor(element) {
+        this.element = element;
+
+    }
+
+    select() {
+        let allItems = document.querySelectorAll('.tabs-item');
+
+        allItems.forEach(elem => {
+            elem.classList.remove('tabs-item-selected');
+        });
+
+        this.element.classList.add('tabs-item-selected');
+    }
+}
+
+let links = document.querySelectorAll('.tabs-link');
+links = Array.from(links).map( link => new TabLink(link));
+
+links[0].select();
