f52f87a51a3665cb53d1c78e482310657473a75d
iAmAdamReid <adam@adamreid.me>
2018-08-20 16:06:57 -0700

    Expand/Collapse Navigation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // JS goes here
+
+let navButton = document.querySelector('.nav-button');
+let navigation = document.querySelector('#navigation');
+let isOpen = false;
+
+navButton.addEventListener('click', function(){
+    if(isOpen == false){
+        navigation.style.display = 'flex';
+        navButton.innerHTML = `<img src = './img/nav-hamburger-close.png'></img>`;
+        isOpen = true;
+    } else {
+        navigation.style.display = 'none';
+        navButton.innerHTML = `<img src = './img/nav-hamburger.png'></img>`
+        isOpen = false;
+    }
+});
