cf89701c2eb8b12bfcd98f3db8f295e462d97638
Benny Oseguera <oseguerabenny27@gmail.com>
2018-08-20 16:16:42 -0700

    Finished everything on home page desktop but navbar expanding (wip).

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // JS goes here
+
+const openMenu = () => {
+    if (menu.style.display = 'none') {
+        menu.style.display = 'block';
+        burger.src = 'img/nav-hamburger-close.png';
+        burger.style.zIndex = '4';
+    } else if (menu.style.display = 'block') {
+        menu.style.display = 'none';
+        burger.src = 'img/nav-hamburger.png';
+        burger.style.zIndex = '1';
+    }
+}
+
+const menu = document.querySelector('.menu');
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', openMenu);
