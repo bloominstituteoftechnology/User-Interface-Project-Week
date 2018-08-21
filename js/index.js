ef4512a861a6e1f4166ba749febdf35a97674082
unknown <cornielleandres@gmail.com>
2018-08-21 16:04:07 -0400

    Complete expanded view design for main-nav home page desktop version.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// JS goes here
+const mainNavBtn = document.querySelectorAll(".main-nav-btn");
+const navMenu = document.querySelector("nav");
+const mainNav = document.querySelector(".main-nav");
+
+mainNavBtn.forEach(btn => btn.addEventListener("click", () => {
+    navMenu.classList.toggle("toggle-display");
+    mainNav.classList.toggle("toggle-height");
+    mainNav.classList.toggle("toggle-opacity");
+    mainNavBtn.forEach(btn => btn.classList.toggle("toggle-display"));
+}));
