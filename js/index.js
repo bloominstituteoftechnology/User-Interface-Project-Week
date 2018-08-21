b84c92b3a67d39f7153f3150e46711bded445bad
iAmAdamReid <adam@adamreid.me>
2018-08-21 12:44:20 -0700

    Mobile Design Homepage

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // JS goes here
 
+// Navigation expand/collapse
+
 let navButton = document.querySelector('.nav-button');
 let navigation = document.querySelector('#navigation');
 let isOpen = false;
@@ -15,3 +17,40 @@ navButton.addEventListener('click', function(){
         isOpen = false;
     }
 });
+
+var width = window.matchMedia('(min-width: 500px)');
+alternate(width);
+width.addListener(alternate);
+
+function alternate(){
+
+// Shifts every other project title to the left on desktop
+
+    
+let projects = document.querySelectorAll('.project');
+let everyOther = [];
+
+projects.forEach(function(each){
+    if(each.dataset.tab % 2 === 0){
+        everyOther.push(each);
+    }
+});
+
+let eoTitle = [];
+
+for(i = 0; i < everyOther.length; i++){
+    eoTitle.push(everyOther[i].querySelector('.project-title'));
+}
+if(width.matches){
+for(i = 0; i < eoTitle.length; i++){
+    eoTitle[i].style.cssText = 'left: 0; right: initial;';
+}
+
+} else {
+    for(i = 0; i < eoTitle.length; i++){
+        eoTitle[i].style.cssText = 'left: initial; right: initial;';
+}
+
+}
+
+}
