1ed97ff350d774ef2b01de11e38b76a368ca86d8
unknown <cornielleandres@gmail.com>
2018-08-21 16:47:51 -0400

    Add current page link color change
    When you open up the menu, the link pointing to the page you are currently in will be highlighted black.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 const mainNavBtn = document.querySelectorAll(".main-nav-btn");
+const aLinks = document.querySelectorAll("nav a");
 const navMenu = document.querySelector("nav");
 const mainNav = document.querySelector(".main-nav");
+
+if (document.querySelector(".container").classList.contains("index")) {
+    aLinks[0].classList.add("selected-link");
+} else if (document.querySelector(".container").classList.contains("services")) {
+    aLinks[1].classList.add("selected-link");
+}
 
 mainNavBtn.forEach(btn => btn.addEventListener("click", () => {
     navMenu.classList.toggle("toggle-display");
