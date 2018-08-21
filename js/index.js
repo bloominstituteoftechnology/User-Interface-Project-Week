e9c932e386ddebe378d375e717adbb8c0106a929
decagondev <decagondevelopment@gmail.com>
2018-08-21 15:14:00 +0100

    (BUGFIXES) : stripped out the allerts and debug console logs

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@ class Tabs {
       return new TabsLink(link, this);
     });
     this.activeLink = this.links[0];
-    console.log(this.activeLink);
     this.activeLink.select();
   }
 
@@ -24,11 +23,8 @@ class Tabs {
 class TabsLink {
   // decided it might be easyer if I just track the parentElement of the objects
   constructor(element, parentElements) {
-    this.element = element;
-    // FIXED: bug in this linethis.tabIndex = element.dataset.tab;
-    this.tabs = parentElements;
+    this.element = element;this.tabs = parentElements;
     this.tabsItem = parentElements.getTab(element.dataset.tab);
-    console.log("TABSITEM: " + this.tabsItem)
     this.tabsItem = new TabsItem(this.tabsItem);
     this.element.addEventListener('click', () => {
       this.tabs.activate(this);
@@ -52,36 +48,25 @@ class TabsItem {
     this.element = element;
   }
 
-  select() {
-    // fixed typo
-    this.element.classList.add("tab-item-selected");
-  }
+  select() { this.element.classList.add("tab-item-selected"); }
 
-  deselect() {
-    // fixed typo
-    this.element.classList.remove("tab-item-selected");
-  }
+  deselect() { this.element.classList.remove("tab-item-selected"); }
 }
 
 // setup the tabs under the services page
-
 let tabs = document.querySelectorAll(".services");
 tabs = Array.from(tabs).map(tab => {
   return new Tabs(tab);
 });
 
 // overlay stuff
+
+// overlay hooks
 let overlay = document.querySelector(".overlay-container");
 let burgerBtn = document.querySelector(".nav-burger");
-alert(burgerBtn);
-
 let closeBtn = document.querySelector(".close-button");
 let nav = document.querySelector(".navbar");
 
-burgerBtn.addEventListener('click', function() {
-  overlay.setAttribute("style", "display: block;")
-});
-
-closeBtn.addEventListener('click', function() {
-  overlay.setAttribute("style", "display: none;")
-});
+// adding overlay event listeners
+burgerBtn.addEventListener('click', function() { overlay.setAttribute("style", "display: block;") });
+closeBtn.addEventListener('click', function() { overlay.setAttribute("style", "display: none;") });
