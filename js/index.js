1c5413f1db8cdb9e394cf895bc3e898171fd9506
Clint Kunz <clintkunz@gmail.com>
2018-08-20 21:48:11 -0600

    javascript navigation system design complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+
+let menu = document.querySelector('.hamburger');
+let navLinks = document.querySelector('.links');
+let xMark = document.querySelector('.x-mark')
+
+function expand() {
+        menu.classList.toggle('hide');
+        navLinks.classList.toggle('hide');
+        xMark.classList.toggle('hide');
+    };
+
+
+menu.addEventListener('click', () => {expand()});
+xMark.addEventListener('click', () => {expand()});
