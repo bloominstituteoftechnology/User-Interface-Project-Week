b2d188cc9fc3980420f4b7865b36a8070a03870d
Michael Trevino <preince560m@gmail.com>
2018-08-21 12:29:44 -0500

    finish the dropdown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS goes here
+const toggleMenu = () => { 
+    menu.classList.toggle('menu--open')
+  }
+
+    const menu = document.querySelector('.menu-content');
+
+    const menuButton = document.querySelector('.hamburger-img');
+
+menuButton.addEventListener('click',event => {
+    toggleMenu();
+  });
