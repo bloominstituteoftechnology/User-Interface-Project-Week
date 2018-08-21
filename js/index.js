df9b60ed61047569695ca0162be98aaa8bf3adcf
James Cassidy <cassidyjamesw@gmail.com>
2018-08-21 16:35:43 -0400

    nav JS complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // JS goes here
+const toggleMenu = () => {
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.toggle(`menu--open`);
+  document.querySelector(".hambaobao").src = "img/nav-hamburger-close.png";
+};
+
+// Start Here: Create a reference to the ".menu" class
+const menu = document.querySelector(".menu");
+
+// create a reference to the ".menu-button" class
+const menuButton = document.querySelector(".menu-button");
+
+// Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener("click", event => {
+  toggleMenu();
+});
