4dc20e56e68a433d39d9b1ff3e537b90c0942e75
marshall lanners <marshalllanners@marshalls-MacBook-Air.local>
2018-08-21 09:05:13 -0700

    finished services tabs navigation

--- js/index.js
+++ js/index.js
@@ -15,5 +15,51 @@ navClose.addEventListener('click', function(){
 	navClose.style.display = "none";
 });
 
+// -----------------------------------------------------------
+
+
+class TabLink {
+	constructor(tab){
+		this.tab = tab;
+		this.tabData = this.tab.dataset.tab;
+		tab.addEventListener('click', () => {this.select(this.tabData)})
+		// console.log(this.tabData);
+	}
+	select(data){
+
+		let mainContent = document.querySelectorAll('.main-content');
+		let tabs = document.querySelectorAll('.btn-s');
+
+		mainContent.forEach(function(item){
+			item.classList.remove("show-tabs");
+		});
+
+		tabs.forEach(function(item){
+			item.classList.remove('btn-new-back')
+		});
+		
+		switch(data){
+			case "1":
+				mainContent[0].classList.add("show-tabs");
+			break;
+			case "2":
+				mainContent[1].classList.add("show-tabs");
+			break;
+			case "3":
+				mainContent[2].classList.add("show-tabs");
+			break;
+			case "4":
+				mainContent[3].classList.add("show-tabs");
+			break;
+		}
+		this.tab.classList.add('btn-new-back');
+	}
+}
+
+
+let tabLinks = document.querySelectorAll('.btn-s');
+tabLinks = Array.from(tabLinks).map(tab => new TabLink(tab));
+tabLinks[0].select("1");
+
 
 
