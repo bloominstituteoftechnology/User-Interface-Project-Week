d04d62fd56804e11fd4710f1ccca6bd2e8078ca2
alejandrok93 <alejandrok93@gmail.com>
2018-08-21 13:48:13 -0700

    Completed Services Tab component

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,9 +2,38 @@ class Tab {
   constructor(element) {
     this.element = element;
     this.data = element.dataset.tab;
-    let item = document.querySelector(`.tab-item[data-tab="${this.data}"]`);
-    this.tabItem = item;
-    console.log(tabItem);
+    this.tabItem = document.querySelector(`.tab-item[data-tab="${this.data}"]`);
+    this.tabItem = new TabItem(this.tabItem);
+    console.log(this.tabItem);
+
+    this.element.addEventListener("click", () => this.selectTab());
+  }
+  selectTab() {
+    //Remove active class from all tab links
+    const tabLinks = document.querySelectorAll(".tab");
+    tabLinks.forEach(item => item.classList.remove("active"));
+
+    //Add active class to this tab
+    this.element.classList.toggle("active");
+
+    //Remove tab-item-active class from all tab items
+    const tabItems = document.querySelectorAll(".tab-item");
+    tabItems.forEach(item => item.classList.remove("tab-item-active"));
+    console.log("removed all tab item active class");
+
+    //Call Tab Item select() method
+    this.tabItem.select();
+  }
+}
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    this.element.classList.toggle("tab-item-active");
+    console.log(this.element + "was clicked");
   }
 }
 
