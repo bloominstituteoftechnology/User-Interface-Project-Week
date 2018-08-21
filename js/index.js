116d6835b498b8c262facd6fe9c304905d8ee4e9
Your Name <tjkisner@gmail.com>
2018-08-21 13:19:18 -0400

    fixed JS source issue, added services button click change

--- js/index.js
+++ js/index.js
@@ -1,33 +1,35 @@
-const hamburgerIMG = document.querySelector('.hamburgerIMG');
-const close = document.querySelector('.close');
-const menu = document.querySelector('.menu');
+const hamburgerIMG = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const menu = document.querySelector(".menu");
 
-hamburgerIMG.addEventListener('click', ()=>{
-    menu.classList.toggle('menu-hidden');
-    hamburgerIMG.classList.toggle('display-none');
-    close.classList.toggle('display-none');
+hamburgerIMG.addEventListener("click", () => {
+  menu.classList.toggle("menu-hidden");
+  hamburgerIMG.classList.toggle("display-none");
+  close.classList.toggle("display-none");
 });
 
-close.addEventListener('click', ()=>{
-    menu.classList.toggle('menu-hidden');
-    hamburgerIMG.classList.toggle('display-none');
-    close.classList.toggle('display-none');
+close.addEventListener("click", () => {
+  menu.classList.toggle("menu-hidden");
+  hamburgerIMG.classList.toggle("display-none");
+  close.classList.toggle("display-none");
 });
 
-class Button{
-    constructor(button){
-        this.button = button;
-        this.button.addEventListener('click', ()=>{
-            this.toggleContent();
-        })
-    }
-    toggleContent(){
-        this.classList.toggle('.button-clicked');
-        console.log('hi');
-    }
+class Button {
+  constructor(button) {
+    this.button = button;
+    this.button.classList.toggle("button-clicked");
+    this.button.addEventListener("click", () => {
+      this.toggleContent();
+    });
+  }
+  toggleContent() {
+      let buttons = document.querySelectorAll('.button');
+    buttons.forEach(button => button.classList.remove("button-clicked"));
+    this.button.classList.toggle("button-clicked");
+    event.preventDefault();
+    
+  }
 }
 
-let buttons = document.querySelectorAll('.button');
+let buttons = document.querySelectorAll(".button");
 buttons = Array.from(buttons).map(button => new Button(button));
-
-
