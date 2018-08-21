9151105c385db29b418142437c63de2f6781cc40
Michael Agard <mdagard@gmail.com>
2018-08-20 20:34:22 -0400

    Menu has been fixed

--- js/index.js
+++ js/index.js
@@ -1,28 +1,19 @@
-class FullScreenMenu {
+class MenuConstructor {
     constructor(element) {
-      
-      // Assign this.element to the dropdown element
       this.element = element;
-      
-      // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
       this.button = element.querySelector('.menu-button');
-      
-      // assign the reference to the ".dropdown-content" class found in the dropdown element
       this.content = element.querySelector('.menu-content');
-      
-      // Add a click handler to the button reference and call the toggleContent method.
+      this.closeButton = element.querySelector('.menu-close');
       this.button.addEventListener('click', () => {this.toggleContent()});
+      this.closeButton.addEventListener('click', () => {this.toggleContent()});
+      this.link = element.querySelectorAll('.menu-link')
     }
   
     toggleContent() {
-      
-      // Toggle the ".dropdown-hidden" class off and on
       this.content.classList.toggle('menu-hidden');
-      console.log(mainMenus)
+      console.log('activating menu')
     }
   }
-  
-  // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-  let mainMenus = document.querySelectorAll('.');
-  mainMenus = Array.from(mainMenus).map( mainMenu => new FullScreenMenu(mainMenu));
-  console.log(mainMenus)
+let mainMenu = document.querySelectorAll('.menu');
+mainMenu = Array.from(mainMenu).map( mainMenuArray => new MenuConstructor(mainMenuArray));
+console.log(mainMenu)
