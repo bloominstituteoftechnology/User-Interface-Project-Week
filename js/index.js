45778e669a60918d6d9dfd5fdf496b263321b7d8
Michael Trevino <preince560m@gmail.com>
2018-08-21 17:48:31 -0500

    working on JS part of project

--- js/index.js
+++ js/index.js
@@ -10,3 +10,53 @@ const toggleMenu = () => {
 menuButton.addEventListener('click',event => {
     toggleMenu();
   });
+// this is the Home nav JS ^^^
+
+class TabLink {
+  constructor(element){
+    this.element = element;
+    this.tabData = this.element.dataset.tab; 
+
+    if(this.tabData === 'all'){
+      this.cards = document.querySelectorAll('.card');
+    } else {
+      this.cards = document.querySelectorAll(`.card[data-tab = "${this.tabData}"]`);
+    }
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
+    
+    this.element.addEventListener('click', ()=> {this.selectTab()
+  });
+  }
+
+  selectTab(){
+    const tabs = document.querySelectorAll('.tab');
+
+    tabs.forEach(function(item){
+      item.classList.remove('active-tab')
+    });
+
+    this.element.classList.add('active-tab');
+
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(function(item){
+      item.style.display = 'none';
+    });
+
+    this.cards.forEach(card => card.selectCard());
+  }
+}
+
+class TabCard {
+  constructor(element){
+    this.thisElement = element;
+  }
+  selectCard(){
+    this.thisElement.style.display = null;
+  }
+
+}
+
+let tabs = document.querySelectorAll('.tab');
+tabs = Array.from(tabs).map(tab => new TabLink(tab));
+tabs[0].selectTab();
