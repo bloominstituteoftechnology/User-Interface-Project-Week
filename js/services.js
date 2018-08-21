1ef6171581df00109f69259a86564aab44083c8c
Luisan <luisangelsc@gmail.com>
2018-08-21 17:07:54 -0600

    tabs and title change color

--- js/services.js
+++ js/services.js
@@ -7,12 +7,20 @@ class TabButton {
 
     this.dataTab = element.dataset.choice
 
-
+    this.element.addEventListener('click', () => this.switch())
+  }
+  switch() {
+    console.log(event.currentTarget.children[0].innerText);
+    options.forEach(item => item.element.classList.remove('active'));
+    this.element.classList.add('active')
+    title.innerText = event.currentTarget.children[0].innerText
   }
 }
 
+const title = d.querySelector('h2')
+
 let options = d.querySelectorAll('.option')
-let first = new TabButton(options[0])
-console.log('first:', first);
+options = Array.from(options).map(option => new TabButton(option))
+// console.log('first:', first);
 // console.log(options);
-console.log(first.dataTab);
+console.log(options[0]);
