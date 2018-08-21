2228c58e65ff37b008c34256d642aa99321daed0
Melanie Barth <mbarth1967@gmail.com>
2018-08-21 09:20:29 -0600

    partial toggle navigation bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-// JS goes here
+(function() {
+      var hamburger = {
+        navToggle: document.querySelector('.nav-toggle'),
+        nav: document.querySelector('nav'),
+        doToggle: function(e) {
+          e.preventDefault();
+          this.navToggle.classList.toggle('expanded');
+          this.nav.classList.toggle('expanded');
+        }
+      };
+      hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+
+      hamburger.nav.addEventListener('click', function(e) {hamburger.doToggle(e);});
+    }());
+    
