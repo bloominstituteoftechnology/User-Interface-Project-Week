1d3366242221a3a43ef0bc0dbea2284938040729
Patrick Thompson <patrick.jthompson93@gmail.com>
2018-08-21 08:37:23 -0700

    working on services tab

--- js/index.js
+++ js/index.js
@@ -1,6 +1,52 @@
 // JS goes here
 
+//Tab Navigator JS
+class LinkCreator {
+    constructor(link){
+        this.link = link;
+        this.link.addEventListener('click', () => {
+            
+        })
+    }
+}
+
+
+
+const links = document.querySelectorAll('.link');
+links = Array.from(links).map( link => new LinkCreator(link));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//navigation JS
 const hamburger = document.querySelector('.hamburger');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 hamburger.addEventListener('click', () => {
 
