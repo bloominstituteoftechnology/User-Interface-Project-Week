829a03ee52dbb211bad576750b8287983a2fa652
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-08-21 16:53:34 -0400

    Completed menu nav functionality

--- js/index.js
+++ js/index.js
@@ -1,61 +1,32 @@
 ////////////////////////
-// JS for tab component
+// JS for menu component
 ////////////////////////
 
-class TabLink {
+class MenuButton {
   constructor(element) {
-    // assign this.element to the element reference
     this.element = element;
-    console.log(this.element);
 
-    // Get the tab data attribute and save the value here
-    this.tabData = this.element.dataset.tab;
-    console.log(this.tabData);
-    // Get card specific to data attribute
-    this.card = document.querySelector(`.card[data-tab="${this.tabData}"]`);
-    // pass card to new Tab Card class
-    this.card = new TabCard(this.card);
-    console.log(this.card);
-    // Add a click event that invokes selectTab
     this.element.addEventListener('click', () => {
-      this.selectTab();
+      this.selectMenu();
     });
-    console.log('\n');
   }
 
-  selectTab() {
-    // Select all elements with the .tab class on them
-    const tabs = document.querySelectorAll('.tab');
-    // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach((tab) => tab.classList.remove('active-tab'));
-    // Add a class of ".active-tab" to this.element
-    this.element.classList.add('active-tab');
+  selectMenu() {
+    let nav = document.querySelector('.nav');
+    nav.classList.toggle('full-screen');
 
-    // Select all of the elements with the .card class on them
-    const cards = document.querySelectorAll('.card');
-    // Set all cards to display none
-    cards.forEach((card) => (card.style.display = 'none'));
-    // Invoke function on specific card
-    this.card.selectCard();
+    let buttons = document.querySelectorAll('.nav-menu');
+    buttons.forEach((button) => {
+      button.classList.toggle('display-none');
+    });
+
+    let navContent = document.querySelector('.nav-content');
+    navContent.classList.toggle('display-none');
   }
 }
 
-class TabCard {
-  constructor(element) {
-    // Assign this.element to the passed in element.
-    this.element = element;
-    console.log(this.element);
-  }
-  selectCard() {
-    // Update the style of this.element to display = flex
-    this.element.style.display = 'flex';
-  }
-}
+let menuButtons = document.querySelectorAll('.nav-menu');
 
-// Create a reference to all ".tab" classes
-let tabs = document.querySelectorAll('.tab');
-// Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map((tabItem) => new TabLink(tabItem));
-
-//Once you are complete, call the .select method on the first tab
-tabs[0].selectTab();
+menuButtons = Array.from(menuButtons).map(
+  (menuButton) => new MenuButton(menuButton)
+);
