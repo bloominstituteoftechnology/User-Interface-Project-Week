d8b9179daf5a6d904a8e01aa3e34b82f12ff1130
Patrick Thompson <patrick.jthompson93@gmail.com>
2018-08-21 10:29:24 -0700

    services page desktop done

--- js/index.js
+++ js/index.js
@@ -2,18 +2,43 @@
 
 //Tab Navigator JS
 class LinkCreator {
-    constructor(link){
-        this.link = link;
+    constructor(linkItem){
+        this.link = linkItem;
+        this.linkData = this.link.dataset.tab;
+        
+
         this.link.addEventListener('click', () => {
-            
-        })
+            this.linkClick();
+        });
+
+        
+
+
+        this.tabContent = document.querySelector(`.content[data-tab="${this.linkData}"]`);
+
+        this.tabContent = new Content(this.tabContent);
+
+    }
+    linkClick() {
+        this.tabContent.toggleContent()
+
+        
+    }
+}
+class Content {
+    constructor(tabContent){
+        this.tabContent = tabContent;
+    }
+
+    toggleContent() {
+        this.tabContent.classList.toggle('tab-toggle');
     }
 }
 
 
 
-const links = document.querySelectorAll('.link');
-links = Array.from(links).map( link => new LinkCreator(link));
+let links = document.querySelectorAll('.link');
+links = Array.from(links).map( linkItem => new LinkCreator(linkItem));
 
 
 
