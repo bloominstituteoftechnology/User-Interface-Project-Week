f400d40654d16cd0dc1ee01c928f639c24661380
Your Name <tjkisner@gmail.com>
2018-08-21 11:58:35 -0400

    more functionality

--- js/index.js
+++ js/index.js
@@ -1,12 +1,33 @@
-// JS goes here
-const hamburger = document.querySelector('.hamburger');
-
+const hamburgerIMG = document.querySelector('.hamburgerIMG');
+const close = document.querySelector('.close');
 const menu = document.querySelector('.menu');
 
-const x = document.querySelector('.x');
-hamburger.addEventListener('click', (event)=>{
+hamburgerIMG.addEventListener('click', ()=>{
     menu.classList.toggle('menu-hidden');
-    console.log('hi');
-})
+    hamburgerIMG.classList.toggle('display-none');
+    close.classList.toggle('display-none');
+});
 
- 
+close.addEventListener('click', ()=>{
+    menu.classList.toggle('menu-hidden');
+    hamburgerIMG.classList.toggle('display-none');
+    close.classList.toggle('display-none');
+});
+
+class Button{
+    constructor(button){
+        this.button = button;
+        this.button.addEventListener('click', ()=>{
+            this.toggleContent();
+        })
+    }
+    toggleContent(){
+        this.classList.toggle('.button-clicked');
+        console.log('hi');
+    }
+}
+
+let buttons = document.querySelectorAll('.button');
+buttons = Array.from(buttons).map(button => new Button(button));
+
+
