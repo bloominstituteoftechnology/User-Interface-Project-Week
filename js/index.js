13daf337bca86cf40e31c3d6c57e6fad09d7b5ed
marshall lanners <marshalllanners@marshalls-MacBook-Air.local>
2018-08-20 20:47:01 -0700

    finished overlay for navigation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// JS goes here
+let navBtn = document.querySelector('.btn-nav');
+let overlay = document.querySelectorAll('.overlay');
+
+let navClose = document.querySelector('.btn-close');
+
+navBtn.addEventListener('click', function(){
+	overlay[0].classList.add('show-nav');
+	navBtn.style.display = "none";
+	navClose.style.display = "block";
+});
+
+navClose.addEventListener('click', function(){
+	overlay[0].classList.remove('show-nav');
+	navBtn.style.display = "block";
+	navClose.style.display = "none";
+});
+
+
+
