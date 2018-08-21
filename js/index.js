33ef5e765a580cfd9e205d3406d2f0d2d355be11
Austinlp4 <apendergrast44@gmail.com>
2018-08-21 15:26:46 -0500

    update #3

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // JS goes here
 const toggleMenu = () => {
     // Toggle the "menu--open" class on your menu refence. 
+    
     menu.classList.toggle('menu--open');
     
   }
@@ -10,9 +11,15 @@ let toggle = false;
 const toggleimg = () => {
     if(toggle === false){
         menuButton.src = 'img/nav-hamburger-close.png';
+        nav.style.maxWidth = '100vw'; 
+        nav.style.left = '0';
         toggle = true;
+        TweenMax.to(menuButton, 2, {x:50, ease:Bounce.easeOut});
+        TweenMax.to(menuButton, 3, {rotation: 360});
     } else if (toggle === true){
         menuButton.src = 'img/nav-hamburger.png';
+        nav.style.maxWidth = '1024px';
+        nav.style.left = null;
         toggle = false;
     }
 }
@@ -22,9 +29,10 @@ const toggleimg = () => {
   // create a reference to the ".menu-button" class
   const menuButton = document.querySelector('.hamburger');
   // Using your menuButton reference, add a click handler that calls toggleMenu
-  
+  const nav = document.querySelector('.main-nav');
   
   menuButton.addEventListener('click', (e) => {
      toggleMenu();
      toggleimg();
+    
   });
