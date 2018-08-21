a20fb65a86136a42c20e3abcf8716815fb25237c
BrianARuff <thelonewolf198927@gmail.com>
2018-08-21 14:23:00 -0400

    nav feature

--- js/index.js
+++ js/index.js
@@ -3,4 +3,17 @@
 // *** HOME PAGE JS ***
 console.log('hi');
 const hamburger = document.querySelector('.nav-burger');
+const nav = document.querySelector('#nav');
+const navMenuContent = document.querySelector('.home-nav-dropdown');
 
+hamburger.addEventListener('click', () => {
+  console.log('hamburger');
+  nav.classList.toggle('expand-nav-500');
+  nav.style = "align-items: flex-start";
+  navMenuContent.classList.toggle('expand-nav-500-menu');
+  navMenuContent.classList.toggle('hide');
+  hamburger.setAttribute("src", '../img/nav-hamburger-close.png');
+  if(!nav.classList.contains('expand-nav-500')){
+    hamburger.setAttribute("src", '../img/nav-hamburger.png');
+  }
+});
