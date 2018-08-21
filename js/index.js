73fd0f16b1700e3c266b5cf23863c6234bb45656
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-08-21 12:58:26 -0700

    Header styling complete but click to expand functionality not working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // JS goes here
+
+const navigationButton = document.querySelector('.navigation-button');
+const navigationContent = document.querySelector('.navigation-content');
+
+navigationButton.addEventListener('click', (event) => {
+    event.navigationContent.classList.toggle('navigation-hidden');
+    console.log('Nav button clicked');
+});
