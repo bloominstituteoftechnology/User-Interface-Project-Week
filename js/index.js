0105d81527f63f3569d59cb5ef17cfb1de870aa4
lndubose <lndubose13@gmail.com>
2018-08-21 16:08:28 -0500

    Nav extend styling done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 // JS goes here
 //Navigation Bar
 
+class Menu {
+  constructor(element) {
+    this.element = element;
+    this.button = this.element.querySelectorAll(".button");
+    
+  }
+}
+
+
+
+let menu = document.querySelectorAll(".navigation");
+
+menu = Array.from(menu).map(menu => new Menu(menu));
