272cbcd10bcb1815aa82b102b1197c5144ef57bf
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-08-21 16:48:03 -0600

    Finished Contact Page, services page, and added some animation to the menu.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,35 @@
 
 // Navigation
-const hamburger = document.querySelector('.hamburger img');
+const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
 const header = document.querySelector('header.header');
 let open = hamburger.addEventListener('click', () => {
-    navigation.classList.toggle('navigation-show');
-    header.classList.toggle('navigation-show');
+
+    
+
+
+
     if (header.classList.contains('navigation-show')) {
-    hamburger.src='img/nav-hamburger-close.png';
-    hamburger.alt='Close Menu';
+        hamburger.classList.remove("is-active");
+    TweenMax.to(header, .3, {height:'50px', onComplete: () => {
+        header.classList.remove('navigation-show');
+        navigation.classList.remove('navigation-show');
+    }});
+
+    TweenMax.fromTo(navigation, .2, {opacity: '1'}, {opacity: '0'});
+
+    // hamburger.src='img/nav-hamburger.png';
+    // hamburger.alt='Open Menu';
+
     } else {
-    hamburger.src='img/nav-hamburger.png';
-    hamburger.alt='Open Menu';
+        hamburger.classList.add("is-active");
+        navigation.classList.add('navigation-show');
+        header.classList.add('navigation-show');
+    TweenMax.to(header, .3, {height:'100%', onComplete: () => {
+    }});
+
+    TweenMax.fromTo(navigation, .2, {opacity: '0'}, {opacity: '1'});
+    // hamburger.src='img/nav-hamburger-close.png';
+    // hamburger.alt='Close Menu';
     }
 });
