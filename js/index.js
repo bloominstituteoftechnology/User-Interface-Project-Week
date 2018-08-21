0c57dfe508b8491f5a65a13ecd99a199dc934df5
lndubose <lndubose13@gmail.com>
2018-08-21 11:14:53 -0500

    Viewport

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // JS goes here
+
+class Tab {
+  constructor(element) {
+    this.element = element;
+    console.log(this.element);
+  }
+}
+
+
+let tabs = document.querySelectorAll(".btn");
+
+tabs = Array.from(tabs).map(tab => new TabCard(tab));
