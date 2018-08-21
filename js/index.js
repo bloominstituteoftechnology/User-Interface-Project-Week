0f894b34cef341388e39648a71ebea27080c06cb
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-08-21 15:54:12 -0700

    moving into JS functionality on homepage

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// JS goes here
+const toggleMenu = () => {
+    // Toggle the "menu--open" class on your menu refence. 
+  menu.classList.toggle('menu--open');
+  }
+  
+  
+  // Start Here: Create a reference to the ".menu" class
+  const menu = document.querySelector('.menu');
+  // create a reference to the ".menu-button" class
+  const menuButton = document.querySelector('.menu-button');
+  console.log(menuButton);
+  // Using your menuButton reference, add a click handler that calls toggleMenu
+  
+  menuButton.addEventListener('click', toggleMenu);// JS goes here
