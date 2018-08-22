db831438df079cc33348e2e02f7891a27c1aef78
verydecent <wnjhwng@gmail.com>
2018-08-22 15:52:24 -0700

    Completed Functionality on Services

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-// JS goes here
+
+
+// MENU DROP DOWN
+
+// Menu toggle function
+
+const toggleDisplay = () => {
+    console.log('button clicked');
+
+    menu.classList.toggle('menu-display-block');
+}
+
+// Grab drop down menu to display
+
+const menu = document.querySelector('.menu');
+
+// Reference hamburger button 
+
+const hamburger = document.querySelector('.hamburger');
+
+// Add event listener to hamburger button
+
+hamburger.addEventListener('click', toggleDisplay);
+
+
+//------------------------------------
+
+
+
