4dc1311076295a933347c373328570a550b5f218
Carlos <cornielleandres@gmail.com>
2018-08-22 14:56:31 -0400

    Use GSOCK to add animation to menu system.

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -11,9 +11,14 @@ if (document.querySelector(".container").classList.contains("index")) {
     aLinks[2].classList.add("selected-link");
 }
 
+
+
 mainNavBtns.forEach(btn => btn.addEventListener("click", () => {
     navMenu.classList.toggle("toggle-display");
     mainNav.classList.toggle("toggle-height");
     mainNav.classList.toggle("toggle-opacity");
     mainNavBtns.forEach(btn => btn.classList.toggle("toggle-display"));
+
+    TweenMax.fromTo(navMenu, 1, {x:-500, scale: 0, skewX: "45deg"}, {ease: Bounce.easeOut, x:0, scale: 1, skewX: "0deg"});
+    TweenMax.fromTo(mainNavBtns, 1, {ease: Bounce.easeOut, scale: 0}, {ease: Bounce.easeOut, scale: 1});
 }));
