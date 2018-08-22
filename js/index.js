c287591a9f3165fba0b4bb2f5b5ca686e5b236f3
mparedes003 <michelle.g.paredes@gmail.com>
2018-08-22 10:08:58 -0700

    1st stab at  MVP #5.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,59 @@
 // JS goes here
+class TopicLink {
+  constructor(topic) {
+    this.element = topic;
+    
+    // Get the custom data attribute on the Topic
+    this.data = this.element.dataset.tab;
+    // Using the custom data attribute get the associated Item element
+    this.item = document.querySelector(`.card[data-tab="${this.data}"]`);
+    // Using the Item element, create a new instance of the TabItem class
+    this.tabItem = new TabItem(this.item);
+    
+    this.element.addEventListener('click', () => {
+      // Call the select method you define below
+      this.select();
+    });
+  };
+
+  select() {
+    // Get all of the elements with the tab class
+    const topics = document.querySelectorAll('.tab');
+
+    // Using a loop or the forEach method remove the 'tab-selected' class from all of the topics
+    topics.forEach( topic => {
+      topic.classList.remove('tab-selected')
+    });
+
+    // Add a class named "tab-selected" to this link
+    this.element.classList.add('tab-selected');
+    
+    // Call the select method on the item associated with this link
+    this.tabItem.select();
+  }
+}
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    // Select all cards elements from the DOM
+    const cards = document.querySelectorAll('.card');
+    // Remove the class "card-selected" from each element
+    cards.forEach( card => {
+      card.classList.remove('card-selected');
+    })
+    // Add a class named "card-selected" to this element 
+    this.element.classList.add('card-selected');
+  }
+}
+
+// START HERE: create a reference to the ".tabs-link" class
+let topics = document.querySelectorAll('.topics');
+// Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
+topics = Array.from(topics).map( topic => new TopicLink(topic));
+
+// Do this last!!!
+topics[0].select()
