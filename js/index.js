e854b89533e37080e3a43f2fe13816ad9459a5c7
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-08-22 14:18:15 -0400

    Services Page Tab JS function working

--- js/index.js
+++ js/index.js
@@ -23,41 +23,4 @@ const toggleMenu = () => {
      toggleimg();
    });
 
-   class TabLink {
-    constructor(element) {
-      this.element = element;
-      
-      // Get the custom data attribute on the Link
-      this.data = this.element.dataset.tab;
-      
-      // Using the custom data attribute get the associated Item element
-      this.itemElement;
-      this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
-      
-      // Using the Item element, create a new instance of the TabItem class
-      this.tabItem = new TabItem(this.itemElement);
-      
-      // Add a click event listener on this instance, calling the select method on click
-      this.element.addEventListener('click', () => {this.select();});
-    };
-  
-    select() {
-      // Get all of the elements with the tabs-link class
-      const links = document.querySelectorAll('.tabs-link');
-  
-      // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-      links.forEach((elem) => {
-        elem.classList.remove('tabs-link-selected');
-      });
-  
-      // Add a class named "tabs-link-selected" to this link
-      this.element.classList.add('tabs-link-selected');
-      
-      // Call the select method on the item associated with this link
-  
-      this.tabItem.select();
-  
-    }
-  }
-  
-  
+   
