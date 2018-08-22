7a53c9a0420b309e76f567a33b36076862396991
m-j-hacker <mjhacker@gmail.com>
2018-08-22 16:35:41 -0500

    Added most of the javascript but am becoming stuck...

--- js/index.js
+++ js/index.js
@@ -10,6 +10,29 @@ class TabLink {
   }
 
   select() {
-    const link = document.querySelectorAll(".tabs-link");
+    const links = document.querySelectorAll(".tabs-link");
+
+    Array.from(links).forEach(link => { link.classList.remove("tabs-link-selected") });
+
+    this.link.classList.add("tabs-link-selected");
+
+    this.tabItem.select();
   }
 }
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    const allItems = document.querySelectorAll(".tabs-item");
+
+    Array.from(allItems).forEach(item => { item.classList.remove("tabs-item-selected") });
+    this.element.classList.add("tabs-item-selected");
+  }
+}
+
+let allLinks = document.querySelectorAll(".tabs-link");
+allLinks = Array.from(allLinks).map( link => new TabLink(link));
+console.log(allLinks);
