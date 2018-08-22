b6f99db7c36a4a0e7b3a2f70ac725085b742b592
Tristen Neu <tristenneu@Tristens-MacBook-Pro.local>
2018-08-21 21:02:26 -0500

    Nav Integration

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // JS goes here
+function openNav() {
+  document.querySelector(".overlay").style.display = "block";
+}
+
+function closeNav() {
+  document.querySelector(".overlay").style.display = "none";
+}
