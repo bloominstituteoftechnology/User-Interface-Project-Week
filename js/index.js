4a48882f3bae993945e1bcfafff1a8de7c5856c6
Kevin Nguyen <kevinn858@gmail.com>
2018-08-22 15:22:15 -0700

    first draft of mobile view

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // JS goes here
+const mobileWidth = 500;
+var mobileImages = [];
+mobileImages.push('./img/home/home-mobile-jumbotron.png');
+mobileImages.push('./img/home/home-mobile-img-1.png');
+mobileImages.push('./img/home/home-mobile-img-2.png');
+mobileImages.push('./img/home/home-mobile-villas-img.png');
+mobileImages.push('./img/home/home-mobile-outskirts-img.png');
+mobileImages.push('./img/home/home-mobile-the-blocks-img.png');
