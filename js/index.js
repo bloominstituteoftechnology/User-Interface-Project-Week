ea6d52608af6cf355a28336c80daebc069e8f808
Matthew Wright <matthewbwright@gmail.com>
2018-08-22 10:34:03 -0700

    fixed nav bar in place

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-// JS goes here
+// class TabLink {
+//     constructor(element){
+//         this.element = element;
+//         this.tabData = this.element.dataset.tab;
+
+//         this.cards = document.querySelectorAll(`.content[data-tab="${this.tabData}"]`);
+//     }
+//     this.cards = Array.from(this.cards).map( card => new TabCard(card));
+
+//     this.element.addEventListener('click', () => {this.selecTab()});
+// }
+
+// selecTab(){
+
+// }
+
+
+
+
+
+
+// let tabs = document.querySelectorAll(".btn");
+
+// tabs = Array.from(tabs).map( tab => new TabLink(tab));
