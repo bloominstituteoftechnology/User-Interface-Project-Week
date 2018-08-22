18a9e8ad3f32b424874d31a6b200dffe580711fb
Vera Simon <ghjkl>
2018-08-22 15:59:57 -0700

    Header Navigation
    Created javascript for header navigation
    TODO: Finish styling for navigation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // JS goes here
+
+//// Header Navigation ////
+// get elements
+const menu = document.querySelector('.hamburglar-links');
+const menuToggle = document.querySelector('.hamburglar-toggle');
+// watch for clicks. do the thing!
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+
+    if(menuToggle.src.indexOf('nav-hamburger.png') !== -1) {
+        menuToggle.src = 'img/nav-hamburger-close.png';
+    } else {
+        menuToggle.src = 'img/nav-hamburger.png';
+    }
+});
+
+
+//// Tab Navigation ////
+
