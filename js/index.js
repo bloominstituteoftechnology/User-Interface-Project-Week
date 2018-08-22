7296c6b08e078b26e9f57754d6ba96a949206205
Colin Preston <prestonresult@gmail.com>
2018-08-22 13:21:05 -0500

    nav works

--- js/index.js
+++ js/index.js
@@ -4,10 +4,48 @@
 
 
 
-function classToggle() {
-	const navs = document.querySelectorAll('.Navbar__Items')
+let nav = document.getElementById("topNav");
+let main = document.getElementById("main");
+let menu = document.getElementsByClassName("menuitems");
+let close = document.getElementById("closebtn");
+
+//default to measure if/else from
+nav.style.height = "50px";
+main.style.marginTop = "50px";
+for (i = 0; i < menu.length; i++){menu[i].style.marginTop="100px";};
+
+close.addEventListener("click", () => {
+  let menuIcon = close.children;
+  for (i = 0; i < menuIcon.length; i++){
+  menuIcon[i].classList.toggle("active");
+  }   
+});
+
+ function navToggle()  {	
+	//to close
+	if (nav.style.height <= "275px") {
+	nav.style.height = "50px";
+	main.style.marginTop = "50px";
 	
-	navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-  }
-  document.querySelector('.Navbar__Link-toggle')
-	.addEventListener('click', classToggle);
+    	let i = 0;
+    	for (i = 0; i < menu.length; i++){
+	menu[i].style.opacity="0.0";
+	menu[i].style.marginTop="100px";
+	};
+    	document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
+	
+	} 
+	//to open
+	else if (nav.style.height <= "50px") {
+	nav.style.height = "275px";
+	main.style.marginTop = "275px";
+    	let i = 0;
+    	for (i = 0; i < menu.length; i++){
+	menu[i].style.opacity="1.0";
+	menu[i].style.marginTop="0px";
+	};
+    	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+	
+	}
+
+};
