d8399e1846c6d8b8c186da2634ef300e705ec40f
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-08-22 07:43:33 -0700

    adding in JS functionality and cross page link activation

--- js/index.js
+++ js/index.js
@@ -1,14 +1,11 @@
 const toggleMenu = () => {
-    // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
   }
-  
-  
-  // Start Here: Create a reference to the ".menu" class
+
   const menu = document.querySelector('.menu');
-  // create a reference to the ".menu-button" class
+
   const menuButton = document.querySelector('.menu-button');
-  console.log(menuButton);
-  // Using your menuButton reference, add a click handler that calls toggleMenu
   
-  menuButton.addEventListener('click', toggleMenu);// JS goes here
+//   console.log(menuButton);
+  
+  menuButton.addEventListener('click', toggleMenu);
