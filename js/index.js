bb5227aeb77e040b6e4e209751016b7eb28dde08
Casey Baker <caseye.baker@gmail.com>
2018-08-21 23:21:48 -0700

    finished expand and collapse functionality for navigation menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
 // JS goes here
+
+// Menu
+
+const toggleMenu = () => {
+     menu.classList.toggle('menu--open');
+  }
+
+  const collapseButton = () => {
+    menuButton.style.display = "none";
+    menuClose.style.display = "block";
+  }
+
+  
+  const expandButton = () => {
+    menuButton.style.display = "block";
+    menuClose.style.display = "none";
+  }
+  
+  
+  const menu = document.querySelector('.menu');
+  const menuClose = document.querySelector('.menu-close');
+
+  const menuButton = document.querySelector('.menu-button');
+ 
+  menuButton.addEventListener('click', () => {
+    toggleMenu();
+    collapseButton();
+});
+
+menuClose.addEventListener('click', () => {
+    toggleMenu();
+    expandButton();
+});
+
+  
+
+
+
