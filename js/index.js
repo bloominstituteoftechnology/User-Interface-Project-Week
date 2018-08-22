b526ea97726d9ab59505bd4941aac43f92163fd2
Keiran <keirankozlowski@gmail.com>
2018-08-22 18:01:10 -0400

    Finished setting up nav system.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // JS goes here
+const toggleMenu = () => {
+    console.log("Toggled menu.");
+
+    menu.classList.toggle('active');
+    menuButton.classList.toggle('not-active');
+    navLogo.classList.toggle('not-active');
+}
+  
+const menu = document.querySelector('.nav-text-extended');
+const menuButton = document.querySelector('.hamburger');
+const menuCloseButton = document.querySelector('.nav-extended-topbar img');
+const navLogo = document.querySelector('.nav-logo');
+
+menuButton.addEventListener('click', (e) => {
+  
+    console.log("Calling the toggleMenu const.");
+    toggleMenu();
+});
+
+menuCloseButton.addEventListener('click', (e) => {
+  
+    console.log("Calling the toggleMenu const.");
+    toggleMenu();
+});
