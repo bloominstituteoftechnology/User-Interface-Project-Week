db630ee3d3677340afd02ce03c4b20fb355db928
CsillaG <csillagok@gmail.com>
2018-08-22 18:37:28 -0400

    renewed, done width the mvp

--- js/index.js
+++ js/index.js
@@ -1,86 +1,82 @@
 // JS goes here
-const menuButton = document.querySelector(".open-hamburger");
-const closeMenuButton = document.querySelector(".closed-hamburger");
-const menuOpenContent = document.querySelector(".menu-content");
 
-const menuItems = document.querySelectorAll(".menu-link");
-const mainHeader = document.querySelector("h1");
-const homeSection = document.querySelectorAll("section");
-const body = document.querySelector("body");
-
-menuButton.addEventListener("click", (e)=> {
-  menuOpenContent.classList.fadeIn(1500),
-menuButton.classList.toggle("hide-this"),
-  closeMenuButton.classList.toggle("show-this");
-});
-
-closeMenuButton.addEventListener("click", (e)=> {
-  menuOpenContent.classList.fadeOut(500),
-  closeMenuButton.classList.toggle("hide-this"),
-  menuButton.classList.toggle("show-this");
-});
-
+//Tabs
 class Tabs {
   constructor(element) {
     this.element = element;
-    console.log(this.element);
-    this.links = this.element.querySelectorAll(".service-tab");
-    this.links = Array.from(this.links).map(link => {
+    this.links = this.element.querySelectorAll(".tabs-link");
+    this.links = Array.from(this.links).map(link=>{
       return new TabsLink(link, this);
     });
     this.activeLink = this.links[0];
-    this.init();
-  }
-
-  init() {
     this.activeLink.select();
   }
 
-  updateActive(newActive) {
+  activate(link) {
     this.activeLink.deselect();
-    this.activeLink = newActive;
+    this.activeLink = link;
   }
 
-  getTab(data) {
-    return this.element.querySelector(`.tab-content[data-tab="${data}"]`);
+  getTab(dataValue) {
+    return this.element.querySelector(`.tab-item[data-tab="${dataValue}"]`);
   }
 }
 
+// tablink
 class TabsLink {
-  constructor(link, parent) {
-    this.link = link;
-    this.tabs = parent;
-    this.tabsItem = parent.getTab(this.link.dataset.tab);
+  //  parentElement of the objects
+  constructor(element, parentElements) {
+    this.element = element;
+    this.tabs = parentElements;
+    this.tabsItem = parentElements.getTab(element.dataset.tab);
     this.tabsItem = new TabsItem(this.tabsItem);
-    this.link.addEventListener("click", () => {
-      this.tabs.updateActive(this);
+    this.element.addEventListener("click", () => {
+      this.tabs.activate(this);
       this.select();
     });
   }
-
   select() {
-    this.link.classList.add("tab-selected");
+    this.element.classList.add("tabs-link-selected");
     this.tabsItem.select();
   }
 
   deselect() {
-    this.link.classList.remove("tab-selected");
+    this.element.classList.remove("tabs-link-selected");
     this.tabsItem.deselect();
   }
 }
 
+// TabsItem
 class TabsItem {
   constructor(element) {
     this.element = element;
   }
+
   select() {
-    this.element.classList.add("display-tab");
+    this.element.classList.add("tab-item-selected");
   }
 
   deselect() {
-    this.element.classList.remove("display-tab");
+    this.element.classList.remove("tab-item-selected");
   }
 }
 
-let tabs = document.querySelectorAll(".tabs");
-tabs = Array.from(tabs).map(tab => new Tabs(tab));
+// "services" Tabs
+let tabs = document.querySelectorAll(".services");
+tabs = Array.from(tabs).map(tab => {
+  return new Tabs(tab);
+});
+
+// overlay
+let overlay = document.querySelector(".overlay-container");
+let burgerBtn = document.querySelector(".nav-burger");
+let closeBtn = document.querySelector(".close-button");
+let nav = document.querySelector(".navbar");
+
+//  overlay EventListeners
+burgerBtn.addEventListener("click", function() {
+  overlay.setAttribute("style", "display: block;");
+});
+closeBtn.addEventListener("click", function() {
+  overlay.setAttribute("style", "display: none;");
+});
