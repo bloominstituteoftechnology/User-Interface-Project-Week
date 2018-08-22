fbe14cc89f9b500e681c92c5d6346d88213700e8
Joannah Laluces <joannahlaluces@Joannahs-MacBook-Air.local>
2018-08-22 12:57:34 -0700

    Finished Tabs Component

--- js/index.js
+++ js/index.js
@@ -12,11 +12,37 @@ function closeNav() {
 
 class TabLink {
     constructor(element) {
-        this.element;
+        this.element = element;
 
         this.data = this.element.dataset.tab;
 
         this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+
+        this.tabItem = new TabItem(this.itemElement);
+
+        this.element.addEventListener('click', () => {this.select()});
+    }
+    select() {
+        const links = document.querySelectorAll('.tabs-link');
+
+        Array.from(links).forEach(links => links.classList.remove('tabs-link-selected'));
+
+        this.element.classList.add('tabs-link-selected');
+
+        this.tabItem.select();
+    }
+}
+
+class TabItem {
+    constructor(tabItems) {
+        this.tabItems = tabItems;
+    }
+    select() {
+        const allItemElements = document.querySelectorAll('.tabs-item');
+
+        Array.from(allItemElements).forEach(allItemElements => allItemElements.classList.remove('tabs-item-selected'));
+
+        this.tabItems.classList.add('tabs-item-selected');
     }
 }
 
@@ -37,3 +63,5 @@ class TabLink {
 let links = document.querySelectorAll('.tabs-link'); 
 
 links = Array.from(links).map(element => new TabLink(element));
+
+links[1].select();
