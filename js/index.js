b1b465dab3c118660a39822f1c98189e4a386568
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-08-22 00:07:17 -0400

    Styled most of the expanded nav menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS goes here
+
+const menu = document.querySelector('.menu')
+const nav = document.querySelector('.nav')
+
+menu.addEventListener('click', () => {
+    nav.classList.add('full-menu');
+});
+
+menu.addEventListener('click', () => {
+    menu.src = "img/nav-hamburger-close.png";
+});
