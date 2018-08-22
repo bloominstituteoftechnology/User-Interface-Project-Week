c9977cd1f63cac0cb432e6da35097ef51e441ef5
unknown <tsaihuangsd@gmail.com>
2018-08-22 16:00:16 -0700

    Project update: basic services page completed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // JS goes here
 
+//Navigation hamburger menu
 const hamburgerButton = document.querySelector(".hamburger-img");
 
 const closeButton = document.querySelector(".hamburger-close-img");
@@ -18,5 +19,37 @@ closeButton.addEventListener('click', (event) => {
     closeButton.classList.toggle("display-off");
     dropdownContent.classList.toggle("display-off");    
 });
+
+
+//Services tabs on Services page
+const serviceArray=["Pre-Construction","Construction","Design Build","Sustainability"]
+
+const serviceImgArray=[
+    "services-tab-pre-construction-img.png",
+    "services-tab-construction-img.png",
+    "services-tab-design-build-img.png",
+    "services-tab-sustainability-img"
+]
+
+let serviceTabTitle = document.querySelector(".tab-title");
+
+let serviceDescriptionImg = document.querySelector(".tab-description-img");
+
+class ServicesTab{
+    constructor(element){
+        this.element = element;
+        console.log(this.element[index]);
+        this.element.addEventListener('click', (event) => {
+            serviceTabTitle.innerText = this.element.innerText;
+            console.log(serviceTabTitle.innerText);
+            serviceDescriptionImg.src = `$serviceImgArray[this.element.index]`;
+            console.log(serviceDescriptionImg.src);
+        });
+    }
+}
+
+let servicesTabArray = document.querySelectorAll(".services-tab");
+
+servicesTabs = Array.from(servicesTabArray).map(servicesTab => new ServicesTab(servicesTab));
 
 
