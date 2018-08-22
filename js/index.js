c94efb0c5f261d28df5ced0f55851422e46841a4
Matthew Wright <matthewbwright@gmail.com>
2018-08-22 16:13:29 -0700

    almost have expanded nav

--- js/index.js
+++ js/index.js
@@ -1,24 +1,47 @@
-// class TabLink {
-//     constructor(element){
-//         this.element = element;
-//         this.tabData = this.element.dataset.tab;
 
-//         this.cards = document.querySelectorAll(`.content[data-tab="${this.tabData}"]`);
-//     }
-//     this.cards = Array.from(this.cards).map( card => new TabCard(card));
+const hamburger = document.querySelector("#ham-closed");
+const closeNav = document.querySelector("#ham-open");
+const mainNav = document.querySelector(".main-nav");
 
-//     this.element.addEventListener('click', () => {this.selecTab()});
-// }
+    hamburger.addEventListener("click", () => {
+        mainNav.classList.toggle("hide-nav");
+        hamburger.classList.toggle("hide-this");
+        closeNav.classList.toggle("hide-hamburger");
+        console.log('hi');
+    });
 
-// selecTab(){
+    closeNav.addEventListener("click", () => {
+        mainNav.classList.toggle("hide-nav");
+        hamburger.classList.toggle("hide-this");
+        closeNav.classList.toggle("hide-hamburger");
+    })
 
-// }
+   
 
 
 
 
+    // Services page-----------------------------
 
+    const button1 = document.querySelector(".btn-1");
+    const button2 = document.querySelector(".btn-2");
+    const button3 = document.querySelector(".btn-3");
+    const button4 = document.querySelector(".btn-4");
+    const middle = document.querySelector(".middle-section");
 
-// let tabs = document.querySelectorAll(".btn");
+    button1.addEventListener("click", () => {
+        middle.classList.toggle("button-1 content");
+        console.log("button 1 clicked");
+    });
 
-// tabs = Array.from(tabs).map( tab => new TabLink(tab));
+    button2.addEventListener("click", () => {
+        middle.classList.toggle("button-2 content");
+    });
+
+    button3.addEventListener("click", () => {
+        middle.classList.toggle("button-3 content");
+    });
+
+    button4.addEventListener("click", () => {
+        middle.classList.toggle("button-4 content");
+    });
