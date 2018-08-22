b5b0ffad02bb6815ae78dde3993ad563c7f34df0
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-08-22 14:28:29 -0400

    MVP cleaned up a few things cleaned up code as well

--- js/index.js
+++ js/index.js
@@ -1,26 +1,24 @@
 const toggleMenu = () => {
-    menu.classList.toggle('menu--open');
-  };
-   
-   let toggle = false;
-   
-   const toggleimg = () => {
-     if (toggle === false) {
-       menuButton.src = 'img/nav-hamburger-close.png';
-       toggle = true;
-     } else if (toggle === true) {
-       menuButton.src = 'img/nav-hamburger.png';
-       toggle = false;
-     }
-   };
-   
-   const menu = document.querySelector('.menu');
-   
-   const menuButton = document.querySelector('.hamburger');
-   
-   menuButton.addEventListener('click', e => {
-     toggleMenu();
-     toggleimg();
-   });
+  menu.classList.toggle('menu--open');
+};
 
-   
+let toggle = false;
+
+const toggleimg = () => {
+  if (toggle === false) {
+    menuButton.src = 'img/nav-hamburger-close.png';
+    toggle = true;
+  } else if (toggle === true) {
+    menuButton.src = 'img/nav-hamburger.png';
+    toggle = false;
+  }
+};
+
+const menu = document.querySelector('.menu');
+
+const menuButton = document.querySelector('.hamburger');
+
+menuButton.addEventListener('click', e => {
+  toggleMenu();
+  toggleimg();
+});
