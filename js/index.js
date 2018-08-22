fa0218b85fbb017137d3adba327d909c36c1cc9f
m-j-hacker <mjhacker@gmail.com>
2018-08-22 14:32:15 -0500

    Began writing javascript for tabs on services.html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+
+class TabLink {
+  constructor(link) {
+    this.link = link;
+    this.data = this.link.dataset.tab;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+    this.tabItem = new TabItem(this.itemElement);
+    this.link.addEventListener('click', () => { this.select() });
+  }
+
+  select() {
+    const link = document.querySelectorAll(".tabs-link");
+  }
+}
