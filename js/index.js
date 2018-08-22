b3d0d1c0bfa09b64bb4b4931deaf89d643554e6f
Joannah Laluces <joannahlaluces@Joannahs-MacBook-Air.local>
2018-08-22 12:33:24 -0700

    Re-made the header navigation for both webpages and implemented the navigation design, working on tab components for services page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
 // JS goes here
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+
+
+class TabLink {
+    constructor(element) {
+        this.element;
+
+        this.data = this.element.dataset.tab;
+
+        this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let links = document.querySelectorAll('.tabs-link'); 
+
+links = Array.from(links).map(element => new TabLink(element));
