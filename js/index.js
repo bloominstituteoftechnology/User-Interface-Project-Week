9464751e47cd87d63ef34c88910a0d3553095732
unknown <tsaihuangsd@gmail.com>
2018-08-22 12:00:08 -0700

    Project Update: navigation system design complete

--- js/index.js
+++ js/index.js
@@ -1,2 +1,22 @@
 // JS goes here
-console.log("update");
+
+const hamburgerButton = document.querySelector(".hamburger-img");
+
+const closeButton = document.querySelector(".hamburger-close-img");
+closeButton.classList.add("display-off");
+
+const dropdownContent = document.querySelector(".dropdown-content");
+dropdownContent.classList.add("display-off");
+
+hamburgerButton.addEventListener('click', (event) => {
+    hamburgerButton.classList.toggle("display-off");
+    closeButton.classList.toggle("display-off");
+    dropdownContent.classList.toggle("display-off");    
+});
+closeButton.addEventListener('click', (event) => {
+    hamburgerButton.classList.toggle("display-off");
+    closeButton.classList.toggle("display-off");
+    dropdownContent.classList.toggle("display-off");    
+});
+
+
