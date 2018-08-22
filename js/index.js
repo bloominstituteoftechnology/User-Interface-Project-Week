c79b2c07f4f632f7d619f1fd30a885a1f735d87f
Colin Preston <prestonresult@gmail.com>
2018-08-22 10:53:40 -0500

    nav not appearing yet(finding out why)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // JS goes here
+
+
+
+
+
+function classToggle() {
+	const navs = document.querySelectorAll('.Navbar__Items')
+	
+	navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  document.querySelector('.Navbar__Link-toggle')
+	.addEventListener('click', classToggle);
