eee201ed32ede3375920423c3c1afac49c907e5a
decagondev <decagondevelopment@gmail.com>
2018-08-23 17:21:20 +0100

    testing carousel

---
+++ js/vuetest.js
@@ -0,0 +1,16 @@
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' },
+      { id: 3, text: "Some new todo" }
+    ]
+  }
+})
