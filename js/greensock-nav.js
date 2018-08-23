31d2bfca8a40e5b13f5a09b39eb14732abbe1c3b
Luisan <luisangelsc@gmail.com>
2018-08-23 15:09:30 -0600

    minor tweaks

--- js/greensock-nav.js
+++ js/greensock-nav.js
@@ -9,12 +9,13 @@
   hamburger.addEventListener('click', () => {
     var tlLinks = new TimelineMax();
     if (nav.classList.length === 2) {
-      TweenMax.fromTo(nav, 1, {opacity: 0, height: "auto"}, {opacity:.96, height: window.innerHeight})
+      TweenMax.fromTo(nav, 1, {opacity: 0, height: "auto"}, {opacity:.92, height: window.innerHeight})
 
       tlLinks.add('start')
-      .from(links[0], 1.8, {opacity: 0, x:-400}, {opacity: 1})
-      .from(links[1], 1.4, {opacity: 0, x:-400}, {opacity: 1}, 'start')
+      .from(links[0], 1.6, {opacity: 0, x:-400}, {opacity: 1})
+      .from(links[1], 1.3, {opacity: 0, x:-400}, {opacity: 1}, 'start')
       .from(links[2], 1, {opacity: 0, x:-400}, {opacity: 1}, 'start')
+      .from(links[3], .7, {opacity: 0, x:-400}, {opacity: 1}, 'start')
     } else {
       TweenMax.to(nav, 1, {height: 50})
       TweenMax.fromTo(hamburger, 1, {opacity: 0}, {opacity: 1})
