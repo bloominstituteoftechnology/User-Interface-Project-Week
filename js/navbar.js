de466b4b4dcbe62a3a3c7aebb6a34ee516477968
Max <maxkajiwara@gmail.com>
2018-08-22 23:41:11 -0700

    Night mode mostly working. Process involved learning a lot of new things + trial and error.

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,18 @@
+// Menu animation
 const toggleNav = () => {
   !open ? (
     navContainer.classList.toggle('flex'),
+    nightMode.classList.toggle('flex'),
     TweenMax.fromTo( navContainer, .3, {height: '0vh'}, {height: '100vh'} ),
     TweenMax.fromTo( nav, .4, {y: -100, opacity: 0}, {y: 0, opacity: 1, delay: .2} ),
+    TweenMax.fromTo( nightMode, .4, {opacity: 0}, {opacity: 1, delay: .2} ),
     open = true
   ) : (
     TweenMax.fromTo( navContainer, .2, {height: '100vh'}, {height: '0vh', delay: .2} ),
     TweenMax.fromTo( nav, .2, {y: 0, opacity: 1}, {y: -33, opacity: 0} ),
+    TweenMax.fromTo( nightMode, .2, {opacity: 1}, {opacity: 0} ),
     open = false,
-    setTimeout( () => navContainer.classList.toggle('flex'), 400 )
+    setTimeout( () => {navContainer.classList.toggle('flex'); nightMode.classList.toggle('flex')}, 400 )
   );
 };
 
@@ -18,3 +22,14 @@ const hamburger = document.querySelector('.hamburger');
 let open = false;
 
 hamburger.addEventListener('click', toggleNav);
+
+// Night mode
+const toggleNightMode = () => {
+  document.querySelector('body').classList.toggle('dark');
+  (document.querySelector('footer').classList.toggle('footer-dark'));
+  Array.from(document.querySelectorAll('p, h2:not(.project-square), h3, .button')).forEach(element => element.classList.toggle('light'));
+};
+
+const nightMode = document.querySelector('.night-mode');
+
+nightMode.addEventListener('click', () => {toggleNightMode(); toggleNav()});
