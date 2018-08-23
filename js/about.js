28d14cc63746ac531930436137cea21b59c996fe
Luisan <luisangelsc@gmail.com>
2018-08-23 12:40:58 -0600

    all three sections change images independently

--- js/about.js
+++ js/about.js
@@ -7,17 +7,31 @@ class TabImage {
 
     this.element.addEventListener('click', () => this.switch())
   }
+
   switch() {
-    images.slice(0,3).forEach(item => display.classList.remove(item.dataTab))
-    display.classList.add(this.dataTab)
+    let i = Math.floor(images.indexOf(this)/3)
+    console.log('i:', i);
+    images.forEach(item => display[i].classList.remove(item.dataTab))
+    display[i].classList.add(this.dataTab)
+    paragraphs.forEach(item => {
+      item.classList.add("inactive")
+      this.dataTab === item.dataset.paragraph ?
+      item.classList.remove('inactive') :
+      ""
+    }
+    )
   }
+
 }
 
 
-const display = d.querySelector('.main-img')
+const display = d.querySelectorAll('.main-img')
+
+const paragraphs = d.querySelectorAll('article > p')
 
 let images = d.querySelectorAll('.img')
 images = Array.from(images).map(image => new TabImage(image))
 
 console.log(images);
 console.log(display);
+console.log(paragraphs);
