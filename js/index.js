eea2f1c47e17852f855902bd8a1e1a4c97a36be9
Casey Baker <caseye.baker@gmail.com>
2018-08-23 11:42:55 -0700

    began incorporating javascript into navigation in prep for animation experiment

--- js/index.js
+++ js/index.js
@@ -59,7 +59,55 @@ menuClose.addEventListener('click', () => {
 
 
 
+
+
+// class NavLink {
+//   constructor(link) {
+//     this.link = link;
+
+//     this.link.addEventListener('click', () => { this.select() });
+
+//     select(){
+//       const links = document.querySelectorAll('.nav-links');
+
+      
+//     }
+    
+//   }
+// }
+
+// const navlinks = document.querySelectorAll('.nav-links');
+
+// navlinks = Array.from(navlinks).map( link => new NavLink(link));
+
+//grab all nav-links
+
+//when a nav-link is clicked, it zooms to right
+//then menu collapses
  
+//
 
 
-  
+/* <html>
+<h2>Navigate to URL</h2>
+<script>
+function navigateToURL() {
+window.location.href = "http://www.roseindia.net";
+}
+</script>
+<a href="#" onclick="navigateToURL();">Click here to Navigate RoseIndia home page</a>
+</html> */
+
+function navigateHome() {
+  window.location.href = "./index.html";
+}
+
+function navigateServices() {
+    window.location.href = "./services.html";
+}
+
+const animateServicesNav = () => {
+  setTimeout(
+    navigateServices, 2000
+  )
+}
