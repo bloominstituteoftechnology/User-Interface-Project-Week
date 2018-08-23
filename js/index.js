22c7a311836bc20614277ed06995793a958201df
Casey Baker <caseye.baker@gmail.com>
2018-08-23 10:32:15 -0700

    fixed some responsiveness issues with home page design; troubleshooting similar issues on services page

--- js/index.js
+++ js/index.js
@@ -5,39 +5,46 @@
 const toggleMenu = () => {
     // menu.classList.toggle('menu--open');
      if(menu.className.includes("menu--open")) {
-        console.log(event.currentTarget.parentNode.className);
+       
         menu.classList.remove("menu--open");
-        TweenLite.to(menu, 2.5, { 
-          ease: Expo.easeOut, 
+        TweenLite.to(menu, 1, { 
+          ease: Circ.easeOut, 
           height: 50
         });
+
+       
       } else {
         menu.classList.add("menu--open");
         TweenLite.set(".menu--open", {clearProps:"all"})
-        TweenLite.from(menu, 2, { 
-          ease: Expo.easeOut, //.config(0.2, 0.7, false), 
+        TweenLite.from(menu, 1, { 
+          ease: Circ.easeOut, 
           height: 50
         });
-      }
-    
+
+       }
     
   }
 
   const collapseButton = () => {
+   // TweenLite.to()
     menuButton.style.display = "none";
+    //move rapidly up and out
     menuClose.style.display = "block";
+    //move rapidly up and in
   }
 
   
   const expandButton = () => {
     menuButton.style.display = "block";
+    //rapidly move up and in
     menuClose.style.display = "none";
+    //rapidly move up and out
   }
   
   
   const menu = document.querySelector('.navbar');
-  const menuClose = document.querySelector('.menu-close');
 
+  const menuClose = document.querySelector('.menu-close');
   const menuButton = document.querySelector('.menu-button');
  
   menuButton.addEventListener('click', () => {
@@ -51,74 +58,8 @@ menuClose.addEventListener('click', () => {
 });
 
 
-    
-
 
  
 
 
-  
-// Tabs
-
-class TabLink {
-    constructor(link) {
-      this.link = link;
-           
-      this.data = this.link.dataset.tab;
-          
-            this.itemElement = document.querySelector(`.tab-item[data-tab="${this.data}"]`);
-      
-     
-      this.tabItem = new TabItem(this.itemElement);
-      
-      
-      this.link.addEventListener('click', () => { this.select() });
-  
-    };
-  
-    select() {
-      
-      const links = document.querySelectorAll('.tab');
-  
-      
-      Array.from(links).forEach(function(links) {
-        links.classList.remove('tab-selected');
-      });
-  
-      
-      this.link.classList.add('tab-selected');
-      
-      
-    this.tabItem.select();
-     }
-  }
-  
-  class TabItem {
-    constructor(tabItems) {
-      this.tabItems = tabItems;
-    }
-  
-    select() {
-  
-      // Select all items elements from the DOM
-      const allItems = document.querySelectorAll('.tab-item');
-      // Remove the class "tabs-item-selected" from each element
-      Array.from(allItems).forEach(function(allItems) {
-        allItems.classList.remove('tab-item-selected');
-      });
-  
-      // Add a class named "tabs-item-selected" to this element 
-      this.tabItems.classList.add('tab-item-selected');
-    }
-  }
-  
-  
-
-  let links = document.querySelectorAll('.tab');
-  
-  
-  links = Array.from(links).map( link => new TabLink(link));
-  
-  
-  links[0].select();
   
---
+++ js/tabs.js
@@ -0,0 +1,64 @@
+// Tabs
+
+class TabLink {
+    constructor(link) {
+      this.link = link;
+           
+      this.data = this.link.dataset.tab;
+          
+            this.itemElement = document.querySelector(`.tab-item[data-tab="${this.data}"]`);
+      
+     
+      this.tabItem = new TabItem(this.itemElement);
+      
+      
+      this.link.addEventListener('click', () => { this.select() });
+  
+    };
+  
+    select() {
+      
+      const links = document.querySelectorAll('.tab');
+  
+      
+      Array.from(links).forEach(function(links) {
+        links.classList.remove('tab-selected');
+      });
+  
+      
+      this.link.classList.add('tab-selected');
+      
+      
+    this.tabItem.select();
+     }
+  }
+  
+  class TabItem {
+    constructor(tabItems) {
+      this.tabItems = tabItems;
+    }
+  
+    select() {
+  
+      // Select all items elements from the DOM
+      const allItems = document.querySelectorAll('.tab-item');
+      // Remove the class "tabs-item-selected" from each element
+      Array.from(allItems).forEach(function(allItems) {
+        allItems.classList.remove('tab-item-selected');
+      });
+  
+      // Add a class named "tabs-item-selected" to this element 
+      this.tabItems.classList.add('tab-item-selected');
+    }
+  }
+  
+  
+
+  let tablinks = document.querySelectorAll('.tab');
+  
+  
+  tablinks = Array.from(tablinks).map( link => new TabLink(link));
+  
+  
+  tablinks[0].select();
+  
