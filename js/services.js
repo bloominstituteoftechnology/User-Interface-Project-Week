90561057b94835b61b49cea1b68c343686ef30ba
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-08-23 10:52:01 -0500

    services js wip

---
+++ js/services.js
@@ -0,0 +1,54 @@
+// services tabs
+
+class Tab {
+    constructor(element) {
+        this.element = element;
+
+        this.data = this.element.dataset.tab;
+
+        this.tabElement = document.querySelector(`.tab-item[data-tab="${this.data}"]`);
+
+        this.tabContent = new TabContent(this.tabElement);
+
+        this.element.addEventListener('click', () => {
+            this.selectTab();
+        });
+    }
+
+    selectTab() {
+        const links = document.querySelectorAll('.button');
+
+        links.forEach(element => {
+            element.classList.remove('tabs-link-selected');
+        })
+
+        this.element.classList.add('tabs-link-selected');
+
+        this.tabContent.select();
+    
+    }
+}
+
+class TabContent {
+    constructor(element) {
+        this.element = element;
+
+    }
+
+    select() {
+        let items = document.querySelectorAll('.tab-item');
+
+        items.forEach(element => {
+            element.classList.remove('tabs-item-selected');
+        })
+
+        this.element.classList.add('tabs-item-selected');
+    }
+
+}
+
+let links = document.querySelectorAll('.button');
+
+links = Array.from(links).map(link => new Tab(link));
+
+links[0].selectTab();
