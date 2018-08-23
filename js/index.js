62fb22c7af2721b97ad55ba78e3892759bb2ca76
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-08-23 15:54:37 -0500

    nav menu functional

--- js/index.js
+++ js/index.js
@@ -1,12 +1,46 @@
 // JS goes here
 
-class Menu {
-    constructor(element) {
-        this.element = element;
-        this.button = this.element.querySelectorAll('.menu-btn');
-    }
+const toggleMenu = () => {
+    
+    menu.classList.toggle('overlay');
 }
 
-let menu = document.querySelectorAll('.main-nav');
+let show = false;
 
-menu = Array.from(menu).map(menu => new Menu(menu));
+const changeImg = () => {
+    if (show === false) {
+        menuButton.src = 'img/nav-hamburger-close.png';
+        show = true;
+    } else {
+        menuButton.src = 'img/nav-hamburger.png';
+        show = false;
+    }
+};
+
+  
+  
+const menu = document.querySelector('.main-nav-extended');
+
+const menuButton = document.querySelector('.hamburger-menu-btn');
+
+menuButton.addEventListener('click', e => {
+    changeImg();
+    toggleMenu();
+    
+});
+
+// const menuButton2 = document.querySelector('.x-menu-btn');
+
+// menuButton2.addEventListener('click', e => {
+//     menuButton2.setAttribute('src', 'img/nav-hamburger.png')
+//     toggleMenu();
+    
+// });
+
+// function changeImg() {
+//     if (image.getAttribute('src') == "img/nav-hamburger.png") {
+//         image.src = "img/nav-hamburger-close.png";
+//     } else {
+//         image.src = "img/nav-hamburger.png"
+//     }
+// }
