938b996a8d3d62b53cb82765e3851af287353854
Max <maxkajiwara@gmail.com>
2018-08-23 14:28:45 -0700

    Made menu animation snappier

--- js/navbar.js
+++ js/navbar.js
@@ -4,17 +4,17 @@ const toggleNav = () => {
     hamburgers.forEach(element => element.classList.toggle('flex')),
     navContainer.classList.toggle('flex'),
     nightMode.classList.toggle('flex'),
-    TweenMax.fromTo( navContainer, .3, {height: '0vh'}, {height: '100vh'} ),
-    TweenMax.fromTo( nav, .4, {y: -100, opacity: 0}, {y: 0, opacity: 1, delay: .2} ),
-    TweenMax.fromTo( nightMode, .4, {opacity: 0}, {opacity: 1, delay: .2} ),
+    TweenMax.fromTo( navContainer, .2, {height: '0vh'}, {height: '100vh'} ),
+    TweenMax.fromTo( nav, .3, {y: -100, opacity: 0}, {y: 0, opacity: 1, delay: .1} ),
+    TweenMax.fromTo( nightMode, .3, {opacity: 0}, {opacity: 1, delay: .2} ),
     open = true
   ) : (
     hamburgers.forEach(element => element.classList.toggle('flex')),
-    TweenMax.fromTo( navContainer, .2, {height: '100vh'}, {height: '0vh', delay: .2} ),
-    TweenMax.fromTo( nav, .2, {y: 0, opacity: 1}, {y: -33, opacity: 0} ),
+    TweenMax.fromTo( navContainer, .2, {height: '100vh'}, {height: '0vh', delay: .1} ),
+    TweenMax.fromTo( nav, .1, {y: 0, opacity: 1}, {y: -25, opacity: 0} ),
     TweenMax.fromTo( nightMode, .2, {opacity: 1}, {opacity: 0} ),
     open = false,
-    setTimeout( () => {navContainer.classList.toggle('flex'); nightMode.classList.toggle('flex')}, 400 )
+    setTimeout( () => {navContainer.classList.toggle('flex'); nightMode.classList.toggle('flex')}, 300 )
   );
 };
 
