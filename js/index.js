8a129d684964fc7dce8fbe3355169d27837b08da
Anthony Greb <agreb255@gmail.com>
2018-08-22 21:18:41 -0400

    working on mobile

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 // JS goes here
+
 class TabLink {
     constructor(element){
         this.element = element;
         this.data = this.element.dataset.tab;
-        this.cards = document.querySelector(`.card[data-tab = "${this.data}"]`);
-        this.cards = Array.from(this.cards).map(card => new TabCard(card));
-        this.element.addEventLestener("click", () => { this.selectTab()});
+        this.card = document.querySelector(`.card[data-tab = "${this.data}"]`);
+        this.card = new TabCard(this.card);
+        this.element.addEventListener("click", () => { this.selectTab()});
     }
     selectTab() {
         const tabs = document.querySelectorAll(".tab");
         tabs.forEach( tab => {
             tab.classList.remove("active-tab");
         });
-        const cards = document.querySelectorAll(".card");
-        cards.forEach(card => card.style.display = "none");
-        this.cards.forEach(card => card.selectCard());
+        this.card.element.style.display = "none";
+        this.card.selectCard();
     }
 }
 class TabCard {
@@ -27,4 +27,4 @@ class TabCard {
 }
 let tabs = document.querySelectorAll(".tab");
 tabs = Array.from(tabs).map(tab => new TabLink(tab));
-tabs[0].selectCard();
+// tabs[0].selectCard();
--- js/services.js
+++
@@ -1,7 +0,0 @@
-class TabLink {
-    constructor(element){
-        this.element = element;
-        this.data = this.element.dataset.tab;
-        this.cards = document.querySelector(`.card[data-tab = "${this.data}"]`);
-    }
-}
