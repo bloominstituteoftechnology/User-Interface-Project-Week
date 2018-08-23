17076725b4595bbb86570328d433a6f4db339d42
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-08-23 13:26:51 -0400

    Added menu animation

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ class MenuButton {
 
   selectMenu() {
     let nav = document.querySelector('.nav');
-    nav.classList.toggle('full-screen');
+    // nav.classList.toggle('full-screen');
 
     let buttons = document.querySelectorAll('.nav-menu');
     buttons.forEach((button) => {
@@ -21,7 +21,15 @@ class MenuButton {
     });
 
     let navContent = document.querySelector('.nav-content');
-    navContent.classList.toggle('display-none');
+    // navContent.classList.remove('display-none');
+    TweenMax.to(navContent, 1, { marginTop: 0 });
+
+    let menuClose = document.querySelector('.menu-close');
+    menuClose.classList.contains('display-none')
+      ? (TweenMax.to(nav, 1, { height: '49px' }),
+        TweenMax.to(navContent, 0.5, { opacity: 0, y: '-300' }))
+      : (TweenMax.to(nav, 1, { height: '100%' }),
+        TweenMax.to(navContent, 1, { opacity: 1, y: 0 }));
   }
 }
 
@@ -30,3 +38,6 @@ let menuButtons = document.querySelectorAll('.nav-menu');
 menuButtons = Array.from(menuButtons).map(
   (menuButton) => new MenuButton(menuButton)
 );
+
+let navContent = document.querySelector('.nav-content');
+// navContent.classList.add('display-none');
