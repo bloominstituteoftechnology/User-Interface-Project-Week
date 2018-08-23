9c895d9d6e6b9dfce7b8f984944fef15a624843a
BrianARuff <thelonewolf198927@gmail.com>
2018-08-22 20:36:37 -0400

    added in gsap animation for tommorrows presentation

--- js/index.js
+++ js/index.js
@@ -1,16 +1,33 @@
 // JS goes here
 
 // *** Nav JS ***
-console.log('hi');
 const hamburger = document.querySelector('.nav-burger');
 const nav = document.querySelector('#nav');
 const navMenuContent = document.querySelector('.home-nav-dropdown');
 
 hamburger.addEventListener('click', () => {
-  console.log('hamburger');
+
+  if(nav.classList.contains('expand-nav-500')){
+    console.log('hi')
+    TweenMax.from('.expand-nav-500', 2, {top: 100});
+    TweenMax.to('.expand-nav-500', 2, {top: 0});
+    TweenMax.from('.expand-nav-500', 2, {height: "100%"});
+    TweenMax.to('.expand-nav-500', 2, {height: "5%"});
+  }
+  
+  if(navMenuContent.classList.contains('expand-nav-500-menu')){
+    console.log('yes');
+    TweenMax.to('.expand-nav-500', 2, {top: 100, opacity: 1});
+    TweenMax.from('.expand-nav-500', 2, {top: 0, opacity: 0});
+  }
+
   nav.classList.toggle('expand-nav-500');
+  TweenMax.from('.expand-nav-500', 2, {top: 0, opacity: 0});
+  TweenMax.to('.expand-nav-500', 2, {top: 100, opacity: 1});
   nav.style = "align-items: flex-start";
   navMenuContent.classList.toggle('expand-nav-500-menu');
+  TweenMax.from('.expand-nav-500', 2, {top: 0, opacity: 0});
+  TweenMax.to('.expand-nav-500', 2, {top: 100, opacity: 1});
   navMenuContent.classList.toggle('hide');
   hamburger.setAttribute("src", '../img/nav-hamburger-close.png');
   if(!nav.classList.contains('expand-nav-500')){
