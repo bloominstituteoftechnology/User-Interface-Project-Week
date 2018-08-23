7064003ab3125083cbbb25f5da0f22ef80ecad4b
Kevin Nguyen <kevinn858@gmail.com>
2018-08-23 09:53:55 -0700

    added Services page tab selection

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // JS goes here
+
+//menu
 let hamburger = document.querySelector('.nav-button');
 let unclicked = true;
 let menuHeader = document.querySelector('.menu-header');
@@ -15,4 +17,39 @@ hamburger.addEventListener('click',()=>{
         hamburger.setAttribute('src','./img/nav-hamburger.png');
         unclicked = true;
     }
-})
+});
+
+// services-tabs
+//grab tabs
+let tabs = document.querySelectorAll('.tab');
+//convert tabs Nodelist into Array of Tab objects
+class Tab{
+    constructor(element){
+        this.element = element;
+        this.tabData = this.element.dataset.tab;
+        //give Tab object the Card associated with it
+        this.card = document.querySelector(`.card[data-tab='${this.tabData}']`);
+        //add click event so that the tab shows its information
+        this.element.addEventListener('click', ()=>{
+            //invis all other cards and show this tabs corresponding card
+            let cards = document.querySelectorAll('.card');
+            cards.forEach(card =>{
+                card.classList.remove('show');
+                card.classList.add('invis');
+            });
+            this.card.classList.add('show');
+            this.card.classList.remove('invis');
+            //remove selected from Tabs and give selected to this tab
+            document.querySelectorAll('.tab').forEach(tab =>{
+                tab.classList.remove('selected');
+            });
+            this.element.classList.add('selected');
+        })
+    }
+}
+class Card{
+    constructor(element){
+        this.element = element;
+    }
+}
+tabs = Array.from(tabs).map(tab => new Tab(tab));
