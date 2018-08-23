a93b5bbe71f859028c855f6a945b568e9ab29850
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-08-23 09:15:36 -0500

    final nav components working.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,31 @@
+//funtionality for nav open/close
 const open = () => {
-    menu.classList.add('menu--open');
+    menu.classList.add('menu--open'); 
   }
 const menuClose = ( ) => {
     menu.classList.remove('menu--open');
 }
+
   
-  const close = document.querySelector('.close');  
-  const menu = document.querySelector('.menu');
-  const menuButton = document.querySelector('.menu-button');
-  menuButton.addEventListener('click', open);
-  close.addEventListener('click', menuClose);
+const close = document.querySelector('.close');  
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.menu-button');
   
-  const btn = document.querySelector('.btn');
-  const none = () => {
-    btn.classList.toggle('none');
-  }
-  btn.addEventListener('click', none);
+menuButton.addEventListener('click', open);
+close.addEventListener('click', menuClose);
+//open/close end
+
+const show = () => {
+  btn1.classList.add('none');
+  btn2.classList.remove('none');
+}
+const hide = () => {
+  btn2.classList.add('none');
+  btn1.classList.remove('none');
+}
+
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+
+btn1.addEventListener('click', show);
+btn2.addEventListener('click', hide);
