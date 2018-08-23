e4112c88677d0e0f111bc3a2dd755be50c41f7aa
BrianARuff <thelonewolf198927@gmail.com>
2018-08-22 20:49:26 -0400

    more gsap for serv page titles

--- js/services.js
+++ js/services.js
@@ -8,12 +8,23 @@ class MenuButton {
     this.btnClick = this.btn.addEventListener('click', () => {
       const allImgs = Array.from(document.querySelectorAll('.construction-img'));
       allImgs.map(img => img.classList.add('hide'));
+      TweenMax.from(this.img, 2, {opacity: 1});
       this.img.classList.remove('hide');
-      buttons.map(btn => btn.classList.remove('btn-color'));
+      TweenMax.from(this.img, 2, {opacity: 0});
+      buttons.map((btn, i) => {
+        btn.classList.remove('btn-color')
+      });
       this.btn.classList.add('btn-color');
+      title.innerText = titles[this.btnData - 1];
+      TweenMax.from(title, 2, {opacity: 0});
+      TweenMax.to(title, 2, {opacity: 1});
     })
   }
 }
 
 const buttons =  Array.from(document.querySelector('.serv-include-btns').children);
 buttons.map(btn => new MenuButton(btn));
+
+const titles = ['Pre-Construction', 'Construction', 'Design Build', 'Sustainability'];
+const title = document.querySelector('.construction-dynamic h2');
+title.innerText = "Pre-Construction";
