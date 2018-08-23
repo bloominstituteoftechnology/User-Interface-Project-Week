0cd1d730ba22029cbfb25997f3b60f10c9287b57
Lucas Beemer <lucasbeemer@Lukes-MBP.hsd1.ca.comcast.net>
2018-08-23 02:35:01 -0700

    final submission

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
-// JS goes here
+// Navigation Bar
+document.querySelector('.hamburger').addEventListener('click', () => openNav());
+document.querySelector('.cross').addEventListener('click', () => closeNav());
+
+function openNav() {
+  document.querySelector('.hamburger').classList.add('hidden');
+  document.querySelector('.cross').classList.remove('hidden');
+  document.querySelector('.navigation').classList.add('modal');
+  document.querySelector('.navLinks').classList.remove('hidden');
+  document.querySelector('.navigation').style.justifyContent = 'center';
+}
+
+// Services Tabs
+class TabLink {
+  constructor(element) {
+    this.element = element;
+    this.data = this.element.dataset.tab;
+      
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+    this.itemLink = document.querySelector(`.tabs-link[data-tab='${this.data}']`);
+
+    this.tabItem = new TabItem(this.itemElement);
+    
+    this.element.addEventListener('click', () => { this.select() });
+  };
+
+  select() {
+    const links = document.querySelectorAll('.tabs-link');
+    Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
+    this.element.classList.add('tabs-link-selected');
+    
+    this.tabItem.select();
+  }
+}
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    const items = document.querySelectorAll('.tabs-item');
+
+    Array.from(items).forEach(item => item.classList.remove('tabs-item-selected'));
+
+    this.element.classList.add('tabs-item-selected');
+
+  }
+}
+
+let links = document.querySelectorAll('.tabs-link');
+links = Array.from(links).map(link => new TabLink(link));
