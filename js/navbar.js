07b2bef172b3ff5729ac0992111a79fd9dfc8872
Max <maxkajiwara@gmail.com>
2018-08-23 18:14:23 -0700

    Added sun icon for day mode. Modified toggle functions to use closure and .src

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,12 @@
-// Menu animation
-const toggleNav = () => {
+//|==================
+//|  Menu Animation  
+//|==================
+const toggleNavClosure = () => {
+  let open = false;
+  return () =>
   !open ? (
-    hamburgers.forEach(element => element.classList.toggle('flex')),
+    hamburger.src = "img\\nav-hamburger-close.png",
+    hamburger.classList.add('close'),
     navContainer.classList.toggle('flex'),
     nightMode.classList.toggle('flex'),
     TweenMax.fromTo( navContainer, .2, {height: '0vh'}, {height: '100vh'} ),
@@ -9,7 +14,8 @@ const toggleNav = () => {
     TweenMax.fromTo( nightMode, .2, {opacity: 0}, {opacity: 1, delay: .2} ),
     open = true
   ) : (
-    hamburgers.forEach(element => element.classList.toggle('flex')),
+    hamburger.src = "img\\nav-hamburger.png",
+    hamburger.classList.remove('close'),
     TweenMax.fromTo( navContainer, .2, {height: '100vh'}, {height: '0vh', delay: .1} ),
     TweenMax.fromTo( nav, .1, {y: 0, opacity: 1}, {y: -25, opacity: 0} ),
     TweenMax.fromTo( nightMode, .2, {opacity: 1}, {opacity: 0} ),
@@ -20,19 +26,30 @@ const toggleNav = () => {
 
 const navContainer = document.querySelector('.nav-container');
 const nav = document.querySelector('nav');
-const hamburgers = Array.from(document.querySelectorAll('.hamburger'));
-let open = false;
+const hamburger = document.querySelector('.hamburger');
+let toggleNav = toggleNavClosure();
 
-hamburgers.forEach(element => element.addEventListener('click', toggleNav));
+hamburger.addEventListener('click', toggleNav);
 
-// Night mode
-const toggleNightMode = () => {
+//|==============
+//|  Night Mode  
+//|==============
+const toggleNightModeClosure = () => {
+  let day = true;
+  return () => {
+  day ? (
+    setTimeout( () => {nightMode.src = "img\\nav-day-mode.png"}, 400 ), day = false
+    ) : (
+    setTimeout( () => {nightMode.src = "img\\nav-night-mode.png"}, 400 ), day = true
+  );
   document.querySelector('body').classList.toggle('dark');
   (document.querySelector('header').classList.toggle('header-dark'));
   (document.querySelector('footer').classList.toggle('footer-dark'));
   Array.from(document.querySelectorAll('p, h2:not(.project-square), h3, .button, label')).forEach(element => element.classList.toggle('light'));
+  }
 };
 
 const nightMode = document.querySelector('.night-mode');
+let toggleNightMode = toggleNightModeClosure();
 
 nightMode.addEventListener('click', () => {toggleNightMode(); toggleNav()});
