40034028f811fad413e1edb51c9b35db9ae6d176
MichelleOk <umbralcompendium@gmail.com>
2018-08-28 13:56:26 -0600

    finished th HTML for the services tabs, working on the Js and Less now

--- js/index.js
+++ js/index.js
@@ -5,3 +5,40 @@ const toggleMenu = () => {
 const menu = document.querySelector(".menu");
 const menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", toggleMenu);
+
+//Service Tabs
+
+class TabLink {
+	constructor(element) {
+		this.element = element;
+		this.data = this.element.dataset.tab;
+		this.item = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+		this.tabItem = new TabItem(this.item);
+		this.element.addEventListener("click", () => {this.select() });
+	}
+	select() {
+		const links =document.querySelectorAll("tabs-link");
+		links.forEach(link => { link.classList.remove("tabs-link-selected")
+	});
+		this.element.classList.add("tabs-link-selected");
+		this.tabItem.select();
+	}
+}
+class TabItem {
+	constructor(element) {
+		this.element = element;
+	}
+	Select() {
+		const items = document.querySelectorAll(".tabs-item");
+		items.forEach(item => { item.classList.remove("tabs-items-selected");
+		})
+		this.element.classList.add("tabs-item-selected");
+	}
+}
+
+
+let links = document.querySelectorAll(".tabs-link");
+links = Array.from(links).map(link => new TabLink(link));
+
+
+
