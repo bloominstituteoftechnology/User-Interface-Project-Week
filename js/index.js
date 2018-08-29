ef620765c6cff4d1ee9b47530365d905561b236c
MichelleOk <umbralcompendium@gmail.com>
2018-08-29 01:09:21 -0600

    tabs still not working, but started on home html/less

--- js/index.js
+++ js/index.js
@@ -9,35 +9,44 @@ menuButton.addEventListener("click", toggleMenu);
 //Service Tabs
 
 class TabLink {
-	constructor(link) {
-		this.link = link; //this reps the links
-		this.link.addEventListener("click", () => {this.linkClick() }); //putting a click event on the link/element
-		this.data = this.link.dataset.tab; //calling the dataset
-		this.tabContent = document.querySelector(`.tabs-item[data-tab="${this.data}"]`); //connecting the dataset. e=link, tCont=content
-		
-		this.tabContent = new Content(this.tabContent); //establishing a new class for the content
-		
-	}
+	constructor(element) {
+		this.element = element; //this reps the links
+		this.data = this.element.dataset.tab; //calling the dataset
+		this.item = document.querySelector(`.tabs-item[data-tab="${this.data}"]`); //connecting the dataset.e=link, tCont=content
+		this.tabItem= new TabItem(this.item); //establishing a new class for the content
+		this.element.addEventListener('click', () => { this.select(); //putting a click event on the link/element
+		}); 
+	};
 
-	linkClick() {
-		this.tabContent.toggleContent(); //method to toggle content
-		console.log("this works?");
+	select() {
+		const links = document.querySelectorAll('.tabs-link');
+		links.forEach( link => {
+			link.classList.remove('tabs-link-selected')
+		});
+		this.element.classList.add('tabs-link-selected');
+		this.tabItem.select();
 	}
 }
 
-class Content {
-	constructor(tabContent) {
-		this.tabContent = tabContent; //this reps the content
+class TabItem {
+	constructor(element) {
+		this.element = element;
 	}
-	toggleContent() {
-		this.tabContent.classList.toggle('tab-toggle');  //the toggle
+	select() {
+		const items = document.querySelectorAll('.tabs-item');
+		items.forEach( item => {
+			item.classList.remove('tabs-item-selected');
+		})
+		this.element.classList.add('tabs-item-selected');
+		console.log("Hey!");
+
 	}
 }
 
 
 let links = document.querySelectorAll(".tabs-link"); //grabbing the links themselves
 links = Array.from(links).map(link => new TabLink(link)); //making the links into an array for easier access
-
+links[0].select()
 
 
 
