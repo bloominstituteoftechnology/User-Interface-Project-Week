051f20c72073ea6cc7fee15fb1e78bdaf66af1d6
Ray Lomeli <raylom96@gmail.com>
2018-08-30 12:37:42 -0500

    dropdown changed

--- js/index.js
+++ js/index.js
@@ -1,31 +1,16 @@
-// class Dropdown {
-//     constructor(element) {
-//         // assign this.element to the dropdown element
-//         this.element = element;
-//         // assign the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-//         this.button = this.element.querySelector('.dropdown-button')
-//         // assign the reference to the ".dropdown-content" class found in the dropdown element
-//         this.content = this.element.querySelector('.dropdown-content');
-//         // Add a click handler to the button reference and call the toggleContent method.
-//         this.button.addEventListener('click', () => {
-//             this.toggleContent()
-//         })
-//         // Instructor note, the reason we must wrap the toggleContent method in an anonymous function is that 'this' in toggleContent
-//         // would refer to the button, NOT the current instance of the class. 
-//     }
-
-//     toggleContent() {
-//         // Toggle the ".dropdown-hidden" class off and on
-//         this.content.classList.toggle('dropdown-hidden');
-//     }
-// }
-
-// let dropdowns = document.querySelectorAll('.dropdown');
-// dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
-
-
+let overlay = document.querySelector(".overlay-container");
+let burgerBtn = document.querySelector(".nav-burger");
+let closeBtn = document.querySelector(".close-button");
+let nav = document.querySelector(".navbar");
 
 /* Open */
+
+burgerBtn.addEventListener('click', function () {
+    document.getElementById("myNav").style.display = "block";
+    // overlay.setAttribute("style", "display: block;");
+});
+
+
 function openNav() {
     document.getElementById("myNav").style.display = "block";
 }
