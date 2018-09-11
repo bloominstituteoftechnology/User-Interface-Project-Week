c878be5974670555aefdd1b66cf0cef3b63ec97c
Joseph Thompson <joe.thompson838@gmail.com>
2018-09-11 19:53:34 -0400

    finished navbar functionality

--- js/index.js
+++ js/index.js
@@ -13,3 +13,36 @@ switchImages("homeImgTwo", './img/home/home-mobile-img-2.png')
 switchImages("villas", './img/home/home-mobile-villas-img.png')
 switchImages("outskirts", './img/home/home-mobile-outskirts-img.png')
 switchImages("blocks", './img/home/home-mobile-the-blocks-img.png')
+
+
+// target the fa-bars
+const bars = document.querySelector('.fa-bars');
+
+// add click event to toggle navbar display
+bars.addEventListener('click', () => {
+
+  // get navbar and display it
+  let nav = document.querySelector('.navbar');
+  nav.style.display = 'flex';
+
+  // hide bars
+  bars.style.display = 'none';
+
+  // show close;
+  close.style.display = 'block';
+});
+
+// set up the close icon to close navbar when clicked
+const close = document.querySelector('.close');
+close.addEventListener('click', () => {
+
+  // get navbar and hide it
+  let nav = document.querySelector('.navbar');
+  nav.style.display = 'none';
+
+  // hide close
+  close.style.display = 'none';
+
+  // show hamburger
+  bars.style.display = 'block';
+});
