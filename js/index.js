148b86bb44ad788d9f66407822c001e74ee7505f
Joseph Thompson <joe.thompson838@gmail.com>
2018-09-11 02:28:35 -0400

    finished the mobile design for home including js function to change image sources

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+
+// replace images if screen width is below 500px
+
+const switchImages = (id, src) => {
+  id = document.getElementById(id);
+
+  screen.width < 501 ? id.setAttribute('src', src) : true;
+}
+
+switchImages("homeImgOne", './img/home/home-mobile-img-1.png')
+switchImages("homeImgTwo", './img/home/home-mobile-img-2.png')
+switchImages("villas", './img/home/home-mobile-villas-img.png')
+switchImages("outskirts", './img/home/home-mobile-outskirts-img.png')
+switchImages("blocks", './img/home/home-mobile-the-blocks-img.png')
