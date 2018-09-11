72a9118209bc0b51c47e72ce8ed6e5408a365597
jryel1970 <jryel1970@gmail.com>
2018-09-11 13:23:09 -0400

    new commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS goes here
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
