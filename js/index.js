8213db57118f6c74ae4ae18c2842010f9ce8b182
Jess <jessdev83@gmail.com>
2018-09-11 22:58:50 -0400

    Footer Functionality
    Functionality added to email input. On focus, input text clears. On enter, email address entered clears and Thanks! is displayed. On blur, input value reverts back to Enter email.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// JS goes here
+//footer
+
+const emailInput = document.querySelector('input');
+emailInput.addEventListener('focus', () => {
+    emailInput.value = "";
+})
+window.addEventListener('keypress', (event) => {
+    if (event.which === 13 || event.which === 27) {
+        event.preventDefault()
+        emailInput.value = "Thanks!";
+    }
+})
+
+emailInput.addEventListener('blur', () => {
+    emailInput.value = "Enter email";
+})
