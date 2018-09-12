8ec9b6526124bdcc5954e48f363c239974804e3c
Joseph Thompson <joe.thompson838@gmail.com>
2018-09-11 22:09:57 -0400

    finished the javascript for the services tab design

--- js/index.js
+++ js/index.js
@@ -46,3 +46,75 @@ close.addEventListener('click', () => {
   // show hamburger
   bars.style.display = 'block';
 });
+
+
+// JavaScript for Services Page //
+
+// create a class for each tab-link
+class TabLink {
+  constructor(element) {
+    this.element = element;
+
+    // get the data attribute that links tab and container
+    this.data = this.element.dataset.tab;
+
+    // get associated container
+    this.containerElement = document.querySelector(`.tab-container[data-tab="${this.data}"]`);
+
+    // create a new TabContainer
+    this.container = new TabContainer(this.containerElement);
+
+    // add an event listener to tab-link to change containers
+    this.element.addEventListener('click', () => {
+      this.select();
+    });
+
+  }
+  select() {
+
+    // get all tab-link elements
+    const links = document.querySelectorAll('.tab-link');
+
+    // deselect all links
+    this.deselect(links);
+
+    // add className 'tab-link-selected' to this link
+    this.element.classList.add('tab-link-selected');
+
+    // call select method for tabContainer class
+    this.container.select();
+  }
+  deselect(links) {
+    Array.from(links).forEach((link) => {
+      link.classList.remove('tab-link-selected');
+    });
+  }
+}
+
+// create a clas for each tab-container
+class TabContainer {
+  constructor(element) {
+    this.element = element;
+  }
+  select() {
+    // select all tab containers
+    const containers = document.querySelectorAll('.tab-container');
+
+    // deselect all selected containers
+    this.deselect(containers);
+
+    // add a className "tab-container-selected"
+    this.element.classList.add('tab-container-selected');
+  }
+  deselect(containers) {
+    // remove all class 'tab-container-selected'
+    Array.from(containers).forEach((container) => {
+      container.classList.remove('tab-container-selected');
+    })
+  }
+}
+
+// create a nodeList of tab-links
+let tabLinks = document.querySelectorAll('.tab-link');
+// coierce into array and assign to class TabLink
+tabLinks = Array.from(tabLinks).map((link) => new TabLink(link));
