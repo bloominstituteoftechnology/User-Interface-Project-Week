03946f13dd849d0b2e76f6203b25ce00bb6bcba3
Kyle Baker <k@gnomiq.com>
2018-09-11 19:20:11 -0700

    Navigation system design

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// JS goes here
+function init() {
+    let navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('hide');
+
+    let navButtons = document.querySelectorAll('.nav-button');
+    Array.from(navButtons).forEach(navButton => navButton.addEventListener('click', () => navLinks.classList.toggle('hide')));
+}
+
+window.addEventListener('load', init());
