672ccb258e4de0eab44fe5aef8046c65b2867480
mglittleton <michaelglittleton@gmail.com>
2018-09-12 01:51:50 -0400

    feat: Navbar works on Home and Services
    Home page is fully functional, complete with a navbar that overwrites the entire screen. The Services page also has a functioning navbar, though it has nothing else beyond a basic framework.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// JS goes here
+class Dropdown {
+  constructor(element) {
+    this.element = element;
+
+    this.buttonOpen = this.element.querySelector(".hamburger");
+    this.buttonClose = this.element.querySelector(".close");
+
+    this.content = this.element.querySelector(".nav-links");
+
+    this.buttonOpen.addEventListener("click", () => {this.openContent();});
+    this.buttonClose.addEventListener("click", () => {this.closeContent();});
+  }
+
+  openContent() {
+    this.buttonOpen.style.display = "none";
+    this.buttonClose.style.display = "block";
+    this.content.classList.add("nav-showing");
+  }
+
+  closeContent() {
+    this.buttonOpen.style.display = "block";
+    this.buttonClose.style.display = "none";
+    this.content.classList.remove("nav-showing");
+  }
+}
+
+let navDropdown = document.querySelector(".navbar");
+navDropDown = new Dropdown(navDropdown);
