ddd15c029333f3d1ed2b67ca211addecb4463ecb
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-09-11 23:35:48 -0500

    navbar functionality built in with JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // JS goes here
+let nav = document.querySelector('.top-bar nav');
+let navBtn = document.querySelector('.nav-btn');
+
+navBtn.addEventListener('click', function(event) {
+  nav.classList.toggle('hide');
+  if (
+    event.currentTarget.innerHTML ===
+    '<img src="img/nav-hamburger-close.png" alt="Nav Button">'
+  ) {
+    event.currentTarget.innerHTML =
+      '<img src="img/nav-hamburger.png" alt="Nav Button">';
+  } else {
+    event.currentTarget.innerHTML =
+      '<img src="img/nav-hamburger-close.png" alt="Nav Button">';
+  }
+});
