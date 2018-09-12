83455d081618b5e5934d77fe0345ed51e33aa13c
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-09-11 20:44:22 -0400

    Created component for home page header styles.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // JS goes here
+
+class Header{
+    constructor(header){
+        this.element = header ;
+        // this.item = document.querySelectorAll('.header');
+        this.element.style.fontSize = '2.8rem';
+        this.element.style.fontWeight = 'Bold';
+        console.log(this.item);
+        this.element.style.fontFamily = 'Roboto';
+    }
+}
+
+
+let headers = document.querySelectorAll('.header');
+headers = Array.from(headers).map(header => new Header(header))
+console.log(headers);
