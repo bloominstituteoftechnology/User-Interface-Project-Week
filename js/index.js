d7522c5b58621c1e86fb2b41e3ad2f51d2bac4dc
combatpacifist <35746304+combatpacifist@users.noreply.github.com>
2018-09-12 15:30:09 -0700

    working on JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-// JS goes here
+// START HERE: create a reference to the ".tabs" classes
+let links = document.querySelectorAll('.tabs-link');
+
+// Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
+links = Array.from(links).map(link => new TabLink(link// JS goes here
