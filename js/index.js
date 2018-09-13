3dd6b3e80eaf7b16f04ff948e94aade08f4e09f5
Jason Hedrick <jasonleehedrick@gmail.com>
2018-09-12 20:37:51 -0700

    Finished most of the mobile design. Putting on finishing touches.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // JS goes here
+class Hamburger {
+    constructor(element) {
+        this.element = element;
+        this.linksContainer = document.querySelector('.hamLinks')
+        this.element.addEventListener('click', () => { this.toggleNav(); });
+        this.closed = true;
+    }
+
+    toggleNav() {
+        console.log(this.linksContainer);
+        this.linksContainer.classList.toggle('displayHam');
+        if (this.closed === true) {
+            this.closed = false;
+        }
+        else {
+            this.closed = true;
+        }
+    }
+}
+
+
+let hamburger = document.querySelector('.hamburger');
+hamburger = new Hamburger(hamburger);
+
+console.log(hamburger);
