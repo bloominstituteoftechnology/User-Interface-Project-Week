6421f332ce0e98f14fd1f9e69d65388b9b8e0c84
Victor G <victorgordian103@gmail.com>
2018-09-12 21:25:51 -0700

    Got the tab to do some function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // JS goes here
+class Dropdown {
+    constructor(element) {
+      
+      this.element = element;
+      
+      this.button = this.element.querySelector(".menu-button");
+      
+      this.content = this.element.querySelector(".menu-content");
+
+      this.close = this.element.querySelector(".icon-close")
+      
+      this.element.addEventListener('click', () => {
+        this.toggleContent()
+      })
+    }
+  
+    toggleContent() {
+      this.content.classList.toggle('menu-hidden');
+    }
+  }
+  
+  let menuContent = document.querySelectorAll('.dropdown-menu');
+  dropdowns = Array.from(menuContent).map( menu => new Dropdown(menu));
