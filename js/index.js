6610ebb8aa38331624d8ee39318f09981917822f
Rick Fleming <elfeming111@yahoo.com>
2018-09-12 19:42:58 -0700

    Added navigation functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // JS goes here
+
+// Navigation button handler
+const nav = document.querySelector('.nav');
+const navButton = document.querySelector('.nav-button');
+const navLinks = document.querySelector('.nav-links');
+
+let fullMenu = false;
+
+navButton.addEventListener('click', () =>{
+    nav.classList.toggle('full-nav-bar');
+
+    if(fullMenu){
+        navButton.src = "img/nav-hamburger.png";
+        fullMenu = !fullMenu;
+    }
+
+    else{
+        navButton.src = "img/nav-hamburger-close.png";
+        fullMenu = !fullMenu;
+    }
+
+    let navLinksHeight = ((window.innerHeight - 55 * 2) / window.innerHeight) * 100;
+    navLinks.style.height = `${navLinksHeight}vh`;
+});
