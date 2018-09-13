4977c6d150113ada91cbf4889c51eb1c9a23d143
Kendra Williams <kmonique.williams@gmail.com>
2018-09-12 21:52:28 -0400

    changed a tag to nav to create the column while hidden

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // JS goes here
+/*on hamburger click event
+change hamburger logo to close "x"
+display the nav at font-size 48px, color FFFFFF
+line height 85px, hover color #222222, height = screen height
+*/
+const hamburger = document.querySelector("img");
+console.log(hamburger);
