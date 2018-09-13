6f277b5ad50c152227fd7d1ba69e7358b614bafc
Clem <stacy.clem@e-hps.com>
2018-09-13 06:22:12 -0700

    Updated Nav to get information

--- less/Nav/Nav.js
+++ less/Nav/Nav.js
@@ -1,4 +1,4 @@
-
+// Toggle the menu active after event has been clicked
 const toggleMenu = () => {
     menu.classList.toggle('menu--active');
 }
@@ -7,8 +7,10 @@ const toggleMenu = () => {
 // Create a reference to the Menu
 const menu = document.querySelector('.menu');
 
+// Create a reference to the hamburger button
 const menuButton = document.querySelector('.hamburger');
 
+//Event Listener on hamburger to open menu
 menuButton.addEventListener('click', (e) => {
     toggleMenu();
 })
