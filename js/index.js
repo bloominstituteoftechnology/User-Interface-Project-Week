3e87bae142cc8740650e7d5bb74e846c392a64e0
Tommy Collison <tommy@collison.ie>
2018-09-12 19:09:59 -0700

    saving nav attempt changes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // JS goes here
+
+function openNavBar() {
+   let navBarOpen = document.querySelector(".dropdown").style.height = "100%";
+
+};
+
+function closeNavBar(){
+    let navBarClosed = document.querySelector(".dropdown").style.height = "0%";
+}
+
