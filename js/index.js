c93f75ead5597579d050c9df8452b2c7fcdd93da
Dewayne87 <dewaynelindsay87@gmail.com>
2018-09-13 05:20:19 -0700

    finished services components

--- js/index.js
+++ js/index.js
@@ -17,36 +17,36 @@ close.addEventListener('click', () => {
     links.style.display = ''
 })
 
-// class TabLink {
-//     constructor(element) {
-//         this.element = element;
-//         this.tabData = element.dataset.tab;
-//         this.card = document.querySelector(`.card[data-tab ='${this.tabData}']`);
-//         this.element.addEventListener('click', () => {
-//         this.selectTab();
-//         });
-//     }
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.tabData = element.dataset.tab;
+        this.card = document.querySelector(`.card[data-tab ='${this.tabData}']`);
+        this.element.addEventListener('click', () => {
+        this.selectTab();
+        });
+    }
 
-//     selectTab() {
-//         let cards = document.querySelectorAll('.card');
-//             cards.forEach(element => {
-//             element.style.display = 'none';
-//         })
-//         this.card.style.display = 'flex';    
-//         const tabs = document.querySelectorAll('.tab');;
-//             tabs.forEach(tab => {
-//             tab.style.backgroundColor = '';
-//             tab.style.color = '';
-//             })
-//         this.element.style.backgroundColor = '#5E9FB9';
-//         this.element.style.color = 'black';
-//     }
+    selectTab() {
+        let cards = document.querySelectorAll('.card');
+            cards.forEach(element => {
+            element.style.display = 'none';
+        })
+        this.card.style.display = 'flex';    
+        const tabs = document.querySelectorAll('.tab');;
+            tabs.forEach(tab => {
+            tab.style.backgroundColor = '';
+            tab.style.color = '';
+            })
+        this.element.style.backgroundColor = '#5E9FB9';
+        this.element.style.color = 'black';
+    }
 
-// }
+}
 
-// let tabs = document.querySelectorAll('.tab');
-// tabs = Array.from(tabs).map(element => {
-//     return new TabLink(element)
-// })
+let tabs = document.querySelectorAll('.tab');
+tabs = Array.from(tabs).map(element => {
+    return new TabLink(element)
+})
 
-// tabs[0].selectTab();
+tabs[0].selectTab();
