2ab4bfcc04f2c9cb256d2921c070f2346b4ab078
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-09-13 11:54:56 -0400

    fix the bug where the hamburger button would not change when nav was opened

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,12 @@
 const toggleMenu = (e) => {
-    menu.classList.toggle('dropdown-hidden')
+        menu.classList.toggle('dropdown-hidden');
+        let menuAttr = menuBtn.getAttribute('src');
+        if (menuAttr === "/img/nav-hamburger.png") {
+            menuBtn.setAttribute('src', '/img/nav-hamburger-close.png');
+            console.log(menuAttr);
+    } else if (menuAttr === "/img/nav-hamburger-close.png") {
+        menuBtn.setAttribute('src', '/img/nav-hamburger.png');
+    }  
 }
 
 let menu = document.querySelector('.dropdown-content');
