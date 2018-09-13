614bd5c655ea004a19efed20cec65a383a488d12
Kendra Williams <kmonique.williams@gmail.com>
2018-09-12 23:23:02 -0400

    navigation expanded completed

--- js/index.js
+++ js/index.js
@@ -1,8 +1,37 @@
-// JS goes here
 /*on hamburger click event
 change hamburger logo to close "x"
-display the nav at font-size 48px, color FFFFFF
-line height 85px, hover color #222222, height = screen height
+change height of header to 100%
+toggle the .hidden class
 */
-const hamburger = document.querySelector("img");
-console.log(hamburger);
+class Dropdown{
+    constructor(el){
+        this.el = el;
+
+        this.button = this.el.querySelector("img");
+        this.navMenu = this.el.querySelector("nav");
+
+        console.log(this.el);
+        console.log(this.button);
+        console.log(this.navMenu);
+
+        this.button.addEventListener("click", () => {
+            this.toggleNav();
+        })
+    }
+    toggleNav(){
+        this.navMenu.classList.toggle("hidden");
+        if(this.navMenu.classList.length > 1) {
+            this.el.style.height = "50px";
+            this.button.src = "img/nav-hamburger.png";
+        } else {
+            this.el.style.height = "100%";
+            this.button.src = "img/nav-hamburger-close.png";
+            console.log(this.button);
+        }
+        console.log("I was clicked!");
+    }
+}
+
+//find the .header from the DOM and create a new dropdown class
+const dropdown = document.querySelector(".header");
+new Dropdown(dropdown);
