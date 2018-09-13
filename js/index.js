17de59e18ccb27479a09217e27124c3aca207909
Dewayne87 <dewaynelindsay87@gmail.com>
2018-09-12 19:24:27 -0700

    nav bar opens and closes with click

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
 // JS goes here
-const button = document.querySelector('.ham');
+const ham = document.querySelector('.ham');
+const close = document.querySelector('.close-btn');
+const navigation = document.querySelector('.nav-bar');
+const links = document.querySelector('.navigation')
+console.log(close)
+ham.addEventListener('click', ()=>{
+    navigation.style.height = '100%';
+    close.style.display='block';
+    ham.style.display='none';
+    links.style.display = 'block'
+})
+close.addEventListener('click', () => {
+    navigation.style.height = '';
+    close.style.display = '';
+    ham.style.display = '';
+    links.style.display = ''
+})
