e0f892ce503b1c4a187ddb1c1a80065b3515a6e2
TheGameFreak720 <luis.alvarez@commithub.com>
2018-09-12 22:54:56 -0400

    navigation system is done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-// JS goes here
+const navButton = document.querySelector('.menu');
+const anchors = document.querySelectorAll('a');
+
+let hamburgerIsClosed = true;
+
+anchors.forEach(a => {
+    a.style.display = 'none';
+});
+
+const hamburgerHandler = function() {
+    if (hamburgerIsClosed) {
+        document.getElementById('hamburger').src = '../img/nav-hamburger-close.png';
+
+        anchors.forEach(a => {
+            a.style.display = '';
+        });
+
+        hamburgerIsClosed = false;
+    } else {
+        document.getElementById('hamburger').src = '../img/nav-hamburger.png';
+
+        anchors.forEach(a => {
+            a.style.display = 'none';
+        });
+        
+        hamburgerIsClosed = true;
+    }
+};
+
+navButton.addEventListener('click', hamburgerHandler);
