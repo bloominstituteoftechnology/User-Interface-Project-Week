4112995cf98afc55f0f9d626339b633ba0994b1f
Mitchell Robles <mitchellr92@live.com>
2018-09-12 20:30:09 -0700

    added some functionality to the dropdown menu, need to fix some styling issues

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // JS goes here
+
+const navImg = document.querySelector('nav img');
+const navExpand = document.querySelector('.nav-expand');
+const navClose = document.querySelector('.nav-expand img');
+
+navImg.addEventListener('click', () => {
+    navExpand.style.display = 'flex';
+    navExpand.style.position = 'absolulte';
+    navImg.style.display = 'none';
+})
+
+navClose.addEventListener('click', () => {
+    navExpand.style.display = 'none';
+    navImg.style.display = 'block';
+})
+
+console.log(navImg);
