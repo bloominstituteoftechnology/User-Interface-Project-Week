6aa6809023af47995bd803614b1a9b33a760f356
Abigayle Franklin <abifranklin@gmail.com>
2018-09-13 15:43:32 -0500

    Navigation extend JS working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// JS goes here
+document.addEventListener("DOMContentLoaded", function(event) {
+    let menuIcon = document.querySelector('.menu__icon');
+    let extendedNav = document.querySelector('.menu__expanded');
+    extendedNav.style.display = "none";
+
+
+
+
+    menuIcon.addEventListener('click', () => {
+        if (extendedNav.style.display === "none") {
+            extendedNav.style.display = "block";
+            menuIcon.src = "img/nav-hamburger-close.png";
+        } else {
+            extendedNav.style.display = "none";
+            menuIcon.src = "img/nav-hamburger.png";
+            console.log('hi');
+        }
+    })
+ 
+
+
+
+
+
+
+})
