41f9512af359edaad469d6326f290b451f0a3721
Obed Lorisson <Obed.lorisson@gmail.com>
2018-09-12 23:52:26 -0400

    add javascript for Menu expanded view

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // JS goes here
+/************Expanded Menu********** */
+class Menu{
+  constructor(element){
+    this.element = element;
+    this.button = document.querySelector('header img');
+    this.button.addEventListener('click', () =>{this.expand()});
+  }
+
+  expand(){
+   this.element.classList.toggle('show'); 
+  
+  }
+}
+
+let nav = document.querySelector('nav')
+nav = new Menu(nav)
