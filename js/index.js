229d7526c5af5d11a7a882d5a5286c491ec7c3e0
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-09-13 15:15:42 -0400

    Started writing components/adding event listeners.

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@
 
 // Home Page JS.
 
-class Header{
-    constructor(header){
-        this.element = header ;
+class Header {
+    constructor(header) {
+        this.element = header;
         // this.item = document.querySelectorAll('.header');
         this.element.style.fontSize = '2.8rem';
         this.element.style.fontWeight = 'Bold';
@@ -13,14 +13,35 @@ class Header{
     }
 }
 
+let navMenu = document.querySelector('.navMenu');
+console.log(navMenu)
+navMenu.addEventListener('click', () => {
+    console.log('got it')
+
+})
 
 // Services Page JS
 let headers = document.querySelectorAll('.header');
 headers = Array.from(headers).map(header => new Header(header))
-console.log(headers);
+// console.log(headers);
 
 
 let dataTabs = document.querySelectorAll(`.textStyling[data-tab]`)
 let buttonData = document.querySelectorAll(`button[data-tab]`)
-console.log(buttonData);
-console.log(dataTabs);
+
+
+class Button {
+    constructor(button) {
+        this.element = button;
+        this.element.addEventListener('click', () => {
+                console.log('got it');
+
+
+            })
+        }
+    }
+    let serviceButtons = Array.from(document.querySelectorAll('.serviceBtn'));
+    serviceButtons.map(button => new Button(button));
+    console.log(serviceButtons);
+    // console.log(buttonData);
+    // console.log(dataTabs);
