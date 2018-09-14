d98b3c4d3eb723016075a40d251937897d8f51b4
Willie Yturralde <willieino@hotmail.com>
2018-09-13 17:01:20 -0700

    completed contact page

--- js/contact.js
+++ js/contact.js
@@ -5,12 +5,15 @@ function contact() {
 
     const menuButton = document.querySelector(".menu-button");
     menuButton.addEventListener("click", clickMenu);
+    
+    const formInput2 = document.querySelector(".input2");
+    formInput2.addEventListener("click", inputFocus2);
+    
     let menu = document.querySelector(".nav");
+    
     const formInput = document.querySelector(".input");
-    formInput.addEventListener("focus", inputFocus);
-    formInput.addEventListener("keyup", inputEnter);
-  
-  
+    formInput.addEventListener("click", inputFocus);
+      
     function clickMenu() {
       if (menuOpen === true) {
         menu.classList.remove("nav-close");
@@ -21,10 +24,7 @@ function contact() {
         menu.classList.add("nav-close");
         menuOpen = true;
       }
-  
-  
     }
-
 
     function inputFocus() {
         // clears the input for the user to type their email
@@ -32,18 +32,14 @@ function contact() {
           formInput.value = "";
         }
       }
-    
-      function inputEnter() {
-        //check for bad input???? Like not an real email address...
-        let theKey = formInput.value;
-        console.log("the key is: " + theKey);
-        if (theKey === "enter") {
-          alert("Thank you for submitting your email. We will be contacting you shortly.");
-          //submit the page
-        };
+
+      function inputFocus2() {
+        // clears the input for the user to type their email
+        if (formInput2.value === "Enter Email:") {
+          formInput2.value = "";
+        }
       }
-
-
+      
 }
 
 contact();
