57dc6fbf248ec2498dd54bb3ea0e20b32dc2ace1
vy3191 <venkyafhs@gmail.com>
2018-09-14 15:07:45 -0400

    Added active class to overlay anchor elements

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // JS goes here
-
+// Over lay -- Desktop & Mobile
 const hamburgerButton = document.querySelector('.menu-button-open');
 const closeButton = document.querySelector('.menu-button-close');
 const menu = document.querySelector('.menu');
@@ -18,5 +18,26 @@ closeButton.addEventListener('click', function(event){
 	    hamburgerButton.classList.remove('button-close');
 	    target.classList.remove('button-open');
 	    menu.classList.remove("menu-open");
+});
 
-})
+// Adding .active class to the anchor elements using - components
+
+class Anchors {
+	constructor(el) {
+		this.el = el;
+		this.el.addEventListener('click', () => {this.clickAnchor()})
+	}
+	clickAnchor() {
+		// const links = menu.querySelectorAll('a');
+		// links.forEach(function(element){
+		// 	element.classList.remove('active');
+		// });
+		this.el.classList.add('active');
+		console.log('working');
+	}
+}
+
+let anchors = menu.querySelectorAll('a');
+
+anchors = Array.from(anchors).map(anchor =>{ return new Anchors(anchor)});
+console.log(anchors);
