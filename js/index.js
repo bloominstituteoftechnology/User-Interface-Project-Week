64dd9de71edbe0c0f49d2f2917c0d13daae79b3c
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2018-09-13 22:14:27 -0500

    Finished less and html for expanded nav finished javascript for nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // JS goes here
+class NavItem {
+    constructor(element) {
+        this.element = element;        
+        this.open = this.element.querySelector('.hamburger-button');
+        this.close =this.element.querySelector('.hamburger-close');
+        this.content = this.element.querySelector('.nav-content');
+        this.open.addEventListener('click', () => {this.dropdown('open');
+        this.open.classList.add('hidden');
+        this.close.classList.remove('hidden');
+            }
+        );
+        this.close.addEventListener('click', () => {this.dropdown('close');
+        this.close.classList.add('hidden');
+        this.open.classList.remove('hidden');
+            }
+        );      
+    }  
+    dropdown(task) {
+    task === 'open' ? this.content.classList.remove('hidden') : this.content.classList.add('hidden');
+    }
+}
+
+let items = document.querySelectorAll('nav');
+items = Array.from(items).map(item => new NavItem(item));
