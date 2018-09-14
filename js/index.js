ee37ecbec4ac228f467ed6625391856929f40453
BrandonMoll <brandonmoll85@gmail.com>
2018-09-13 22:18:05 -0500

    finished services page JS component

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.hamburger');
 const header = document.querySelector('.header');
 const links = document.querySelector('.links');
 const close = document.querySelector('.close');
+let buttons = document.querySelectorAll('.button');
 
 const expandMenu = (event) => {
     header.classList.toggle('expand');
@@ -23,3 +24,60 @@ const expandMenu = (event) => {
 
 hamburger.addEventListener('click', expandMenu);
 close.addEventListener('click', expandMenu);
+
+
+//services page component
+class Tab {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.itemElement = document.querySelector(`.card[data-tab="${this.data}"]`);
+        this.cardItem = new Card(this.itemElement);
+
+        this.element.addEventListener('click', () => {this.deselect()});
+        this.element.addEventListener('click', () => {this.select()});
+
+    };
+
+    select() {
+        const buttons = document.querySelectorAll('.button');
+        this.element.classList.add('buttonSelected');
+        this.cardItem.select();
+    };
+
+    deselect() {
+        let lastClicked = document.querySelector('.buttonSelected')
+        lastClicked.classList.remove('buttonSelected');
+    };
+
+}
+
+class Card {
+    constructor(element) {
+        this.element = element;
+    };
+
+    select() {
+    const items = document.querySelectorAll('.card');
+
+    Array.from(items).forEach((item) => {
+        item.classList.remove('cardSelected');
+    });
+
+    this.element.classList.add('cardSelected');
+
+    };
+}
+
+
+
+
+
+
+
+buttons = Array.from(buttons).map((link) => {
+    return new Tab(link);
+});
+console.log(buttons);
+buttons[0].select();
+
