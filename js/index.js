700d28732961cf47e3507afb02bc83f0ee4a2062
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2018-09-14 00:50:25 -0400

    Desktop and Mobile design for homepage completed. Working on navigation functionality. If i cannot get it to work within a couple hours, will move on to services and contact page and come back to navication.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+
+
+let menuOpenButton = document.getElementsByClassName("openOpenButton");
+console.log(menuOpenButton[0]);
+
+let menuArray = Array.from(menuOpenButton[0]);
+console.log(menuArray);
+
+
+function toggleMenuOpenButton () {
+ menuOpenButton[0].classList.toggle = "openMenu";
+}
+
+toggleMenuOpenButton();
