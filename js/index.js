a52455cbedca176c2412f87fed864c338feebf99
BrandonMoll <brandonmoll85@gmail.com>
2018-09-13 20:39:19 -0500

    added nav bar expansion styling i forgot, made links functional, and made it work with services page too

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// JS goes here
+const hamburger = document.querySelector('.hamburger');
+const header = document.querySelector('.header');
+const links = document.querySelector('.links');
+
+const expandMenu = (event) => {
+    header.classList.toggle('expand');
+    if (links.style.display === 'block') {
+    links.style.display = 'none';
+    } else {
+        links.style.display = 'block';
+    }
+}
+
+hamburger.addEventListener('click', expandMenu);
