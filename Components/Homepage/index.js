0dd39a71dd9dbe757f6bef2cd70edc28ed1b02b3
Jess <jessdev83@gmail.com>
2018-09-14 19:24:10 -0400

    Mobile Images
    Added javascript and styling to replace images with mobile image files.

--- Components/Homepage/index.js
+++ Components/Homepage/index.js
@@ -1,3 +1,7 @@
+const topImages = document.querySelectorAll('.top-section-img img');
+const villas = document.querySelector('#villas img');
+const outskirts = document.querySelector('#outskirts img');
+const blocks = document.querySelector('#blocks img');
 const fadeHTML = function() {
     let elements;
     let windowHeight;
@@ -27,4 +31,23 @@ const fadeHTML = function() {
     }
 }
 
+function checkWidth() {
+    if (window.innerWidth > 450) {
+        topImages[0].setAttribute('src', 'img/home/home-img-1.png');
+        topImages[1].setAttribute('src', 'img/home/home-img-2.png');
+        villas.setAttribute('src', 'img/home/home-villas-img.png');
+        outskirts.setAttribute('src', 'img/home/home-outskirts-img.png');
+        blocks.setAttribute('src', 'img/home/home-the-blocks-img.png');
+    } else if (window.innerWidth <= 450) {
+        topImages[0].setAttribute('src', 'img/home/home-mobile-img-1.png');
+        topImages[1].setAttribute('src', 'img/home/home-mobile-img-2.png');
+        villas.setAttribute('src', 'img/home/home-mobile-villas-img.png');
+        outskirts.setAttribute('src', 'img/home/home-mobile-outskirts-img.png');
+        blocks.setAttribute('src', 'img/home/home-mobile-the-blocks-img.png');
+    }
+}
+
+window.addEventListener('resize', checkWidth);
+
+checkWidth();
 fadeHTML().init();
--- Components/ServicesPage/services.js
+++ Components/ServicesPage/services.js
@@ -36,4 +36,25 @@ let tabs = document.querySelectorAll('.tab');
 tabs = Array.from(tabs).map((tab) => {
     return new TabLink(tab);
 })
+
+//mobile images
+const cardImages = document.querySelectorAll('.card-img img');
+
+function checkWidth() {
+    if (window.innerWidth > 450) {
+        cardImages[0].setAttribute('src', 'img/services/services-tab-pre-construction-img.png');
+        cardImages[1].setAttribute('src', 'img/services/services-tab-construction-img.png');
+        cardImages[2].setAttribute('src', 'img/services/services-tab-design-build-img.png');
+        cardImages[3].setAttribute('src', 'img/services/services-tab-sustainability-img.png');
+    } else if (window.innerWidth <= 450) {
+        cardImages[0].setAttribute('src', 'img/services/services-tab-mobile-pre-construction-img.png');
+        cardImages[1].setAttribute('src', 'img/services/services-tab-mobile-construction-img.png');
+        cardImages[2].setAttribute('src', 'img/services/services-tab-mobile-design-build-img.png');
+        cardImages[3].setAttribute('src', 'img/services/services-tab-mobile-sustainability-img.png');
+    }
+}
+
+window.addEventListener('resize', checkWidth);
+
+checkWidth();
 tabs[0].selectTab()
