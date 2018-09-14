b9ed392126d3d70e5d1e793af2d88e6efca73365
BrandonMoll <brandonmoll85@gmail.com>
2018-09-13 20:46:02 -0500

    added closing X to nav bar expansion

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
 const hamburger = document.querySelector('.hamburger');
 const header = document.querySelector('.header');
 const links = document.querySelector('.links');
+const close = document.querySelector('.close');
 
 const expandMenu = (event) => {
     header.classList.toggle('expand');
+
     if (links.style.display === 'block') {
     links.style.display = 'none';
     } else {
         links.style.display = 'block';
     }
+
+    if (close.style.display === 'block') {
+        close.style.display = 'none';
+        hamburger.style.display = 'block';
+    } else {
+        close.style.display = 'block';
+        hamburger.style.display = 'none';
+    }
 }
 
 hamburger.addEventListener('click', expandMenu);
+close.addEventListener('click', expandMenu);
