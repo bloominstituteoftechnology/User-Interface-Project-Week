631cca8a8a193142721bcabe2a0b25ff0fe85fb3
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-09-14 02:34:31 -0400

    updates to component

--- js/index.js
+++ js/index.js
@@ -32,18 +32,26 @@ class Button {
     constructor(element) {
         this.element = element;
         this.number = this.element.dataset.button;
-        this.imageElement = document.querySelector(`.image[data-button = "${this.number}"]`);
+        this.imageElement = document.querySelector(
+            `.image[data-button = "${this.number}"]`
+        );
         this.imageClass = new Image(this.imageElement);
         this.element.addEventListener('click', () => {
-            this.clickHandler()
-        })
+            this.clickHandler();
+        });
     }
 
     clickHandler() {
+        let buttons = document.querySelectorAll('.servicebutton');
+        buttons = Array.from(buttons);
+        buttons.forEach(function (item) {
+            item.classList.remove('currentbutton');
+        });
+
+        this.element.classList.add('currentbutton')
         this.imageClass.imageToggle();
     }
 }
-
 
 class Image {
     constructor(element) {
@@ -51,19 +59,19 @@ class Image {
     }
 
     imageToggle() {
-        let images = document.querySelectorAll('.image')
+        let images = document.querySelectorAll('.image');
         images.forEach(function (image) {
-            image.classList.remove('image-selected')
-        })
-        this.element.classList.toggle('image-selected')
+            image.classList.remove('image-selected');
+        });
+        this.element.classList.toggle('image-selected');
     }
-
 }
 
 let buttons = document.querySelectorAll('.servicebutton');
-let buttonsArray = Array.from(buttons).map((button) => {
-    return new Button(button)
+let buttonsArray = Array.from(buttons).map(button => {
+    return new Button(button);
 });
-let images = document.querySelectorAll('.image')
+let images = document.querySelectorAll('.image');
 images = Array.from(images);
+
 buttonsArray[0].clickHandler();
