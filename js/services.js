15ef06146676b5d84dad870ee2796e3dc88999e6
Andres Rivera <andres.rivera.toro@gmail.com>
2018-09-14 04:57:45 -0400

    refactored HTML/LESS, services tab nav functional

---
+++ js/services.js
@@ -0,0 +1,34 @@
+class ServicesTab {
+  constructor(el) {
+    this.el = el;
+    this.data = this.el.dataset.tab;
+    this.tabElement = document.querySelector(`.services-tab-content[data-tab="${this.data}"]`);
+    this.tabContent = new ServicesTabContent(this.tabElement);
+    this.el.addEventListener('click', () => this.select());
+    this.el.addEventListener('mousedown', () => event.preventDefault());
+  }
+
+  select() {
+    const tabs = document.querySelectorAll('.services-tab');
+    Array.from(tabs).forEach(el => el.classList.remove('tab-selected'));
+    this.el.classList.add('tab-selected');
+    this.tabContent.select();
+  }
+}
+
+class ServicesTabContent {
+  constructor(el) {
+    this.el = el;
+  }
+
+  select() {
+    const content = document.querySelectorAll('.services-tab-content');
+    Array.from(content).forEach(el => el.classList.remove('content-selected'));
+    this.el.classList.add('content-selected');
+  }
+}
+
+let tabs = document.querySelectorAll('.services-tab');
+tabs = Array.from(tabs).map(el => new ServicesTab(el));
+
+tabs[0].select();
