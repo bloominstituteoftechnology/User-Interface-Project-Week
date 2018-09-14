d6233702222a90d533b8eea237ec3f0081326340
Jason Hedrick <jasonleehedrick@gmail.com>
2018-09-13 20:05:09 -0700

    Wrapped up services.html desktop design and the tab elements.

--- js/index.js
+++ js/index.js
@@ -23,8 +23,64 @@ class Hamburger {
     }
 }
 
+class TabController {
+    constructor(element) {
+        this.links = document.querySelectorAll('.tab');
+        this.links = Array.from(this.links).map(link => {
+            link = new Tab(link);
+            return link;
+        });
+
+        this.links[0].select();
+        this.selected = this.links[0];
+    }
+}
+
+class Tab {
+    constructor(element, controller) {
+        this.element = element;
+        this.controller = controller;
+
+        this.data = element.dataset.tab;
+        this.card = document.querySelector(`.card[data-tab='${this.data}']`);
+        this.card = new Card(this.card);
+
+        this.element.addEventListener('click', () => { this.select(); });
+    }
+
+    select() {
+        this.deselect();
+        this.element.classList.add('tab-selected');
+        this.card.select();
+    }
+
+    deselect() {
+        const tabs = document.querySelectorAll('.tab');
+        tabs.forEach(tab => {
+            tab.classList.remove('tab-selected');
+        });
+    }
+}
+
+class Card {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select() {
+        this.deselect();
+        this.element.classList.add('card-visible');
+    }
+
+    deselect() {
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(card => {
+            card.classList.remove('card-visible');
+        })
+    }
+}
 
 let hamburger = document.querySelector('.hamburger');
 hamburger = new Hamburger(hamburger);
 
-console.log(hamburger);
+const tabController = new TabController();
