1a4398be691a00f676a6450b59137c90b3185885
marcusbass323 <marcus@bassconsultantgroup.com>
2018-09-14 07:08:18 -0700

    Desktop overlay needs some tweaking

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // JS goes here
+function toggleOverlay(){
+	var overlay = document.getElementById('overlay');
+	var specialBox = document.getElementById('specialBox');
+	overlay.style.opacity = .8;
+	if(overlay.style.display == "block"){
+		overlay.style.display = "none";
+		specialBox.style.display = "none";
+	} else {
+		overlay.style.display = "block";
+		specialBox.style.display = "block";
+	}
+}
