e6696bb6a7b3095add32944823ff997378637fef
Joseph Thompson <joe.thompson838@gmail.com>
2018-09-13 23:16:10 -0400

    fixed a bunch of css issues and setup mobile view for projects and about

--- js/index.js
+++ js/index.js
@@ -1,21 +1,29 @@
 // JS goes here
+const body = document.querySelector("body");
 
 // check for body to load and then fade in
 window.addEventListener("load", () => {
-const body = document.querySelector("body");
-
   TweenMax.fromTo(body, 3, {opacity:0},{opacity:1});
 });
 
+// check for navbar link clicks to change body opacity
+let navbar = document.querySelector('.navbar');
+
+let navbarLinks = navbar.querySelectorAll('a');
+
+Array.from(navbarLinks).map((link) => {
+  link.addEventListener('click', () => {
+    TweenMax.fromTo(body, 3, {opacity:1},{opacity:0});
+  });
+});
 // replace images if screen width is below 500px
 
 const switchImages = (id, src) => {
-  
+
   id = document.getElementById(id);
 
   screen.width < 501 ? id.setAttribute('src', src) : true;
 }
-
 // target the fa-bars
 const bars = document.querySelector('.fa-bars');
 
@@ -41,11 +49,11 @@ close.addEventListener('click', () => {
 
   // get navbar and hide it
   let nav = document.querySelector('.navbar');
-  
-  
+
+
   // add tweenmax to slowly bring down nav
   TweenMax.to(nav,0.5,{opacity: 0});
-  
+
   // hide close
   close.style.display = 'none';
 
