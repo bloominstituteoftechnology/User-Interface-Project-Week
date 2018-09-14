44db2d1679cb4776a68e48a57a306c5160a512c6
vy3191 <venkyafhs@gmail.com>
2018-09-14 14:38:23 -0400

    completed overlay functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // JS goes here
+
+const hamburgerButton = document.querySelector('.menu-button-open');
+const closeButton = document.querySelector('.menu-button-close');
+const menu = document.querySelector('.menu');
+
+hamburgerButton.addEventListener('click', function(event){
+	     let target = event.target
+	     target.classList.add("button-close");
+	     closeButton.classList.add('button-open');
+         menu.classList.add("menu-open");
+          event.stopPropagation();
+
+        
+});
+closeButton.addEventListener('click', function(event){
+	    let target = event.target;
+	    hamburgerButton.classList.remove('button-close');
+	    target.classList.remove('button-open');
+	    menu.classList.remove("menu-open");
+
+})
