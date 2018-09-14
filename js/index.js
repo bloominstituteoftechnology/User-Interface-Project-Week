1350b80cd6664b89f1e32ea358377da1a6118c61
Robert Billington <chillingtonbillington@gmail.com>
2018-09-14 10:53:20 -0500

    updated js file

--- js/index.js
+++ js/index.js
@@ -6,14 +6,13 @@ class TabLink{
         //gathering the corresponding data-set 
         this.data = element.dataset.tab;
         //linking the tab-img/item with the tab that has a corresponding dat-set attribute
-        this.itemElement = document.querySelector(`.tab-content-section[data-set = "${this.data}"]`);
+        this.itemElement = document.querySelector(`.tab-content-container[data-tab = "${this.data}"]`);
         //creating new class item for each tab-img selected
-        this.tabItem = new tabItem(this.itemElement);
+        this.tabItem = new TabItem(this.itemElement);
         //adding event listener which will call the select function wihen eachtab link is clicked
-        this.element.addEventListener('click', () => {this.select()});//still unsure why there are no curly brackets being used here
-
+        this.element.addEventListener('click', () => this.select());
     }
-    selct(){
+    select(){
         const links = document.querySelectorAll('.tab-link');
         //creating an array of tab links --removing the tab-selected styling form each
         Array.from(links).forEach(link => {
@@ -31,11 +30,11 @@ class TabItem{
         this.element = element;
     }
     select(){
-        let items = document.querySelectorAll('tab-img');
+        let items = document.querySelectorAll('.tab-content-container');
         Array.from(items).forEach(item => {
-            item.classList.remove('tab-content-section');
+            item.classList.remove('tab-content-selected');
         });
-        this.element.classList.add('tab-content-section');
+        this.element.classList.add('tab-content-selected');
         
     }
 }
@@ -43,13 +42,9 @@ class TabItem{
 
 
 let links = document.querySelectorAll('.tab-link');
-links = Array.from(links).map(link =>{
-    new TabLink(link);
-})
+links = Array.from(links).map(link =>
+    new TabLink(link));
 
-function openNavBar(){
-    document.querySelector('.openNav').style.height = "100%";
-}
-function closeNavBar(){
-    document.querySelector('openNav').style.height = "0%";
-}
+
+
+links[0].select();
