d86b4baef99bfafd3bb992f06e9f9a9cb59ef183
Mitchell Robles <mitchellr92@live.com>
2018-09-13 20:03:09 -0700

    Gave functionality to the tabs on the services page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // JS goes here
 
+// Event listeners for the nav bar
+
 const navImg = document.querySelector('nav img');
 const navExpand = document.querySelector('.nav-expand');
 const navClose = document.querySelector('.nav-expand img');
@@ -15,4 +17,54 @@ navClose.addEventListener('click', () => {
     navImg.style.display = 'block';
 })
 
-console.log(navImg);
+// Event listners for the services tabs
+
+class Tab {
+    constructor(element) {
+        this.element = element;
+
+        this.data = this.element.dataset.tab;
+
+        this.tabText = document.querySelector(`.tabs-text[data-tab="${this.data}"`);
+
+        this.tabText = new TabInfo(this.tabText);
+
+        this.element.addEventListener('click', () => this.selectTab());
+    }
+
+    selectTab() {
+        const tabs = document.querySelectorAll('.tab-button');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('active-tab');
+        });
+
+        this.element.classList.toggle('active-tab');
+        console.log(this.element);
+        const tabInfo = document.querySelectorAll('.tabs-text');
+
+        tabInfo.forEach(tab => {
+            tab.style.display = 'none';
+        });
+
+        this.tabText.selectInfo();
+    }
+}
+console.log(Tab);
+
+class TabInfo {
+    constructor(element) {
+        this.element = element;
+    }
+
+    selectInfo() {
+        this.element.style.display = null;
+    }
+    
+}
+
+let tabButton = document.querySelectorAll('.tab-button');
+
+tabButton = Array.from(tabButton).map(tab => new Tab(tab));
+
+tabButton[0].selectTab();
