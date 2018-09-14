c8378fe8c1dc6417a18b5f9c694f12c82811d88a
Joseph Thompson <joe.thompson838@gmail.com>
2018-09-14 00:23:26 -0400

    playing with greensock and shifting images into view on scroll

--- js/home.js
+++ js/home.js
@@ -5,3 +5,17 @@ switchImages("homeImgOne", './img/home/home-mobile-img-1.png');
 switchImages("homeImgTwo", './img/home/home-mobile-img-2.png');
 switchImages("villas", './img/home/home-mobile-villas-img.png');
 switchImages("outskirts", './img/home/home-mobile-outskirts-img.png');
+
+// get all large sections and move them out of the screen view
+let largeSections = document.querySelectorAll('.large-section section');
+largeSections[0].style.position = 'relative';
+
+document.addEventListener('scroll', (e) => {
+  if (largeSections[0].offsetTop - 500 > e.pageY) {
+    TweenMax.fromTo(largeSections[0], 1, {right: '-9999px'}, {right: 0})
+
+    if (largeSections[0].style.right === 0 ) {
+      return true;
+    }
+  }
+});
