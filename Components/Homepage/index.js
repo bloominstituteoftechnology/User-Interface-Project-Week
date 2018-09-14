094a7273df7ef08c01b6ee33c272217b0d6dd8e6
Jess <jessdev83@gmail.com>
2018-09-13 22:58:45 -0400

    Scroll animation
    Elements fade-in on scroll on home page.

--- Components/Homepage/index.js
+++ Components/Homepage/index.js
@@ -1,2 +1,30 @@
+const fadeHTML = function() {
+    let elements;
+    let windowHeight;
 
+    function init() {
+        elements = document.querySelectorAll('.hidden');
+        windowHeight = window.innerHeight;
+        scrollHandlers();
+        checkPosition();
+    }
 
+    function scrollHandlers() {
+        window.addEventListener('scroll', checkPosition);
+        window.addEventListener('resize', init);
+    }
+
+    function checkPosition() {
+        for (let i = 0; i < elements.length; i++) {
+            let positionTop = elements[i].getBoundingClientRect().top;
+            if (positionTop - windowHeight <= 0) {
+                elements[i].className = elements[i].className.replace('hidden', 'fade-in');
+            }
+        }
+    }
+    return {
+        init: init
+    }
+}
+
+fadeHTML().init();
