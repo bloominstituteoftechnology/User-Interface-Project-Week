1994c973f38bdf86c5230a765a59ba969487a7d8
marcusbass323 <marcus@bassconsultantgroup.com>
2018-09-14 07:39:16 -0700

    fixed overlay

--- js/index.js
+++ js/index.js
@@ -2,12 +2,14 @@
 function toggleOverlay(){
 	var overlay = document.getElementById('overlay');
 	var specialBox = document.getElementById('specialBox');
-	overlay.style.opacity = .8;
+
 	if(overlay.style.display == "block"){
 		overlay.style.display = "none";
-		specialBox.style.display = "none";
+        specialBox.style.display = "none"; 
+        content.style.display = "block"
 	} else {
 		overlay.style.display = "block";
-		specialBox.style.display = "block";
+        specialBox.style.display = "block";
+        content.style.display = "none"
 	}
 }
