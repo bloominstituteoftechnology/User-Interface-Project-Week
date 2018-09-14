a2a0bd85122696b2937534125323e8803ccce636
vy3191 <venkyafhs@gmail.com>
2018-09-14 17:09:09 -0400

    Service page navigator completed

--- js/index.js
+++ js/index.js
@@ -21,23 +21,42 @@ closeButton.addEventListener('click', function(event){
 });
 
 // Adding .active class to the anchor elements using - components
-
-class Anchors {
-	constructor(el) {
-		this.el = el;
-		this.el.addEventListener('click', () => {this.clickAnchor()})
+class Anchorlink {
+	constructor(element) {
+		this.element = element;
+		this.tabNumber = this.element.dataset.tab;
+		this.tabElement = document.querySelector(`.tab[data-tab="${this.tabNumber}"]`);
+		this.tab = new Tab(this.tabElement);
+		this.element.addEventListener('click', () => {this.clickAnchor()});
+		console.log(this.tabNumber);
 	}
+
 	clickAnchor() {
-		// const links = menu.querySelectorAll('a');
-		// links.forEach(function(element){
-		// 	element.classList.remove('active');
-		// });
-		this.el.classList.add('active');
-		console.log('working');
+		const links = document.querySelectorAll('.button');
+		links.forEach(function(link){
+			return link.classList.remove('selected');
+		});
+		this.element.classList.add('selected');
+		this.tab.clickTabs();
 	}
 }
 
-let anchors = menu.querySelectorAll('a');
+class Tab {
+	constructor(el) {
+		this.el = el;
+	}
+	clickTabs() {
+        const tabs = document.querySelectorAll('.tab');
+        tabs.forEach(function(element){
+        	 return element.style.display = 'none';
+        })
 
-anchors = Array.from(anchors).map(anchor =>{ return new Anchors(anchor)});
-console.log(anchors);
+        this.el.style.display = 'block';
+	}
+}
+
+const serviceButtons = document.querySelector('.services-buttons');
+let links = serviceButtons.querySelectorAll("a");
+
+links = Array.from(links).map(link =>new Anchorlink(link));
+console.log(links);
