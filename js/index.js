e83e0aee7ddffd6451374b548ed288a917e61a18
unknown <jerrardsmith215@gmail.com>
2018-09-15 01:38:48 -0400

    UI Project-I Jerrard Smith updated

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // JS goes here
+
+/* Nav bar expansion code
+
+let navImg = document.getElementById("NavImg");
+
+function MenuClickHandler (item) {
+
+    console.log(item.src);
+if (item.src=='/img/nav-hamburger.png')
+    {
+        item.setAttribute('src', '/img/nav-hamburger-close');
+    }
+}
+
+navImg.addEventListener('click', MenuClickHandler(event));
+ */
