fc6edf17d924fdd7e70bbaa2ec430b1a7e2890b4
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2018-09-15 07:20:51 -0500

    Added carousel mostly working buttons overlaping nav bar bug

--- js/services.js
+++ js/services.js
@@ -24,6 +24,40 @@ class NavItem {
 let items = document.querySelectorAll('nav');
 items = Array.from(items).map(item => new NavItem(item));
 
+//SERVICES
+
+class TabLink {
+    constructor(element) {
+    this.element = element;
+    this.data = this.element.dataset.tab;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+    this.tabItem = new TabItem(this.itemElement);
+    this.element.addEventListener('click', () => {this.select()});
+    };
+
+    select() {
+    const links = document.querySelectorAll('.tabs-link');
+    links.forEach(item => item.classList.remove('selected'));
+    this.element.classList.add('selected');
+    this.tabItem.select();
+    }
+}
+
+class TabItem {
+    constructor(element) {
+        this.element = element;
+    }
+    select() {
+        const tabs = document.querySelectorAll('.tabs-item');
+        tabs.forEach(item => item.style.display = 'none');
+        this.element.style.display = 'block';
+    }
+}
+
+let links = document.querySelectorAll('.tabs-link');
+links = Array.from(links).map(element => new TabLink(element));
+links[0].select();
+
 //Carousel
 
 class Carousel {
