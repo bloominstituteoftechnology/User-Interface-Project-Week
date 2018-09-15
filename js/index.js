fecf6fdfd8a6510925ba63717101ba3e4bad2b90
Witanday <witanday.cd@gmail.com>
2018-09-14 23:32:31 -0400

    Navigation system design Expand and non expand view Completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // JS goes here
+
+const btnHambourger = document.querySelector('.nav-top--btn'),
+       navMenu = document.querySelector('.nav ul');
+       
+
+
+btnHambourger.addEventListener('click', ()=>{
+        this.event.target.classList.toggle('close');
+        this.event.target.classList.toggle('open');
+        navMenu.classList.toggle('nav--links__open');
+        
+    }
+
+
+)
