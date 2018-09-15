64f4a2dbb1ca8056af3ee7adab3a80278558f74a
stevekutz <stkutz@gmail.com>
2018-09-15 16:01:15 -0500

    built JS for menu navigation

--- js/index.js
+++ js/index.js
@@ -1,2 +1,29 @@
 // JS goes here
+class DropDown{
+  constructor(element) {
+    this.element = element;
 
+    this.menuOpen = this.element.querySelector('.openMe');
+    this.menuClose = this.element.querySelector('.closeMe');
+
+    this.menuItems = this.element.querySelector('.dropdown-content');
+
+    this.menuOpen.addEventListener('click', () => this.expandMenu());
+    this.menuClose.addEventListener('click', () => this.closeMenu());
+  }
+
+  expandMenu(){
+    this.menuOpen.style.display = 'none';
+    this.menuClose.style.display = 'block';
+    this.content.classList.add('menuList');
+  }
+
+  closeMenu(){
+    this.menuOpen.style.display = 'block';
+    this.menuClose.style.display = 'none';
+    this.content.classList.remove('menuList');
+  }
+}
+
+let menuDropdown = document.querySelector('.menu-container');
+menuDropdown = new DropDown(menuDropdown);
