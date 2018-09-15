3e0ec55a7326cd29e944bcf93dbe8748d0f3a4dc
FreeKeyBoy <jjhutton81@gmail.com>
2018-09-15 09:48:06 -0700

    Designed a nav bar and wrote the js to toggle to menu open and closed.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// JS goes here
+const toggleMenu = () => {
+    menu.classList.toggle("menu--open");
+  }
+  
+  const menu = document.querySelector(".menu");
+  const menuButton = document.querySelector(".menu-button");
+  const menuButtonClose = document.querySelector(".menu-button-close");
+  menuButton.addEventListener("click", toggleMenu);
+  menuButtonClose.addEventListener("click", toggleMenu);
