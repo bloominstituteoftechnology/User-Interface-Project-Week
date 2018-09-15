40b75d04ca28c1f14fa8c0a70850ac4dfa973363
paulapivat <paul.apivat@gmail.com>
2018-09-16 01:21:25 +0700

    add expand function to hamburger menu, need to add close

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,21 @@
 // menu.js
+
+const toggleMenu = () => {
+    navigation.classList.toggle('navigation--open');
+    hamburgerOpen.classList.toggle('hamburger--open');
+}
+
+
+
+
+
+
+
+
+let navigation = document.querySelector('.navigation');
+
+let hamburger = document.querySelector('.hamburger');
+
+let hamburgerClick = hamburger.addEventListener('click', toggleMenu);
+
+let hamburgerOpen = document.querySelector('.hamburger-close');
