e5978c6dbd99a1190a86257f895a074471563f70
Robin U Familara <robin.familara@cgius.com>
2018-09-15 15:00:50 -0500

    Build service page and mipulated js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // JS goes here
+document.querySelector('.hamburger').addEventListener('click', () => openNav());
+document.querySelector('.cross').addEventListener('click', () => closeNav());
+
+function openNav() {
+  document.querySelector('.hamburger').classList.add('hidden');
+  document.querySelector('.cross').classList.remove('hidden');
+  document.querySelector('.navigation').classList.add('modal');
+  document.querySelector('.navLinks').classList.remove('hidden');
+  document.querySelector('.navigation').style.justifyContent = 'center';
+}
+
+function closeNav() {
+  document.querySelector('.hamburger').classList.remove('hidden');
+  document.querySelector('.cross').classList.add('hidden');
+  document.querySelector('.navigation').classList.remove('modal');
+  document.querySelector('.navLinks').classList.add('hidden');
+  document.querySelector('.navigation').style.justifyContent = 'space-between';
+}
