cd2fa5f92efeb39403744e79af3d999744674146
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-09-14 20:50:45 -0400

    Refactored HTML, Less, added JS for tab navigation.

--- js/index.js
+++ js/index.js
@@ -13,66 +13,114 @@ class Header {
     }
 }
 
+let headers = document.querySelectorAll('.header');
+headers = Array.from(headers).map(header => new Header(header))
+
 let navMenu = document.querySelector('.navMenu');
-console.log(navMenu)
+// console.log(navMenu)
 // navMenu.addEventListener('click', () => {
 //     console.log('got it')
 
 // })
 
-// Services Page JS
-let headers = document.querySelectorAll('.header');
-headers = Array.from(headers).map(header => new Header(header))
-// console.log(headers);
-
-
-let dataTabs = document.querySelectorAll(`.textStyling[data-tab]`)
-let buttonData = document.querySelectorAll(`button[data-tab]`)
-
-
-class Button {
-    constructor(button) {
-        this.element = button;
-        this.element.addEventListener('click', () => {
-            console.log('got it');
-
-
-        })
-    }
-}
-let serviceButtons = Array.from(document.querySelectorAll('.serviceBtn'));
-serviceButtons.map(button => new Button(button));
-console.log(serviceButtons);
-// console.log(buttonData);
-// console.log(dataTabs);
-
-
 
 let modal = document.querySelector('#navOpen');
 let open = document.getElementById('openModal')
+
+let anchors = Array.from(document.querySelectorAll('.navAnchor'));
+// console.log(anchors);
 navMenu.addEventListener('click', function () {
 
     open.style.display = 'block';
     //  if (event.target == open) {
-        modal.style.display = 'none';
+    modal.style.display = 'none';
     // }
+
+    anchors.forEach((anchor) => {
+        anchor.style.fontWeight = 'Bold';
+
+        anchor.style.lineHeight = '8.5rem';
+
+        anchor.style.fontSize = '4.8rem';
+        anchor.style.display = 'block';
+    })
 })
 
 let close = document.querySelector('.closedNav');
 close.addEventListener('click', function () {
     open.style.display = 'none';
     modal.style.display = 'block';
+
     console.log('something')
 })
 
-// window.addEventListener('click', function () {
-  
-// })
+// Services Page JS
+
+// console.log(headers);
+
+
+let buttonData = document.querySelectorAll(`button[data-tab]`)
+
+// let articleData = document.querySelectorAll(``)
+class Button {
+    constructor(button) {
+
+        this.element = button;
+        this.btnData = this.element.dataset.tab
+        console.log(this.btnData)
+        // this.element = button;
+
+        // this.btnData = this.element.dataset.tab;
+
+        if (this.btnData === 'all') {
+            this.services = document.querySelectorAll('.service');
+
+        } else {
+
+            this.services = document.querySelectorAll(`.service[data-tab='${this.btnData}']`);
+        }
+
+        this.services = Array.from(this.services).map(service => new Servicer(service));
 
 
 
+        // console.log(this.dataTabs);
 
-let anchors = Array.from(document.querySelectorAll('.navAnchor'));
+        this.element.addEventListener('click', () => {
+            console.log('got it');
 
-// anchors = anchors.map(anchor => new Anchor(anchor));
-console.log(anchors);
+            this.serviceSelector();
+
+
+        })
+    }
+
+    serviceSelector() {
+
+        let services = document.querySelectorAll('.service');
+        // console.log(DataTabs)
+        services.forEach(function (service) {
+            service.style.display = 'none';
+        })
+
+        this.services.forEach(service => service.selectService());
+    }
+
+
+}
+
+class Servicer {
+    constructor(item) {
+        this.item = item;
+    }
+
+    selectService() {
+        this.item.style.display = null;
+    }
+}
+
+let serviceButtons = Array.from(document.querySelectorAll('.tab'));
+serviceButtons.map(button => new Button(button));
+console.log(serviceButtons);
+console.log('this is buttonData:', buttonData);
+// console.log('this is dataTabs:', this.dataTabs);
