35dd1517cec0cd5307f0b11e5d04d5515bffed97
Emmanium <pradoe.ep@gmail.com>
2018-09-15 00:06:55 -0600

    finished expanded nav layout, phew

--- Components/Dropdown/Dropdown.js
+++ Components/Dropdown/Dropdown.js
@@ -7,17 +7,25 @@ class Dropdown {
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
     
-    // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.element.querySelector('.dropdown-content');
+    // assign the reference to the ".dropdown-content" class
+    this.content = document.querySelector('.dropdown-content');
+
+    //assign reference to .hamburger-close class in .dropdown-content
+    this.closeButton = document.querySelector('.hamburger-close');
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
       return this.toggleContent();
     })
+
+    // Add a click handler to closeButton reference and call the toggleContent method.
+    this.closeButton.addEventListener('click', () => {
+      return this.toggleContent();
+    })
   }
 
   toggleContent() {
-    
+    //assign reference to .hamburger-close class in .dropdown-content
     // Toggle the ".dropdown-hidden" class off and on
     this.content.classList.toggle('dropdown-hidden');
   }
