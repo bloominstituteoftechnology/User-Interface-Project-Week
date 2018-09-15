ada92567b35b88505787d7a8b6d70e9bbe2b8735
Jesse <theiseja@gmail.com>
2018-09-15 12:08:29 -0500

    Finished desktop styling of services.html, have to add tab JS and completed nav JS as well. Text isn't centered in nav yet

--- js/index.js
+++ js/index.js
@@ -1,2 +1,22 @@
 // JS goes here
 
+let hamburger = document.querySelector('.hamOpen');
+let hamClose = document.querySelector('.hamClose');
+let links = document.querySelector('.links');
+let nav = document.querySelector('.nav');
+
+console.log(links);
+
+hamburger.addEventListener('click', () => {
+    hamburger.style.display = 'none';
+    hamClose.style.display = 'block';
+    links.style.display = 'flex';
+    nav.classList.toggle('open');
+});
+
+hamClose.addEventListener('click', () => {
+    hamburger.style.display = 'block';
+    hamClose.style.display = 'none';
+    links.style.display = 'none';
+    nav.classList.toggle('open');
+})
---
+++ js/services.js
@@ -0,0 +1,2 @@
+// Services tab component
+
