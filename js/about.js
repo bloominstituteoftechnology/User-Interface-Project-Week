8e5937347dd0c89acda40e82d5fffa7e7920ead4
Willie Yturralde <willieino@hotmail.com>
2018-09-15 09:08:27 -0700

    cleaning up the code

--- js/about.js
+++ js/about.js
@@ -9,39 +9,34 @@ function about() {
     menuButton.addEventListener("click", clickMenu);
     const navButton = document.querySelector(".nav-button");
     navButton.addEventListener("click", clickMenu);
-    //console.log("this is navbutton:" + navButton);
+
     const formInput2 = document.querySelector(".input2");
     formInput2.addEventListener("click", inputFocus2);
-    
+
     let menu = document.querySelector(".nav");
-    
-   
-      
+
     function clickMenu() {
-      if (menuOpen === true) {
-        menuButton.classList.add("menu-button-close");
-        menu.classList.remove("nav-close");
-        menu.classList.add("nav-open");
-        menuOpen = false;
-      } else {
-        menuButton.classList.remove("menu-button-close");
-        menu.classList.remove("nav-open");
-        menu.classList.add("nav-close");
-        menuOpen = true;
-      }
+        if (menuOpen === true) {
+            menuButton.classList.add("menu-button-close");
+            menu.classList.remove("nav-close");
+            menu.classList.add("nav-open");
+            menuOpen = false;
+        } else {
+            menuButton.classList.remove("menu-button-close");
+            menu.classList.remove("nav-open");
+            menu.classList.add("nav-close");
+            menuOpen = true;
+        }
     }
-   
 
-    
-
-      function inputFocus2() {
+    function inputFocus2() {
         // clears the input for the user to type their email
         if (formInput2.value === "Enter Email:") {
-          formInput2.value = "";
+            formInput2.value = "";
         }
-      }
-     
-      let index2 = 0;
+    }
+
+    let index2 = 0;
 
     class Carousel {
         constructor(element) {
@@ -91,17 +86,17 @@ function about() {
                 clearInterval(timer);
                 showFlg = false;
                 this.controlButton.innerHTML = "Click Here to Start";
-            } else { 
+            } else {
                 let delay = document.querySelector("#myRange").value;
                 delay = delay * 1000;
-                console.log("this is the delay: " + delay);
+
                 this.controlButton.innerHTML = "Click Here to Stop";
                 showFlg = true;
-                timer = setInterval( () => { this.beginShow() }, delay);
+                timer = setInterval(() => { this.beginShow() }, delay);
             }
         }
         beginShow() {
-                this.selectRight();
+            this.selectRight();
         }
         showTransition() {
             if (transitionFlg) {
@@ -114,15 +109,10 @@ function about() {
             }
         }
 
-       
-}
-
-
+    }
 
     let carousel = document.querySelectorAll(".carousel");
 
-
-    // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
     carousel = Array.from(carousel).map(item => new Carousel(item))
 }
 
