8af89e9e65d13124dfb21a8e931d2d836df1cba2
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-09-15 19:30:48 -0600

    Start of Javascript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+
+const menu = document.querySelector('.nav img')
+
+function Menu () {
+    if (menu.src === 'img/nav-hamburger-close.png') {
+        menu.src = 'img/nav-hamburger.png';
+    }
+    else (menu.src = 'img/nav-hamburger-close.png')
+}
+
+
+menu.addEventListener('click',function() {Menu()})
+
+console.log(menu);
