edf82292d26bc82774eb325e91d2336f6366acc3
Paul DeYonghe <p.deyonghe@gmail.com>
2018-09-16 13:55:15 -0500

    working on navigation, stuck on hamburger toggle

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // JS goes here
+
+const toggleMenu = () => {
+  menu.classList.toggle("menu--open");
+  let hamburger = document.querySelector("menu-button");
+  console.log(hamburger);
+  /*if (hamburger === "img/nav-hamburger.png"){
+    document.querySelector("menu-button").innerHTML = "img/nav-hamburger-close.png"
+  }
+  else {
+    document.querySelector("menu-button").innerHTML = "img/nav-hamburger.png"
+  }*/
+}
+
+const menu = document.querySelector(".menu");
+const menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function(){
+  return toggleMenu();
+});
