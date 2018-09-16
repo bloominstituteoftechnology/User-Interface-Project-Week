a75dc2239584698da95ab1fcdd135b818c58b097
jbierly1 <james.bierly@gmail.com>
2018-09-16 18:28:59 -0400

    finished nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // JS goes here
+
+const navItems=document.querySelector(".nav-items");
+console.log(navItems);
+const nav=document.querySelector("nav");
+console.log(nav);
+
+const  burger=document.querySelector(".burger");
+
+const toggleNav=function(){
+    navItems.classList.toggle("nav-items-on");
+    nav.classList.toggle("nav--open");
+    navItems.classList.toggle("nav-items");
+    console.log(navItems);
+}
+
+burger.addEventListener('click', toggleNav);
+
