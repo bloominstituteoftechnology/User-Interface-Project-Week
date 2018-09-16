0a4614e20e853a27f5fcb0e6d1d6a58d8ab5bd9f
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-09-15 19:18:04 -0600

    Created nav menu component

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-// JS goes here
+// menu
+class Menu {
+  constructor(menuName, menuButtonName) {
+    this.menu = document.querySelector('.' + menuName);
+    this.menuButton = document.querySelector('.' + menuButtonName);
+    this.menuButton.addEventListener("click", () => { this.toggleMenu() });
+    this.isOpen = false;
+  }
+
+  toggleMenu() {
+    this.menu.classList.toggle("menu--open");
+    this.menu.parentElement.classList.toggle("partial-transparent");
+    if (this.isOpen) {
+      this.menuButton.src = "img/nav-hamburger.png";
+      this.isOpen = false;
+    } else {
+      this.menuButton.src = "img/nav-hamburger-close.png";
+      this.isOpen = true;
+    }
+
+  }
+}
+
+const menu = new Menu("menu", "menu-button");
