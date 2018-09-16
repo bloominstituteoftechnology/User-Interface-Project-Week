24507ac144c10539122c9c74e644f3608b950f8e
Griffin <griffinshuth@gmail.com>
2018-09-16 12:59:56 -0600

    added js to tabs

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 // JS goes here
+const tabs1  = document.querySelectorAll('.tabs1')
+tabs1.forEach(function(element){
+    element.addEventListener('click', function(event){
+        const tabContent = document.querySelectorAll('.row')
+        const selectedTab = event.currentTarget
+        const tabs = document.querySelectorAll('.tabs1')
+        tabs.forEach(function(tab){
+            if(selectedTab === tab){
+                tab.classList.add('selected')
+            }
+            else{
+                tab.classList.remove('selected')
+            }
+        
+
+        })
+
+        tabContent.forEach(function(content){
+            
+           const tabNumber = selectedTab.dataset.tab
+           const contentNumber = content.dataset.content
+        //    current tab
+           if (tabNumber === contentNumber){
+           
+    
+            content.classList.remove('hide')
+           }
+        //    not selected tabs
+           else {
+               content.classList.add('hide')
+           }
+        })
+    }) 
+})
+
