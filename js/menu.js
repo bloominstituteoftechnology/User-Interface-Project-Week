0c08d66993acf353fb65e4a5c46879a86e9b48cc
Henry Moland <henrymoland@gmail.com>
2018-09-16 11:18:56 -0700

    Finished all MVP features

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,14 @@ let hamburger = document.querySelector(".hamburger-menu");
 let hamburgerClose = document.querySelector(".menu-close");
 let links = document.querySelector(".menu-open");
 let nav = document.querySelector(".nav");
+let overlay = document.querySelector(".overlay");
+
 console.log(links);
 hamburger.addEventListener("click", ()  =>  {
     hamburger.style.display = "none";
     hamburgerClose.style.display = "block";
     links.style.display = "flex";
+    overlay.style.width = "100%";
     nav.classList.toggle("open")
 })
 
@@ -14,5 +17,6 @@ hamburgerClose.addEventListener("click",    ()  =>  {
     hamburger.style.display = "block";
     hamburgerClose.style.display = "none";
     links.style.display = "none";
+    overlay.style.width = "0%";
     nav.classList.toggle("open")
 })
