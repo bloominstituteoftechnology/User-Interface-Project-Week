7e92b643f73654966cbcbe9c3ca2cb703c94f0a6
Rob Salzberg <robsalzberg@gmail.com>
2018-09-16 14:31:12 -0400

    desktop services page finished

---
+++ js/services.js
@@ -0,0 +1,34 @@
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.tabData = this.element.dataset.tab;
+        this.cards = document.querySelectorAll(`.card[data-tab=${this.tabData}`);
+        this.cards = Array.from(this.cards).map((card) => new TabCard(card));
+        this.element.addEventListener('click', () => this.selectTab());
+    }
+     selectTab() {
+        const tabs = document.querySelectorAll('.tab');
+        tabs.forEach((tab) => {
+            tab.classList.remove('selected-tab')
+        })
+        this.element.classList.add('selected-tab');
+        const cards = document.querySelectorAll('.card');
+        cards.forEach((card) => {
+            card.classList.remove('selected-card');
+        })
+        this.cards.forEach((card) => card.selectCard());
+    }
+}
+ class TabCard {
+    constructor(element) {
+        this.element = element;
+    }
+    selectCard() {
+        this.element.classList.add('selected-card');
+    }
+ }
+ let tabs = document.querySelectorAll('.tab');
+tabs = Array.from(tabs).map((tab) => {
+    return new TabLink(tab);
+})
+tabs[0].selectTab() 
