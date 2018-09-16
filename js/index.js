d14c89be09a58a4acb10270cbffd1a02084247cc
Marc de la Torre <mdelatorre1314@gmail.com>
2018-09-16 02:41:02 -0700

    Completed Nav functionality with styling for both desktop versions of home and services pages. will update mobile views.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // JS goes here
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open'); 
+  }
+  const menu = document.querySelector('.menu');
+  const menuButton = document.querySelector('.nav-button');
+  menuButton.addEventListener('click', toggleMenu);
+
+ const navClose = document.querySelector('.nav-close');
+ navClose.addEventListener('click', toggleMenu);
