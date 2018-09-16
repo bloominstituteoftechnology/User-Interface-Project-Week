4774c7ed9357b33157040fff6a13c075df49e520
Pascale Pierre <pascaleee@gmail.com>
2018-09-16 17:57:01 -0500

    completed navbar styling

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// JS goes here
+
+const toggleMenu = () => {
+  // Toggle the "menu--open" class on your menu refence.
+  console.log("I was clicked!")
+     // Global selector
+     menu.classList.toggle("menu--open")
+}
+// Start Here: Create a reference to the ".menu" class
+const menu = document.querySelector('.menu');
+// create a reference to the ".menu-button" class
+const menuButton = document.querySelector('.menu-button');
+
+const closeButton = document.querySelector('.close-button');
+// Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', toggleMenu);
