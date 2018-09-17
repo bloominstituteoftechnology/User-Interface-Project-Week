03cf90904d45b3ed4713710c88e5a85bd68a667a
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-09-17 18:52:14 -0400

    Minor tweaks to css/mobile stylings/html classes for styling.

--- js/index.js
+++ js/index.js
@@ -93,7 +93,44 @@ class Button {
             console.log('got it');
 
             this.serviceSelector();
-            
+            this.element.style.background = '#5E9FB9';
+            this.element.style.color = '#FFFFF';
+
+            this.services.map(() => {
+                    if (event.currentTarget !== this.services.dataset.tab){
+                        // this.element.style.color = 'black';
+                        // this.element.style.background = 'white';
+                        this.element.style.background = '#5E9FB9';
+                        this.element.style.color = '#FFFFF';
+                    } 
+                    else {
+                        this.element.style.color = 'black';
+                        this.element.style.background = 'white';
+                    }
+            })
+          console.log(this.services);
+            console.log(event.currentTarget)
+            // let color = this.services.style.color ;
+            // let BG = this.services.style.background ;
+            // if (this.element.dataset.tab || this.services !== 'all'){
+            //     console.log(this.element.dataset.tab);
+            //     console.log(this.services);
+            //     this.element.style.background = 'white';
+            //     this.element.style.color = 'black';
+            // }
+
+            // if (this.element.dataset.tab === 'all'){
+            //     // this.element.style.background = 'white';
+            //     // this.element.style.color = 'black';
+            //     this.element.style.background = '#5E9FB9';
+            //     this.element.style.color = '#FFFFF';
+            // }
+
+            // else if (this.element.dataset.tab === this.services){
+            //       this.element.style.background = 'white';
+            //     this.element.style.color = 'black';
+            // }
+
 
         })
     }
