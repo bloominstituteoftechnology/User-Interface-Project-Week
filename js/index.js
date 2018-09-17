623c445271857b617c5cfab01f3f87a309f8bb8c
Javier Alavrez <javieralvarez82@live.com>
2018-09-17 08:41:03 -0700

    completed nav bar sith hamburger functionality. starting work on mobile view.

--- js/index.js
+++ js/index.js
@@ -2,16 +2,27 @@ class Dropdown {
     constructor(element) {
         this.element = document.querySelector('.menu');
         this.button = this.element.querySelector('.menuButton');
+        this.hamburger = document.querySelector('.hamburger');
         this.content = document.querySelector('.expandMenu');
         this.button.addEventListener('click', () => {
             this.toggleContent();
         })
+
     }
 
     toggleContent() {
-        this.content.classList.toggle('menu-hidden');
+        if(this.content.className === 'expandMenu'){
+
+            this.content.className += ' menu-hidden';
+            this.hamburger.src ="./img/nav-hamburger.png";
+        }
+
+        else {
+            this.content.className = 'expandMenu';
+            this.hamburger.src ="./img/nav-hamburger-close.png"
+        }
     }
-}
+} 
 
 
 
