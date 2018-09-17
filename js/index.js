fd8d8620f6975105a8cc43582250029840e6d514
JeffJohnson <jeff.jj.9494@gmail.com>
2018-09-17 14:32:50 -0600

    I believe that i accomplished all five of the MVP features im not entirely sure if i did feature number 5 correctly but it works how i wanted it to i am now going to work and see if i cant get a mobile design for services i may also try to lessen my coding using mixins and possibly working imports in

--- js/index.js
+++ js/index.js
@@ -1,58 +1,21 @@
-// JS goes here
-class TabLink {
-    constructor(element){
-      // assign this.element to the element reference
-      this.element=element;
-      // Get the tab data attribute and save the value here
-      this.tabData = this.element.dataset.tab; 
-      // Find all elements with the .card class in index.html that correspond to the tab data attribute
-      // If the data is 'all' then select all cards regardless of their data attribute
-      if(this.tabData==='all'){
-        this.info = document.querySelectorAll(`.info`);
-      } else {this.info = document.querySelectorAll(`.info[data-tab='${this.tabData}'`);
-    }
-      // Map over the cards array and convert each card element into a new instance of the TabCard class. 
-    // Pass in the card object to the TabCard class.
-    this.info = Array.from(this.info).map(info => new TabCard(info));
-    // Add a click event that invokes selectTab
-    this.element.addEventListener('click',()=>{this.selectTab();
-    });
-  }
-  selectTab(){
-
-    // Select all elements with the .tab class on them
-    const buttons = document.querySelectorAll('.buttons');
-    // Iterate through the NodeList removing the .active-tab class from each element
-    buttons.forEach(function(each){
-      each.classList.remove('active-tab');
-    })
-    // Add a class of ".active-tab" to this.element
-    this.element.classList.add('active-tab');
-    // Select all of the elements with the .card class on them
-    const information = document.querySelectorAll('.info') ;
-    // Iterate through the NodeList setting the display style each one to 'none'
-    information.forEach(function(each){
-      each.style.display='none';
-    })
-    // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
-    this.info.forEach(info => info.selectCard());
-  }
-}
-class TabCard {
-    constructor(element){
-      // Assign this.element to the passed in element.
-      this.element=element;
-    }
-    selectCard(){
-      // Update the style of this.element to display = null
-      this.element.style.display=null;
-    }
+let hamburger = document.getElementById("hamburger");
+let dropDown = document.getElementById("drop-down")
+let navSpecs = document.getElementById("nav-specifics") 
+let navContent = document.getElementById("nav-content")
+let navBar = document.getElementById("nav-bar")
+let closeBtn = document.getElementById("close-btn")
+hamburger.addEventListener("click",function(){
+  navContent.style.width="100%";
+  navBar.style.display="relative";
   
-  }
-  // Create a reference to all ".tab" classes
-let tabs = document.querySelectorAll('.buttons');
-// Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map(tabs=>new TabLink(tabs))
+})
+closeBtn.addEventListener("click",function(){
+  navContent.style.width="0%";
+  navContent.style.left="0%";
+})
 
-//Once you are complete, call the .select method on the first tab
-tabs[0].selectTab();
+
+
+
+
+
