360244bf05a9c30454ad796d3862b87fadeae97e
erscriber <erscriber@gmail.com>
2018-09-17 16:38:29 -0400

    Navigation component in progress

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // JS goes here
+
+//Navigation
+
+const navButton = document.querySelector('.menu');
+const anchors = document.querySelectorAll('a');
+
+let closedHamburger = true;
+
+anchors.forEach(a => {
+	a.style.display = 'none';
+});
+
+const hamburgerHandler = function() {
+	if(closedHamburger) {
+		document.getElementbyClass('hamburger').src = './img/nav-hamburger-close.png';
+
+		anchors.forEach(a => {
+			a.style.display = '';
+		});
+
+		closedHamburger = false;
+	} else {
+		document.getElementbyClass('hamburger').src = './img/nav-hamburger.png';
+
+		anchors.forEach(a => {
+			a.style.display = 'none';
+		});
+
+		closedHamburger = true;
+	}
+};
+
+navButton.addEventListener('click', hamburgerHandler);
