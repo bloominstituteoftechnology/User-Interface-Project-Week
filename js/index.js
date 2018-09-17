0836e19105e80595334ac5a806e6efcbf603343a
Courtney Buratto <you@example.com>
2018-09-17 14:42:23 -0700

    home-mobile complete

--- js/index.js
+++ js/index.js
@@ -1,38 +1,53 @@
-class TabLink {
-    constructor(element) {
-        this.element = element;
-        this.data = this.element.dataset.tab;
-        this.itemElement = document.querySelector(`.tab-content[data-tab=
-        "${this.data}"]`);
-        this.tabItem = new tTabItem(this.itemElement);
-        this.element.addEventListener('click', () => {
-            this.selectTab()
-        });
-    }
+// NAV
+// const nav = document.querySelector('.nav');
+// const burger = document.querySelector('.burger');
+// const navLinks = document.querySelector('.links');
 
-    selectTab() {
-        const links = document.querySelectorAll('.tab-services');
-        links.forEach(item => item.classList.remove("selected"));
-        this.element.classList.aded("selected");
-        this.tabItem.selectTab();
-    }
-}
 
-class TabItem {
-    constructor(element) {
-        this.element = element;
-    }
 
-    selectTab() {
-        const tabs = document.querySelectorAll('.tab-content');
-        tabs.forEach(item => item.style.display = 'none');
-        this.element.style.display = 'block';
-    }
-}
 
-let links = document.querySelectorAll('.tab-services');
-links = Array.from(links).map(element => new TabLink(element));
 
-links[0].select();
+
+
+
+
+
+// TABS
+// class TabLink {
+//     constructor(element) {
+//         this.element = element;
+//         this.data = this.element.dataset.tab;
+//         this.itemElement = document.querySelector(`.tab-content[data-tab=
+//         "${this.data}"]`);
+//         this.tabItem = new tTabItem(this.itemElement);
+//         this.element.addEventListener('click', () => {
+//             this.selectTab()
+//         });
+//     }
+
+//     selectTab() {
+//         const links = document.querySelectorAll('.tab-services');
+//         links.forEach(item => item.classList.remove("selected"));
+//         this.element.classList.aded("selected");
+//         this.tabItem.selectTab();
+//     }
+// }
+
+// class TabItem {
+//     constructor(element) {
+//         this.element = element;
+//     }
+
+//     selectTab() {
+//         const tabs = document.querySelectorAll('.tab-content');
+//         tabs.forEach(item => item.style.display = 'none');
+//         this.element.style.display = 'block';
+//     }
+// }
+
+// let links = document.querySelectorAll('.tab-services');
+// links = Array.from(links).map(element => new TabLink(element));
+
+// links[0].select();
 
    
