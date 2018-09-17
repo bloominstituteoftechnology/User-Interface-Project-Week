9f12047ae0a6bbc34fc1d0a4c67543e5251faa90
Cesar Aponte <caponte4@win.coat.com>
2018-09-17 17:02:24 -0400

    working on tabs

--- js/services.js
+++ js/services.js
@@ -10,21 +10,29 @@ class Tablinks {
         this.el.addEventListener('click', () =>     {this.clickHandler()});
     }
     clickHandler(){
-        
         this.el.classList.toggle("tabsBackgroundColorSelected");
         this.el.classList.toggle("tabsColorSelected");
-        this.tabInfo.toggle();
+        this.tabInfo.toggle('tabsBackgroundColorSelected');
+        
     }
+
 }
 
 class TabInfo {
     constructor(el){
         this.el = el;
     }
-    
+
+
     toggle(){
-        this.el.classList.toggle("tabs-show");
+
+        for(let i=0; i < this.tabInfo.length; i++){
+            this.tabInfo.style.display = "none";
+        }
+
+            this.el.classList.toggle("tabs-show")
     }
+
 }
 
 tabLink = Array.from(tabLink).map((tablinks) => {return new Tablinks(tablinks)});
