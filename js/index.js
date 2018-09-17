97493ff782fbda4911341b3733e9bb92d053863c
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-09-17 06:22:04 -0600

    Created service page tab navigator component

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// menu
+/* menu */
 class Menu {
   constructor(menuName, menuButtonName) {
     this.menu = document.querySelector('.' + menuName);
@@ -21,4 +21,37 @@ class Menu {
   }
 }
 
+/* Tab */
+class Tab {
+  constructor(tab) {
+    this.tab = tab;
+    this.tabContent = document.querySelector(`.tab-content[data-tab="${ this.tab.dataset.tab }"]`);
+  }
+
+  setActive(active) {
+    active ? this.tab.classList.add('active') : this.tab.classList.remove('active');
+    active ? this.tabContent.classList.add('active') : this.tabContent.classList.remove('active');
+  }
+}
+
+/* Tab Navigator */
+class TabNavigator {
+  constructor(tabNavigatorName) {
+    this.tabNavigator = document.querySelector('.' + tabNavigatorName);
+    this.tabs = this.tabNavigator.querySelectorAll('.tab');
+    this.tabs = Array.from(this.tabs).map(tab => new Tab(tab));
+    // this.tabContents = Array.from(document.querySelectorAll('.tab-content'));
+    this.tabs.forEach((tab) => { tab.tab.addEventListener('click', (e) => { this.select(e) } )});
+  }
+
+  select(e) {
+    this.tabs.forEach(tab => {
+      if (tab.tab.dataset.tab === e.target.dataset.tab) tab.setActive(true);
+      else tab.setActive(false);
+    });
+  }
+}
+
+/* Create menu and tab navigator */
 const menu = new Menu("menu", "menu-button");
+const tabNavigator = new TabNavigator("tab-navigator");
