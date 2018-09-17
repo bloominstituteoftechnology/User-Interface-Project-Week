295fc569e1171334aca9fa4d9f852b1e92db56ae
Courtney Buratto <you@example.com>
2018-09-17 09:33:40 -0700

    JS started, also made changes to all files

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-// JS goes here
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.itemElement = document.querySelector(`.tab-content[data-tab=
+        "${this.data}"]`);
+        this.tabItem = new tTabItem(this.itemElement);
+        this.element.addEventListener('click', () => {
+            this.selectTab()
+        });
+    }
+
+    selectTab() {
+        const links = document.querySelectorAll('.tab-services');
+        links.forEach(item => item.classList.remove("selected"));
+        this.element.classList.aded("selected");
+        this.tabItem.selectTab();
+    }
+}
+
+class TabItem {
+    constructor(element) {
+        this.element = element;
+    }
+
+    selectTab() {
+        const tabs = document.querySelectorAll('.tab-content');
+        tabs.forEach(item => item.style.display = 'none');
+        this.element.style.display = 'block';
+    }
+}
+
+let links = document.querySelectorAll('.tab-services');
+links = Array.from(links).map(element => new TabLink(element));
+
+links[0].select();
+
+   
