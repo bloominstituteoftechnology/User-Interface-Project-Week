3d2cf32638035681112036029211e1f7c844a6d1
Robin U Familara <robin.familara@cgius.com>
2018-09-17 22:42:54 -0500

    Fixed the navigation and menu as well as the animations

--- js/index.js
+++ js/index.js
@@ -1,64 +1,70 @@
 // JS goes here
-// Navigation Bar
-document.querySelector('.hamburger').addEventListener('click', () => openNav());
-document.querySelector('.cross').addEventListener('click', () => closeNav());
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open'); 
+  }
+  const menu = document.querySelector('.menu');
+  const menuButton = document.querySelector('.nav-button');
+  menuButton.addEventListener('click', toggleMenu);
 
-function openNav() {
-  document.querySelector('.hamburger').classList.add('hidden');
-  document.querySelector('.cross').classList.remove('hidden');
-  document.querySelector('.navigation').classList.add('modal');
-  document.querySelector('.navLinks').classList.remove('hidden');
-  document.querySelector('.navigation').style.justifyContent = 'center';
-}
+ const navClose = document.querySelector('.nav-close');
+ navClose.addEventListener('click', toggleMenu);
 
-function closeNav() {
-  document.querySelector('.hamburger').classList.remove('hidden');
-  document.querySelector('.cross').classList.add('hidden');
-  document.querySelector('.navigation').classList.remove('modal');
-  document.querySelector('.navLinks').classList.add('hidden');
-  document.querySelector('.navigation').style.justifyContent = 'space-between';
-}
-// Services Tabs
-class TabLink {
+ class TabLink {
     constructor(element) {
       this.element = element;
+      
       // Get the custom data attribute on the Link
       this.data = this.element.dataset.tab;
-       // Using the custom data attribute get the associated Item element
-      this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
-      this.itemLink = document.querySelector(`.tabs-link[data-tab='${this.data}']`);
-       // Using the Item element, create a new instance of the TabItem class
+      
+      // Using the custom data attribute get the associated Item element
+      this.itemElement = document.querySelector(`.tabs-content[data-tab='${this.data}']`);
+    
+      // Using the Item element, create a new instance of the TabItem class
       this.tabItem = new TabItem(this.itemElement);
-      
+  
       // Add a click event listener on this instance, calling the select method on click
-      this.element.addEventListener('click', () => { this.select() });
+      this.element.addEventListener('click', () => {this.select()})
     };
-     select() {
+  
+    select() {
       // Get all of the elements with the tabs-link class
-      const links = document.querySelectorAll('.tabs-link');
-       // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-      Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
-       // // Add a class named "tabs-link-selected" to this link
-      this.element.classList.add('tabs-link-selected');
+      const links = document.querySelectorAll('.tab');
+  
+      // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+      Array.from(links).forEach(links => {
+        links.classList.remove('tab-selected');
+      });
+  
+      // Add a class named "tabs-link-selected" to this link
+        this.element.classList.add('tab-selected');
       
-      // // Call the select method on the item associated with this link
-      this.tabItem.select();
+      // Call the select method on the item associated with this link
+      this.tabItem.select(); 
     }
   }
-   class TabItem {
+  
+  class TabItem {
     constructor(element) {
       this.element = element;
     }
-     select() {
+  
+    select() {
       // Select all items elements from the DOM
-      const items = document.querySelectorAll('.tabs-item');
-       // Remove the class "tabs-item-selected" from each element
-      Array.from(items).forEach(item => item.classList.remove('tabs-item-selected'));
-       // Add a class named "tabs-item-selected" to this element 
-      this.element.classList.add('tabs-item-selected');
+      const items = document.querySelectorAll('.tabs-content');
+      // Remove the class "tabs-item-selected" from each element
+      Array.from(items).forEach(items => {
+        items.classList.remove('tabs-content-selected');
+      });  
+      // Add a class named "tabs-item-selected" to this element 
+      this.element.classList.add('tabs-content-selected');
     }
   }
-   // START HERE: create a reference to the ".tabs" classes
-  let links = document.querySelectorAll('.tabs-link');
-   // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
-  links = Array.from(links).map(link => new TabLink(link));
+  
+  
+  // START HERE: create a reference to the ".tabs" classes
+  let links = document.querySelectorAll('.tab');
+  
+  // Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item. 
+  links = Array.from(links).map(link => {
+  new TabLink(link);
+  })
