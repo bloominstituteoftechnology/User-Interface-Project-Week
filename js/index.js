7d9f8dd23e532624f23af098a3b6c3ebb96bae19
Alex Martin <atonymartin20@yahoo.com>
2018-09-17 19:08:28 -0500

    fixed top navbar dropdown feature

--- js/index.js
+++ js/index.js
@@ -4,17 +4,20 @@ class Dropdown {
 
     this.element = element;
     this.button = this.element.querySelector(".dropdown-button");
-    this.content = this.element.querySelector(".dropdown-content");
-
-    this.button.addEventListener('click', () => {
-        this.toggleContent();
-    })
-    }
-
+    this.closeButton = this.element.querySelector(".close-button");
+    this.content = this.element.querySelector(".full-menu");
+    this.hideHeader = this.element.querySelector('header');
+    this.closeButton.addEventListener('click', () => {this.toggleContent()});
+    this.button.addEventListener('click', () => {this.toggleContent()})};
+    
     toggleContent() {
         this.content.classList.toggle("dropdown-hidden");
+        this.hideHeader.classList.toggle("dropdown-hidden");
     }
 }
 
-let dropdowns = document.querySelectorAll('.dropdown');
-dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+// let dropdowns = document.querySelectorAll('.dropdown');
+// dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
+let dropdown = document.querySelector('body');
+dropdown = new Dropdown(dropdown);
