02fe4bf2ab8a741f5572a34625a7a0b7f2f8d6e9
Victor G <victorgordian103@gmail.com>
2018-09-17 18:55:16 -0700

    Finishing Services page

--- js/index.js
+++ js/index.js
@@ -46,3 +46,48 @@
 
   let dropdowns = new Dropdown();
   
+
+
+
+  class TabLink {
+    constructor(element) {
+      this.element = element;
+      this.data = this.element.dataset.tab
+      this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+      this.tabItem = new TabItem(this.itemElement);
+      this.element.addEventListener('click', () => {
+        this.select()
+      })
+  
+    };
+  
+  
+    select() {
+      const links = document.querySelectorAll('.tabs-link'); 
+      let notSeen = document.querySelectorAll('.tabs-item');
+      notSeen.forEach((item)=>{
+        item.style = 'display: none';
+      })  
+      this.tabItem.select();
+    }
+  }
+  
+  class TabItem {
+    constructor(element) {
+      this.element = element;
+
+    }
+  
+    select() {
+        let allItems = document.querySelectorAll('.tabs-item');
+        Array.from(allItems).forEach((item)=>{
+            item.style = 'display: none';
+        });
+
+        this.element.style = 'display: flex'
+    }
+  }
+
+  let links = document.querySelectorAll(".tabs-link");
+  links = Array.from(links).map(links => new TabLink(links));
+  links[0].select();
