b7f4c9e1c5092e8ac0657525487df76444f2706d
FBT <flaming.bunny.tomb@gmail.com>
2018-09-17 19:02:05 -0700

    nav bar done, services page still broken, making progress

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
-// JS goes here
+
+
+
+
+openButton = document.querySelector('#toOpen');
+closeButton = document.querySelector('#toClose')
+dropdownPage = document.querySelector('.dropdown');
+
+openButton.addEventListener('click', function(){
+openButton.style.display = 'none'
+closeButton.style.display = 'flex'
+dropdownPage.style.display = 'flex'
+});
+
+closeButton.addEventListener('click', function(){
+    closeButton.style.display = 'none'
+    openButton.style.display = 'flex'
+    dropdownPage.style.display = 'none'
+}); 
+
+
+
+class ButtonSelector{
+    constructor(element){
+        this.element = element;
+        this.data = this.element.innerHTML;
+        this.card = document.querySelector(`#${this.data}-tab`);
+        this.element.addEventListener('click', this.Clicks.bind(this));
+    }
+    Clicks(){
+        const allButtonSelector = document.querySelectorAll('.button');
+        allButtonSelector.forEach(function(element){
+            element.classList.remove('activeButton')
+        });
+        this.element.classList.add('activeButton');
+         const allInfoBox = document.querySelectorAll('.infoBox');
+        allInfoBox.forEach(function(element){
+            element.style.display = 'none'
+        });
+        this.card.style.display = 'flex';
+    }
+};
+ let tabList = document.querySelectorAll('.button');
+ tabList = Array.from(tabList).map(function(element){
+    return new ButtonSelector(element)
+});
