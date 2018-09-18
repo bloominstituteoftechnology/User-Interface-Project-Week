4f1f9691d21aac5f457b5f4b9180449c1cb46cfa
Courtney Buratto <you@example.com>
2018-09-17 17:28:59 -0700

    this is what i have so far

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
 // NAV
-// const nav = document.querySelector('.nav');
+const nav = document.querySelector('.nav');
 // const burger = document.querySelector('.burger');
 // const navLinks = document.querySelector('.links');
 
+// class NavDrop {
+//     constructor(el) {
+//         this.el = el;
+//         this.button = this.el.querySelector('.burger');
+//         this.nav = this.el.querySelector('.nav');
+        
+//         this.button.addEventListener('click', () => {
+//             this.toggle();
+//         })
+//     }
+//     toggle(){
+//         this.nav.classList.toggle("links");
+//         if(this.nav.classList.length > 1) {
+//             this.el.style.height = "50px";
+//             this.button.src = "img/nav-hamburger.png";
+//         } else {
+//             this.el.style.height = "0px";
+//         }
+//     }
+// }
 
+// const drop = document.querySelector('.header');
+// new NavDrop(drop);
 
 
 
@@ -13,41 +35,37 @@
 
 
 // TABS
-// class TabLink {
-//     constructor(element) {
-//         this.element = element;
-//         this.data = this.element.dataset.tab;
-//         this.itemElement = document.querySelector(`.tab-content[data-tab=
-//         "${this.data}"]`);
-//         this.tabItem = new tTabItem(this.itemElement);
-//         this.element.addEventListener('click', () => {
-//             this.selectTab()
-//         });
-//     }
+class TabLink {
+    constructor(el) {
+        this.el = el;
+        this.data = this.el.dataset.tab;
+        this.itemElement = document.querySelectorAll(`.tab-content[data-tab="${this.data}"]`);
+        this.tabItem = new TabItem(this.itemElement);
+        this.el.addEventListener('click', () => {
+            this.select()
+        });
+    }
 
-//     selectTab() {
-//         const links = document.querySelectorAll('.tab-services');
-//         links.forEach(item => item.classList.remove("selected"));
-//         this.element.classList.aded("selected");
-//         this.tabItem.selectTab();
-//     }
-// }
+    select() {
+        const buttons = document.querySelectorAll('.tab');
+        buttons.forEach(item => item.classList.remove('.selected'));
+        this.element.classList.add('.selected');
+        this.tabItem.select();
+    }
+}
 
-// class TabItem {
-//     constructor(element) {
-//         this.element = element;
-//     }
+class TabItem {
+    constructor(el) {
+        this.el = el;
+    }
+    select() {
+       const tabs = document.querySelectorAll('.tab-content');
+       tabs.forEach(item => item.style.display = 'none');
+       this.el.style.display = 'block';
+    }
+}
 
-//     selectTab() {
-//         const tabs = document.querySelectorAll('.tab-content');
-//         tabs.forEach(item => item.style.display = 'none');
-//         this.element.style.display = 'block';
-//     }
-// }
-
-// let links = document.querySelectorAll('.tab-services');
-// links = Array.from(links).map(element => new TabLink(element));
-
-// links[0].select();
-
+let links = document.querySelectorAll('.tab');
+links = Array.from(links).map(el => new TabLink(el));
+links[0].select();
    
