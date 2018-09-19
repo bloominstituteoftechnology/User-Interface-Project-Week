8bc86d0f91f0df8145054aedd827b29ac851518a
unknown <jerrardsmith215@gmail.com>
2018-09-18 23:51:36 -0400

    Jerrard Smith UI Project-I Home Page Complete

--- js/index.js
+++ js/index.js
@@ -2,15 +2,33 @@
 
 /* Nav bar expansion code */
 const navButton = document.getElementById("NavImg");
-const navExpansion = document.querySelector('.NavExpansion');
-console.log(navExpansion);
+const navExpansion = document.querySelector(".NavExpansion");
+console.log(navExpansion.style.display)
+
+
 navButton.onclick = function (event) {
-    if (navExpansion.style.display == "flex") {
-        navExpansion.style.display = "none";
-    }
-    
-    else if (navExpansion.style.display !== "flex") {
+//Expanded nav display.
+    let displayState = navExpansion.style.display;
+    switch (displayState) {
+        case "flex":
+        navExpansion.style.display = "";
+        break;
+
+        case "":
         navExpansion.style.display = "flex";
+        break;
     }
-};
-console.log(navButton);
+    console.log (displayState);
+
+//Button icon change.
+    switch (navButton.getAttribute('src')) {
+        case "/img/nav-hamburger.png":
+        this.setAttribute('src', '/img/nav-hamburger-close.png');
+        break;
+
+        case "/img/nav-hamburger-close.png":
+        this.setAttribute('src', '/img/nav-hamburger.png');
+        break;
+}
+console.log(this.getAttribute('src'));
+}
