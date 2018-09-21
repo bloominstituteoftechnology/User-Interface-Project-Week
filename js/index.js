a3149adea6d84911556fbcd2776de9355b37719a
unknown <jerrardsmith215@gmail.com>
2018-09-20 23:55:21 -0400

    Jerrard Smith, completed project

--- js/index.js
+++ js/index.js
@@ -32,3 +32,42 @@ navButton.onclick = function (event) {
 }
 console.log(this.getAttribute('src'));
 }
+
+/**********************Services*************************/
+let leadTabs = document.querySelectorAll('.button');
+const serviceH2 = document.querySelector('.service_h2')
+const serviceImage = document.querySelector('.image');
+const serviceText = document.querySelector(".service_text");
+
+class Button {
+    constructor(el) {
+        this.el = el;
+        this.el.dataset.number = el.dataset.number;
+        this.el.addEventListener('click', () => { this.buttonClick() 
+        });
+    }
+
+    buttonClick() {
+        //change header
+        serviceH2.innerHTML=this.el.innerHTML;
+        //Change image
+        switch (this.el.dataset.number) {
+            case "1":
+            serviceImage.setAttribute('src', '/img/services/services-tab-pre-construction-img.png'); 
+            break;
+            case "2":
+            serviceImage.setAttribute('src', '/img/services/services-tab-construction-img.png');
+            break;
+            case "3":
+            serviceImage.setAttribute('src', '/img/services/services-tab-design-build-img.png');
+            break;
+            case "4":
+            serviceImage.setAttribute('src', '/img/services/services-tab-sustainability-img.png');
+            break;
+        }
+        console.log(this.el.dataset.number);
+        console.log(serviceImage.getAttribute("src"));
+    }
+}
+
+leadTabs = Array.from(leadTabs).map(el => new Button(el));
