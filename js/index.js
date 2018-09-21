68cc44e8bffc5d044f67c2d467e35f90c7a8937e
Courtney Buratto <you@example.com>
2018-09-21 14:59:17 -0700

    made minor changes to homepage, services and nav

--- js/index.js
+++ js/index.js
@@ -1,33 +1,17 @@
 //NAV
-// class Dropdown {
-//     constructor(element) {
-//         this.element = element;
-//         this.button = this.element.querySelector('.burger');
-//         this.content = this.element.querySelector('.links');
-//         this.button.addEventListener('click', () =>
-//     this.toggleContent())
-//     }
-
-//     toggleContent() {
-//         this.content.classList.toggle('dropdown-hidden');
-//     }
-// }
-
-// let dropdowns = document.querySelectorAll('.dropdown');
-// dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
-
 class Dropdown {
     constructor(element) {
-      this.element = element;
-      this.button = this.element.querySelector(".burger");
-      this.content = this.element.querySelector(".links");
-      this.button.addEventListener('click', () => 
-        this.toggleContent())
+        this.element = element;
+        this.button = this.element.querySelector('.burger');
+        this.content = this.element.querySelector('.links');
+        this.button.addEventListener('click', () =>
+    this.toggleContent())
     }
 
     toggleContent(event) {
-      this.content.classList.toggle("links");
+        this.content.classList.toggle('dropdown-hidden'); //links
     }
-  }
-  let dropdowns = document.querySelectorAll('.dropdown');
-  dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+}
+
+let dropdowns = document.querySelectorAll('.dropdown');
+dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
--- js/services.js
+++ js/services.js
@@ -3,39 +3,43 @@ class TabLink {
     constructor(element) {
        this.element = element;
        this.data = this.element.dataset.tab;
-       this.itemElement = document.querySelectorAll(`.content[data-tab="${this.data}"]`);
+       this.itemElement = document.querySelectorAll(`.content[data-tab = "${this.data}"]`);
        this.tabItem = new TabItem(this.itemElement);
        this.element.addEventListener('click', () => {
            this.select()});
     };
+        
 
+
+    
     select() {
         const links = document.querySelectorAll('.tab');
-        Array.from(links).forEach((element) => {
-            element.classList.remove('tab-selected');
-        })       
-        this.element.classList.add('tab-selected');
-        this.tabItem.select();
-    }
+      Array.from(links).forEach((el) => {
+          el.classList.remove("tab-selected");
+      });
+      this.element.classList.add("tab-selected");
+      this.tabItem.select();
+    };
 }
 
 class TabItem {
     constructor(element) {
         this.element = element;
-    }
+    };
 
     select() {
         const items = document.querySelectorAll('.content');
-        Array.from(items).forEach((element) => {
-            element.classList.add('content-selected');
-        });
-        this.element.classList.remove('content-selected');
-    }
+        Array.from(items).forEach((el) => {
+           el.classList.remove("content-selected");
+       });
+
+       this.element.classList.add('content-selected');
+    };
 }
 
 let links = document.querySelectorAll('.tab');
-links = Array.from(links).map((element) => {
-    return new TabLink(element);
+link = Array.from(links).map((el) => {
+    return new TabLink(el);
 });
 
 links[0].select();
