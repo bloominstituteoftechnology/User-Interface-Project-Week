88dde5637e1d62b037b348b8646820fc8ecffc75
Courtney Buratto <you@example.com>
2018-09-21 15:56:23 -0700

    JS complete, tabs are functional on services page

--- js/services.js
+++ js/services.js
@@ -2,24 +2,47 @@
 class TabLink {
     constructor(element) {
        this.element = element;
-       this.data = this.element.dataset.tab;
-       this.itemElement = document.querySelectorAll(`.content[data-tab = "${this.data}"]`);
-       this.tabItem = new TabItem(this.itemElement);
-       this.element.addEventListener('click', () => {
-           this.select()});
-    };
+       this.tabData = this.element.dataset.tab;
+       if(this.tabData === 'all') {
+           this.content = document.querySelectorAll('.content');
+       } else {
+           this.content = document.querySelectorAll(`.content[data-tab = "${this.tabData}"]`);
+       }
+    //    this.itemElement = document.querySelectorAll(`.content[data-tab = "${this.data}"]`);
+    //    this.tabItem = new TabItem(this.itemElement);
+    //    this.element.addEventListener('click', () => {
+    //        this.select()});
+    this.content = Array.from(this.content).map((el) => {
+        return new TabItem(el);
+    });
+    this.element.addEventListener('click', () => {
+        this.select();
+    });
+};
         
 
 
     
     select() {
-        const links = document.querySelectorAll('.tab');
-      Array.from(links).forEach((el) => {
-          el.classList.remove("tab-selected");
-      });
-      this.element.classList.add("tab-selected");
-      this.tabItem.select();
-    };
+        const tabs = document.querySelectorAll('.tab');
+        tabs.forEach((el) => {
+            el.classList.remove('active-tab');
+        });
+        this.element.classList.add('active-tab');
+
+        const content = document.querySelectorAll('.content');
+        content.forEach((el) => {
+            el.style.display = 'none';
+        });
+
+        this.content.forEach(content => content.select());
+    //   Array.from(links).forEach((el) => {
+    //       el.classList.remove("tab-selected");
+    //   });
+    //   this.element.classList.add("tab-selected");
+    //   this.tabItem.select();
+    // };
+    }
 }
 
 class TabItem {
@@ -28,21 +51,22 @@ class TabItem {
     };
 
     select() {
-        const items = document.querySelectorAll('.content');
-        Array.from(items).forEach((el) => {
-           el.classList.remove("content-selected");
-       });
+        this.element.style.display = '';
+    //     const items = document.querySelectorAll('.content');
+    //     Array.from(items).forEach((el) => {
+    //        el.classList.remove("content-selected");
+    //    });
 
-       this.element.classList.add('content-selected');
+    //    this.element.classList.add('content-selected');
     };
 }
 
-let links = document.querySelectorAll('.tab');
-link = Array.from(links).map((el) => {
+let tabs = document.querySelectorAll('.tab');
+tabs = Array.from(tabs).map((el) => {
     return new TabLink(el);
 });
 
-links[0].select();
+tabs[0].select();
 
 
 
