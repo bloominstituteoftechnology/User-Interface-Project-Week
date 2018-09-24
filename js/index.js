ac3d2947b92558275b1019dc64587ecd3ac6d32b
Riley-Brown <riley@riley.gg>
2018-09-24 17:51:22 -0400

    Added nav menu and X toggle on click

--- js/index.js
+++ js/index.js
@@ -5,4 +5,6 @@ const nav = document.querySelector(".hamburger-menu");
 const navIcon = document.querySelector(".hamburger-icon");
 navIcon.addEventListener("click", function() {
   nav.classList.toggle("hamburger-menu-toggle");
+  document.querySelector(".nav-menu").classList.toggle("nav-hide");
+  document.querySelector(".nav-x").classList.toggle("nav-show");
 });
