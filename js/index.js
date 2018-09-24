9f1c4e8879929bb4779ee9cda22bd7c086642476
Riley-Brown <riley@riley.gg>
2018-09-24 17:37:55 -0400

    Added nav bar click functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // JS goes here
+
+// nav bar click handler
+const nav = document.querySelector(".hamburger-menu");
+const navIcon = document.querySelector(".hamburger-icon");
+navIcon.addEventListener("click", function() {
+  nav.classList.toggle("hamburger-menu-toggle");
+});
