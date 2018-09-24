6038b4f5341bfaaa2000a7099ddc4dd901981f26
FBT <flaming.bunny.tomb@gmail.com>
2018-09-24 12:21:10 -0700

    refactored services page related code, finally got services tabs working :D :D :D

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 
 
-
+// START OF NAV BAR
 openButton = document.querySelector('#toOpen');
 closeButton = document.querySelector('#toClose')
 dropdownPage = document.querySelector('.dropdown');
@@ -17,30 +17,96 @@ closeButton.addEventListener('click', function(){
     openButton.style.display = 'flex'
     dropdownPage.style.display = 'none'
 }); 
+// END OF NAV BAR
 
 
-
-class ButtonSelector{
-    constructor(element){
-        this.element = element;
-        this.data = this.element.innerHTML;
-        this.card = document.querySelector(`#${this.data}-tab`);
-        this.element.addEventListener('click', this.Clicks.bind(this));
+class Tab {
+    constructor(element) {
+      this.element = element;
+      this.links = this.element.querySelectorAll('.button');
+      this.links = Array.from(this.links).map( link => {
+        return new TabLink(link, this);
+      });
+      this.activeLink = this.links[0];
+      this.activeLink.select();
     }
-    Clicks(){
-        const allButtonSelector = document.querySelectorAll('.button');
-        allButtonSelector.forEach(function(element){
-            element.classList.remove('activeButton')
-        });
-        this.element.classList.add('activeButton');
-         const allInfoBox = document.querySelectorAll('.infoBox');
-        allInfoBox.forEach(function(element){
-            element.style.display = 'none'
-        });
-        this.card.style.display = 'flex';
+  
+    activate(link) {
+      this.activeLink.deselect();
+      this.activeLink = link;
     }
-};
- let tabList = document.querySelectorAll('.button');
- tabList = Array.from(tabList).map(function(element){
-    return new ButtonSelector(element)
-});
+    getTab(dataValue) {
+      return this.element.querySelector(`.infoBox[data-tab='${dataValue}']`);
+    }
+  }
+  
+  class TabLink {
+    constructor(element, parent) {
+      this.element = element;
+      this.tabs = parent;
+      this.tabItem = parent.getTab(element.dataset.tab);
+      this.tabItem = new TabItem(this.tabItem);
+      
+      this.element.addEventListener('click', () => {
+        this.tabs.activate(this);
+        this.select();
+      });
+    }
+    
+    select() {
+      this.element.classList.add('button-selected');
+      this.tabItem.select();
+    }
+    deselect() {
+      this.element.classList.remove('button-selected');
+      this.tabItem.deselect();
+    }
+  }
+  
+  class TabItem {
+    constructor(element) {
+    this.element = element;
+    }
+    select() {
+      console.log('testing');
+    this.element.classList.add('infoBox-selected');
+    }
+    deselect() {
+    this.element.classList.remove('infoBox-selected');
+  }
+  }
+  
+  
+  let tabs = document.querySelectorAll('.tabspage');
+  
+  tabs = Array.from(tabs).map(tab => {
+    return new Tab(tab);
+  });
+  console.log(tabs);
+
+
+// BAD TABS SECTION SAVE FOR REFERENCE
+// class ButtonSelector{
+//     constructor(element){
+//         this.element = element;
+//         this.data = this.element.innerHTML;
+//         this.card = document.querySelector(`#${this.data}-tab`);
+//         this.element.addEventListener('click', this.Clicks.bind(this));
+//     }
+//     Clicks(){
+//         const allButtonSelector = document.querySelectorAll('.button');
+//         allButtonSelector.forEach(function(element){
+//             element.classList.remove('activeButton')
+//         });
+//         this.element.classList.add('activeButton');
+//          const allInfoBox = document.querySelectorAll('.infoBox');
+//         allInfoBox.forEach(function(element){
+//             element.style.display = 'none'
+//         });
+//         this.card.style.display = 'none';
+//     }
+// };
+//  let tabList = document.querySelectorAll('.button');
+//  tabList = Array.from(tabList).map(function(element){
+//     return new ButtonSelector(element)
+// });
