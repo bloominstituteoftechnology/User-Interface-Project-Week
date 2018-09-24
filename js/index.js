a28d4ac3b71c017feb1d7bd9b3601c3ae161225f
frankbenevento <fb.mentoslabs@gmail.com>
2018-09-24 13:54:32 -0700

    initial pass at the menu overlay

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// JS goes here
+
+let menuBtn = document.querySelector('#mainMenu');
+let closeBtn = document.querySelector('#mainMenuClose');
+let menuDiv = document.querySelector('#menuDiv');
+let navMenu = document.querySelector('#navMenu');
+let headerTopNavDiv = document.querySelector('#headerTopNavDiv');
+
+closeBtn.classList.add('hidden');
+navMenu.classList.add('hidden');
+
+
+menuBtn.addEventListener('click',() => {
+    menuDiv.classList.add('header-overlay');
+    menuBtn.classList.add('hidden');
+    closeBtn.classList.remove('hidden');
+    headerTopNavDiv.classList.add('header-top-nav-expanded');
+    navMenu.classList.remove('hidden');
+})
+
+closeBtn.addEventListener('click',() => {
+    menuDiv.classList.remove('header-overlay');
+    menuBtn.classList.remove('hidden');
+    closeBtn.classList.add('hidden');
+    headerTopNavDiv.classList.remove('header-top-nav-expanded');
+    navMenu.classList.add('hidden');
+})
