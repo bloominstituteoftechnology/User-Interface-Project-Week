02031319dd67072f51f79bc35d50fb9ccc9f6de1
Rahul Desai <rd825@georgetown.edu>
2018-09-24 17:30:02 -0400

    Completed the nav toggle functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // JS goes here
+
+let dropdown = document.querySelector(".dropdown-button");
+let content = document.querySelector(".dropdown-content");
+
+dropdown.addEventListener('click', function() {
+    dropdown.src = "../img/nav-hamburger-close.png";
+    content.classList.toggle('dropdown-hidden');
+
+    if (content.classList.contains('dropdown-hidden') === true) {
+        dropdown.src = "../img/nav-hamburger.png";
+    }
+});
