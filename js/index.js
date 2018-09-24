878c1c0e1fc9f3d8f66aed9d62aae16a2dd0eb6a
Blake <koenneckerb@gmail.com>
2018-09-24 14:45:30 -0700

    Expanded view of navigation complete.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // JS goes here
+const hamburger = document.querySelector('.nav-img');
+const navbar = document.querySelector('.hidden');
+console.log(hamburger)
+let open = false;
+
+hamburger.addEventListener('click', () => open === false ? openNav() : closeNav());
+
+const openNav = () => {
+    navbar.style.display = 'block';
+    hamburger.src = "img/nav-hamburger-close.png";
+    open = true;
+};
+
+const closeNav = () => {
+    navbar.style.display = 'none';
+    hamburger.src = "img/nav-hamburger.png";
+    open = false;
+}
