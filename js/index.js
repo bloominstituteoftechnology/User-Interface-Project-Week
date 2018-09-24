bdcc1375ba3e6b4c26e6bbd0041cbf5ab1d70302
erscriber <erscriber@gmail.com>
2018-09-24 11:37:00 -0400

    Tabs are finally funtional

--- js/index.js
+++ js/index.js
@@ -15,3 +15,32 @@ const menuClosed = document.querySelector('.x');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
 menuClosed.addEventListener('click', toggleClose);
+
+
+//class DropDown {
+ // constructor (element) { //element = div class="navigation"
+    this.element = element;
+
+    this.menuOpen = this.element.querySelector('.hamburger'); //img class="hamburger" src="./img/nav-hamburger.png"
+    this.menuClosed = this.element.querySelector('.x'); //img class="x" src="./img/nav-hamburger-close.png"
+
+    this.menuItems = this.element.querySelector('.menuSeen');
+
+    this.menuOpen.addEventListener('click', () => this.expandMenu());
+    this.menuClosed.addEventListener('click', () => this.closeMenu());
+ //}
+
+  //expandMenu() {
+    this.menuOpen.style.display = 'none';
+   this.menuClosed.style.display = 'block';
+    this.menuItems.classList.add('menuList');
+  //}
+
+  //closeMenu() {
+    this.menuOpen.style.display = 'block';
+    this.menuClosed.style.display = 'none';
+    this.menuItems.classList.remove('menuList');
+  //}
+//}
+//let menuDropdown = document.querySelector('.navigation') //div class="navigation"
+//menuDropdown = new DropDown(menuDropdown);
