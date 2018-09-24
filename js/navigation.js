7ef703d3d0426c7ec7df60818e73638cf2d379b5
kyranmccann <kyran.mccann@gmail.com>
2018-09-24 13:51:28 -0700

    navigation dropdown desktop

---
+++ js/navigation.js
@@ -0,0 +1,22 @@
+
+
+let nav = document.querySelector('nav div');
+let navButton = document.querySelector('.nav-hamburger');
+
+navButton.addEventListener('click', drop, {once:true});
+
+function close(){
+  nav.classList.remove('dropped');
+  document.querySelector('.full-width-nav').style = 'opacity: 1';
+  nav.classList.add('dropdown');
+  navButton.innerHTML = '<img src="./img/nav-hamburger.png" alt="menu button" />';
+  navButton.addEventListener('click', drop, {once:true});
+}
+
+function drop(){
+  nav.classList.remove('dropdown');
+  document.querySelector('.full-width-nav').style = 'opacity: .95';
+  nav.classList.add('dropped');
+  navButton.innerHTML = '<img src="./img/nav-hamburger-close.png" alt="menu button" />';
+  navButton.addEventListener('click', close, {once:true});
+}
