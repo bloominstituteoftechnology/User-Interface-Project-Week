ad2db56b8c29d74e20af2fa3370b4d2e19295a06
Peter Pham <phamhere@gmail.com>
2018-09-24 16:14:23 -0600

    finished menu bar JS bugs

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-// JS goes here
+// navigation bar JS
 let hamburger = document.querySelector('.navigation img');
 let navigation = document.querySelector('.navigation');
 
 hamburger.addEventListener('click', () => {
   navigation.classList.toggle('navigation-active');
-  // hamburger.src === '../img/nav-hamburger.png' ? hamburger.src = '../img/nav-hamburger-close.png' : hamburger.src = '../img/nav-hamburger.png';
+  hamburger.src.includes('img/nav-hamburger.png') ? hamburger.src = 'img/nav-hamburger-close.png' : hamburger.src = 'img/nav-hamburger.png';
 })
+
+// services page tab content JS
+
+
+// let tabs = document.querySelectorAll('')
