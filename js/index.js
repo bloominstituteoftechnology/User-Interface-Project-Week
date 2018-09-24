53b01598f0a13323f4e5fcf60085acd45b502d81
Peter Pham <phamhere@gmail.com>
2018-09-24 15:26:22 -0600

    finished most of menu bar JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // JS goes here
+let hamburger = document.querySelector('.navigation img');
+let navigation = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', () => {
+  navigation.classList.toggle('navigation-active');
+  // hamburger.src === '../img/nav-hamburger.png' ? hamburger.src = '../img/nav-hamburger-close.png' : hamburger.src = '../img/nav-hamburger.png';
+})
