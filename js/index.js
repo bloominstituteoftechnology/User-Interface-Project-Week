27b67349309ef71f854acc283d3d388d0d76ff88
Rahul Desai <rd825@georgetown.edu>
2018-09-25 16:00:52 -0400

    Completed animations for menu and buttons

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
 // Navigation JS
 let dropdown = document.querySelector(".dropdown-button");
+let content = document.querySelector(".dropdown-content");
+content.style.opacity = '0';
+content.style.visibility = 'hidden';
+
 dropdown.addEventListener('click', function() {
     // change the hamburger to an X
     dropdown.src = "./img/nav-hamburger-close.png";
     
     // toggle the class to hide the nav menu
-    let content = document.querySelector(".dropdown-content");
     content.classList.toggle('dropdown-hidden');
 
     // toggle the X back to hamburger
     if (content.classList.contains('dropdown-hidden') === true) {
         dropdown.src = "./img/nav-hamburger.png";
+        TweenMax.to(content, 0.5, {autoAlpha:0, y:0});
+    }
+    else {
+        TweenMax.to(content, 0.5, {autoAlpha:0.9, y:0});
     }
 });
 
--- js/services.js
+++ js/services.js
@@ -36,18 +36,25 @@ links = Array.from(links).map(link => {return new TabLink(link);});
 links[0].select();
 
 
-// Navigation JS -----------------------------------------------
+// Navigation JS -----------------------------------------
 let dropdown = document.querySelector(".dropdown-button");
+let content = document.querySelector(".dropdown-content");
+content.style.opacity = '0';
+content.style.visibility = 'hidden';
+
 dropdown.addEventListener('click', function() {
     // change the hamburger to an X
-    dropdown.src = "../img/nav-hamburger-close.png";
+    dropdown.src = "./img/nav-hamburger-close.png";
     
     // toggle the class to hide the nav menu
-    let content = document.querySelector(".dropdown-content");
     content.classList.toggle('dropdown-hidden');
 
     // toggle the X back to hamburger
     if (content.classList.contains('dropdown-hidden') === true) {
-        dropdown.src = "../img/nav-hamburger.png";
+        dropdown.src = "./img/nav-hamburger.png";
+        TweenMax.to(content, 0.5, {autoAlpha:0, y:0});
+    }
+    else {
+        TweenMax.to(content, 0.5, {autoAlpha:0.9, y:0});
     }
 });
