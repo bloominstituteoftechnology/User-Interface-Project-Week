5326fa90446b04f10e5ff476e5a33044863e8c51
Katy Hollobaugh <khollobaugh@gmail.com>
2018-09-25 14:09:05 -0400

    finished expanded menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 //Menu Toggle
 const toggleMenu = () => {
+    console.log('menu',menu);
     menu.classList.add('open-menu');
     xButton.classList.add('show-x-button');
     menuButton.classList.add('hide-menu-button');
@@ -28,3 +29,59 @@ const toggleMenu = () => {
 
 //Tab-Navigation
 
+// class TabLink {
+//   constructor(element){
+//     this.element = element;
+//     this.tabData = this.element.dataset.tab;
+//     this.cards = document.querySelector(`.tab-card[data-tab='this.tabData']`);
+//     this.cards = Array.from(this.cards).map( card => new TabCard(card));
+//     this.element.addEventListener('click', () => {this.selectTab() });
+//   }
+//   selectTab() {
+//       let tabs = document.querySelectorAll('.tab-link');
+//   }
+// }
+
+//   selectTab(){
+
+//     // Select all elements with the .tab class on them
+//     let tabs = document.querySelectorAll('.tab');
+//     // Iterate through the NodeList removing the .active-tab class from each element
+//     tabs.forEach((tab) => {tab.classList.remove('.active-tab');
+//   });
+//     // Add a class of ".active-tab" to this.element
+//     this.element.classList.add('.active-tab');
+
+//     // Select all of the elements with the .card class on them
+//     let cards = document.querySelectorAll('.card');
+
+//     // Iterate through the NodeList setting the display style on each one to 'none'
+//     cards.forEach((card) => {card.style.display = 'none'});
+
+//     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
+//     this.cards.forEach(card => card.selectCard());
+
+//   }
+// }
+
+// class TabCard {
+//   constructor(element){
+//     // Assign this.element to the passed in element.
+//     this.element = element;
+//   }
+//   selectCard(){
+//     // Update the style of this.element to display = null
+//     // console.log('this',this);
+//     this.element.style = 'display: null;';
+//   }
+
+// }
+
+let tabs = document.querySelectorAll('.tab-link');
+tabs = Array.from(tabs).map( tab => new TabLink(tab));
+
+
+// // Create a reference to all ".tab" classes
+// let tabs = document.querySelectorAll(".tab");
+// // Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
+// tabs = Array.from(tabs).map( tab => new TabLink(tab));
