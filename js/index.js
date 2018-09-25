57c9b592d2a4460eea5d5274db991b18ac9e908a
cembreyfarquhar <cembreyfarquhar@gmail.com>
2018-09-25 13:00:54 -0500

    finished expanding nav

--- js/index.js
+++ js/index.js
@@ -2,22 +2,31 @@
 
 // navigation opener
 
-const hamburger = document.querySelector(".hamburger");
-const headerDiv = document.querySelector(".header");
+const hamburger = document.querySelector("#hamburger");
+const dotHeader = document.querySelector(".header");
 const nav = document.querySelector(".nav");
 const headerElement = document.querySelector("header");
 const container = document.querySelector(".container");
+const headerDiv = document.querySelector(".header-div");
+let counter = 2;
 hamburger.addEventListener("click", () => {
-  nav.classList.remove("nav-hidden");
-  headerDiv.classList.add("expanded");
-  nav.classList.add("expanded-nav");
-  headerElement.classList.add("expanded-header");
+  if (counter % 2 === 0) {
+    nav.classList.remove("nav-hidden");
+    dotHeader.classList.add("expanded");
+    nav.classList.add("expanded-nav");
+    headerElement.classList.add("expanded-header");
+    headerDiv.classList.add("expanded-header-div");
+    hamburger.src = "img\\nav-hamburger-close.png";
+  } else {
+    nav.classList.add("nav-hidden");
+    dotHeader.classList.remove("expanded");
+    nav.classList.remove("expanded-nav");
+    headerElement.classList.remove("expanded-header");
+    headerDiv.classList.remove("expanded-header-div");
+    hamburger.src = "img\\nav-hamburger.png";
+  }
+  counter++;
 });
-
-
-
-
-
 
 document.addEventListener("keydown", e => {
   if (e.keyCode === 66) {
@@ -35,7 +44,7 @@ document.addEventListener("keydown", e => {
                           if (e.keyCode === 73) {
                             document.addEventListener("keydown", e => {
                               if (e.keyCode === 80) {
-                                //do a backflip 
+                                //do a backflip
                               }
                             });
                           }
