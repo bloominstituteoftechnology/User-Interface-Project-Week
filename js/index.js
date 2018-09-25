990f74f16163593d6ac9e95fe8735d6fbb216ced
unknown <quan.nguyen199712@gmail.com>
2018-09-25 13:29:38 -0400

    finished with desktop home page and navigation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 // JS goes here
+class Dropdown {
+    constructor(element) {
+      
+
+      this.element=element;
+      
+
+      this.button = document.querySelector('.hamButton');
+      
+      
+      this.content=this.element.querySelector('.dropdown-content');
+      
+      
+      this.button.addEventListener('click', (event) => {
+          this.toggleContent(event)
+  
+      })
+      this.closeButton = document.querySelector('.close-button');
+       
+      this.closeButton.addEventListener('click', (event) => {
+          this.toggleContent(event)
+  
+      })
+    }
+  
+    toggleContent() {
+      
+      
+      this.content.classList.toggle('dropdown-hidden');
+    }
+  }
+  
+  
+  let dropdowns = document.querySelectorAll('.dropdown');
+  dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
