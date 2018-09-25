4630cc809171f531631af67ca219ed590e887460
Adam <ajmboosh@gmail.com>
2018-09-24 22:05:04 -0500

    finished functionality of opening and closing nav menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // JS goes here
+
+const menuButton = document.querySelector(".home-page .menu .button-img img");
+console.log(menuButton);
+const closedURL =
+  "https://github.com/DaftBeowulf/User-Interface-Project-Week/blob/master/img/nav-hamburger.png?raw=true";
+const openURL =
+  "https://github.com/DaftBeowulf/User-Interface-Project-Week/blob/master/img/nav-hamburger-close.png?raw=true";
+const menuNav = document.querySelector(".menu .nav");
+menuButton.addEventListener("click", () => {
+  if (menuButton.src === closedURL) {
+    event.currentTarget.src = openURL;
+    menuNav.style.display = "flex";
+  } else {
+    event.currentTarget.src = closedURL;
+    menuNav.style.display = "none";
+  }
+});
