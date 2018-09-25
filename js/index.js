c042053a063d7d20a37419c5f14edc8b08a5d908
Adam Hinckley <adamhinckley@Adams-MacBook-Pro.local>
2018-09-25 14:57:26 -0600

    navigation LESS and JS done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// JS goes here
+// navigation
+
+const openNav = document.querySelector(".nav-hamburger-closed");
+const closeNav = document.querySelector(".close-nav-X");
+const nav = document.querySelector(".open-nav");
+
+openNav.addEventListener("click", () => {
+  nav.style.display = "flex";
+});
+closeNav.addEventListener("click", () => {
+  nav.style.display = "none";
+});
