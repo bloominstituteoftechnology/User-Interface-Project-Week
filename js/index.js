5c00b7b1e99bdbc66085bb22f34377e08606984e
Andre Myrick <amyrick.820@gmail.com>
2018-09-25 13:11:44 -0400

    completed nav bar minus bug marked in trello

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// JS goes here
+const toggleMenu = () => {
+    nav.classList.toggle("expand");
+    menu.classList.toggle("menu-open");
+
+    if (menu.classList.contains("menu-open")) {
+    menuButton.src="/img/nav-hamburger-close.png";
+    }
+    else {
+        menuButton.src="/img/nav-hamburger.png";
+    }
+}
+
+const menu = document.querySelector(".menu");
+const nav = document.querySelector(".navigation");
+const menuButton = document.querySelector(".menu-button");
+// menuButton.src="/img/nav-hamburger.png";
+
+menuButton.addEventListener("click", toggleMenu);
