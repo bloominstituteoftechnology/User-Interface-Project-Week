cd30bbfda6f272a262d06610d9473ff61abfc8c1
Adam <ajmboosh@gmail.com>
2018-09-25 13:09:10 -0500

    finished tab navigator functionality, added images for all tab items, added cursor:pointer for navigator button, set pre-construction tab to be preselected on pageload in JS

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-// JS goes here
-
+// expanding nav menu functionality. doesn't specify home-page or services since it needs
+//to function similarly on both
 const menuButton = document.querySelector(".menu .button-img img");
-console.log(menuButton);
+
 const closedURL =
   "https://github.com/DaftBeowulf/User-Interface-Project-Week/blob/master/img/nav-hamburger.png?raw=true";
 const openURL =
@@ -16,3 +16,44 @@ menuButton.addEventListener("click", () => {
     menuNav.style.display = "none";
   }
 });
+
+//tab navigator for services
+class TabLink {
+  constructor(element) {
+    this.element = element;
+    this.data = this.element.dataset.tab;
+    this.item = document.querySelector(`.tab-item[data-tab='${this.data}']`);
+    this.tabItem = new TabItem(this.item);
+    this.element.addEventListener("click", event => {
+      this.select(event);
+    });
+  }
+
+  select() {
+    const links = document.querySelectorAll(".tab-link");
+    Array.from(links).forEach(link =>
+      link.classList.remove("tab-link-selected")
+    );
+    this.element.classList.add("tab-link-selected");
+    this.tabItem.select();
+  }
+}
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    const items = document.querySelectorAll(".tab-item");
+    Array.from(items).forEach(item =>
+      item.classList.remove("tab-item-selected")
+    );
+    this.element.classList.add("tab-item-selected");
+  }
+}
+let links = document.querySelectorAll(".tab-link");
+
+links = Array.from(links).map(link => new TabLink(link));
+
+links[0].select();
