ccbf354f2a1c012e3615ea6a945425fa465cab6a
Unknown <zachary.arney@gmail.com>
2018-09-25 17:19:18 -0500

    Added tab functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
-// JS goes here
+// --TABS--
+class TabLink {
+  constructor(element) {
+    this.element = element;
+    this.data = this.element.dataset.tab;
+    this.tabItem = document.querySelector(`.tab-item[data-tab='${this.data}']`);
+    this.tabItem = new TabItem(this.tabItem);
+    this.element.addEventListener('click', event => {
+      this.select(event);
+    });
+  }
+  select() {
+    let links = document.querySelectorAll('.tab-link');
+    Array.from(links).forEach(element => {
+      element.classList.remove('tab-link-selected');
+    });
+    this.element.classList.add('tab-link-selected');
+    this.tabItem.select();
+  }
+}
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    let items = document.querySelectorAll('.tab-item');
+
+    Array.from(items).forEach(element => {
+      element.classList.remove('tab-item-selected');
+    });
+
+    this.element.classList.toggle('tab-item-selected');
+  }
+}
+
+let links = document.querySelectorAll('.tab-link');
+
+links = Array.from(links).map(link => new TabLink(link));
+
+links[0].select();
