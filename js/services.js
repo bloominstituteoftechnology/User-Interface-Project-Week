31133ad256f9f4b5a3dab87ea9dcd5bdb67abc87
Cwright0428 <cwright0428@yahoo.com>
2018-09-25 15:38:15 -0500

    lots of small design fixes

--- js/services.js
+++ js/services.js
@@ -1,9 +1,39 @@
-const hamburger = document.querySelector(".navbar .burger");
-const dropdown = document.querySelector(".menu-items");
-let visibility = false;
+class TabNav {
+    constructor(tabLinks, tabContent) {
+        this.tabLinks = Array.from(tabLinks).map((eachTab) => {
+            eachTab.addEventListener('click', (event) => this.updateActive(event.target));
+            return eachTab;
+        });
+        this.tabContent = tabContent;
+        this.activeTab = this.tabLinks[0];
+        this.activeContent = this.tabContent.querySelector("[data-content='0']");
+        this.init();
+    };
 
-hamburger.addEventListener("click", (event) => {
-  visibility = dropdown.classList.toggle("menu-items_revealed");
-  !visibility ? hamburger.src = "../img/nav-hamburger.png" : hamburger.src = "../img/nav-hamburger-close.png";
-  console.log(I was clicked)
-})
+    init() {
+        this.activeTab.classList.add('tab_selected');
+        this.activeContent.classList.add('tab-content_revealed');
+    };
+
+
+    updateActive(selectedTab) {
+        this.activeTab.classList.remove('tab_selected');
+        this.activeTab = selectedTab;
+        this.activeTab.classList.add('tab_selected');
+
+        const tabNumber = this.activeTab.dataset.tab;
+        const selectedContent = this.tabContent.querySelector(`[data-content=${tabNumber}]`);
+
+        this.activeContent.classList.remove('tab-content_revealed');
+        this.activeContent = selectedContent;
+        this.activeContent.classList.add('tab-content_revealed');
+
+
+
+
+    };
+};
+
+const tabLinks = document.querySelectorAll('.each-tab');
+const tabContent = document.querySelector('.all-tab-content');
+let tabNav = new TabNav(tabLinks, tabContent);
