22d258c9559aba881000942332b219d2db8df7a8
Hargobind Atwal <ethaipath@gmail.com>
2018-09-25 14:28:55 -0700

    Finished Nav functionality

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 // JS goes here
 
 // toggle nav
+const getRestOfStrAfter = function(str, search) {
+  return str.substring(str.indexOf(search));
+}   
+
 const navButton = document.getElementById('menu-button');
 const navBar = document.querySelector('.menu');
+let imgStates = {
+  'on': 'img/nav-hamburger-close.png',
+  'off': 'img/nav-hamburger.png'
+
+}
 navButton.addEventListener('click', function(event) {
+  navButton.dataset.state = navButton.dataset.state === 'off' ? 'on' : 'off';
+  navButton.src = imgStates[navButton.dataset.state];
   navBar.parentNode.classList.toggle('nav-expanded');
   navBar.classList.toggle('menu-show');
+
+  
 })
