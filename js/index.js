519fe1bb7b80133276782c1e4d5cc7202d0daa47
Blake <koenneckerb@gmail.com>
2018-09-25 11:20:09 -0700

    JS for tab navigator finished.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // JS goes here
+
+// navigation
 const hamburger = document.querySelector('.nav-img');
 const navbar = document.querySelector('.hidden');
 let open = false;
@@ -16,3 +18,37 @@ const closeNav = () => {
     hamburger.src = "img/nav-hamburger.png";
     open = false;
 }
+
+// services page tabs
+
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.itemElement = document.querySelector(`.tab-item[data-tab='${this.data}']`);
+        this.tabItem = new TabItem(this.itemElement);
+        this.element.addEventListener('click', () => this.select())
+    };
+
+    select() {        
+        const links = document.querySelectorAll('.tab-link');       
+        Array.from(links).forEach(cb => cb.classList.remove('tab-link-selected'));        
+        this.element.classList.add('tab-link-selected');
+        this.tabItem.select();
+    }
+}
+
+class TabItem {
+    constructor(element) {
+        this.element = element;
+    }
+    select() {
+        const items = document.querySelectorAll('.tab-item')
+        Array.from(items).forEach(cb => cb.classList.remove('tab-item-selected'))
+        this.element.classList.add('tab-item-selected')
+    }
+}
+
+let links = document.querySelectorAll('.tab-link');
+links = Array.from(links).map(cb => new TabLink(cb))
+links[0].select();
