e07aeeb9a23ef81d8e1bedae45aae41172060d4e
Steven O'Neill <oneillsteven957@gmail.com>
2018-09-25 16:42:55 -0500

    tab navigator (with problems)

--- js/index.js
+++ js/index.js
@@ -23,3 +23,45 @@ class NavDropdown {
   
   let navDropdowns = document.querySelectorAll('.nav-bar');
   navDropdowns = Array.from(navDropdowns).map( navDropdown => new NavDropdown(navDropdown));
+
+
+
+  class TabLink {
+    constructor(element) {
+      this.element = element;
+      this.data = this.element.dataset.tab;
+      this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+      this.tabItem = new TabItem(this.itemElement);
+      this.element.addEventListener('click', (event) => {
+        this.select(event)
+      });
+    };
+  
+    select() {
+      const links = document.querySelectorAll('.tabs-link');
+      Array.from(links).forEach( (link) => {
+        link.classList.remove('tabs-link-selected')
+      });
+      this.element.classList.add('tabs-link-selected');
+      this.tabItem.select();
+    }
+  }
+  
+  class TabItem {
+    constructor(element) {
+      this.element = element;
+    }
+  
+    select() {
+      let items =  document.querySelectorAll('.tabs-item');
+      Array.from(items).forEach( (item) => {
+        item.classList.remove('tabs-item-selected')
+      });
+      this.element.classList.add('tabs-item-selected');
+    }
+  }
+  
+  let links = document.querySelectorAll('.tabs-link');
+  links = Array.from(links).map( links => new TabLink(links));
+  links[0].select();
+  
