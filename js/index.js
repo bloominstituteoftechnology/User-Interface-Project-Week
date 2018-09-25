9d5b02dc7c4575ef7020ecbccc83050b422082e1
Mindysueasaurus <mymindyyeager@gmail.com>
2018-09-25 14:32:31 -0500

    finished tabs styling. JS functionality for tabs finished

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // JS goes here
+
+
+//dropdown menu
 class Dropdown {
   constructor(element){
     this.element = element;
@@ -16,3 +19,46 @@ class Dropdown {
 
 let dropdowns = document.querySelectorAll('.dropdown');
 dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
+
+
+
+//tabs
+class TabLink {
+  constructor(element) {
+    this.element = element;
+    this.data = this.element.dataset.tab;
+    this.item = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+
+    this.tabItem = new TabItem(this.item);
+    this.element.addEventListener('click', () => {
+      this.selected();
+    });
+  };
+  selected() {
+    const links = document.querySelectorAll('.tabs-link');
+    links.forEach( link => {
+      link.classList.remove('tabs-link-selected');
+    });
+    this.element.classList.add('tabs-link-selected');
+    this.tabItem.selected();
+  }
+}
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+  selected() {
+    const items = document.querySelectorAll('.tabs-item');
+    items.forEach( item => {
+      item.classList.remove('selected');
+
+    })
+    this.element.classList.add('selected');
+  }
+}
+let links = document.querySelectorAll('.tabs-link');
+links = Array.from(links).map( link => new TabLink(link));
+
+links[0].selected()
