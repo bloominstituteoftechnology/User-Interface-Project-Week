3e653bc739ba7abfa9f9559191ad248a95e45f65
Peter Pham <phamhere@gmail.com>
2018-09-25 15:22:10 -0600

    added some menu animation

--- js/index.js
+++ js/index.js
@@ -1,36 +1,43 @@
 // navigation bar JS
 let hamburger = document.querySelector('.navigation img');
 let navigation = document.querySelector('.navigation');
+let nav = document.querySelector('.navigation nav');
 
 hamburger.addEventListener('click', () => {
   navigation.classList.toggle('navigation-active');
-  hamburger.src.includes('img/nav-hamburger.png') ? hamburger.src = 'img/nav-hamburger-close.png' : hamburger.src = 'img/nav-hamburger.png';
+  hamburger.src.includes('img/nav-hamburger.png') ? 
+    (hamburger.src = 'img/nav-hamburger-close.png', TweenMax.from(navigation, 1, {opacity: 0})) : 
+    hamburger.src = 'img/nav-hamburger.png';
 })
 
 // scrolling animations
 ScrollReveal().reveal('.scroll1', {
   origin: 'left',
-  duration: 2000,
+  duration: 1000,
+  delay: 1000,
   distance: '300px',
 })
 ScrollReveal().reveal('.scroll2', {
   origin: 'right',
-  duration: 2000,
+  duration: 1000,
   distance: '300px',
 })
 ScrollReveal().reveal('.top-article', {
   origin: 'left',
-  duration: 2000,
+  duration: 1000,
+  delay: 500,
   distance: '300px',
 })
 ScrollReveal().reveal('.mid-article', {
   origin: 'right',
-  duration: 2000,
+  duration: 1000,
+  delay: 500,
   distance: '300px',
 })
 ScrollReveal().reveal('.bottom-article', {
   origin: 'left',
-  duration: 2000,
+  duration: 1000,
+  delay: 500,
   distance: '300px',
 })
 
