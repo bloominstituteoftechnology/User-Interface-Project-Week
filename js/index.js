e87402044ab43654e8eb40c40e5e089567b10096
Edward J <edjeong99@gmail.com>
2018-09-25 15:45:13 -0400

    implement nav expanded JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+
+
+let hamburgerTag = document.querySelector('#hamburger');
+let hamburgerCloseTag = document.querySelector('#hamburgerClose');
+let navLinkTag = document.querySelector('.navLink');
+
+hamburgerTag.addEventListener('click', navClassToggle);
+hamburgerCloseTag.addEventListener('click', navClassToggle);
+
+function navClassToggle() {
+    hamburgerTag.classList.toggle('disappear');
+    hamburgerCloseTag.classList.toggle('disappear');
+    navLinkTag.classList.toggle('disappear');
+}
