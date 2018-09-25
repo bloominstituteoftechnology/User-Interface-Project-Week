eb52799ac8020e6a61e63700e64d4c3a7cbdbfb5
Axolotl <samuelmachat@gmail.com>
2018-09-25 18:12:45 -0400

    completed services page tab navigator

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// menu model
 class HamburgerOpen {
   constructor(element) {
     this.element = element
@@ -11,7 +12,6 @@ class HamburgerOpen {
     document.querySelector('.menu-modal').classList.remove('modal-hidden')
   }
 }
-
 
 class HamburgerClose {
   constructor(element) {
@@ -32,3 +32,30 @@ let hamburgerExpanded = document.querySelector('header .expanded')
 
 hamburgerNonExpanded = new HamburgerOpen(hamburgerNonExpanded)
 hamburgerExpanded = new HamburgerClose(hamburgerExpanded)
+
+// services page tabs content
+class Tab {
+  constructor(element) {
+    this.element = element
+    this.tabData = this.element.dataset.tab
+    this.content = Array.from(document.querySelectorAll('.content')).find(
+      item => item.dataset.tab === this.tabData
+    )
+    this.element.addEventListener('click', () => this.selectTab())
+  }
+
+  selectTab() {
+    console.log(this.content)
+    const tabs = document.querySelectorAll('.tab')
+    tabs.forEach(tab => tab.classList.remove('active-tab'))
+    this.element.classList.add('active-tab')
+
+    const contents = document.querySelectorAll('.content')
+    contents.forEach(content => (content.style.display = 'none'))
+    this.content.style.display = null
+  }
+}
+
+let tabs = document.querySelectorAll('.tab')
+tabs = Array.from(tabs).map(tab => new Tab(tab))
+tabs[0].selectTab()
