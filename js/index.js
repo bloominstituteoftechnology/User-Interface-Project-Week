357ba0189b4c9adb24e5d3a975df00d7e9196e03
bkwillia052 <bkwillia052@gmail.com>
2018-09-25 15:50:49 -0500

    JS nav functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // JS goes here
+
+//Upon click, which the hamburger image to 'close' and change nav display: none => flex;
+
+let hamburger = document.querySelector('.nav-ham');
+let nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', fromHam, {once: true});
+
+function fromHam(){
+    if(hamburger.src = '/img/nav-hamburger.png'){
+        hamburger.src = '/img/nav-hamburger-close.png';
+        nav.style.display = 'flex';
+        hamburger.addEventListener('click', toHam, {once: true} );
+    }
+}
+
+function toHam(){
+    if(hamburger.src = '/img/nav-hamburger-close.png'){
+        hamburger.src = '/img/nav-hamburger.png';
+        nav.style.display = 'none';
+        hamburger.addEventListener('click', fromHam, {once: true});
+    }
+}
+
+
+
