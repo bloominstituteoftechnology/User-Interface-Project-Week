4b0884eeeaccbeda9370a2ff4e76721d16727ef3
lola <lev@levs-air.lan>
2018-09-25 19:09:27 -0400

    Added navigation functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// JS goes here
+// Navigation System
+document.querySelector('.nav-button').addEventListener('click', () => {
+    document.querySelector('.hamburger').classList.toggle('hidden');
+    document.querySelector('.close').classList.toggle('hidden');
+    document.querySelector('.menu').classList.toggle('hidden');
+    document.querySelector('.nav').classList.toggle('expanded-nav');
+});
+
+// Services Tab System
