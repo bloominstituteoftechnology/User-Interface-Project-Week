9273edc39ce607b9f36d39e9a4fc93b393b27c58
mikaelacurrier <mikaela.currier@gmail.com>
2018-09-25 18:41:38 -0400

    added navigation functionality - not pretty. Also added basic services page - no functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-// JS goes here
+// class Button {
+//     constructor(domElement) {
+//         this.domElement = domElement;
+//         this.expanded = document.querySelector('.nav');
+//         this.domElement.addEventListener('click', () => {
+//             this.expandNav();
+//         });
+//     }
+//         expandNav() {
+//             this.expanded.classList.toggle('expanded');
+//             console.log('works');
+//         }
+//     }
+
+// let navButton = document.querySelectorAll('.nav-button');
+// navButton = Array.from(navButton).map(domElement => new Button(domElement));
+
+const toggleNav = element => {
+    nav.classList.toggle('expanded');
+}
+
+const nav = document.querySelector('.nav');
+const navButton = document.querySelector('.nav-button');
+navButton.addEventListener('click', () => {toggleNav();
+});
