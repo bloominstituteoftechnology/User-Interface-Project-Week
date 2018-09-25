418ea09d8b6782344ba17466397ba3a0c58ac74b
Rahul Desai <rd825@georgetown.edu>
2018-09-25 12:33:47 -0400

    Fixing paths some more

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 let dropdown = document.querySelector(".dropdown-button");
 dropdown.addEventListener('click', function() {
     // change the hamburger to an X
-    dropdown.src = "../img/nav-hamburger-close.png";
+    dropdown.src = "./img/nav-hamburger-close.png";
     
     // toggle the class to hide the nav menu
     let content = document.querySelector(".dropdown-content");
@@ -10,7 +10,7 @@ dropdown.addEventListener('click', function() {
 
     // toggle the X back to hamburger
     if (content.classList.contains('dropdown-hidden') === true) {
-        dropdown.src = "../img/nav-hamburger.png";
+        dropdown.src = "./img/nav-hamburger.png";
     }
 });
 
@@ -25,7 +25,7 @@ window.addEventListener('resize', function() {
     let blocks = document.querySelector('#Blocks');
 
     if (width <= 500) {
-        bkgd.style.backgroundImage = "url('../img/home/home-mobile-jumbotron.png')";
+        bkgd.style.backgroundImage = "url('./img/home/home-mobile-jumbotron.png')";
         blueprint.src = "./img/home/home-mobile-img-1.png";
         reflection.src = "./img/home/home-mobile-img-2.png";
         villas.src = "./img/home/home-mobile-villas-img.png";
@@ -33,7 +33,7 @@ window.addEventListener('resize', function() {
         blocks.src = "./img/home/home-mobile-the-blocks-img.png";
     }
     else if (width > 500) {
-        bkgd.style.backgroundImage = "url('../img/home/home-jumbotron.png')";
+        bkgd.style.backgroundImage = "url('./img/home/home-jumbotron.png')";
         blueprint.src = "./img/home/home-img-1.png";
         reflection.src = "./img/home/home-img-2.png";
         villas.src = "./img/home/home-villas-img.png";
