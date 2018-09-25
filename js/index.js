1e6796fa9c2dce0b4cf99f85378872c02f97920c
txiong000 <tkx422@gmail.com>
2018-09-25 13:43:31 -0500

    finish functionality of burger nav button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // JS goes here
+
+//Navigation for home-page
+
+let burgerBtn = document.querySelector('.burger');
+let myHomepage = document.querySelector('.container');
+let copyRight = document.querySelector('.copyright');
+let openNav = document.querySelector('.open-nav');
+let closeBtn = document.querySelector('.opened-nav');
+burgerBtn.addEventListener('click', ()=> {
+    myHomepage.style.display = "none";
+    copyRight.style.display = "none";
+    openNav.style.display = "block";
+} )
+
+closeBtn.addEventListener('click', ()=> {
+    myHomepage.style.display = "block";
+    copyRight.style.display = "block";
+    openNav.style.display = "none";
+})
