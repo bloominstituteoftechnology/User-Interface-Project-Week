46b1c6ffe8cce3e6b919470e8a82fce87a7dbc25
theryanclausen <theryanclausen@gmail.com>
2018-09-25 11:46:50 -0700

    navigation js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+let btn = document.querySelector('.nav-button');
+let navItems = document.querySelector('.nav-items');
+
+btn.addEventListener('click', () => navToggle());
+
+const navToggle =() =>{
+    navItems.classList.toggle('active');
+    if (navItems.classList.contains('active')){
+    btn.src = 'img/nav-hamburger-close.png';
+    }
+    else {
+        btn.src = 'img/nav-hamburger.png';
+    }
+}
