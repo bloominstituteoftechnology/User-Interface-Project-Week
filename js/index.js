ace02df0c75ff2cf78401d034a4a80178905b353
Yusuf Nafey <yusufnafey@yahoo.com>
2018-09-25 15:49:40 -0700

    finished with navigation button JS, almost done with it's CSS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-// JS goes here
+class Overlay {
+    constructor(element) {
+        this.element = element
+
+        this.button = this.element.querySelector(".navButton")
+        
+        this.content = this.element.querySelector(".navContent")
+
+        this.button.addEventListener("click", ()=> {
+            this.toggleContent(event);
+        } )
+    }
+
+    toggleContent() {
+        this.content.classList.toggle("navHidden")
+    }
+}
+
+let overlay = document.querySelectorAll(".overlay")
+overlay = Array.from(overlay).map(overlay => new Overlay(overlay))
