d5a0d228076b385e0c54b01232e73fcac4c3aebf
cembreyfarquhar <cembreyfarquhar@gmail.com>
2018-09-24 19:51:28 -0500

    Adding hamburger functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // JS goes here
+
+
+// navigation opener    
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav')
+hamburger.addEventListener('click', () => {
+    nav.classList.remove('nav-hidden');
+    nav.classList.add('expanded');
+})
+
+
