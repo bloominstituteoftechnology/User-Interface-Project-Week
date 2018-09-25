bea39ca132461c45fa8a425a4ce8cbf2672105c1
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-09-25 17:29:57 -0400

    [chore] added no scroll when modal open

--- js/index.js
+++ js/index.js
@@ -3,11 +3,14 @@
 
 const toggleMenu = () => {
     menu.classList.toggle("menu-open");
+    body.classList.toggle("body-modal-open");
     
 };
 
 
 const menu = document.querySelector(".menu");
 const expandedMenu = document.querySelector(".expand-menu");
+const body = document.querySelector("body");
+console.log(body);
 
 expandedMenu.addEventListener("click", toggleMenu);
