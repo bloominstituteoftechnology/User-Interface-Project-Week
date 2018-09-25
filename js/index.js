99b3e032295a0d61477f9082824e724d5957b0f5
TRIF3X <thewizardofcrypto@gmail.com>
2018-09-25 10:42:05 -0500

    services page tab functionality

--- js/index.js
+++ js/index.js
@@ -5,3 +5,34 @@ for (let i = 0; i < selectBackground.length; i++) {
         event.target.style.color = '#FFF'
     }) 
 }
+
+class TabLink {
+    constructor(link) {
+        this.link = link;
+        this.linkData = this.link.dataset.tab;
+        this.tabContent = document.querySelector(`.middlePara[data-tab='${this.linkData}']`);
+        this.link.addEventListener('click', () => {this.linkClick()});
+        this.tabContent = new Content(this.tabContent);
+    }
+    linkClick() {
+        this.tabContent.toggleContent();
+    }
+}
+
+class Content {
+    constructor(tabContent){
+        this.tabContent = tabContent;
+    }
+    toggleContent() {
+        this.tabContent.classList.toggle('change');
+    }
+}
+
+
+
+
+
+
+
+
+links = Array.from(selectBackground).map( link => new TabLink(link));
