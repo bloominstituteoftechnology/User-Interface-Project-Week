453a6006673162a4e9113272bc8264a8251eab10
cembreyfarquhar <cembreyfarquhar@gmail.com>
2018-09-25 12:20:23 -0500

    Made nav and footer full width

--- js/index.js
+++ js/index.js
@@ -1,13 +1,54 @@
 // JS goes here
 
+// navigation opener
 
-// navigation opener    
+const hamburger = document.querySelector(".hamburger");
+const headerDiv = document.querySelector(".header");
+const nav = document.querySelector(".nav");
+const headerElement = document.querySelector("header");
+const container = document.querySelector(".container");
+hamburger.addEventListener("click", () => {
+  nav.classList.remove("nav-hidden");
+  headerDiv.classList.add("expanded");
+  nav.classList.add("expanded-nav");
+  headerElement.classList.add("expanded-header");
+});
 
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav')
-hamburger.addEventListener('click', () => {
-    nav.classList.remove('nav-hidden');
-    nav.classList.add('expanded');
-})
 
 
+
+
+
+document.addEventListener("keydown", e => {
+  if (e.keyCode === 66) {
+    document.addEventListener("keydown", e => {
+      if (e.keyCode === 65) {
+        document.addEventListener("keydown", e => {
+          if (e.keyCode === 67) {
+            document.addEventListener("keydown", e => {
+              if (e.keyCode === 75) {
+                document.addEventListener("keydown", e => {
+                  if (e.keyCode === 70) {
+                    document.addEventListener("keydown", e => {
+                      if (e.keyCode === 76) {
+                        document.addEventListener("keydown", e => {
+                          if (e.keyCode === 73) {
+                            document.addEventListener("keydown", e => {
+                              if (e.keyCode === 80) {
+                                //do a backflip 
+                              }
+                            });
+                          }
+                        });
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
