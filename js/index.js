d03a65d894a65f3503d732070dc1ae03b89c7caf
Jordan Stoddard <jjsincorporated@gmail.com>
2018-09-25 15:39:18 -0700

    [services page][complete] page design - tab navigator design - tab navigator functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
-// JS goes here
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.contentElement = document.querySelector(`.tab-content[data-tab='${this.data}']`);
+        this.tabContent = new TabContent(this.contentElement);
+
+        this.element.addEventListener('click', () => {this.select()});
+    }
+    select() {
+        let tab = document.querySelectorAll('.tab')
+
+        tab = Array.from(tab).forEach( item => {
+            item.classList.remove('tab-selected')
+        });
+    
+        this.element.classList.add('tab-selected')
+        this.tabContent.selectContent();
+    }
+}
+
+class TabContent {
+    constructor(element) {
+        this.element = element;
+    }
+    selectContent() {
+        let tabGrabber = document.querySelectorAll('.tab-content')
+        tabGrabber = Array.from(tabGrabber).forEach( item => {
+            item.classList.remove('tab-content-selected')
+        });
+        this.element.classList.add('tab-content-selected');
+    }
+}
+
+
+
+
+
+
+
+
+let tabs = document.querySelectorAll('.tab');
+
+tabs = Array.from(tabs).map(tabs => new TabLink(tabs));
+
+tabs[0].select();
+
+
