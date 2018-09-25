be6e591c6458c6b8aa764369fa8757372592a940
Katy Hollobaugh <khollobaugh@gmail.com>
2018-09-24 21:07:40 -0400

    menu toggle working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// JS goes here
+//Menu Toggle
+const toggleMenu = () => {
+    menu.classList.toggle('menu-open');
+  }
+
+  const menu = document.querySelector('.menu');
+
+  const menuButton = document.querySelector('.menu-button');
+
+  menuButton.addEventListener('click', () => {
+    toggleMenu();
+  });
+
+
+//Tab-Navigation
+
