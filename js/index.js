97a1332a273f679d7796dbf0aa805b7d1c56736d
Steven O'Neill <oneillsteven957@gmail.com>
2018-09-25 15:14:21 -0500

    Nav dropdown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // JS goes here
+class NavDropdown {
+    constructor(element) {
+      this.element = element;
+      this.button = this.element.querySelector('.nav-button');
+      this.content = this.element.querySelector('.nav-content');
+      this.closeButton = this.element.querySelector('.close-button')
+      this.button.addEventListener('click', (event) => {
+        this.toggleContent(event)
+      });
+      this.closeButton.addEventListener('click', (event) => {
+        this.toggleContent(event)
+      });
+    }
+  
+    toggleContent() {
+      
+      this.content.classList.toggle('nav-hidden');
+      this.button.classList.toggle('img-hide');
+      this.closeButton.classList.toggle('img-hide');
+    }
+  }
+  
+  let navDropdowns = document.querySelectorAll('.nav-bar');
+  navDropdowns = Array.from(navDropdowns).map( navDropdown => new NavDropdown(navDropdown));
