c1e482dbf6ff8c8f3a6362fa12307921fd4da6e1
Hargobind Atwal <ethaipath@gmail.com>
2018-09-25 13:45:27 -0700

    Basic nav functionality.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // JS goes here
+
+// toggle nav
+const navButton = document.getElementById('menu-button');
+const navBar = document.querySelector('.menu');
+navButton.addEventListener('click', function(event) {
+  navBar.parentNode.classList.toggle('nav-expanded');
+  navBar.classList.toggle('menu-show');
+})
