344f8e1214b4671f78b8c459239343dc6c4d190c
rytwalker <rytwalker@gmail.com>
2018-09-25 10:11:25 -0400

    Added functionality to navigation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-// JS goes here
+// EXPAND NAVIGATION
+const navigation = document.querySelector('.navigation');
+const navigationMenu = document.querySelector('.navigation__menu');
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.navigation__list');
+const navItems = document.querySelectorAll('.navigation__text');
+
+// Add event listener to navigationMenu
+navigationMenu.addEventListener('click', () => {
+  navigation.classList.toggle('navigation--expanded');
+  navigation.firstElementChild.classList.toggle(
+    'navigation__container--expanded'
+  );
+  navList.classList.toggle('navigation__list--expanded');
+  navItems.forEach(item => item.classList.toggle('navigation__text--expanded'));
+
+  // hamburger
+  hamburger.classList.toggle('closed');
+  if (hamburger.classList.contains('closed')) {
+    hamburger.src = 'img/nav-hamburger-close.png';
+  } else {
+    hamburger.src = 'img/nav-hamburger.png';
+  }
+});
