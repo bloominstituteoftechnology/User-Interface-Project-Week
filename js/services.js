fe15e5fc090b24aed73f4db1b2e37e9cb15ee82c
Cwright0428 <cwright0428@yahoo.com>
2018-09-25 02:21:12 -0500

    changes to modal bug fixes

--- js/services.js
+++ js/services.js
@@ -1,8 +1,9 @@
-const hamburger = document.querySelector(".navbar img");
+const hamburger = document.querySelector(".navbar .burger");
 const dropdown = document.querySelector(".menu-items");
 let visibility = false;
 
 hamburger.addEventListener("click", (event) => {
   visibility = dropdown.classList.toggle("menu-items_revealed");
-  !visibility ? hamburger.src = "./img/nav-hamburger.png" : hamburger.src = "./img/nav-hamburger-close.png";
+  !visibility ? hamburger.src = "../img/nav-hamburger.png" : hamburger.src = "../img/nav-hamburger-close.png";
+  console.log(I was clicked)
 })
