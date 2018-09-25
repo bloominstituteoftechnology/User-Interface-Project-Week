8cf55a61b2db057e144e35bd5f3fef36b90f3546
Michael Checo <checomichael2@gmail.com>
2018-09-25 11:13:08 -0400

    added navigation system design

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // JS goes here
+let menuButton = document.querySelector('.nav-bar img');
+menuButton.addEventListener('click', () => {
+   menu.classList.toggle('nav-bar-active');
+   menuButton.src.includes('img/nav-hamburger.png') ? menuButton.src = 'img/nav-hamburger-close.png' : menuButton.src = 'img/nav-hamburger.png';
+})
+let menu = document.querySelector('.nav-bar');
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open');
+}
