ee190ab10bab2b7fb8dff42c471393a4a1ce8101
TRIF3X <thewizardofcrypto@gmail.com>
2018-09-25 09:57:38 -0500

    added javascript on click services page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-// JS goes here
+let selectBackground = document.querySelectorAll('.backgroundChange')
+for (let i = 0; i < selectBackground.length; i++) {
+    selectBackground[i].addEventListener('click', () => {
+        event.target.style.background = '#5E9FB9'
+        event.target.style.color = '#FFF'
+    }) 
+}
