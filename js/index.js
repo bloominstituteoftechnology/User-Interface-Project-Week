727162cabfc8986ce7a7cdc662e0f17dac4fadc4
Mindysueasaurus <mymindyyeager@gmail.com>
2018-09-25 09:28:55 -0500

    got the drop down working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // JS goes here
+class Dropdown {
+  constructor(element){
+    this.element = element;
+    this.button = this.element.querySelector('.dropdown-button');
+    this.content = this.element.querySelector('.dropdown-content');
+    this.button.addEventListener('click', () => {
+      this.toggleContent() })
+    
+
+  }
+  toggleContent() {
+    this.content.classList.toggle('dropdown-hidden');
+  }
+}
+
+let dropdowns = document.querySelectorAll('.dropdown');
+dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
