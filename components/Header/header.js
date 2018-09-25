4e6439f0cd87977232515f1495428a90ae667394
Sarah Tennis <sarahtennis@Sarahs-MacBook-Air.local>
2018-09-25 16:39:35 -0400

    latest commit for help

--- components/Header/header.js
+++ components/Header/header.js
@@ -2,21 +2,30 @@ class Hamburger {
     constructor(element){
         this.element = element;
         this.active = false;
+        this.top = this.element.querySelector('.topBar');
+        this.middle = this.element.querySelector('.middleBar');
+        this.bottom = this.element.querySelector('.bottomBar');
+        this.modal = document.querySelector('.modal');
 
         this.element.addEventListener('click', () => this.clickFunction());
     }
 
     clickFunction () {
         if(this.active){
-            modal.style.display = 'none';
+            this.top.classList.toggle('change');
+            this.middle.classList.toggle('change');
+            this.bottom.classList.toggle('change');
+            this.modal.style.display = 'none';
             this.active = false;
         } else {
-            modal.style.display = 'flex';
+            this.top.classList.toggle('change');
+            this.middle.classList.toggle('change');
+            this.bottom.classList.toggle('change');
+            this.modal.style.display = 'flex';
             this.active = true;
         }
     }
 }
 
-let hamburger = document.querySelector('.header-hamburger');
+let hamburger = document.querySelector('.fixed-header-hamburger');
 hamburger = new Hamburger(hamburger);
-const modal = document.querySelector('.modal');
