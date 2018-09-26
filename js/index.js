29689758b6d22d5909e1d9e475576c620f18e527
Hunter315 <hunter.mckay.smith@gmail.com>
2018-09-26 10:56:24 -0600

    navMenu js and css in. need close button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-// JS goes here
+class Hamburger{
+    constructor(element) {
+        this.element = element;
+        this.button = this.element.querySelector('.menu-button');
+        this.content = this.element.querySelector('.menu-content');
+        this.button.addEventListener('click', () => {
+            this.toggleContent();
+            // console.log('youclicked');
+        })//end click listener
+
+    }//end constructor
+
+    toggleContent(){
+        this.content.classList.toggle('menu-hidden');
+    } //end toggleContent
+
+} // end Hamburger class
+
+let navMenu = document.querySelectorAll('.menu');
+navMenu = Array.from(navMenu).map(menu => new Hamburger(menu));
+
+
