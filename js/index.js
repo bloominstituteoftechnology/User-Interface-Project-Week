e442fe2f76497a7aec0cdf4a43d540c24838d408
Yanrong Xu <xuyanrong@icloud.com>
2018-09-26 15:13:02 -0400

    MVP complete

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 let hamburger = document.querySelector('.navigation img');
 // console.log(hamburger);
 let navigation = document.querySelector('.navigation');
-console.log(navigation);
+// console.log(navigation);
 let nav = document.querySelector('.navigation nav');
 // console.log(nav);
 
@@ -12,3 +12,40 @@ hamburger.addEventListener('click', () => {
         (hamburger.src = 'img/nav-hamburger-close.png') :
         hamburger.src = 'img/nav-hamburger.png';
 })
+
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.item = document.querySelector(`.tab-item[data-tab='${this.data}']`);
+        this.tabItem = new TabItem(this.item);
+        this.element.addEventListener('click', ()=> {
+            this.select();
+        })
+    }
+    select() {
+        const links = document.querySelectorAll('.tabs-links');
+        // console.log(links);
+        Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
+        this.element.classList.add('tabs-link-selected');
+        this.tabItem.select();
+    }
+}
+
+class TabItem {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select() {
+        const items = document.querySelectorAll('.tab-item');
+        console.log(items);
+        items.forEach(item => item.classList.remove('tabs-item-selected'))
+        this.element.classList.add('tabs-item-selected');
+    }
+}
+
+let links = document.querySelectorAll('.tabs-links');
+// console.log(links);
+links = Array.from(links).map(link => new TabLink(link));
+links[0].select()
