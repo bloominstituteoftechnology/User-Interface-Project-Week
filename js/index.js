240b6f5261bf9456e645ed164ef253ef88aaaa8b
Troy Amlee <troyamlee19@gmail.com>
2018-09-26 10:38:29 -0500

    [mvp] added dropdown to the nav bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 // JS goes here
+
+
+
+
+
+
+class Dropdowner {
+    constructor (element) {
+        console.log("this?")
+        this.element = element;
+
+        this.button = this.element.querySelector('.img-hamburger');
+
+        this.content = this.element.querySelector('.ninja-nav');
+
+        this.button.addEventListener('click', () => {
+            this.toggleContent();
+        })
+    }
+    toggleContent(){
+        console.log("does this work");
+        this.content.classList.toggle('ninja-hidden');
+        // this.button.classList.toggle('img-hamburger');
+        this.button.classList.toggle('img-x');
+    }
+
+
+}
+
+
+
+
+let dropdown = document.querySelectorAll(".tilted");
+console.log(dropdown);
+dropdown = Array.from(dropdown).map( dropdowner => new Dropdowner(dropdowner));
