b30eac55f03ad541bed30f67cc12ebf6ecbb7aad
Blake <koenneckerb@gmail.com>
2018-09-26 15:47:56 -0700

    Added animations to homepage and navigation.

--- js/index.js
+++ js/index.js
@@ -8,9 +8,16 @@ let open = false;
 hamburger.addEventListener('click', () => open === false ? openNav() : closeNav());
 
 const openNav = () => {
-    navbar.style.display = 'block';
+
     hamburger.src = "img/nav-hamburger-close.png";
     open = true;
+    tl
+        .staggerFrom('div.hidden li', 1, {
+            cycle: {
+                y: [-50],
+            }, autoAlpha: 0, ease: Power2.easeInOut
+        }, 0.1);
+    navbar.style.display = 'block';
 };
 
 const closeNav = () => {
@@ -18,3 +25,46 @@ const closeNav = () => {
     hamburger.src = "img/nav-hamburger.png";
     open = false;
 }
+
+// animations
+var tl = new TimelineLite();
+
+let controller = new ScrollMagic.Controller();
+
+
+let scene1 = new ScrollMagic.Scene({
+    triggerElement: "#trigger-element1"
+})
+.setTween(tl.staggerFrom(('div.left-text'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
+
+
+let scene2 = new ScrollMagic.Scene({
+    triggerElement: "#trigger-element2"
+})
+.setTween(tl.staggerFrom(('div.right-text'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
+
+let scene6 = new ScrollMagic.Scene({
+    triggerElement: "#trigger-element6"
+})
+.setTween(tl.staggerFrom(('section.bottom-content .top-h2'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
+
+let scene3 = new ScrollMagic.Scene({
+    triggerElement: "#trigger-element3"
+})
+.setTween(tl.staggerFrom(('div.top-third'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
+
+
+let scene4 = new ScrollMagic.Scene({
+    triggerElement: "#trigger-element4"
+})
+.setTween(tl.staggerFrom(('div.middle-third'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
+
+
+let scene5 = new ScrollMagic.Scene({
+    triggerElement: "#trigger-element5"
+})
+.setTween(tl.staggerFrom(('div.bottom-third'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
+
+
+controller.addScene([scene1, scene2, scene3, scene4, scene5, scene6]);
+
