5236fe7806a8508a732bba4bea3bbc60f39b5746
Jasmine Galang <jsmn.glng@gmail.com>
2018-09-25 17:49:10 -0700

    Added animation when toggling nav menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,1 @@
-// JS goes here
+TweenMax.from('body', 1.5, {y: -100, opacity:0, ease: Back.easeOut});
--- js/navigation.js
+++ js/navigation.js
@@ -3,9 +3,15 @@ const navExpand = document.querySelector('.navbar-expand');
 const navbar = document.querySelector('.fixed-navbar');
 const closeBtn = document.querySelector('.nav-toggler-close');
 
+let animateToggle = TweenMax.fromTo(navExpand, 0.5, {opacity: 0}, {opacity: 1}).reverse();
+
 function toggleNavMenu() {
   navExpand.classList.toggle('show-navbar-expand');
   navbar.classList.toggle('hide-navbar');
+
+  if (!animateToggle.isActive()) {
+    animateToggle.reversed(!animateToggle.reversed());
+  }
 }
 
 navToggler.addEventListener('click', toggleNavMenu);
