ed37d684707c06fa3dc750cfe4458a2997d9d96b
rytwalker <rytwalker@gmail.com>
2018-09-26 13:39:00 -0400

    Added working functionality to tab navigator carousel

--- js/tab-navigator.js
+++ js/tab-navigator.js
@@ -1,43 +1,57 @@
 class TabNavigator {
   constructor(tabNavigator) {
     this.tabNavigator = tabNavigator;
+    // TABS
     this.tabs = Array.from(document.querySelectorAll('.tab')).map(
       tab => new Tab(tab)
     );
+    // CURRENT TAB
     this.currentTab = this.tabs.filter(tab =>
       tab.tab.classList.contains('tab-active')
     )[0];
-
-    this.tabs.forEach(tab => {
-      tab.tab.addEventListener('click', () => {
-        this.currentTab.deselect();
-        tab.select();
-        this.currentTab = tab;
-      });
-    });
-
-    // this.imgs = Array.from(document.querySelectorAll('.img')).map(
-    //   img => new Img(img)
-    // );
-
-    // this.currentImg = this.imgs.filter(image =>
-    //   image.img.classList.contains('current-img')
-    // );
 
     // Buttons
     this.rightButton = document.querySelector('.right-button');
     this.leftButton = document.querySelector('.left-button');
 
-    // Button Event Listeners
-    // this.rightButton.addEventListener('click', () => this.imgSelect());
-    // this.leftButton.addEventListener('click', () => this.imgSelect());
+    // EVENT LISTENERS
+    this.tabs.forEach(tab => {
+      tab.tab.addEventListener('click', () => {
+        this.currentTab.deselect();
+        this.currentTab = tab;
+        this.currentTab.select();
+      });
+    });
 
-    // console.log(this.imgs[0].img);
-    // console.log(this.currentImg);
-  }
-  imgSelect() {
-    this.img.classList.add('current-img');
-    this.currentCard.select();
+    this.rightButton.addEventListener('click', () => {
+      this.tabs.forEach((tab, i) => {
+        if (tab.tab.classList.contains('current-img')) {
+          this.currentTab.deselect();
+          if (i === this.tabs.length - 1) {
+            this.currentTab = this.tabs[0];
+          } else {
+            console.log(i);
+            this.currentTab = this.tabs[i + 1];
+          }
+          this.currentTab.select();
+        }
+      });
+    });
+
+    this.leftButton.addEventListener('click', () => {
+      this.tabs.forEach((tab, i) => {
+        if (tab.tab.classList.contains('current-img')) {
+          this.currentTab.deselect();
+          if (i === 0) {
+            this.currentTab = this.tabs[this.tabs.length - 1];
+          } else {
+            console.log(i);
+            this.currentTab = this.tabs[i - 1];
+          }
+          this.currentTab.select();
+        }
+      });
+    });
   }
 }
 
