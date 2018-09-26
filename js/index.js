551bc4988ff7907bddf5dfc435c6ce524cc94e68
rytwalker <rytwalker@gmail.com>
2018-09-26 17:33:50 -0400

    Added greensock animation to navigation

--- js/index.js
+++ js/index.js
@@ -8,11 +8,28 @@ const navItems = document.querySelectorAll('.navigation__text');
 // Add event listener to navigationMenu
 navigationMenu.addEventListener('click', () => {
   navigation.classList.toggle('navigation--expanded');
-  navigation.firstElementChild.classList.toggle(
-    'navigation__container--expanded'
-  );
   navList.classList.toggle('navigation__list--expanded');
-  navItems.forEach(item => item.classList.toggle('navigation__text--expanded'));
+  // navItems.forEach(item => item.classList.toggle('navigation__text--expanded'));
+  if (navigation.classList.contains('navigation--expanded')) {
+    TweenMax.to(navigation, 0.25, {
+      height: '100vh',
+      ease: Power0.easeOut
+    });
+    TweenMax.to(navItems, 0.25, {
+      opacity: 1,
+      ease: Power0.easeOut,
+      delay: 0.5
+    });
+  } else {
+    TweenMax.to(navigation, 0.25, {
+      height: '5rem',
+      ease: Power0.easeOut
+    });
+    TweenMax.to(navItems, 0, {
+      opacity: 0,
+      ease: Power0.easeOut
+    });
+  }
 
   // hamburger
   hamburger.classList.toggle('closed');
@@ -39,4 +56,9 @@ window.addEventListener('resize', () => {
       img.src = `img/projects/project-slider-${i + 1}.jpg`;
     }
   });
+});
+
+window.addEventListener('scroll', () => {
+  const scrolled = window.scrollY;
+  console.log(scrolled);
 });
