b5ec21cbe55be712f17e9823b233749cd565f8c1
unknown <brandongrych@gmail.com>
2018-09-26 13:09:50 -0500

    All less files added, servecies completed, nav working as expected.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // JS goes here
+// menu model
+class HamburgerOpen {
+    constructor(element) {
+      this.element = element
+      this.element.addEventListener('click', () => this.openModal())
+    }
+     openModal() {
+      document.querySelector('.non-expanded').classList.add('modal-hidden')
+      document.querySelector('.expanded').classList.remove('modal-hidden')
+      document.querySelector('.expanded').classList.remove('modal-hidden')
+      document.querySelector('.menu-modal').classList.remove('modal-hidden')
+    }
+  }
+   class HamburgerClose {
+    constructor(element) {
+      this.element = element
+      this.element.addEventListener('click', () => this.closeModal())
+    }
+     closeModal() {
+      document.querySelector('.non-expanded').classList.remove('modal-hidden')
+      document.querySelector('.expanded').classList.add('modal-hidden')
+      document.querySelector('.expanded').classList.add('modal-hidden')
+      document.querySelector('.menu-modal').classList.add('modal-hidden')
+    }
+  }
+   let hamburgerNonExpanded = document.querySelector('header .non-expanded')
+  let hamburgerExpanded = document.querySelector('header .expanded')
+   hamburgerNonExpanded = new HamburgerOpen(hamburgerNonExpanded)
+  hamburgerExpanded = new HamburgerClose(hamburgerExpanded)
