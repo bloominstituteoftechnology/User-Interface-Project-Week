0779a4c129e3ecc76417b464a1a92a5904e7142f
Wanda <doubleuevans@gmail.com>
2018-09-26 15:22:09 -0500

    refactored navigaton html/less,css. Current: error in index.js line 10

--- js/index.js
+++ js/index.js
@@ -1,29 +1,12 @@
 // JS goes here
-class Dropdown {
-  constructor(dropdown) {
-
-  //assign this.element to the dropdown element
-  this.element = dropdown;
-
-  //get the element with the '.dropdown-button' class found in
-  //the dropdown element (html for context)
-  this.button = this.element.querySelector('.dropdown-button');
-
-  //assign the reference to the '.dropdown-content' class found in
-  //the dropdown element
-  this.content = this.element.querySelector('.dropdown-content');
-
-  //add a click handler to the button reference and
-  //toggleContent
-  this.button.addEventListener('click', () => this.toggleContent());
-  }
-
-  toggleContent() {
-    //toggle the '.dropdown-hidden' class
-    this.content.classList.toggle('dropdown-hidden');  
-  }
+const toggleMenu = () => {
+  dropdown.classList.toggle('dropdown-open');
 }
 
+const dropdown = document.querySelector('.dropdown-content');
 
-let dropdowns = document.querySelectorAll('.dropdown');
-dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+const button = document.querySelector('.dropdown-button');
+
+button.addEventListener('click', () => {
+  return toggleMenu(dropdown);
+});
