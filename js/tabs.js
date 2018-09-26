677f06fa770913b6b0a3e960d0ad3627a5b791af
Yusuf Nafey <yusufnafey@yahoo.com>
2018-09-26 10:27:48 -0700

    finished with JS tab functionality, need to style it

--- js/tabs.js
+++ js/tabs.js
@@ -2,17 +2,49 @@ class TabLink {
     constructor(element) {
         this.element = element
 
-        this.data = thiss.element.dataset.tab
+        this.data = this.element.dataset.tab
 
-        this.itemElement = document.querySelector(`.tabs`)
+        this.itemElement = document.querySelector(`.tabItem[data-tab="${this.data}"]`)
 
+        this.tabItem = new TabItem(this.itemElement)
 
+        this.element.addEventListener("click", () => {
+            this.select()
+        })
+
+    }
+    
+    select() {
+        const links = document.querySelectorAll(".tabLink")
+    
+        Array.from(links).forEach((link) => {
+            link.classList.remove("tabLinkSelected")
+        })
+
+        this.element.classList.add("tabLinkSelected")
+
+        this.tabItem.select()
+
+        console.log(this.element)
+        
     }
 }
 
+class TabItem {
+    constructor(element) {
+        this.element = element
+    }
 
+    select() {
+        let content = document.querySelectorAll(".tabItem")
 
+        Array.from(content).forEach((item) => {
+            item.classList.remove("tabItemSelected")
+        })
 
+        this.element.classList.add("tabItemSelected")
+    }
+}
 
 
 let links = document.querySelectorAll(".tabLink")
@@ -20,3 +52,5 @@ let links = document.querySelectorAll(".tabLink")
 links = Array.from(links).map(tab => {
     new TabLink(tab)
 })
+
+links[0].select();
