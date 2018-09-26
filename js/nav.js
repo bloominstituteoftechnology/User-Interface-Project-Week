f27a4a9cddd1f25511b252ec04d3a1512dfa45b7
Rahul Desai <rd825@georgetown.edu>
2018-09-26 18:02:25 -0400

    Animated nav links

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,8 @@
 let dropdown = document.querySelector(".dropdown-button");
 let content = document.querySelector(".dropdown-content");
 let header = document.querySelector("header");
+let links = document.querySelectorAll(".nav-item");
+
 content.style.opacity = '0';
 content.style.visibility = 'hidden';
 
@@ -17,10 +19,11 @@ dropdown.addEventListener('click', function() {
         dropdown.src = "./img/nav-hamburger.png";
         TweenMax.to(content, 0.5, {autoAlpha:0, y:0});
         header.style.opacity = "1";
-
     }
     else {
         TweenMax.to(content, 0.5, {autoAlpha:0.98, y:0});
         header.style.opacity = "0.98";
+        let tl = new TimelineMax();
+        links.forEach(x => tl.from(x, .2, {autoAlpha:0, x: -100}))
     }
 });
