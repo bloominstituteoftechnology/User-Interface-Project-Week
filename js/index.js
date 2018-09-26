92825be4859785684803b6490c7652cf1cc5a51e
Carlos Lantigua <clantigua954@gmail.com>
2018-09-26 12:57:49 -0400

    completed tabs and cards for service page

--- js/index.js
+++ js/index.js
@@ -7,49 +7,47 @@ class TabLink {
     this.link = link;
     // dataset numbers 1 - 4
     this.linkData = this.link.dataset.tab;
-    // content by dataset numbers
-    this.content = document.querySelector(`.content[data-tab="${this.linkData}"]`);
+
+    if (this.linkData === '0') {
+      this.cards = document.querySelector('.content');
+    } else {
+      // content by dataset numbers
+      this.cards = document.querySelectorAll(`.content[data-tab="${this.linkData}"]`);
+    }
+
+    this.cards = Array.from(this.cards).map(cards => new TabCard(cards));
+
     // event listener that calls method
-    this.link.addEventListener('click', (event) => {
+    this.link.addEventListener('click', () => {
       event.stopPropagation();
-      this.linkClick(event)
+      this.selectCard()
     });
     // create new content class
-    this.content = new Content(this.content);
   } // methods go here
-  linkClick() {
+  selectCard() {
     //call content toggle method
-    this.content.toggleContent();
+    const link = document.querySelectorAll('.link')
+    link.forEach(content => { content.classList.remove('active-link') });
+    this.link.classList.add("active-link");
+
+    const content = document.querySelectorAll('.content');
+    content.forEach(content => { content.style.display = "none" });
+    this.cards.forEach(content => content.selectCard());
+
   }
 } // end of TabLink
 
-class Content {
+class TabCard {
   constructor(content) {
     this.content = content;
   }
-  toggleContent() {
-    this.content.classList.toggle('change');
+  selectCard() {
+    this.content.style.display = "flex";
   }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let links = document.querySelectorAll('.link');
 links = Array.from(links).map(link => new TabLink(link));
+
+links[0].selectCard();
