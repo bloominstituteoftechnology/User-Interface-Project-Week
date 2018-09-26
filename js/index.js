d40295c8ab205baf458a5cf353ea9bc77a0108b1
SabrinaGear <sabrinagear.ft@gmail.com>
2018-09-25 20:23:03 -0500

    improvements to menu toggle

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 function openNav() {
     let expander = document.querySelector(".expander");
     expander.style.display = "block";
+    let hamburger = document.querySelector(".menu-button");
+    hamburger.style.display = "none";
 }
 
 function closeNav() {
     let expander = document.querySelector(".expander");
     expander.style.display = "none";
+    let hamburger = document.querySelector(".menu-button");
+    hamburger.style.display = "block";
 }
