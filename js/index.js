1fd55a57aa2e7b26d03c0992cd8ccad64e01afeb
Yanrong Xu <xuyanrong@icloud.com>
2018-09-26 13:51:22 -0400

    navigation system design complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // JS goes here
+let hamburger = document.querySelector('.navigation img');
+// console.log(hamburger);
+let navigation = document.querySelector('.navigation');
+console.log(navigation);
+let nav = document.querySelector('.navigation nav');
+// console.log(nav);
+
+hamburger.addEventListener('click', () => {
+    navigation.classList.toggle('navigation-click');
+    hamburger.src.includes('img/nav-hamburger.png') ?
+        (hamburger.src = 'img/nav-hamburger-close.png') :
+        hamburger.src = 'img/nav-hamburger.png';
+})
