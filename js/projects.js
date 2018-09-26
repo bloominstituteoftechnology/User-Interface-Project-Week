08e4a6d00e69e317b5bb35fd8deb514210323850
Rahul Desai <rd825@georgetown.edu>
2018-09-26 10:37:51 -0400

    Made significant progress on projects page desktop layout

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,55 @@
+class Carousel {
+    constructor(element){
+        this.element = element;
+        this.pics = document.querySelectorAll('.cImg');
+
+        this.left = this.element.firstElementChild;
+        this.left.addEventListener('click', () => {this.goLeft()});
+
+        this.right = this.element.lastElementChild;
+        this.right.addEventListener('click', () => {this.goRight()})
+        this.i = 0;
+    }
+
+    goLeft(){
+        if (this.i === 0) {
+            this.i = 3;
+        }
+        else {
+            this.i--;
+        }
+        this.pics.forEach(img => {
+            img.style.visibility = 'hidden';
+        });
+        this.pics[this.i].style.visibility = 'visible';
+    };
+
+    goRight(){
+        if (this.i === 3) {
+            this.i = 0;
+        }
+        else {
+            this.i++;
+        }
+        this.pics.forEach(img => {
+            img.style.visibility = 'hidden';
+        });
+        this.pics[this.i].style.visibility = 'visible';
+    };
+}
+
+let carousel = document.querySelector('.carousel');
+let imgCarousel = new Carousel(carousel);
+imgCarousel.pics.forEach(item => item.visibility = 'hidden');
+imgCarousel.pics[0].style.visibility = 'visible';
+
+
+
+
+
+
+
+
 // Navigation JS -----------------------------------------
 let dropdown = document.querySelector(".dropdown-button");
 let content = document.querySelector(".dropdown-content");
