85d7b3e59df9be4e5ed22745c6032b346bbe17c6
Rahul Desai <rd825@georgetown.edu>
2018-09-26 18:22:02 -0400

    Prevent scrolling on nav open

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,7 @@ let dropdown = document.querySelector(".dropdown-button");
 let content = document.querySelector(".dropdown-content");
 let header = document.querySelector("header");
 let links = document.querySelectorAll(".nav-item");
+let body = document.querySelector('body');
 
 content.style.opacity = '0';
 content.style.visibility = 'hidden';
@@ -19,11 +20,13 @@ dropdown.addEventListener('click', function() {
         dropdown.src = "./img/nav-hamburger.png";
         TweenMax.to(content, 0.5, {autoAlpha:0, y:0});
         header.style.opacity = "1";
+        body.style.overflow = 'visible';
     }
     else {
         TweenMax.to(content, 0.5, {autoAlpha:0.98, y:0});
         header.style.opacity = "0.98";
         let tl = new TimelineMax();
         links.forEach(x => tl.from(x, .2, {autoAlpha:0, x: -100}))
+        body.style.overflow = 'hidden';
     }
 });
