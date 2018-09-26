c2606ea969d5305b919c13830ae869faadd4ca2e
Cwright0428 <cwright0428@yahoo.com>
2018-09-26 12:34:31 -0500

    proj complete!

--- js/services.js
+++ js/services.js
@@ -1,39 +1,65 @@
 class TabNav {
-    constructor(tabLinks, tabContent) {
-        this.tabLinks = Array.from(tabLinks).map((eachTab) => {
-            eachTab.addEventListener('click', (event) => this.updateActive(event.target));
-            return eachTab;
-        });
-        this.tabContent = tabContent;
-        this.activeTab = this.tabLinks[0];
-        this.activeContent = this.tabContent.querySelector("[data-content='0']");
-        this.init();
+    constructor(tabLink) {
+        this.tabLink = tabLink;
+        this.dataTab = this.tabLink.dataset.tab;
+        this.tabContent = document.querySelector(`.tab-content[data-tab='${this.dataTab}']`)
+        this.tabLink.addEventListener('click', (event) => {
+            this.selected(event);
+        })
+        // this.tabLinks = Array.from(tabLinks).map((eachTab) => {
+        //     eachTab.addEventListener('click', (event) => this.updateActive(event.target));
+        //     return eachTab;
+        // });
+        // this.tabContent = tabContent;
+        // this.activeTab = this.tabLinks[0];
+        // this.activeContent = this.tabContent.querySelector("[data-content='0']");
+        // this.init();
+    };
+    selected() {
+        let tabContent = document.querySelectorAll('.tab-content');
+        let tabLinks = document.querySelectorAll('.each-tab');
+
+        tabLinks.forEach(tab => tab.classList.remove('tab-selected'))
+        this.tabLink.classList.add('tab-selected');
+        tabContent.forEach(tab => tab.classList.add('hide'))
+        this.tabContent.classList.remove('hide');
+
     };
 
-    init() {
-        this.activeTab.classList.add('tab_selected');
-        this.activeContent.classList.add('tab-content_revealed');
-    };
-
-
-    updateActive(selectedTab) {
-        this.activeTab.classList.remove('tab_selected');
-        this.activeTab = selectedTab;
-        this.activeTab.classList.add('tab_selected');
-
-        const tabNumber = this.activeTab.dataset.tab;
-        const selectedContent = this.tabContent.querySelector("[data-content='0']");
-
-        this.activeContent.classList.remove('tab-content_revealed');
-        this.activeContent = selectedContent;
-        this.activeContent.classList.add('tab-content_revealed');
+    // init() {
+    //     this.activeTab.classList.add('tab-selected');
+    //     this.activeContent.classList.add('tab-content_revealed');
+    // };
 
 
 
-
-    };
 };
+let tabLinks = document.querySelectorAll('.each-tab');
 
-const tabLinks = document.querySelectorAll('.each-tab');
-const tabContent = document.querySelector('.all-tab-content');
-let tabNav = new TabNav(tabLinks, tabContent);
+tabLinks = [...tabLinks].map(each => new TabNav(each))
+console.log(tabLinks)
+tabLinks[0].selected();
+
+
+
+
+
+
+
+
+// function onTabClick(event) {
+//     let activeTabs = document.querySelectorAll('.each-tab');
+
+
+//     activeTabs.forEach(function(tab) {
+//       tab.className = tab.className.replace('each-tab', '');
+//     });
+
+//     // activate new tab and panel
+//     event.target.parentElement.className += ' each-tab';
+//     document.getElementById(event.target.href.split('#')[1]).className += ' each-tab';
+//   }
+
+//   const element = document.getElementById('nav-tab');
+
+//   element.addEventListener('click', onTabClick, false);
