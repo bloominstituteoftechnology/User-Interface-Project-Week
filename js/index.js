66c71db7aaf60412194d6f588a1821e49da4b972
theryanclausen <theryanclausen@gmail.com>
2018-09-25 19:46:48 -0700

    mvp

--- js/index.js
+++ js/index.js
@@ -13,3 +13,50 @@ const navToggle =() =>{
         btn.src = 'img/nav-hamburger.png';
     }
 }
+
+class Content{
+    constructor(element){
+        this.element = element;
+        this.data = this.element.dataset.tab;
+    }
+}
+
+
+
+let contents = document.querySelectorAll('.tabbed-content');
+
+contents = Array.from(contents).map(x => new Content(x))
+
+
+
+class Tab{
+    constructor(element){
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        contents.forEach(x => {
+            if (x.data == this.data){
+                this.content = x.element;
+                return;
+            }
+        })
+        this.element.addEventListener('click', () => this.selector())
+    }
+    selector(){
+
+        tabs.forEach(x => {
+            x.element.classList.remove('active-tab');
+            x.content.classList.remove('active-content');
+        })
+        this.element.classList.toggle('active-tab');
+        this.content.classList.toggle('active-content');
+    }
+}
+
+
+
+
+
+let tabs = document.querySelectorAll('.tab');
+
+tabs = Array.from(tabs).map(x => new Tab(x));
+
