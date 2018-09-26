e5c2408a5793a0f0bca991e1e15cc6d1163337f9
Carlos Lantigua <clantigua954@gmail.com>
2018-09-26 12:06:07 -0400

    started functionality for the services tabs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,55 @@
 // JS goes here
-// variables here
-//const home
+
+// Services page tabs
+
+class TabLink {
+  constructor(link) {
+    this.link = link;
+    // dataset numbers 1 - 4
+    this.linkData = this.link.dataset.tab;
+    // content by dataset numbers
+    this.content = document.querySelector(`.content[data-tab="${this.linkData}"]`);
+    // event listener that calls method
+    this.link.addEventListener('click', (event) => {
+      event.stopPropagation();
+      this.linkClick(event)
+    });
+    // create new content class
+    this.content = new Content(this.content);
+  } // methods go here
+  linkClick() {
+    //call content toggle method
+    this.content.toggleContent();
+  }
+} // end of TabLink
+
+class Content {
+  constructor(content) {
+    this.content = content;
+  }
+  toggleContent() {
+    this.content.classList.toggle('change');
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let links = document.querySelectorAll('.link');
+links = Array.from(links).map(link => new TabLink(link));
