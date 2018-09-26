383fa30ca171a06e80aab10889492d606a63187b
Justin <jdlow2@gmail.com>
2018-09-25 21:42:39 -0400

    completed expanding nav functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// JS goes here
+const navButton = document.querySelector('#nav-icon');
+const navBar = document.querySelector('.main-nav');
+
+navButton.addEventListener('click', function() {
+  if (navBar.classList.contains('fullscreen-nav')) {
+    navBar.classList.remove('fullscreen-nav');
+    navButton.src = 'img/nav-hamburger.png';
+  }
+
+  else {
+  navBar.classList.add('fullscreen-nav');
+  navButton.src = 'img/nav-hamburger-close.png';
+  }
+});
