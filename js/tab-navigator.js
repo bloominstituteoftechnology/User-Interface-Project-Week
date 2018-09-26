054237b3c1e38f53e83eb13d11953af68ca6b606
rytwalker <rytwalker@gmail.com>
2018-09-26 13:01:15 -0400

    Refactored tab-navigator

--- js/tab-navigator.js
+++ js/tab-navigator.js
@@ -4,10 +4,40 @@ class TabNavigator {
     this.tabs = Array.from(document.querySelectorAll('.tab')).map(
       tab => new Tab(tab)
     );
-    this.imgs = Array.from(document.querySelectorAll('.img')).map(
-      img => new Img(img)
-    );
-    console.log(this.imgs);
+    this.currentTab = this.tabs.filter(tab =>
+      tab.tab.classList.contains('tab-active')
+    )[0];
+
+    this.tabs.forEach(tab => {
+      tab.tab.addEventListener('click', () => {
+        this.currentTab.deselect();
+        tab.select();
+        this.currentTab = tab;
+      });
+    });
+
+    // this.imgs = Array.from(document.querySelectorAll('.img')).map(
+    //   img => new Img(img)
+    // );
+
+    // this.currentImg = this.imgs.filter(image =>
+    //   image.img.classList.contains('current-img')
+    // );
+
+    // Buttons
+    this.rightButton = document.querySelector('.right-button');
+    this.leftButton = document.querySelector('.left-button');
+
+    // Button Event Listeners
+    // this.rightButton.addEventListener('click', () => this.imgSelect());
+    // this.leftButton.addEventListener('click', () => this.imgSelect());
+
+    // console.log(this.imgs[0].img);
+    // console.log(this.currentImg);
+  }
+  imgSelect() {
+    this.img.classList.add('current-img');
+    this.currentCard.select();
   }
 }
 
@@ -18,18 +48,15 @@ class Tab {
     this.currentCard = new Card(
       document.querySelector(`.card[data-tab='${this.dataTab}']`)
     );
-    this.tab.addEventListener('click', () => this.select());
   }
   select() {
-    this.deselect();
     this.tab.classList.add('tab-active');
     this.currentCard.select();
   }
 
   deselect() {
-    // this.tab.classList.remove('tab-active');
-    let tabs = document.querySelectorAll('.tab');
-    tabs.forEach(tab => tab.classList.remove('tab-active'));
+    this.tab.classList.remove('tab-active');
+    this.currentCard.deselect();
   }
 }
 
@@ -37,20 +64,14 @@ class Img extends Tab {
   constructor(img) {
     super(img);
     this.img = img;
-    // Buttons
-    this.rightButton = document.querySelector('.right-button');
-    this.leftButton = document.querySelector('.left-button');
-
-    // Button Event Listeners
-    this.rightButton.addEventListener('click', () => this.imgSelect());
-    this.leftButton.addEventListener('click', () => this.imgSelect());
   }
 
-  imgSelect() {
-    if (this.img.classList.contains('current-img')) {
-      this.currentCard.select();
-    }
-  }
+  // imgSelect() {
+  //   let imgs = document.querySelectorAll('.img');
+  //   imgs.forEach(img => img.classList.remove('card-active'));
+  //   this.img.classList.add('current-img');
+  //   this.currentCard.select();
+  // }
 }
 
 class Card {
@@ -59,14 +80,11 @@ class Card {
   }
 
   select() {
-    this.deselect();
     this.card.classList.add('card-active');
   }
 
   deselect() {
-    // this.card.classList.remove('card-active');
-    let cards = document.querySelectorAll('.card');
-    cards.forEach(card => card.classList.remove('card-active'));
+    this.card.classList.remove('card-active');
   }
 }
 
