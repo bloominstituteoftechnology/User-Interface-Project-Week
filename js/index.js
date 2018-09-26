d184ee831e1553feb6770f8a66651c1dc128ec17
ssmodish <smodish@hotmail.com>
2018-09-26 16:19:07 -0400

    Going to have to call for help, pomodoro says long break now though

--- js/index.js
+++ js/index.js
@@ -29,33 +29,51 @@
 //\\//\\//\\//\\//\\//\\//\\
 class TabNavigatorLink {
   constructor(element) {
+    // assign this.element to the element reference
     this.element = element;
-    this.tabData = this.element.dataset.tab;
-    this.tabContents = document.querySelector(`.tab--content[data-tab='${this.tabData}']`);
-    this.tabContents = tabContent => new TabContent(tabContent);
-    console.log(this.tabContents);
 
+    // get the tab data attribute and save the value
+    this.tabData = this.element.dataset.tab;
+console.log(this.tabData);
+
+    // find all elements with the .tab--content class that correspond to the tab data attribute
+    // (there should only be one for each tab)
+    this.tabContent = document.querySelectorAll(`.tab--content[data-tab='${this.tabData}']`);
+    console.log(this.tabContent);
+
+
+    // Map over tabContents array and convert to TabContent
+    this.tabContent = Array.from(this.tabContent).map(content => new TabContent(content));
+console.log(this.tabContent);
+    // add click event that invokes selectTab
     this.element.addEventListener('click', (e) => { this.selectTab(e) });
   }
 
   selectTab() {
+    // select all elements with the .tab class
     const tabs = document.querySelectorAll('.tab');
 
+    // remove the .tab--selected class from each of them
     tabs.forEach(tab => tab.classList.remove('tab--selected'));
 
+    // add .tab--selected to this.element
     this.element.classList.add('tab--selected');
 
+    // select all of the contents elements
     const contents = document.querySelectorAll('.tab--content');
+console.log(this.contents);
+    // iterate through the nodelist setting display to none
+    contents.forEach(content => content.style.display= 'none');
 
-    contents.forEach(content => content.style.display= 'inline');
-    this.tabContents.selectContent();
+    // call selectContent on all of the contents
+    this.contents.forEach(content => selectContent());
   }
 }
 
 class TabContent {
   constructor(element) {
     this.element = element;
-    console.log('contents');
+    console.log(this.element);
   }
   selectContent() {
     this.element.style.display = '';
@@ -65,3 +83,5 @@ class TabContent {
 let tabs = document.querySelectorAll('.tab');
 
 tabs = Array.from(tabs).map(tab => new TabNavigatorLink(tab));
+
+tabs[0].selectTab();
