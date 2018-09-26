efa11e5718da29c079ec1c39569f929b5a79afd6
MEBonham <programmer@mckaybonham.com>
2018-09-26 10:06:22 -0600

    Added open/close code for nav and used sessionStorage to make open nav persist

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // JS goes here
+const navHamburger = document.querySelector(".nav-hamburger");
+const closeNav = document.querySelector(".close-nav");
+const navContent = document.querySelector(".open-nav");
+
+function toggleNav() {
+    navContent.classList.toggle("hidden");
+
+    if (navContent.classList.contains("hidden")) {
+        navHamburger.style.display = "block";
+        closeNav.style.display = "none";
+        sessionStorage.setItem("openNav", "false");
+    } else {
+        navHamburger.style.display = "none";
+        closeNav.style.display = "block";
+        sessionStorage.setItem("openNav", "true");
+    }
+}
+
+navHamburger.addEventListener("click", toggleNav);
+closeNav.addEventListener("click", toggleNav);
+
+window.onload = function() {
+    if (sessionStorage.openNav) {
+        navContent.classList.remove("hidden");
+        navHamburger.style.display = "none";
+        closeNav.style.display = "block";
+    }
+}
