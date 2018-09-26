988a7b48cc2b09659ee08a372fd3bc523e723d7a
SavannahGreuel <smallidgesavannah@gmail.com>
2018-09-26 12:17:45 -0600

    fixed a bug on the home page

--- js/index.js
+++ js/index.js
@@ -24,41 +24,33 @@ close.addEventListener('click', () => {
 
 // Tab Navigator Component 
 
-class Tabs {
-    constructor(element){
+class TabLink {
+  constructor(element) {
       this.element = element;
-      this.tabData = element.dataset.tab;
-      this.tabContent = document.querySelector(`.card [data-tab='${this.tabData}']`);
-      this.tabContent = new TabContent(this.tabContent);
-      this.element.addEventListener('click', () => {
-        this.select();
-      })
-    }
-    select() {
-      const tabs = document.querySelectorAll('.tab');
-      Array.from(tabs).forEach(tab =>
-      tab.classList.remove('active-tab'));
-      this.element.classList.add('active-tab');
-      this.tabContent.select();
-    }
-  };
-  
-  class TabContent {
-    constructor(element) {
-      this.element = element;
-    }
-    select() {
-      const allContent = document.querySelectorAll('.card');
-      Array.from(allContent).forEach(item =>
-      item.classList.remove('active-tab'));
+      this.data = this.element.dataset.tab;
+       this.itemElement = document.querySelector(`.tab[data-tab="${this.data}"]`);
+      this.tabItem = new TabItem(this.itemElement);
       
-      this.element.classList.add('active-tab');
-    }
+      this.element.addEventListener('click', (event) => {this.select(); event.preventDefault();});
+  };
+   select() {
+      const tabLinks = document.querySelectorAll('.tab');
+      Array.from(tabLinks).forEach(item => item.classList.remove('tab-link-selected'));
+       this.element.classList.add('tab-link-selected');
+      this.tabItem.select();
   }
-  
-  let tabs = document.querySelectorAll('.tab');
-  
-  tabs = Array.from(tabs).map(tab =>
-  new Tabs(tab));
-  
-  tabs[0].select();
+}
+class TabItem {
+  constructor(element) {
+      this.element = element;
+  }
+   select() {
+      let items = document.querySelectorAll('.card');
+      items.forEach(item => item.classList.remove('tab-item-selected'));
+       this.element.classList.add('tab-item-selected');
+      
+  }
+}
+let links = document.querySelectorAll('.tab');
+links = Array.from(links).map(link => {return new TabLink(link);});
+// links[0].select();
