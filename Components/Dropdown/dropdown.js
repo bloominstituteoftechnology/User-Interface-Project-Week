bb1477eb12e020f2605d339bdadd008ccaad7a0c
Jordan Stoddard <jjsincorporated@gmail.com>
2018-09-26 13:37:42 -0700

    fixed double opacity at nav dropdown.

--- Components/Dropdown/dropdown.js
+++ Components/Dropdown/dropdown.js
@@ -1,10 +1,12 @@
+this.header = document.querySelector('.headerWrapper')
+console.log(header)
+
 class Dropdown {
     constructor(element) {
         this.element = element;
         this.button = this.element.querySelector('.nav-hamburger');
-        this.closeButton = this.element.querySelector('.nav-hamburger-X')
+        this.closeButton = this.element.querySelector('.nav-hamburger-X');
         this.content = this.element.querySelector('.nav-dropdown');
-        console.log(this.closeButton)
         this.button.addEventListener('click', () => {
             this.toggleContent();
         });
@@ -14,15 +16,15 @@ class Dropdown {
     }
     toggleContent() {
        this.content.classList.toggle('nav-hidden');
-       this.button.classList.toggle('nav-hamburger-hidden')
-       this.closeButton.classList.toggle('nav-hamburger-X-visible')
-       this.element.style.opacity = ".96"
+       this.button.classList.toggle('nav-hamburger-hidden');
+       this.closeButton.classList.toggle('nav-hamburger-X-visible');
+       header.style.opacity = '.96';
        
     }
     closeContent() {
-        this.content.classList.toggle('nav-hidden')
-        this.closeButton.classList.toggle('nav-hamburger-X-visible')
-        this.button.classList.toggle('nav-hamburger-hidden')
+        this.content.classList.toggle('nav-hidden');
+        this.closeButton.classList.toggle('nav-hamburger-X-visible');
+        this.button.classList.toggle('nav-hamburger-hidden');
     }
 }
 
