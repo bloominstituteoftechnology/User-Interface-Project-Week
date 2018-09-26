938aaeb211accec151df2ccd4913addf026baa89
Troy Amlee <troyamlee19@gmail.com>
2018-09-26 12:04:27 -0500

    [mvp] completed tab system in services page

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 // JS goes here
 
-
-
-
-
-
 class Dropdowner {
     constructor (element) {
         console.log("this?")
@@ -34,3 +29,106 @@ class Dropdowner {
 let dropdown = document.querySelectorAll(".tilted");
 console.log(dropdown);
 dropdown = Array.from(dropdown).map( dropdowner => new Dropdowner(dropdowner));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Tabber {
+    constructor(element) {
+        this.element = element;
+
+        this.tabData = this.element.dataset.tab;
+
+        this.itemElement = document.querySelectorAll(`.article[data-tab='${this.tabData}']`);
+
+        this.article = this.itemElement;
+
+        this.article = Array.from(this.article).map(element => new TabArticle(element));
+        
+        this.element.addEventListener('click', () => {
+            this.selectTab();
+          });
+        };
+      
+        selectTab(){
+      
+          // Select all elements with the .tab class on them
+          const tabs = document.querySelectorAll('.tab');
+          // Iterate through the NodeList removing the .active-tab class from each element
+          Array.from(tabs).forEach(function(item){
+            item.classList.remove("active-tab");
+          })
+          // Add a class of ".active-tab" to this.element
+          
+          this.element.classList.add('active-tab');
+          
+      
+          // Select all of the elements with the .card class on them
+          const article = document.querySelectorAll('.article');
+          // Iterate through the NodeList setting the display style each one to 'none'
+          Array.from(article).forEach(function(item){
+            item.style.display = "none";
+          })
+          // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
+          this.article.forEach(articles => articles.selectArticle());
+          
+      
+          
+        }
+      }
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class TabArticle {
+    constructor(element){
+      // Assign this.element to the passed in element.
+      this.element = element;
+  
+    }
+    selectArticle(){
+      // Update the style of this.element to display = null
+      this.element.style.display = "flex"
+      
+      
+      
+      
+    }
+  
+    
+  
+  }
+  
+
+
+let tabs = document.querySelectorAll('.tab');
+tabs = Array.from(tabs).map(element => new Tabber(element));
