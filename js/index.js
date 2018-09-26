f971901166c32252e7bc244edce8f5955e0d30a5
MEBonham <programmer@mckaybonham.com>
2018-09-26 10:48:47 -0600

    Commented javascript code for clarity

--- js/index.js
+++ js/index.js
@@ -23,6 +23,7 @@ function toggleNav() {
 navHamburger.addEventListener("click", toggleNav);
 closeNav.addEventListener("click", toggleNav);
 
+// Preserve the navigation menu's being open when switching to a new page
 window.onload = function() {
     if (sessionStorage.openNav === "true") {
         navContent.classList.remove("hidden");
@@ -36,9 +37,13 @@ window.onload = function() {
 class Tabs {
     constructor(element) {
         this.element = element;
+
+        // Gather all tab links into a child item
         this.links = this.element.querySelectorAll(".tab-link");
         this.links = Array.from(this.links).map( link => new TabLink(link, this) );
-        if (this.links) {
+        
+        // Select the first tab by default
+        if (this.links) { // (In case there are no tabs on the page)
             this.links[0].select();
         }
     }
@@ -58,21 +63,34 @@ class TabLink {
     constructor(element, parent) {
         this.element = element;
         this.parent = parent;
+
+        // Get the custom data index on the link
         this.data = this.element.dataset.tab;
+
+        // Make a child object representing the tab's content
         this.contentElement = document.querySelector(`.tab-content[data-tab="${this.data}"]`);
         this.content = new TabContent(this.contentElement);
+
+        // Add a click event listener to select this tab
         this.element.addEventListener("click", (event) => this.select(event));
     }
 
     select() {
+        // Deselect the current selected tab, if any
         this.parent.deselectCurrent();
+
+        // Select this tab and its content-child
         this.element.classList.add("selected-tab");
         this.content.select();
+
+        // Report to the controller that this tab is now selected
         this.parent.redefineSelectedTab(this);
     }
 
     deselect() {
         this.element.classList.remove("selected-tab");
+
+        // Deselect content-child
         this.content.deselect();
     }
 }
