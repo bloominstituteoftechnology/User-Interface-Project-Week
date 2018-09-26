202ddb67ebd3686142275a611a6667746ea67a19
bkwillia052 <bkwillia052@gmail.com>
2018-09-26 11:09:04 -0500

    Services page desktop design and JS functionality complete.

--- js/index.js
+++ js/index.js
@@ -22,22 +22,81 @@ function toHam(){
         hamburger.addEventListener('click', fromHam, {once: true});
     }
 }
+// ----- SERVICE PAGE COMPONENTS ----- \\
 
+//TAB COMPONENT
 class Tab {
     constructor(element){
 
         this.element = element;
+        console.log(this.element);
+        this.name = element.name;
+        console.log(this.name);
+        this.data = element.dataset.tab;
+        console.log(this.data);
+        this.content = document.querySelector(`.content[data-tab="${this.data}"]`);
+        this.tabContent = new TabContent(this.content);
+        
 
 
-        this.element.addEventListener('click', )
+        this.element.addEventListener('click', () => { this.select()}); 
 
+    }
+
+    select(){
+        const tabs = document.querySelectorAll('.tab');
+        tabs.forEach( function(item){
+            item.classList.remove('tab-selected');
+        });
+        event.target.classList.add("tab-selected");
+
+        this.tabContent.select();
     }
 }
 
-class TabItem {
-    constructor(element){
+//TAB CONTENT COMPONENT
+class TabContent {
+    constructor(content){
+
+        this.content = content;
+        
+        
 
     }
+
+    select(){
+        let allTabContent = document.querySelectorAll(".content");
+        
+        allTabContent.forEach( item => 
+           item.classList.remove('shown'));
+
+        this.content.classList.add('shown');   
+    }
 }
+
+
+let tabs = document.querySelectorAll('.tab');
+
+tabs = Array.from(tabs).map((item) => new Tab(item));
+
+let tabContent = document.querySelectorAll(".content");
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(tabs);
+
+/* console.log(tabContent[0].children[0].innerText);
+
+console.log(tabs[0]); */
 
 
