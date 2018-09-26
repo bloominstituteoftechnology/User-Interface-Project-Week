ac5fe5e24676fcc9d1d78c35156eee73b660912a
mikaelacurrier <mikaela.currier@gmail.com>
2018-09-26 11:30:13 -0400

    added js to services page- still needs adaptable CSS

--- js/index.js
+++ js/index.js
@@ -1,20 +1,3 @@
-// class Button {
-//     constructor(domElement) {
-//         this.domElement = domElement;
-//         this.expanded = document.querySelector('.nav');
-//         this.domElement.addEventListener('click', () => {
-//             this.expandNav();
-//         });
-//     }
-//         expandNav() {
-//             this.expanded.classList.toggle('expanded');
-//             console.log('works');
-//         }
-//     }
-
-// let navButton = document.querySelectorAll('.nav-button');
-// navButton = Array.from(navButton).map(domElement => new Button(domElement));
-
 const toggleNav = element => {
     nav.classList.toggle('expanded');
 }
@@ -23,3 +6,66 @@ const nav = document.querySelector('.nav');
 const navButton = document.querySelector('.nav-button');
 navButton.addEventListener('click', () => {toggleNav();
 });
+
+//SERVICES PAGE TAB SELECTION
+
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.links = document.querySelectorAll('.tab')
+        this.links = Array.from(this.links).map((link) => {
+            return new Link(link, this);
+        })
+        this.activeLink = this.links[0];
+        this.init()
+    }
+        init() {
+            this.activeLink.select()
+        }
+        updateActive(newActive) {
+            this.activeLink.deselect()
+            this.activeLink = newActive
+        }
+        getTab (data) {
+           return this.element.querySelector(`.card[data-tab='${data}']`)
+        }
+
+
+}
+class Link {
+    constructor(link, parent) {
+        this.element = link
+        this.tabs = parent
+        this.card = parent.getTab(this.element.dataset.tab)
+        this.card = new TabsCard(this.card)
+        this.element.addEventListener('click', () => {
+            this.tabs.updateActive(this)
+            this.select()
+            console.log('yeah')
+        })
+        console.log(this.element)
+    }
+    select() {
+        this.element.classList.add('.card')
+        this.card.select()
+    }
+    deselect() {
+        this.element.classList.add('.card')
+        this.card.deselect()
+    }
+}
+class TabsCard {
+    constructor(tabCard) {
+        this.element = tabCard
+    }
+    select() {
+        this.element.classList.add('.card')
+    }
+    deselect() {
+        this.element.classList.remove('.card')
+    }
+}
+
+let tabs = document.querySelectorAll('.tabs');
+tabs = Array.from(tabs).map(domElement => new TabLink(domElement));
+console.log(tabs);
