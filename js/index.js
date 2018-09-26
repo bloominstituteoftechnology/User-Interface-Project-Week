2d0bdea8066e6cd2cdf2ddeae036a9d5c5ef3979
Justin <jdlow2@gmail.com>
2018-09-25 22:56:33 -0400

    first pass on mobile home page

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 const navButton = document.querySelector('#nav-icon');
 const navBar = document.querySelector('.main-nav');
 
-navButton.addEventListener('click', function() {
+function expandNavBar() {
   if (navBar.classList.contains('fullscreen-nav')) {
     navBar.classList.remove('fullscreen-nav');
     navButton.src = 'img/nav-hamburger.png';
   }
 
   else {
-  navBar.classList.add('fullscreen-nav');
-  navButton.src = 'img/nav-hamburger-close.png';
+    navBar.classList.add('fullscreen-nav');
+    navButton.src = 'img/nav-hamburger-close.png';
   }
-});
+}
+
+navButton.addEventListener('click', expandNavBar);
