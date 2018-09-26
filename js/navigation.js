fd4b393d2ac45967f046b3846992b2c50a5d973b
kyranmccann <kyran.mccann@gmail.com>
2018-09-26 11:27:42 -0700

    working on tablet

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,4 @@
 
-
 let nav = document.querySelector('nav div');
 let navButton = document.querySelector('.nav-hamburger');
 
@@ -8,6 +7,7 @@ navButton.addEventListener('click', drop, {once:true});
 function close(){
   nav.classList.remove('dropped');
   document.querySelector('.full-width-nav').style = 'opacity: 1';
+
   nav.classList.add('dropdown');
   navButton.innerHTML = '<img src="./img/nav-hamburger.png" alt="menu button" />';
   navButton.addEventListener('click', drop, {once:true});
@@ -16,7 +16,10 @@ function close(){
 function drop(){
   nav.classList.remove('dropdown');
   document.querySelector('.full-width-nav').style = 'opacity: .95';
+
   nav.classList.add('dropped');
   navButton.innerHTML = '<img src="./img/nav-hamburger-close.png" alt="menu button" />';
   navButton.addEventListener('click', close, {once:true});
 }
+
+  // TweenMax.to("#menu", 4, {width:"100vw", height:"100vh"})
