86127087feb618cee3250322c4a689b7da9550e0
SabrinaGear <sabrinagear.ft@gmail.com>
2018-09-25 19:23:22 -0500

    expanding menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// JS goes here
+function openNav() {
+    let expander = document.querySelector(".expander");
+    expander.style.display = "block";
+}
+
+function closeNav() {
+    let expander = document.querySelector(".expander");
+    expander.style.display = "none";
+}
