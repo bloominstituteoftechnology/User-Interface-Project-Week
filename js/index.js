52464fb160800330d9218b3dcdd8e4597bb04422
bummings <sup@fauxkno.com>
2018-09-27 02:05:06 -0400

    adding hamburger change on click

--- js/index.js
+++ js/index.js
@@ -1,50 +1,73 @@
+// navigation toggle
+
+const hamburger = document.querySelector('.nav__hamburger');
+const navMenu = document.querySelector('.expandedNav');
+// const navMenuLinks = document.querySelector('.expandedNav__links');
+const nav = document.querySelector('.nav');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('expand');
+    if (hamburger.src === "http://localhost:1337/img/nav-hamburger.png") {
+      hamburger.src = "img/nav-hamburger-close.png";
+    } else {
+      hamburger.src = "http://localhost:1337/img/nav-hamburger.png";
+    }
+});
+
+// only works if i have the src equal to my dev server's address, i.e. http://localhost:1337/img/nav-hamburger.png and http://localhost:1337/img/nav-hamburger-closed.png 
+
+
+
+
+// tab component for services page
+
 class TabLink {
-  constructor(element) {
-    this.element = element;
+    constructor(element) {
+        this.element = element;
 
-    this.data = this.element.dataset.tab;
+        this.data = this.element.dataset.tab;
 
-    this.item = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+        this.item = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
 
-    this.tabItem = new TabItem(this.item);
+        this.tabItem = new TabItem(this.item);
 
-    this.element.addEventListener('click', ()=> {
-      this.select();
-    });
-  };
+        this.element.addEventListener('click', () => {
+            this.select();
+        });
+    };
 
-  select() {
-    const links = document.querySelectorAll('.servicesButtons__btn');
+    select() {
+        const links = document.querySelectorAll('.servicesButtons__btn');
 
-    // so as to distinguish all links as non-selected
-    links.forEach(link => {
-      link.classList.remove('servicesButtons__btn--selected')
-    });
+        // so as to distinguish all links as non-selected
+        links.forEach(link => {
+            link.classList.remove('servicesButtons__btn--selected')
+        });
 
-    // except for this one
-    this.element.classList.add('servicesButtons__btn--selected');
+        // except for this one
+        this.element.classList.add('servicesButtons__btn--selected');
 
-    // since you clicked on this one, execute the select method
-    this.tabItem.select();
-  }
+        // since you clicked on this one, execute the select method
+        this.tabItem.select();
+    }
 }
 
 class TabItem {
-  constructor(element) {
-    this.element = element;
-  }
+    constructor(element) {
+        this.element = element;
+    }
 
-  select() {
-    const items = document.querySelectorAll('.tabs-item');
+    select() {
+        const items = document.querySelectorAll('.tabs-item');
 
-    items.forEach(item => {
-      // item.classList.remove('servicesButtons__btn--selected');
-      item.classList.add('servicesFocusHide');
-    })
+        items.forEach(item => {
+            // item.classList.remove('servicesButtons__btn--selected');
+            item.classList.add('servicesFocusHide');
+        })
 
-    // this.element.classList.add('servicesButtons__btn--selected');
-    this.element.classList.remove('servicesFocusHide');
-  }
+        // this.element.classList.add('servicesButtons__btn--selected');
+        this.element.classList.remove('servicesFocusHide');
+    }
 }
 
 
