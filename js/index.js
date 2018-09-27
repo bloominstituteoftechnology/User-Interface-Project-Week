66deb13417219e2e6f3bde4b3c89b1deef8b7d89
Axolotl <samuelmachat@gmail.com>
2018-09-27 12:38:29 -0400

    add menu modal open gsap animation

--- js/index.js
+++ js/index.js
@@ -8,8 +8,31 @@ class HamburgerOpen {
   openModal() {
     document.querySelector('.non-expanded').classList.add('modal-hidden')
     document.querySelector('.expanded').classList.remove('modal-hidden')
-    document.querySelector('.expanded').classList.remove('modal-hidden')
     document.querySelector('.menu-modal').classList.remove('modal-hidden')
+    const menuModal = document.querySelector('.menu-modal')
+    const menuLinks = document.querySelectorAll('.links-wrapper a')
+
+    TweenMax.from(menuModal, 0.4, {
+      position: 'absolute',
+      top: '50vh',
+      left: '50vw',
+      height: 1,
+      width: 1
+    })
+
+    TweenMax.staggerFrom(
+      menuLinks,
+      0.4,
+      {
+        cycle: {
+          left: [500, -500]
+        },
+        opacity: 0,
+        position: 'relative',
+        delay: 0.4
+      },
+      0.4
+    )
   }
 }
 
@@ -21,7 +44,6 @@ class HamburgerClose {
 
   closeModal() {
     document.querySelector('.non-expanded').classList.remove('modal-hidden')
-    document.querySelector('.expanded').classList.add('modal-hidden')
     document.querySelector('.expanded').classList.add('modal-hidden')
     document.querySelector('.menu-modal').classList.add('modal-hidden')
   }
