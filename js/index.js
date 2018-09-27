b89e5b901a6c8e679a1680dbd9241460e93555de
Wanda <doubleuevans@gmail.com>
2018-09-26 20:52:16 -0500

    got the services tabs to work

--- js/index.js
+++ js/index.js
@@ -1,65 +1,19 @@
-// JS goes here
+//JS goes here
 
 //index navigation js
-// const toggleMenu = () => {
-//   dropdown.classList.toggle('dropdown-open');
-// }
 
-// const dropdown = document.querySelector('.dropdown-content');
-
-// const button = document.querySelector('.dropdown-button');
-
-// button.addEventListener('click', () => {
-//   return toggleMenu(dropdown);
-// });
-
-// let dropdowns = document.querySelectorAll('.dropdown');
-// dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
-
-//services
-
-class Tab {
-  constructor(element) {
-    this.element = element;
-    this.tabData = this.element.dataset.tab;
-
-    if(this.card){
-      this.card = document.querySelector(`.card[data-tab="${this.tabData}"]`);
-    }
-
-    //click event listener
-    this.element.addEventListener('click', () => {
-    this.selectTab();
-    });  
-  }
-
-  selectTab() {
-    const tabs = document.querySelectorAll('.tab');
-
-    Array.from(tabs).forEach(function(tab){
-      tab.classList.remove('active-tab')
-    })
-
-    this.element.classList.add('active-tab');
-
-    const tabitem = document.querySelector('.tab-item')
-    tabitem.style.display = "none";
-
-    this.card.forEach(card => card.selectTab());
-  }
+const toggleMenu = () => {
+  dropdown.classList.toggle('dropdown-open');
 }
 
-class TabItem {
-  constructor(element){
-    this.element = element;
-  }
+const dropdown = document.querySelector('.dropdown-content');
 
-  selectTab(){
-    this.element.style.display = "";
-  }
-}
+const button = document.querySelector('.dropdown-button');
 
-let tabs = document.querySelectorAll('.tab');
-tabs = Array.from(tabs).map(element => {
-  return new TabLink(element)
+button.addEventListener('click', () => {
+  return toggleMenu(dropdown);
 });
+
+let dropdowns = document.querySelectorAll('.dropdown');
+dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
---
+++ js/services.js
@@ -0,0 +1,57 @@
+//services
+
+class Tab {
+  constructor(element) {
+    this.element = element;
+    this.tabData = this.element.dataset.tab;
+
+    this.itemElement = document.querySelector(`.tab-item[data-tab="${this.tabData}"]`)
+
+    this.tabItem = new TabItem(this.itemElement);
+
+    //click event listener
+    this.element.addEventListener('click', () => {
+      this.select();
+    });  
+  }
+
+  select() {
+    const tabs = document.querySelectorAll('.tab');
+
+    Array.from(tabs).forEach(function(tab){
+      tab.classList.remove('tab-item-selected');
+    });
+
+    this.element.classList.add('tab-item-selected');
+
+    this.tabItem.select();
+    // const tabitems = document.querySelectorAll('.tab-item')
+    // tabitems.forEach(function(tabitem){
+    //   tabitem.style.display = "none"
+    // })
+    // console.log(tabitems)   
+
+    // this.tabitems.forEach(tabitem => tabitem.selectItem());
+  }
+}
+
+class TabItem {
+  constructor(element){
+    this.element = element;
+  }
+
+  select(){
+    const items = document.querySelectorAll('.tab-item')
+
+    Array.from(items).forEach(function(item){
+      item.classList.remove('tab-item-selected');
+    });
+
+    this.element.classList.add('tab-item-selected');
+  }
+}
+
+let tabs = document.querySelectorAll('.tab');
+tabs = Array.from(tabs).map(element => {
+  return new Tab(element)
+});
