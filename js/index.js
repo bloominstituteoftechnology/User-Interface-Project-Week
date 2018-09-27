9d4588c881e82bf41d6599c4aa6e1b6dfa2ec37d
SeanOne <basement365@gmail.com>
2018-09-26 22:35:38 -0700

    finished desktop extended nav design and javascript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // JS goes here
+
+const hamburger = document.querySelector('.burgerMenu img');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', function(event) {
+    let close = 'close';
+    if (hamburger.src.includes(close)) {
+        hamburger.src = './img/nav-hamburger.png';
+    } else {
+        hamburger.src = './img/nav-hamburger-close.png';
+    }
+    menu.classList.toggle('expand');
+})
