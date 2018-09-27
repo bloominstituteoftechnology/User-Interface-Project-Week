b528ed0fffdffa5b0033356393c5e773b7e86873
mm2079 <vayne3252@gmail.com>
2018-09-27 14:36:52 -0400

    Almost done with home page, working on the services page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // JS goes here
+let hamburger = document.querySelector( ".hamburger" );
+let navMenu = document.querySelector( ".navigation" );
+let navMenuClose = document.querySelector( ".close-button" );
+console.log( hamburger );
+hamburger.addEventListener( "click", function( event )
+{
+    navMenu.style.display = "flex"; 
+    this.style.display = "none";
+    navMenuClose.style.display = "block";
+
+})
+navMenuClose.addEventListener( "click", function( event )
+{
+    navMenu.style.display = "none";
+    navMenuClose.style.display = "none";
+    hamburger.style.display = "block";
+})
