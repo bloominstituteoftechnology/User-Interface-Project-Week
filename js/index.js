da3088dab637defdae5e893de405be1de0c29523
Elvis Ibarra <ielvisd@gmail.com>
2018-09-27 10:57:54 -0700

    Main navigator works!

--- js/index.js
+++ js/index.js
@@ -2,32 +2,31 @@
 class Dropdown {
   constructor(element) {
     // assign this.element to the dropdown element
-    console.log('hello')
     this.element = element;
     // assign the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.toggle-overlay')
-    console.log(this.button);
+    this.closebutton = this.element.querySelector('.closebutton')
     // assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content = this.element.querySelector('.aside');
-    console.log( this.content);
+    this.closebuttoncontent = document.querySelector('.aside');
+    
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', (event) => { this.toggleContent(event) })
+    this.closebutton.addEventListener('click', (event) => { this.closeContent(event) })
     // Instructor note, the reason we must wrap the toggleContent method in an anonymous function is that 'this' in toggleContent
     // would refer to the button, NOT the current instance of the class. 
   }
 
   toggleContent(event) {
     // Toggle the ".dropdown-hidden" class off and on
-    console.log('hello, you clicked')
-    // console.log( this.element);
-    // console.log( this.button);
-    // console.log(dropdowns);
-    // console.log( this.content);
-    // this.content.classList.toggle('close');
     this.content.classList.toggle('open');
+  }
+
+  closeContent(event) {
+    // Toggle the ".dropdown-hidden" class off and on
+    this.closebuttoncontent.classList.toggle('open');
   }
 }
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('header');
-console.log(dropdowns);
 dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
