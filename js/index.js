702bcc02cffcb5bf595d396926e2913cb7ef5856
SeanOne <basement365@gmail.com>
2018-09-27 10:34:47 -0700

    finished services tab navigation

--- js/index.js
+++ js/index.js
@@ -1,37 +1,8 @@
-// JS goes here
-class Tabnav {
-    constructor(element) {
-        this.element = element;
-        this.data = this.element.dataset.tab;
-
-        this.tabItem = document.querySelector(`.tabItem[data-tab='${this.data}']`);
-        
-
-        console.log(this.tabItem);
-    }
-}
-
-class TabItem {
-    constructor(element) {
-        this.element = element;
-    }
-
-    // select() {
-    //     const tabsItem = document.querySelectorAll()
-    // }
-}
-
-
-
-let tabs = document.querySelectorAll(".tab");
-
-Array.from(tabs).map(tab => new Tabnav(tab));
-
 // nav expanded javascript
 const hamburger = document.querySelector('.burgerMenu img');
 const menu = document.querySelector('.menu');
 
-hamburger.addEventListener('click', function(event) {
+hamburger.addEventListener('click', function (event) {
     let close = 'close';
     if (hamburger.src.includes(close)) {
         hamburger.src = './img/nav-hamburger.png';
@@ -41,6 +12,48 @@ hamburger.addEventListener('click', function(event) {
     menu.classList.toggle('expand');
 })
 
+// JS goes here
+class Tabnav {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+
+        this.tabItem = document.querySelector(`.tabItem[data-tab='${this.data}']`);
+        
+        this.content = new TabItem(this.tabItem);
+
+        this.element.addEventListener('click', () => {
+            event.preventDefault();
+            this.select();
+        });
+
+    }
+
+    select() {
+        const tabs = document.querySelectorAll('.tab');
+        Array.from(tabs).forEach(tab => tab.classList.remove('selected'))
+        this.element.classList.add('selected')
+        this.content.select();
+    }
+}
+
+class TabItem {
+    constructor(element) {
+        this.element = element;
+
+    }
+
+    select() {
+        const tabItem = document.querySelectorAll('.tabItem');
+
+        Array.from(tabItem).forEach(item => item.classList.remove('active'));
+        this.element.classList.add('active')
+    }
+}
+
+let tabs = document.querySelectorAll(".tab");
+tabs = Array.from(tabs).map(tab => new Tabnav(tab));
+tabs[0].select();
 
 // very hacky way of making the images change based on screen size
 const imgOne = document.querySelector('.imgOne');
