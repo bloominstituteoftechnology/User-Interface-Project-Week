b451dad4110124384d175a0f16a20d67197b3d2e
Jasmine Galang <jsmn.glng@gmail.com>
2018-09-26 20:00:47 -0700

    .

--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
-TweenMax.from('.fixed-navbar', 1, {y:-50}).delay(0.5)
-TweenMax.from('.cta h1', 1, {y: -50, opacity: 0, ease: Back.easeOut.config(4)}).delay(1.3);
+TweenMax.from('.fixed-navbar', 1, {y:-60});
+TweenMax.from('.cta h1', 1, {y: -50, opacity: 0, ease: Back.easeOut.config(4)}).delay(0.8);
