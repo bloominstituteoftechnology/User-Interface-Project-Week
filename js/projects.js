ad039e1cacc53a088718e41351b7f2c24ee49569
Adam <ajmboosh@gmail.com>
2018-09-26 21:37:56 -0500

    got JS for carousel working with minimal styling. will finish up LESS for page tomorrow

--- js/projects.js
+++ js/projects.js
@@ -4,12 +4,36 @@ class Carousel {
   constructor(domElement) {
     this.domElement = domElement;
     this.leftButton = domElement.querySelector(".left-button");
-    this.rightButton = domElement.querySelector(".right-buttton");
+    this.rightButton = domElement.querySelector(".right-button");
     this.images = domElement.querySelectorAll("img");
-    this.currentIndex = 0;
     this.images[0].style.display = "block";
-    console.log(this.images);
+    this.index = 0;
+    this.leftButton.addEventListener("click", () => {
+      this.cycleLeft();
+    });
+    this.rightButton.addEventListener("click", () => {
+      this.cycleRight();
+    });
+  }
+  cycleLeft() {
+    this.images[this.index].style.display = "none";
+    if (this.index === 0) {
+      this.index = this.images.length - 1;
+    } else {
+      this.index--;
+    }
+    this.images[this.index].style.display = "block";
+  }
+  cycleRight() {
+    this.images[this.index].style.display = "none";
+    if (this.index === this.images.length - 1) {
+      this.index = 0;
+    } else {
+      this.index++;
+    }
+    this.images[this.index].style.display = "block";
   }
 }
 let carousels = document.querySelectorAll(".carousel");
+
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
