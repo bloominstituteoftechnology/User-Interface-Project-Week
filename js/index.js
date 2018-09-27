1e1b0ad260f1e537b928746f54cffa023013f033
mm2079 <vayne3252@gmail.com>
2018-09-27 16:45:42 -0400

    Done with services page without the component

--- js/index.js
+++ js/index.js
@@ -16,3 +16,31 @@ navMenuClose.addEventListener( "click", function( event )
     navMenuClose.style.display = "none";
     hamburger.style.display = "block";
 })
+
+// Services js
+let tabs = document.querySelectorAll( ".tabs" );
+let tab1 = Array.from( tabs );
+class tab{
+    constructor( element ){
+        this.element = element;
+    }
+    tabOpenMethod()
+    {   
+        this.element.addEventListener( "click", function( event )
+        {
+            event.style.display = "none;";
+            for( i = 0; i < tab1.length; i++ )
+            {
+                // if( tab1[ i ] === )
+                console.log( tab[i]);
+            }
+        });
+    }
+    
+}
+
+let tabMap = tab1.map( function( element )
+{
+    return new tab( element );
+})
+console.log( tabMap );
