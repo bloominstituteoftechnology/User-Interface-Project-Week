55f6e2aa95f01faa4110e8826d9901a7ce7c2441
Justin <jdlow2@gmail.com>
2018-09-26 23:34:51 -0400

    menu animations on open and close

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
 const navButton = document.querySelector('#nav-icon');
 const navBar = document.querySelector('.main-nav');
+const links = navBar.querySelectorAll('a');
+
+// set flag to see if bar is expanded
+let expanded = false;
 
 function expandNavBar() {
-  if (navBar.classList.contains('fullscreen-nav')) {
-    TweenMax.to(navBar, .5, {width: "0vw", height: "0vh"});
-    navBar.classList.remove('fullscreen-nav');
+  if (expanded === true) {
+    TweenMax.to(navBar, 0.3, { width: '0vw', height: '0vh' });
     navButton.src = 'img/nav-hamburger.png';
+    expanded = false;
   }
 
   else {
-    navBar.classList.add('fullscreen-nav');
-    TweenMax.to(navBar, .5, {width: "100vw", height: "100vh"});
+    // navBar.classList.add('fullscreen-nav');
+    TweenMax.to(navBar, 0.3, { width: '100vw', height: '100vh' });
     navButton.src = 'img/nav-hamburger-close.png';
-
+    expanded = true;
   }
 }
 
