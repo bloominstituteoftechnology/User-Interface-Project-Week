25a25909f51e931d2c64071a42e7880e186c70ff
SabrinaGear <sabrinagear.ft@gmail.com>
2018-09-27 14:29:53 -0500

    gallery styles

--- js/index.js
+++ js/index.js
@@ -24,3 +24,64 @@
         this.closeNav();
     });
 }
+
+//lightbox
+{
+    let modal = document.querySelector('.modal');
+
+    let slide = document.querySelector('.mySlide');
+    let closeM = document.querySelector('.modalX');
+
+    function openModal(event, url) {
+
+        modal.style.width = '100vw';
+        closeM.style.display = 'block';
+        slide.style.backgroundImage = `url(${url})`;
+        console.log(slide.style.backgroundImage);
+    }
+
+    function closeModal() {
+        modal.style.width = '0vw';
+
+    }
+}
+
+//gallery view buttons
+{
+
+    // Get the elements with class="column"
+    const elements = document.querySelectorAll(".column");
+    const hoverDivs = document.querySelectorAll(".column img");
+    console.log(elements);
+    console.log(hoverDivs);
+    // Full-width images
+    function one() {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.width = "100%";
+
+        }
+        Array.from(hoverDivs).forEach(function(image) {
+            image.style.height = "80vh";
+        });
+    }
+
+    // Two images side by side
+    function two() {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.width = "50%";
+        }
+        Array.from(hoverDivs).forEach(function(image) {
+            image.style.height = "30rem";
+        });
+    }
+
+    // Four images side by side
+    function four() {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.width = "25%";
+        }
+        Array.from(hoverDivs).forEach(function(image) {
+            image.style.height = "60%";
+        });
+    }
+}
