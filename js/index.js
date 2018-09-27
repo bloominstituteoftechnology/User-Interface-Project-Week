e4c4100bf61030a64b495b1a88362baaaf43c9f4
Elvis Ibarra <ielvisd@gmail.com>
2018-09-27 08:12:16 -0700

    Added tabs.js and fixed navigation links to correctly point to the right page

--- js/index.js
+++ js/index.js
@@ -19,9 +19,11 @@ class Dropdown {
   toggleContent(event) {
     // Toggle the ".dropdown-hidden" class off and on
     console.log('hello, you clicked')
-    console.log( this.element);
-    console.log( this.button);
-    console.log( this.content);
+    // console.log( this.element);
+    // console.log( this.button);
+    // console.log(dropdowns);
+    // console.log( this.content);
+    // this.content.classList.toggle('close');
     this.content.classList.toggle('open');
   }
 }
