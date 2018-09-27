7367eed714aa2f1c9f054d3852e2a99e13e069ad
Jordan Stoddard <jjsincorporated@gmail.com>
2018-09-27 09:18:31 -0700

    [Feature] fixed navigation animation, drops down bounces, overflow hidden.

--- Components/Dropdown/dropdown.js
+++ Components/Dropdown/dropdown.js
@@ -1,12 +1,15 @@
-this.header = document.querySelector('.headerWrapper')
-console.log(header)
-
 class Dropdown {
     constructor(element) {
         this.element = element;
         this.button = this.element.querySelector('.nav-hamburger');
         this.closeButton = this.element.querySelector('.nav-hamburger-X');
         this.content = this.element.querySelector('.nav-dropdown');
+        this.header = this.element.querySelector('.nav-dropdown')
+
+        
+
+    
+
         this.button.addEventListener('click', () => {
             this.toggleContent();
         });
@@ -18,35 +21,18 @@ class Dropdown {
        this.content.classList.toggle('nav-hidden');
        this.button.classList.toggle('nav-hamburger-hidden');
        this.closeButton.classList.toggle('nav-hamburger-X-visible');
-       header.style.opacity = '.96';
-       
+       TweenMax.fromTo(this.header, 2, {height: '4.8vh'}, {height: '100vh', ease: Bounce.easeOut, y: '10'})
+       TweenMax.fromTo(this.header, 2, {opacity: '1'}, {opacity: '.96'})
+      
     }
     closeContent() {
         this.content.classList.toggle('nav-hidden');
         this.closeButton.classList.toggle('nav-hamburger-X-visible');
         this.button.classList.toggle('nav-hamburger-hidden');
+        TweenMax.fromTo(this.header, 2, {height: '100vh'}, {height: '0vh', ease: Bounce.easeOut, y: '-10'});
+        // TweenMax.fromTo(this.header, 2, {opacity: '.96'}, {opacity: '0'})
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
