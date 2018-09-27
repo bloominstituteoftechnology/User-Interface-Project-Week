1dfcfaa6e97712377df668f8d9c5accea58ce851
Justin <jdlow2@gmail.com>
2018-09-26 22:14:58 -0400

    gsap animation on nav open

--- js/index.js
+++ js/index.js
@@ -3,13 +3,16 @@ const navBar = document.querySelector('.main-nav');
 
 function expandNavBar() {
   if (navBar.classList.contains('fullscreen-nav')) {
+    TweenMax.to(navBar, .5, {width: "0vw", height: "0vh"});
     navBar.classList.remove('fullscreen-nav');
     navButton.src = 'img/nav-hamburger.png';
   }
 
   else {
     navBar.classList.add('fullscreen-nav');
+    TweenMax.to(navBar, .5, {width: "100vw", height: "100vh"});
     navButton.src = 'img/nav-hamburger-close.png';
+
   }
 }
 
