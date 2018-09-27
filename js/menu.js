1aaf30d646a4d6f497c531c84f097695ac87ce59
Katie Cleary <katiecleary@airmaster.lan>
2018-09-27 09:11:32 -0400

    updated HTML tab mark-up for js logic

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,31 @@
 const toggleMenu = () => {
   menu.classList.toggle('menu--open')
+  menuButtonExpanded.classlist.toggle('menu-buttonExpanded--open')
+  logoExpanded.classList.toggle('logoExpanded--open')
  //Toggle the "menu--open" class on your menu reference. 
 }
-
+const toggleButton = () => {
+  menuButtonExpanded.classlist.toggle('menu-buttonExpanded--open')
+ //Toggle the "menu--open" class on your menu reference. 
+}
+const toggleLogo = () => {
+  logoExpanded.classList.toggle('logoExpanded--open')
+ //Toggle the "menu--open" class on your menu reference. 
+}
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 
-console.log(menu)
+
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
+const menuButtonExpanded = document.querySelector('.menu-buttonExpanded')
+const logoExpanded = document.querySelector('.logoExpanded')
 
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 menuButton.addEventListener ('click',() => {
   toggleMenu();
+  toggleButton();
+  toggleLogo();
 });
