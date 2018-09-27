5b91991ea943011a0532a8bd3ef19654ec5bcd55
Blake <koenneckerb@gmail.com>
2018-09-27 09:03:42 -0700

    Fixed broken animations.

--- js/index.js
+++ js/index.js
@@ -11,12 +11,12 @@ const openNav = () => {
 
     hamburger.src = "img/nav-hamburger-close.png";
     open = true;
-    tl
-        .staggerFrom('div.hidden li', 1, {
-            cycle: {
-                y: [-50],
-            }, autoAlpha: 0, ease: Power2.easeInOut
-        }, 0.1);
+
+    TweenMax.staggerFrom('div.hidden li', 1, {
+        cycle: {
+            y: [-50],
+        }, autoAlpha: 0, ease: Power2.easeInOut
+    }, 0.1);
     navbar.style.display = 'block';
 };
 
@@ -27,43 +27,45 @@ const closeNav = () => {
 }
 
 // animations
-var tl = new TimelineLite();
 
 let controller = new ScrollMagic.Controller();
 
+let tween = TweenMax.staggerFrom(('div.left-text'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1);
 
 let scene1 = new ScrollMagic.Scene({
     triggerElement: "#trigger-element1"
 })
-.setTween(tl.staggerFrom(('div.left-text'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
-
+    .setTween(tween)
+    .addTo(controller);
 
 let scene2 = new ScrollMagic.Scene({
     triggerElement: "#trigger-element2"
 })
-.setTween(tl.staggerFrom(('div.right-text'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
+    .setTween(TweenMax.staggerFrom(('div.right-text'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1))
+    .addTo(controller);
 
 let scene6 = new ScrollMagic.Scene({
     triggerElement: "#trigger-element6"
 })
-.setTween(tl.staggerFrom(('section.bottom-content .top-h2'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
+    .setTween(TweenMax.staggerFrom(('section.bottom-content .top-h2'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1))
+    .addTo(controller);
 
 let scene3 = new ScrollMagic.Scene({
     triggerElement: "#trigger-element3"
 })
-.setTween(tl.staggerFrom(('div.top-third'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
+    .setTween(TweenMax.staggerFrom(('div.top-third'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1))
+    .addTo(controller);
 
 
 let scene4 = new ScrollMagic.Scene({
     triggerElement: "#trigger-element4"
 })
-.setTween(tl.staggerFrom(('div.middle-third'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
+    .setTween(TweenMax.staggerFrom(('div.middle-third'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1))
+    .addTo(controller);
 
 
 let scene5 = new ScrollMagic.Scene({
     triggerElement: "#trigger-element5"
 })
-.setTween(tl.staggerFrom(('div.bottom-third'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1));
-
-
-controller.addScene([scene1, scene2, scene3, scene4, scene5, scene6]);
+    .setTween(TweenMax.staggerFrom(('div.bottom-third'), 1, { cycle: { y: [-50] }, autoAlpha: 0, ease: Power2.easeInOut }, 0.1))
+    .addTo(controller);
