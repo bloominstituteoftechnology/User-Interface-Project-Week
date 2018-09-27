fbef6f10719ee662ba196d73f47273dcdf889e97
Katie Cleary <katiecleary@airmaster.lan>
2018-09-27 14:04:19 -0400

    finished expanded nav, styled links, linked pages, and put final button behavior in place

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,30 @@
 const toggleMenu = () => {
   menu.classList.toggle('menu--open');
   menuHeader.classList.toggle('menuHeaderExpanded');
-
+  menuButton.style.display ="none";
+  menuButtonExpanded.style.display = "block";
+}
+const collapseMenu = () => {
+  menuButton.style.display ="block";
+  menuButtonExpanded.style.display = "none";
 }
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 const menuHeader =document.querySelector('.menuHeader');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
+const menuButtonExpanded = document.querySelector('.menuButtonExpanded')
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener ('click',() => {
   toggleMenu();
 });
 
+menuButtonExpanded.addEventListener ('click',() => {
+  toggleMenu();
+  collapseMenu();
+});
 
-//// EXPANDED BEHAVIOR ----------
+
 
 
 
