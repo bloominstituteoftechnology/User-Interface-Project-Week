6886c321133949706ddc400a8bbaf67fc8cf168f
bummings <sup@fauxkno.com>
2018-09-27 04:22:40 -0400

    re-invented the hamburger icon logic

--- js/index.js
+++ js/index.js
@@ -2,19 +2,31 @@
 
 const hamburger = document.querySelector('.nav__hamburger');
 const navMenu = document.querySelector('.expandedNav');
-// const navMenuLinks = document.querySelector('.expandedNav__links');
-const nav = document.querySelector('.nav');
+const topNav = document.querySelector('.nav');
+const hamburgerClosed = document.querySelector('.expandedNav__hamburgerClosed');
 
-let counter = 0
+let counter = 0;
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('expand');
+  topNav.classList.toggle('fixedNav');
+
+
+  // alternate solution for a non-fixed navbar
+  // counter for even or odd clicks to change icon
   counter++;
-  // console.log(counter);
   if (counter % 2 != 0) {
     hamburger.src = './img/nav-hamburger-close.png';
   } else {
     hamburger.src = './img/nav-hamburger.png';
   }  
+});
+
+
+
+hamburgerClosed.addEventListener('click', () => {
+  navMenu.classList.toggle('expand');
+  topNav.classList.toggle('fixedNav');
+  hamburger.src = './img/nav-hamburger.png'
 });
 
 
