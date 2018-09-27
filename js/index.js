ca1bd14138e96f22416d5eec1fcf5ec207ce7cc0
Adam Hinckley <adamhinckley@Adams-MacBook-Pro.local>
2018-09-27 10:15:10 -0600

    added scroll reveal to home page

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
 // navigation
-
 const openNav = document.querySelector(".nav-hamburger-closed");
 const closeNav = document.querySelector(".close-nav-X");
 const nav = document.querySelector(".open-nav");
 const home = document.querySelector(".go-home");
+const animateProjects = document.querySelector("#home-animate");
+const animateDiv = document.querySelector("#animate-div");
+console.log(animateProjects);
 
 openNav.addEventListener("click", () => {
   nav.style.display = "flex";
-  TweenMax.fromTo(nav, 0.5, { y: -800 }, { y: 0 });
+  TweenMax.fromTo(nav, 0.4, { y: -800 }, { y: 0 });
   TweenMax.to(openNav, 0.5, {
     scale: 1,
     rotation: 90
   });
-  console.log("clicked open");
 });
 closeNav.addEventListener("click", () => {
-  TweenMax.fromTo(nav, 0.5, { y: 0 }, { y: -1000 });
+  TweenMax.fromTo(nav, 0.5, { y: 0 }, { y: -900 });
   TweenMax.to(openNav, 0.5, {
     scale: 1,
     rotation: 0
@@ -28,10 +29,12 @@ home.addEventListener("click", () => {
   console.log("clicked");
 });
 
-let controller = new ScrollMagic.controller();
-
-let homeScene = new ScrollMagic.Scene({
-  triggerElement: "."
+animateProjects.addEventListener("mouseenter", () => {
+  TweenLite.to(animateDiv, 3, { ease: SlowMo.ease.config(0.7, 0.7, false), x: -404 });
 });
 
-console.log(controller);
+animateProjects.addEventListener("mouseleave", () => {
+  TweenLite.from(animateDiv, 1, { ease: Elastic.easeOut.config(1, 0.3), y: -50 });
+});
+
+ScrollReveal().reveal(".architect, .designs, .project-1, .project-2, .project-3", { delay: 300 });
