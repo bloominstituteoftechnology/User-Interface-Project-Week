cd0ca913d4c1d22967e49c16ed5d543c12558cd0
Kat Fries <katjohnnson@Kats-MacBook-Air.local>
2018-09-28 02:33:12 -0400

    Post Submission changes

--- components/mainServices/mainServices.js
+++ components/mainServices/mainServices.js
@@ -60,3 +60,32 @@ links = Array.from(links).map(link => new TabLink(link))
 // DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
 links[0].select();
 
+/*  Creating clickable tabs
+
+
+1. Create a variable for all the tab links using querySelectorAll (node list). All the tab-links should have a dataset tab to select them separately.
+2. Assign that variable to map itself to create an array (which can access all array methods) of all new instances of the class TabLink.
+3. Jump up above the code created in 1. Create a class for TabLink.
+4. Create constructor with the parameter elements.
+5. Assign this.element to element.
+6. Use querySelector to get a reference to the current data tab.
+7. Create a new variable and assign it to a new instance of the TabContent class.
+8. Add an eventListener for clicks on tabs.
+9. Create a Select method to select tabs.
+10. Within the Select method create a variable for the tab links (same as step 1).
+11. Use forEach to deselect all tabs by removing the classList.
+12. Add tab-selected to 'this' classList.
+13. Call the new instance (using 'this' and the variable created in 7) with the method.
+14. Create a class for the Tab content.
+15. Create constructor
+16 Assign this.element to this.
+17. Create a Select method (same method name).
+18. Create a variable using querySelectorAll for all content (that also have a current data tab).
+19. Use forEach to remove all selected content.
+20. Add selected content using this.
+21. Finally, jump to the bottom of the code and call the first item in the array with the method.
+
+
+
+
+*/
