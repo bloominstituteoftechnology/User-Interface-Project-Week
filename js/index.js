6f5fbc4b0413c9fbaa2a1b4aa2d9eafab3571f76
jaklundun <govi.escudero@gmail.com>
2018-09-27 18:57:11 -0700

    Expanded hamburger menu modal is now operational

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // JS goes here
+// https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+
+document.addEventListener("DOMContentLoaded", function(event) {
+let hamburgerMenu = document.querySelector('.hamburger-menu-icon-closed');
+let extendedNavModal = document.querySelector('.menu__expanded');
+extendedNavModal.style.display = "none";
+
+
+
+
+
+
+hamburgerMenu.addEventListener('click', () => {
+    if (extendedNavModal.style.display === "none") {
+        extendedNavModal.style.display = "block";
+        hamburgerMenu.src = "img/nav-hamburger-close.png";
+    } else {
+        extendedNavModal.style.display = "none";
+        hamburgerMenu.src = "img/nav-hamburger.png";
+    }
+})
+
+    console.log("It works")
+
+})
