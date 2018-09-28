6d49e10853e604b35d3165a12b08238451e6c71e
SeanOne <basement365@gmail.com>
2018-09-27 22:41:07 -0700

    got opacity working on extended nav

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 // nav expanded javascript
 const hamburger = document.querySelector('.burgerMenu img');
 const menu = document.querySelector('.menu');
+const nav = document.querySelector('nav');
 
 hamburger.addEventListener('click', function (event) {
+    event.preventDefault();
     let close = 'close';
     if (hamburger.src.includes(close)) {
         hamburger.src = './img/nav-hamburger.png';
     } else {
         hamburger.src = './img/nav-hamburger-close.png';
     }
+    nav.classList.toggle('bgChange');
     menu.classList.toggle('expand');
 })
