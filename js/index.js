0fc9de2131657b9fdf31de05c5d337cb3c514e02
jaklundun <govi.escudero@gmail.com>
2018-09-29 14:57:42 -0700

    Finished laying out services page with HTML

--- js/index.js
+++ js/index.js
@@ -1,26 +1,3 @@
 // JS goes here
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 
-document.addEventListener("DOMContentLoaded", function(event) {
-let hamburgerMenu = document.querySelector('.hamburger-menu-icon-closed');
-let extendedNavModal = document.querySelector('.menu-expanded');
-let characterToggler = document.querySelector('.character');
-extendedNavModal.style.display = "none";
-characterToggler.style.display = "relative";
-
-
-hamburgerMenu.addEventListener('click', () => {
-    if (extendedNavModal.style.display === "none") {
-        extendedNavModal.style.display = "block";
-        hamburgerMenu.src = "img/nav-hamburger-close.png";
-        characterToggler.style.display = "none";
-    } else {
-        extendedNavModal.style.display = "none";
-        hamburgerMenu.src = "img/nav-hamburger.png";
-        characterToggler.style.display = "block";
-    }
-})
-
-    console.log("It works")
-
-})
