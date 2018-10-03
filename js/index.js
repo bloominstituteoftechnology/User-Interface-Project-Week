ea1be6764e6ee8b64d4e6bc32e6a7c10e6e3f5e0
2thumbstech <2thumbstech@gmail.com>
2018-10-02 23:19:00 -0600

    Button menu complete

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 // JS goes here
 
 // DROPDOWN
+function openNav(){
+    document.getElementById('nav').style.width='100%';
+}
+function closeNav(){
+    document.getElementById('nav').style.width='0%';
+}
