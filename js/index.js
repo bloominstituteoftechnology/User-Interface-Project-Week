2e28ad46fb97ffbd877389bb95678b3e753d979c
Jake Thomas <jmaxt12@gmail.com>
2018-10-03 11:25:20 -0600

    The dropdown navigation is mostly functional. One more change needed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
-// JS goes here
+// Navigation System
+class Dropdown {
+    constructor(element) {
+      
+      // Assign this.element to the dropdown element
+      this.element = element;
+      
+      // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
+      this.button = this.element.querySelector('.hamburger');
+      
+      // assign the reference to the ".dropdown-content" class found in the dropdown element
+      this.content = this.element.querySelector('.navigation');
+      
+      // Add a click handler to the button reference and call the toggleContent method.
+      this.button.addEventListener('click', () => { this.toggleContent()
+  
+      })
+    }
+  
+    toggleContent() {
+      
+      // Toggle the ".dropdown-hidden" class off and on
+      this.content.classList.toggle('dropdown-hidden');
+    }
+  }
+  
+  // Nothing to do here, just study what the code is doing and move on to the Dropdown class
+  let dropdowns = document.querySelectorAll('.dropdown');
+  dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
+
+
+
+
+//Tab Functionality
