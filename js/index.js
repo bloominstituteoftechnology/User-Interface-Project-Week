24b5f89aa1906df7f42675b49af60875a4032efc
Phil Hart <phil@Phils-iMac.local>
2018-10-03 14:33:48 -0600

    Finished tabs JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
-// JS goes here
+class TabLink {
+    constructor(link) {
+        this.link = link;
+        this.linkData = this.link.dataset.tab;
+
+        if (this.linkData === '0') {
+            this.cards = document.querySelector('.tab');
+        } else {
+            this.cards = document.querySelectorAll(`.tab[data-tab="${this.linkData}"]`);
+        }
+        this.cards = Array.from(this.cards).map(cards => new TabCard(cards));
+
+        this.link.addEventListener('click', () => {
+            this.selectCard()
+        });
+
+    }
+    selectCard() {
+        const link = document.querySelectorAll('.link')
+        link.forEach(tab => {
+            tab.classList.remove('active-link')
+        });
+        this.link.classList.add("active-link");
+
+        const tab = document.querySelectorAll('.tab');
+        tab.forEach(tab => {
+            tab.style.display = "none"
+        });
+        this.cards.forEach(tab => tab.selectCard());
+
+    }
+}
+class TabCard {
+    constructor(tab) {
+        this.tab = tab;
+    }
+    selectCard() {
+        this.tab.style.display = "flex";
+    }
+}
+let links = document.querySelectorAll('.link');
+links = Array.from(links).map(link => new TabLink(link));
+
+links[0].selectCard();
