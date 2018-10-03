40b2c2d9d88ee60cc6e97b5d7a72d89199a4b97c
jaklundun <govi.escudero@gmail.com>
2018-10-03 13:16:09 -0700

    Implemented desktop services page image change upon tab click

--- js/services.js
+++ js/services.js
@@ -1,61 +1,60 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    class TabLink {
-        constructor(element) {
-          this.element = element;
-          this.data = this.element.dataset.num;
-          this.itemElement = document.querySelector(`.tabs-btn[data-num="${this.data}"]`);
-          this.tabItem = new TabElement (this.itemElement);
-          this.itemImg = document.querySelector(`.tc-img[data-num="${this.data}"]`);
-          this.tabImg = new TabImage(this.itemImg);
-          this.element.addEventListener('click', () => {
-            this.select();
-          })
-      
-        };
-      
-        select() {
-          const links = document.querySelectorAll('.tabs-btn');
-          Array.from(links).forEach((link) => {
-            link.classList.remove('selected-btn');
-          });
-      
-          this.element.classList.toggle('selected-btn');
-          this.tabItem.select();
-          this.tabImg.select();
-        }
+  class TabLink {
+      constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.num;
+        this.itemElement = document.querySelector(`.tabs-btn[data-num="${this.data}"]`);
+        this.tabItem = new TabItem (this.itemElement);
+        this.itemImg = document.querySelector(`.tc-img[data-num="${this.data}"]`);
+        this.tabImg = new TabImg(this.itemImg);
+        this.element.addEventListener('click', () => {
+          this.select();
+        })
+    
+      };
+    
+      select() {
+        const links = document.querySelectorAll('.tabs-btn');
+        Array.from(links).forEach((link) => {
+          link.classList.remove('selected-btn');
+        });
+    
+        this.element.classList.toggle('selected-btn');
+        this.tabItem.select();
+        this.tabImg.select();
       }
-      
-      class TabElement {
-        constructor(element) {
-          this.element = element;
-        }
-      
-        select() {
-          const items = document.querySelectorAll('.tc-text-title');
-          Array.from(items).forEach((item) => {
-            item.classList.remove('tc-title-selected');
-          });
-          this.element.classList.toggle("tc-title-selected");
-        }
+    }
+    
+    class TabItem  {
+      constructor(element) {
+        this.element = element;
       }
+    
+      select() {
+        const items = document.querySelectorAll('.tc-text-title');
+        Array.from(items).forEach((item) => {
+          item.classList.remove('tc-title-selected');
+        });
+        this.element.classList.toggle('tc-title-selected');
+      }
+    }
 
-
-      class TabImage {
-        constructor(element) {
-          this.element = element;
-        }
-      
-        select() {
-          const items = document.querySelectorAll('.tc-img');
-          Array.from(items).forEach((item) => {
-            item.classList.remove('tc-img-selected');
-          });
-          this.element.classList.toggle("tc-img-selected");
-        }
+    class TabImg {
+      constructor(element) {
+        this.element = element;
       }
-      
-      
-      let links = document.querySelectorAll('.tabs-btn');
-      links = Array.from(links).map((link) => new TabLink(link));
-      links[0].select();
+    
+      select() {
+        const items = document.querySelectorAll('.tc-img');
+        Array.from(items).forEach((item) => {
+          item.classList.remove('tc-img-selected');
+        });
+        this.element.classList.toggle('tc-img-selected');
+      }
+    }
+    
+    
+    let links = document.querySelectorAll('.tabs-btn');
+    links = Array.from(links).map((link) => new TabLink(link));
+    links[0].select();
 })
