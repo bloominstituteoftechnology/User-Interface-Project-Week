a4548bea0179040b084c6692d5792017643b85da
Scott Bren <smbren@ncsu.edu>
2018-10-04 15:31:57 -0400

    completed services tabs functionality

---
+++ js/Tabs.js
@@ -0,0 +1,43 @@
+class TabLink {
+    constructor(element) {
+      this.element = element;
+      
+      this.data = this.element.dataset.tab;
+      
+      this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+      
+      this.tabItem = new TabItem(this.itemElement);
+      
+      this.element.addEventListener('click', (event) => {this.select(event)});
+    };
+  
+    select() {
+      const links = document.querySelectorAll('.tabs-link');
+  
+      Array.from(links).forEach( link => {link.classList.remove('tabs-link-selected')});
+  
+      this.element.classList.add('tabs-link-selected');
+      
+      this.tabItem.select();
+    }
+  }
+  
+  class TabItem {
+    constructor(element) {
+      this.element = element;
+    }
+  
+    select() {
+      const items = document.querySelectorAll('.tabs-item');
+  
+      items.forEach(item => {item.classList.remove('tabs-item-selected')});
+  
+      this.element.classList.add('tabs-item-selected');
+    }
+  }
+  
+  let links = document.querySelectorAll('.tabs-link');
+  
+  links = Array.from(links).map( link => new TabLink(link));
+  
+  links[0].select();
