2b7c38e1d1a88ae660224f934e7ac8b5a32cf2c9
jaklundun <govi.escudero@gmail.com>
2018-10-04 15:18:42 -0700

    Desktop services page is fully styled with fully functioning tabs.

--- js/services.js
+++ js/services.js
@@ -1,60 +1,59 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  class TabLink {
-      constructor(element) {
-        this.element = element;
-        this.data = this.element.dataset.num;
-        this.itemElement = document.querySelector(`.tabs-btn[data-num="${this.data}"]`);
-        this.tabItem = new TabItem (this.itemElement);
-        this.itemImg = document.querySelector(`.tc-img[data-num="${this.data}"]`);
-        this.tabImg = new TabImg(this.itemImg);
-        this.element.addEventListener('click', () => {
-          this.select();
-        })
-    
-      };
-    
-      select() {
-        const links = document.querySelectorAll('.tabs-btn');
-        Array.from(links).forEach((link) => {
-          link.classList.remove('selected-btn');
-        });
-    
-        this.element.classList.toggle('selected-btn');
-        this.tabItem.select();
-        this.tabImg.select();
-      }
-    }
-    
-    class TabItem  {
-      constructor(element) {
-        this.element = element;
-      }
-    
-      select() {
-        const items = document.querySelectorAll('.tc-text-title');
-        Array.from(items).forEach((item) => {
-          item.classList.remove('tc-title-selected');
-        });
-        this.element.classList.toggle('tc-title-selected');
-      }
-    }
 
-    class TabImg {
-      constructor(element) {
-        this.element = element;
-      }
-    
-      select() {
-        const items = document.querySelectorAll('.tc-img');
-        Array.from(items).forEach((item) => {
-          item.classList.remove('tc-img-selected');
-        });
-        this.element.classList.toggle('tc-img-selected');
-      }
-    }
-    
-    
-    let links = document.querySelectorAll('.tabs-btn');
-    links = Array.from(links).map((link) => new TabLink(link));
-    links[0].select();
-})
+class Tabs {
+  constructor(element) {
+      this.element = element;
+      this.links = this.element.querySelectorAll('.tabs-link-button');
+      this.links = Array.from(this.links).map(link => {
+          return new TabsLink(link, this);
+      });
+      this.activeLink = this.links[0];
+      this.init();
+  }
+  init() {
+      this.activeLink.select();
+  }
+  updateActive(newActive) {
+      this.activeLink.deselect();
+      this.activeLink = newActive;
+  }
+  getTab(data) {
+      return this.element.querySelector(`.tabs-item[data-tab='${data}']`);
+  }
+}
+
+class TabsLink {
+  constructor(element, parent) {
+      this.element = element;
+      this.tabs = parent;
+      this.tabsItem = parent.getTab(this.element.dataset.tab);
+      this.tabsItem = new TabsItem(this.tabsItem);
+      this.element.addEventListener('click', () => {
+          this.tabs.updateActive(this);
+          this.select();
+      });
+  }
+  select() {
+      this.element.classList.add('tabs-link-button-selected');
+      this.tabsItem.select();
+  }
+  deselect() {
+      this.element.classList.remove('tabs-link-button-selected');
+      this.tabsItem.deselect();
+  }
+}
+
+class TabsItem {
+  constructor(element) {
+      this.element = element;
+  }
+  select() {
+      this.element.classList.add('tabs-item-selected');
+  }
+  deselect() {
+      this.element.classList.remove('tabs-item-selected');
+  }
+} 
+
+
+let tabs = document.querySelectorAll('.tabs-container');
+tabs = Array.from(tabs).map(tab => new Tabs(tab));
