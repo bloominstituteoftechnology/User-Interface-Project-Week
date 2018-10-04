5fbf02ac976a8bb1253e150c2034763ec1e2a01a
2thumbstech <2thumbstech@gmail.com>
2018-10-04 13:58:16 -0600

    Working on Tabs

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 // JS goes here
 
 // DROPDOWN
+
 function openNav(){
     document.getElementById('nav').style.width='100%';
 }
 function closeNav(){
     document.getElementById('nav').style.width='0%';
+}
+
+// Service Tabs
+
+
+content=document.getElementsByClassName('content');
+for(i = 0; i <content.length; i++){
+    content[i].style.display='none';
+}
+
+tablinks = document.getElementsByClassName('tablink');
+    for(i = 0; i < tablinks.length; i++){z
+        tablinks[i].style.backgroundColor = '';
 }
