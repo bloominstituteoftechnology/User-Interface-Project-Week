639a0dbee162aafad8ae0f9d35add9f1bd0f945d
Barbara Hunter <commandandcode@gmail.com>
2018-10-04 12:24:59 -0400

    css overhaul

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // JS goes here
+
+let container = document.querySelector('.overlay');
+let burger = document.querySelector('.burger');
+let logo = document.querySelector('.logo');
+let navH1 = document.querySelector('.navH1');
+console.log(logo);
