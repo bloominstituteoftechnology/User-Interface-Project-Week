05be6201c020867a65d28092bf0c7f0cac505eb9
Orlando Castillo <omaddoc@gmail.com>
2018-10-04 17:12:34 -0400

    finale

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // JS goes here
+class DropDown {
+    constructor(element){
+      this.element = element;
+    }
+  
+    speaks(){
+      console.log('Bark!');
+    }
+  }
+  
+  const lab = new Dog('Labrador');
+  const poodle = new Dog('Poodle');
+
+  const buttons = document.querySelectorAll('.button');
+  class Button {
+    constructor(element){
+      this.element = element;
+      this.element.addEventListener('click', () => { this.buttonClick() });
+    }
+    buttonClick(event){
+      console.log('button clicked!');
+    } 
+  }
