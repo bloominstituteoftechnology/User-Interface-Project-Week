79774ed664acc02d5ef37a7e78af93c8048ec73e
2thumbstech <2thumbstech@gmail.com>
2018-10-08 21:40:45 -0600

    Stuck on tabs. service

--- js/index.js
+++ js/index.js
@@ -10,3 +10,20 @@ function closeNav(){
 }
 
 // Service Tabs
+function opentab(evt, tablinks) {
+    
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(Id).style.display = "block";
+    evt.currentTarget.className += " active";
+}
