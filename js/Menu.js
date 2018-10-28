a206a42b6c0d302a78c6fc094d158dd274dd753c
jamespagedev <jamespagedev@outlook.com>
2018-10-28 02:12:32 -0700

    nav almost working

---
+++ js/Menu.js
@@ -0,0 +1,12 @@
+const navHeaderButtonOpen = document.querySelector('.nav-header-button-open');
+const navHeaderButtonClose = document.querySelector('.nav-header-button-close');
+const navBarItems = document.querySelector('.nav-bar-items');
+
+const toggleMenu = (navBarItems) => {
+  navHeaderButtonOpen.classList.toggle('hide');
+  navHeaderButtonClose.classList.toggle('hide');
+  navBarItems.classList.toggle('invisible');
+}
+
+navHeaderButtonOpen.addEventListener('click', () => toggleMenu(navBarItems));
+navHeaderButtonClose.addEventListener('click', () => toggleMenu(navBarItems));
