6ab082ce2563e91fb5d92ba453766f66fc43f966
jamespagedev <jamespagedev@outlook.com>
2018-10-28 13:24:21 -0700

    home section main complete

--- js/Menu.js
+++ js/Menu.js
@@ -1,11 +1,13 @@
 const navHeaderButtonOpen = document.querySelector('.nav-header-button-open');
 const navHeaderButtonClose = document.querySelector('.nav-header-button-close');
 const navBarItems = document.querySelector('.nav-bar-items');
+const homeJumbotronTitles = document.querySelectorAll('.jumbotron-title');
 
 const toggleMenu = (navBarItems) => {
   navHeaderButtonOpen.classList.toggle('hide');
   navHeaderButtonClose.classList.toggle('hide');
   navBarItems.classList.toggle('invisible');
+  homeJumbotronTitles.forEach(title => title.classList.toggle('nav-items-faded-text'))
 }
 
 navHeaderButtonOpen.addEventListener('click', () => toggleMenu(navBarItems));
