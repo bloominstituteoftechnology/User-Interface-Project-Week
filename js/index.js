0e73e1b982a1d9b6bab72f84f25847a6da86302c
Nick O'Ferrall <nickoferrall@gmail.com>
2018-10-29 11:32:54 -0500

    Home Page Desktop and Mobile

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // JS goes here
+
+
+const toggleMenu = () => {
+    // Toggle the "menu--open" class on your menu reference. 
+    menu.classList.toggle('menu--open');
+  }
+  
+  // Start Here: Create a reference to the ".menu" class
+  const menu = document.querySelector('.menu');
+  
+  // create a reference to the ".menu-button" class
+  const menuButton = document.querySelector('.menu-button');
+  console.log(menuButton);
+  console.log(menu);
+  console.log(toggleMenu);
+  
+  // Using your menuButton reference, add a click handler that calls toggleMenu
+  
+  menuButton.addEventListener('click', toggleMenu);
