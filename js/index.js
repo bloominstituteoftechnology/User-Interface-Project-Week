2976e5bd3f1be2ed1d6ea53b2ed0019c42956f6e
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2018-10-29 17:20:29 -0500

    Nav bar Fully Functional

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// JS goes here
+//navbar
+class Header{
+    constructor(header){
+        this.header = header;
+        this.navBtn = this.header.querySelector('.nav-button');
+        this.nav = this.header.querySelector('nav');
+        this.x = false;
+
+        this.navBtn.addEventListener('click', () => this.open());
+    }
+    open(){
+        this.nav.classList.toggle('nav-open');
+        this.header.classList.toggle('header-open');
+        if(this.x){
+            this.navBtn.textContent = "☰";
+            this.x = false;
+        }else{
+            this.navBtn.textContent = "✖";
+            this.x = true;
+        }
+        
+    }
+}
+
+
+let header = document.querySelector('header');
+header  = new Header(header);
+
