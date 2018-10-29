f8be13c05a1ca9730276504531287120337876fb
kmarker1101 <kmarker1101@icloud.com>
2018-10-29 16:45:24 -0500

    home page work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // JS goes here
+class HamburgerOpen {
+  constructor(element) {
+    this.element = element;
+    this.element.addEventListener("click", () => this.openModal());
+  }
+  openModal() {
+    document.querySelector(".non-expanded").classList.add("modal-hidden");
+    document.querySelector(".expanded").classList.remove("modal-hidden");
+    document.querySelector(".expanded").classList.remove("modal-hidden");
+    document.querySelector(".menu-modal").classList.remove("modal-hidden");
+  }
+}
+class HamburgerClose {
+  constructor(element) {
+    this.element = element;
+    this.element.addEventListener("click", () => this.closeModal());
+  }
+  closeModal() {
+    document.querySelector(".non-expanded").classList.remove("modal-hidden");
+    document.querySelector(".expanded").classList.add("modal-hidden");
+    document.querySelector(".expanded").classList.add("modal-hidden");
+    document.querySelector(".menu-modal").classList.add("modal-hidden");
+  }
+}
+let hamburgerNonExpanded = document.querySelector("header .non-expanded");
+let hamburgerExpanded = document.querySelector("header .expanded");
+hamburgerNonExpanded = new HamburgerOpen(hamburgerNonExpanded);
+hamburgerExpanded = new HamburgerClose(hamburgerExpanded);
