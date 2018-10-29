43e2c94dcdb86bd26c5a2ea1a33cc173c930ee29
Jasmine <jasmineanderson315@yahoo.com>
2018-10-29 16:26:37 -0400

    finished navbar -- need to fix JS issues but will come back to it later

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // JS goes here
+
+// navbar menu
+
+class Navbar {
+    constructor(element) {
+        this.element = element
+        this.button = this.element.querySelector('.navBurger')
+        this.content = this.element.querySelector('.dropdownNav')
+
+        this.button.addEventListener("click", () => {
+            this.toggleContent(event);
+        })
+    }
+
+    toggleContent() {
+        this.content.classList.toggle("navHidden")
+    }
+}
+
+let navbar = document.querySelectorAll(".navbar")
+navbar = Array.from(navbar).map(navbar => new Navbar(navbar))
+
+let navBurger = document.querySelector(".navBurger")
+
+navBurger.addEventListener("click", () => {
+    navBurger.classList.toggle("dropdownNavToggle")
+})
