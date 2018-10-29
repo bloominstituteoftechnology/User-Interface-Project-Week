4664c8bbdefab8f968f907ccf50c8d756a8d642c
Wesley Meeks <wesmeeks88@gmail.com>
2018-10-29 15:44:55 -0600

    Got the navbar functioning properly.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // JS goes here
+// adding functionality to menu
+
+let menu = document.querySelector(".menu");
+let navExpanded = false;
+
+console.log(menu);
+
+menu.addEventListener("click", function(e) {
+  menu.setAttribute("src", "./img/nav-hamburger-close.png");
+  let nav = document.querySelector(".nav-container");
+  nav.classList.toggle("expanded");
+  document.querySelector(".nav").classList.toggle("nav-active");
+
+  //change menu icon
+  menu.classList.toggle("menu-close");
+  console.log("menu was clicked");
+});
+
+function submit() {
+  alert("Thank you for your form!");
+}
