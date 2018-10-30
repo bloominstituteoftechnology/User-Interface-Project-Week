fad04bd7650a75c5bf6ebedbf3e0d37131220194
Grace <gracekang.93@gmail.com>
2018-10-30 15:47:15 -0700

    touch ups on less and JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// JS goes here
+let hamImages = document.querySelectorAll(".hamburger img");
+let expanded = document.querySelector('.hamburger-expanded');
+hamImages.forEach(x => {
+    x.addEventListener('click', () => {
+        hamImages[0].classList.toggle('display');
+        hamImages[1].classList.toggle('display');
+        expanded.classList.toggle('display');
+    })
+    x.addEventListener('mouseenter', () => {
+        hamImages[0].style.cursor = 'pointer';
+        hamImages[1].style.cursor = 'pointer';
+    })
+});
