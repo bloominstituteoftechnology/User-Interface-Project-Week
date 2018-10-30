a3210b796b274566acdec49e07786d86af8cd16a
Lidii <lidiiany92@gmail.com>
2018-10-30 13:45:36 -0400

    Finished expanded nav bar layout and functionality

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
 // JS goes here
 
+//Expanded nav bar
+const navExpanded = document.querySelector('#nav');
+const hamburger = document.querySelector('#hamburger');
+const headerWrapper = document.querySelector('.header-wrapper');
+const navCloseBtn =  document.querySelector('#nav-close');
+
+hamburger.addEventListener('click', function() {
+    headerWrapper.classList.add('nav-hidden');
+    navExpanded.classList.add('nav-open');
+});
+
+navCloseBtn.addEventListener('click', function(){
+    navExpanded.classList.remove('nav-open');
+    headerWrapper.classList.remove('nav-hidden');
+})
