2dd1421424f6c803225ad5ba466da0d0a61c4fdc
dyiar <austin.hl.blake@gmail.com>
2018-10-30 17:41:55 -0600

    started working on nav bar js

--- js/index.js
+++ js/index.js
@@ -59,6 +59,34 @@ class TabItem {
     }
   }
 
+  class Dropdown {
+    constructor(element) {
+      
+      // Assign this.element to the dropdown element
+      this.element = element;
+      
+      // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
+      this.button = this.element.querySelector('.dropdown-button');
+      
+      // assign the reference to the ".dropdown-content" class found in the dropdown element
+      this.content = this.element.querySelector('.dropdown-content');
+      
+      // Add a click handler to the button reference and call the toggleContent method.
+      this.button.addEventListener('click', () => {
+        this.toggleContent()
+      });
+    }
+  
+    toggleContent() {
+      
+      // Toggle the ".dropdown-hidden" class off and on
+      this.content.classList.toggle('dropdown-hidden');
+    }
+  }
+
 
 let links = document.querySelectorAll('.tabs-link');
 links = Array.from(links).map( link => new TabLink(link));
+
+let dropdowns = document.querySelectorAll('.dropdown');
+dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
