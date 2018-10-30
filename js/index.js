d1022e9c6c1825977e7fe3d988b7e9038c78db0a
dan2014 <dmen326@gmail.com>
2018-10-30 15:11:39 -0400

    finished home desktop version

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// JS goes here
+const navButton = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+const navLinks = document.querySelector(".links");
+
+function navToggle(){
+    nav.classList.toggle("nav-height");
+    navLinks.classList.toggle("links-toggle");
+}
+
+
+navButton.addEventListener("click",navToggle);
