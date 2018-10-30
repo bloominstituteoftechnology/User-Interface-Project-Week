16ecec14ec670963b00145c1af54bc3b86f9b39d
turkworks <cengizbug@Cengizs-iMac.local>
2018-10-30 14:27:48 -0600

    setting up files

---
+++ components/Menu/Menu.js
@@ -0,0 +1,21 @@
+// class Menu {
+//   constructor(element) {
+    
+//     // Assign this.element to the dropdown element
+//     this.element = element;
+    
+//     this.button = this.element.querySelector('.dropdown-button');
+//     this.content = this.element.querySelector('.dropdown-content');
+    
+//     this.button.addEventListener('click', () => {this.toggleContent()
+//     });
+//   }
+
+//   toggleContent() {
+//     this.content.classList.toggle('dropdown-hidden');
+//   }
+// }
+
+// // Nothing to do here, just study what the code is doing and move on to the Dropdown class
+// let menu = document.querySelectorAll('.menu');
+// menus = Array.from(menus).map( menus => new Menu(menus));
