56e25da14580da8a5a4cc7bf6414bb94ae53eaf7
Holden B <holden.bucher.92@gmail.com>
2018-10-29 19:29:37 -0500

    added nav javascript for full-screen view

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-// JS goes here
+const toggleNav = () => {
+    console.log('yay');
+    nav.classList.toggle('menu-showing');
+    if (nav.classList.contains('menu-showing')) {
+        navButton.src = "./img/nav-hamburger-close.png";
+        console.log(navButton.src);
+        navContainer.style.paddingTop = '2.2%';
+        navContainer.style.paddingBottom = '0';
+    } else {
+        navButton.src="./img/nav-hamburger.png";
+        navContainer.style.paddingTop = '1.25%';
+        navContainer.style.paddingBottom = '1.25%'; 
+    }
+
+}
+
+const navButton = document.getElementById('nav-img');
+console.log(navButton);
+const nav = document.querySelector('.main-nav');
+const navContainer = document.querySelector('.nav-container');
+navButton.addEventListener('click', toggleNav);
+
