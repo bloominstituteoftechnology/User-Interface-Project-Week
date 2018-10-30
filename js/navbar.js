ace2181c1a1f32f1ed757e4aa4820fc15a451b81
jheinz1999 <jonathan.heinz@aol.com>
2018-10-30 09:35:45 -0700

    JS roughly finished

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,20 @@
 // navbar.js - navbar logic on every page
 
 const navButton = document.querySelector('nav img');
+const navItems = document.querySelector('.nav-items');
+
+let navState = false;
+
+navButton.addEventListener('click', () => {
+
+  navState = !navState;
+
+  if (navState)
+    navButton.src = "./img/nav-hamburger-close.png";
+
+  else
+    navButton.src = "./img/nav-hamburger.png";
+
+  navItems.style.display = (navState) ? "block" : "none";
+
+});
