e4bb85bb25d5e7d90143b2bbd6f760be1d4c4710
Civoremo <omerovic_n@hotmail.com>
2018-10-30 15:32:52 -0400

    selecting tabs works; not the content yet

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
 // JS goes here
+class TabButton {
+    constructor(tabButton) {
+        this.tabButton = tabButton;
+        this.dataTab = this.tabButton.dataset.tabbtn;
+
+        this.itemTab = document.querySelector(`.tab-content[data-tabcont="${this.dataTab}"]`);
+
+        this.tabButton.addEventListener('click', () => this.select());
+
+    }
+    // methods
+    select() {
+        const tabs = document.querySelectorAll('.tab-link');
+
+        Array.from(tabs).forEach( tab => {
+            tab.classList.remove('tab-selected');
+        });
+
+        this.tabButton.classList.toggle('tab-selected');
+        console.log(this.tabButton);
+    }
+}
+
+
+class TabContent {
+    constructor(tabContent) {
+        this.tabContent = tabContent;
+        // console.log(this.tabContent);
+
+        this.dataContent = this.tabContent.dataset.tabcont;
+        // console.log(this.dataContent);
+    }
+    // methods
+}
+
+
+
+let tabButtons = document.querySelectorAll('.tab-link');
+tabButtons = Array.from(tabButtons).map( tabButton => new TabButton(tabButton));
+
+
+let tabsContents = document.querySelectorAll('.tab-content');
+tabsContents = Array.from(tabsContents).map( tabContent => new TabContent(tabContent));
+
+// console.log(tabsContents);
