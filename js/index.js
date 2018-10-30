3ebed59d3687882cfb902d07532f9c424dce6963
BrandCam <BrandCam>
2018-10-30 17:58:36 -0500

    JS done on MVP

--- js/index.js
+++ js/index.js
@@ -11,7 +11,50 @@ navImg.addEventListener('click',() => {
 }
 else{
     navImg.src = 'img/nav-hamburger-close.png'
-}
-    
-  
+}  
 })
+
+
+//services tabs
+class Tabs {
+    constructor(tab){
+        this.tab = tab;
+        this.data = this.tab.dataset.tab;
+        this.item = document.querySelector(`.tab-content[data-tab=${this.data}]`);
+        this.tabContent = new TabContent(this.item);
+        this.tab.addEventListener('click',() => this.select());
+console.log(this.item)
+    };
+    select(){
+        const tabs = document.querySelectorAll('.tab');
+        
+        tabs.forEach(tab =>{
+            if(tab.dataset.tab !== this.data){
+                tab.classList.remove('selected-tab');
+            }
+        });
+        this.tab.classList.add('selected-tab');
+        this.tabContent.select();
+    }
+}
+
+class TabContent{
+    constructor(tabContent){
+        this.tabContent = tabContent
+    }
+    select(){
+        const contents = document.querySelectorAll('.tab-content');
+        console.log(this.tabContent.classList);
+        contents.forEach(content =>{
+            if(content.dataset.tab !== this.data){
+                content.classList.add("hide");
+                content.classList.remove('tab-content');
+            }
+            this.tabContent.classList.remove('hide');
+            this.tabContent.classList.add('tab-content');
+        })
+    }
+}
+
+let tabs = document.querySelectorAll('.tab');
+tabs= Array.from(tabs).map(tab=>new Tabs(tab));
