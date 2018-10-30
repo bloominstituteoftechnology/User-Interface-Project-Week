b784d11099072d8e260108aa363c3d35b28fcc44
ToMMaaY <thuynh629@gmail.com>
2018-10-30 15:37:07 -0700

    Completed the tab navigation for the desktop version

--- js/index.js
+++ js/index.js
@@ -20,3 +20,42 @@ const menuIcon = document.querySelector('.menu-icon');
 
 menuIcon.addEventListener('click', toggleMenu);
 closeIcon.addEventListener('click', closeMenu);
+
+
+
+// Tabbed Content for the Services Page
+
+class Tablink {
+    constructor(ele) {
+        this.element = ele;
+        this.tabData = this.element.dataset.tab;
+        const mq = window.matchMedia( "(min-width: 320px)" );
+        this.content = document.querySelectorAll(`.desktop-tab[data-tab='${this.tabData}']`);
+        this.content = Array.from(this.content).map( item => new TabContent(item));
+        this.element.addEventListener('click', () => this.selectTab() );
+    }
+
+    selectTab() {
+        let content = document.querySelectorAll('.desktop-tab');
+        content.forEach( item => item.classList.remove('selected') );
+        let tabs = document.querySelectorAll('.tab');
+        tabs.forEach( item => item.classList.remove('selected') );
+        this.element.classList.add('selected');
+        this.content.forEach(item => item.selectContent());
+    }
+}
+
+class TabContent {
+    constructor(ele) {
+        this.element = ele;
+    }
+    selectContent() {
+        this.element.classList.add('selected');
+    }
+}
+
+
+let tabs = document.querySelectorAll('.tab');
+
+tabs = Array.from(tabs).map( tab => new Tablink(tab));
+
