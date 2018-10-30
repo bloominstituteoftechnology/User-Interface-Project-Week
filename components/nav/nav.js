0807df311b35fe4e072a184415db9896ac91d8ab
Josh Z <jzieger10@gmail.com>
2018-10-30 16:04:30 -0400

    Refactored nav js. Moving on to mobile version

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,17 +1,19 @@
-const navOpen = document.querySelector('.hamburger');
+const navBtn = document.querySelectorAll('.nav-menu-btn');
+const navOpen = document.querySelector('.open');
 const navClose = document.querySelector('.close');
 const nav = document.querySelector('nav');
 let navLinks = document.querySelectorAll('.nav-link');
 const topBar = document.querySelector('.topbar');
 
-navOpen.addEventListener('click', () => {
-    console.log('Hamburger clicked');
-        topBar.style.display = 'none';
-        nav.style.display = 'flex';
+navBtn.forEach( (item) => {
+    item.addEventListener('click', () => {
+        console.log('Hamburger clicked');
+        nav.classList.toggle('nav-display-toggle');
         console.log(nav.classList);
-    navClose.addEventListener('click', () => { 
-        nav.style.display = 'none';
-        topBar.style.display = 'flex';
+        navOpen.classList.toggle('display-toggle');
+        console.log(navOpen.classList);
+        navClose.classList.toggle('display-toggle');
+        console.log(navClose.classList);
     })
-    })
+})
 
