274850a7a3607bbc0aefd7d0b39056fcf1711b16
Aman <amsingh714@gmail.com>
2018-10-30 16:15:53 -0400

    swapped images for mobile

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+let images = document.querySelectorAll('.section-img');
+images = Array.from(images);
+console.log(images);
+
+function mobileImages(arr) {
+    let mediaQuery = window.matchMedia( "(max-width: 600px)" );
+    if (mediaQuery.matches) {
+        arr[0].src = "img/home/home-mobile-villas-img.png"
+        arr[1].src = "img/home/home-mobile-outskirts-img.png"
+        arr[2].src = "img/home/home-mobile-the-blocks-img.png"
+    }
+}
+
+mobileImages(images);
