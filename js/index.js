a798ae300302fd849bf321aed09a53f41aede061
ChyChy <c.otikor@gmail.com>
2018-10-30 16:57:10 -0600

    Completed navigation functionality and styling

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-// JS goes here
+function toggleMenuOn() {
+  closedNav.style.display = 'none';
+  openNav.style.display = 'block';
+  navItems.style.display = 'block';
+  nav.style.height = '100%';
+}
+function toggleMenuOff() {
+  closedNav.style.display = 'block';
+  openNav.style.display = 'none';
+  navItems.style.display = 'none';
+  nav.style.height = 'auto';
+}
+
+const nav = document.querySelector('.nav-bar');
+const navItems = document.querySelector('nav');
+
+const closedNav = document.querySelector('.closed');
+const openNav = document.querySelector('.open');
+
+console.log(navItems);
+console.log(closedNav);
+console.log(openNav);
+
+closedNav.addEventListener('click', toggleMenuOn);
+openNav.addEventListener('click', toggleMenuOff);
