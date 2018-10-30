d6faf9bfd6d4dabd93f077b9d8daf7da592bfc57
dan2014 <dmen326@gmail.com>
2018-10-30 18:31:30 -0400

    finished mobile design for the home page.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 const navButton = document.querySelector(".hamburger");
-const nav = document.querySelector("nav");
 const navLinks = document.querySelector(".links");
 
 function navToggle(){
-    nav.classList.toggle("nav-height");
+    navLinks.classList.toggle("links");
     navLinks.classList.toggle("links-toggle");
 }
 
