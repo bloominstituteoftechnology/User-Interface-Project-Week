c0713fe062c63d05e1c8c1fa31d59c41fc8e01ba
brooks <bpoltl1@gmail.com>
2018-10-30 14:00:39 -0500

    added navigation toggle feature

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // JS goes here
+
+// toggling nav classes
+const toggleNav= () => {
+    navigation.classList.toggle ('navigation-active');
+    dropdown.classList.toggle ('nav-active');
+}
+
+let navigation = document.querySelector('.navigation');
+let dropdown = document.querySelector('.nav-dropdown');
+let ham = document.querySelector('.nav-ham');
+let close = document.querySelector('.nav-close')
+console.log(navigation)
+console.log(dropdown)
+ham.addEventListener('click',toggleNav);
+close.addEventListener('click', toggleNav);
