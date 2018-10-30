814c458328f66b92ce3c3c786193db18f67b1dcb
Addison-Stavlo <AddieStavlo@gmail.com>
2018-10-30 16:20:10 -0400

    tabs complete

--- js/index.js
+++ js/index.js
@@ -11,3 +11,33 @@ navButton.addEventListener('click',()=>{
     buttonImgs[1].classList.toggle('current-btn');
 });
 //End of Nav Button
+class Tab {
+    constructor(element){
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.content = document.querySelector(`.tab-content[data-tab='${this.data}']`);
+        this.tabContent = new TabContent (this.content);
+
+        this.element.addEventListener('click',()=>this.select());
+    };
+    select() {
+        // remove tab-selected class from all tabs
+        let tabs = document.querySelectorAll('.tab');
+        Array.from(tabs).forEach(tab => tab.classList.remove('selected-tab'));
+        this.element.classList.add('selected-tab');
+        this.tabContent.select();
+    }
+}
+class TabContent {
+    constructor(banana){
+        this.element = banana;
+    }
+    select () {
+        let contentList = document.querySelectorAll('.tab-content');
+        Array.from(contentList).forEach(content=>content.classList.remove('selected-content'));
+        this.element.classList.add('selected-content');
+    }
+}
+
+let tabs = document.querySelectorAll('.tab');
+tabs = Array.from(tabs).map(tab => new Tab(tab));
