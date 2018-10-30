513e15ede6aeff0a5efd7f3070783a854b214a26
ToMMaaY <thuynh629@gmail.com>
2018-10-29 19:14:05 -0700

    Compelted the navigation toggle for the desktop version

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // JS goes here
+
+// Expand Navigation
+
+const toggleMenu = () => {
+    menu.classList.toggle('open-menu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
+}
+
+const closeMenu = () => {
+    menu.classList.toggle('open-menu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+}
+
+const menu = document.querySelector('nav');
+const closeIcon = document.querySelector('.expanded');
+const menuIcon = document.querySelector('.menu-icon');
+
+menuIcon.addEventListener('click', toggleMenu);
+closeIcon.addEventListener('click', closeMenu);
