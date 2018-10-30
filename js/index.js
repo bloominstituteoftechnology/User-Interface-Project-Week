b3ba7fbfbffb1d15c35959d3d4cd5f894dd9826a
wcolts2000 <wcolts2000@gmail.com>
2018-10-30 08:40:09 -0700

    added navigation js functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // JS goes here
+
+
+// ***************************************
+// *********NAVIGATION BACKDROP***********
+// ***************************************
+
+const hamburger = document.querySelector('img.header__nav-links__hamburger');
+const navBackdrop = document.querySelector(".nav-backdrop");
+const navContainer = document.querySelector('.nav-link-container');
+
+
+hamburger.addEventListener('click', () => {
+  if(!navBackdrop.classList.contains('active') && !navContainer.classList.contains('open')) {
+    navBackdrop.classList.add('active');
+    navContainer.classList.add("open");
+    hamburger.src="img/nav-hamburger-close.png";
+  } else if (navBackdrop.classList.contains('active') && navContainer.classList.contains('open')) {
+    navBackdrop.classList.remove('active');
+    navContainer.classList.remove("open");
+    hamburger.src="img/nav-hamburger.png";
+  }
+})
