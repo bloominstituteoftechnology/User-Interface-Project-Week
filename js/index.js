d4c3b0f5fa846584eee64de8ed8798725d66732c
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2018-10-30 12:58:26 -0500

    Services page tabs working properly

--- js/index.js
+++ js/index.js
@@ -26,3 +26,45 @@ class Header{
 let header = document.querySelector('header');
 header  = new Header(header);
 
+
+//services tabs ========================================
+
+class TabLink {
+    constructor(element) {
+      this.element = element;
+      console.log(element);
+      this.data = this.element.dataset.tab;
+      console.log(this.data);
+      this.itemElement = document.querySelector(`.content[data-tab="${this.data}"]`);
+      console.log(this.itemElement);
+      this.tabItem = new TabItem(this.itemElement);
+
+      this.element.addEventListener('click', () =>  this.select() );
+  
+    }
+    select() {
+        const links = document.querySelectorAll('.tabs-link');
+        Array.from(links).forEach( link => link.classList.remove('tabs-link-selected'));
+        this.element.classList.toggle('tabs-link-selected');
+        this.tabItem.select();
+    }
+}
+
+class TabItem {
+    constructor(element) {
+      this.element = element;
+    }
+  
+    select() {
+        const items = document.querySelectorAll('.content');
+        Array.from(items).forEach( item => item.classList.remove('content-selected'));
+        this.element.classList.toggle('content-selected');
+        // TweenMax.from(".tabs-item-selected", 1.5, {y: 50, opacity: 0});
+    }
+  }
+
+let links = document.querySelectorAll('.tabs-link');
+links = Array.from(links).map( link => new TabLink(link));
+
+links[0].select();
+
