b519c41099b73d3ada98d8bb979f14eaa58901ea
Holden B <holden.bucher.92@gmail.com>
2018-10-29 21:44:04 -0500

    tweaked desktop styling for homepage and services page

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
 const toggleNav = () => {
-    console.log('yay');
     nav.classList.toggle('menu-showing');
     if (nav.classList.contains('menu-showing')) {
         navButton.src = "./img/nav-hamburger-close.png";
-        console.log(navButton.src);
         navContainer.style.paddingTop = '2.2%';
         navContainer.style.paddingBottom = '0';
     } else {
@@ -14,7 +12,6 @@ const toggleNav = () => {
 }
 
 const navButton = document.getElementById('nav-img');
-console.log(navButton);
 const nav = document.querySelector('.main-nav');
 const navContainer = document.querySelector('.nav-container');
 navButton.addEventListener('click', toggleNav);
@@ -25,7 +22,6 @@ class Tab {
         this.data = this.element.dataset.tab;
         this.card = document.querySelector(`.card[data-tab="${this.data}"]`);
         this.card = new Card(this.card);
-        console.log(this.card);
         this.element.addEventListener('click', () => this.select())
     }
     select(){
@@ -35,7 +31,6 @@ class Tab {
         this.card.select();
     }
 }
-
 class Card {
     constructor(element) {
         this.element = element;
@@ -43,16 +38,10 @@ class Card {
     select() {
         const cards = document.querySelectorAll('.card');
         cards.forEach(item => item.classList.remove('card-selected'));
-        console.log(this.element);
         this.element.classList.add('card-selected');
     }
 }
-
-
-
-
 let tab = document.querySelectorAll('.tab');
 tab = Array.from(tab).map(item => new Tab(item));
-
-tab[0].select();
+tab[1].select();
 
