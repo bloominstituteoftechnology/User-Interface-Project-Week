09c6e17a4d6f569fe068ff07c037e4f971a42fe8
BrandCam <BrandCam>
2018-10-30 15:19:59 -0500

    expand nav done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// JS goes here
+//nav
+const nav = document.querySelector('.nav');
+const navMenu = document.querySelector('.nav-menu');
+const navImg = document.querySelector('.nav-btn');
+
+navImg.addEventListener('click',() => {
+    navMenu.classList.toggle('hide')
+    nav.classList.toggle('nav-expand')
+    if(navImg.src === 'file:///C:/Users/Brandon/School/User-Interface-Project-Week/img/nav-hamburger-close.png'){
+    navImg.src = 'img/nav-hamburger.png';
+}
+else{
+    navImg.src = 'img/nav-hamburger-close.png'
+}
+    
+  
+})
