da297bcdc870f36994815596faba065cb4fa6a74
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-10-30 13:34:56 -0500

    add click event to navbar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// JS goes here
+(function() {
+  const navbarButton = document.querySelector('.header--hamburger');
+  const openBurger = document.querySelector('.header--hamburger .burger');
+  const closeBurger = document.querySelector('.header--hamburger .burger-close');
+  const navbarMenu = document.querySelector('.header .navbar');
+
+  navbarButton.addEventListener('click', function() {
+    navbarMenu.classList.toggle('display__toggle');
+    openBurger.classList.toggle('close');
+    closeBurger.classList.toggle('close');
+  });
+})()
