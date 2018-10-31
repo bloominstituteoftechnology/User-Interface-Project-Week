48a35461bcf60cb9d1b84df26b27d9b7ac7eb420
Josh Z <jzieger10@gmail.com>
2018-10-31 17:55:04 -0400

    Services mobile version complete. Reworked some of the code to be more organized. Move jumbotron to separate LESS file.

--- js/index.js
+++ js/index.js
@@ -61,8 +61,5 @@ class TabContent {
 
 let links = document.querySelectorAll(".tab-link");
 
-// Following the code in the Dropdown file, iterate through the array you created above creating a new instance of the TabLink class for each item.
 links = Array.from(links).map(link => new TabLink(link));
-
-// DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
 links[0].select();
