c88da5615dbad803285a645538f0ef980c820207
ChyChy <c.otikor@gmail.com>
2018-10-31 15:10:13 -0600

    Completed Tab component and cleaned up code on Home page

--- js/index.js
+++ js/index.js
@@ -23,3 +23,45 @@ console.log(openNav);
 
 closedNav.addEventListener('click', toggleMenuOn);
 openNav.addEventListener('click', toggleMenuOff);
+
+
+class Tab {
+  constructor(element) {
+    this.element = element;
+    this.data = this.element.dataset.tab;
+    this.buttonElement = document.querySelector(`.button[data-tab='${this.data}']`);
+    this.tabElement = document.querySelector(`.tab[data-tab='${this.data}']`);
+    this.tabContent = new TabContent(this.tabElement);
+    this.element.addEventListener('click', () => { this.click() })
+  };
+
+  click() {
+    const tabs = document.querySelectorAll('.button');
+    Array.from(tabs).forEach((tab) => {
+      tab.style.background = '#FFF';
+      tab.style.color = '#222222';
+    });
+    this.element.style.background = '#5e9fb9';
+    this.element.style.color = '#FFF';
+    this.tabContent.select();
+  }
+}
+
+class TabContent {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    const content = document.querySelectorAll('.tab')
+    Array.from(content).forEach((tab) => {
+      tab.style.display = 'none';
+    });
+    this.element.style.display = 'flex'; 
+  }
+
+}
+
+let buttons = document.querySelectorAll('.button');
+
+buttons = Array.from(buttons).map(button => new Tab(button));
