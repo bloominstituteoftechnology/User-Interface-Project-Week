3f9c8975e9d3b62e53223c2a04e92080359d0009
steelworx <eileencuevas62@gmail.com>
2018-10-31 15:28:25 -0400

    mobile styles added for index.html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// JS goes here
+let jumbo = document.querySelector('.jumbotron-img');
+let firstAboutImg = document.querySelector('.about-top-right');
+let secondAboutImg = document.querySelector('.about-bottom-left');
+let villas = document.querySelector('.villa-img');
+let outskirts = document.querySelector('.outskirts-img');
+let blocks = document.querySelector('.blocks-img');
+//const breakpoint = window.matchMedia('(max-width:')
+
+if (window.matchMedia('(max-width: 500px)').matches) {
+    jumbo.src = 'img/home/home-mobile-jumbotron.png';
+    firstAboutImg.src = 'img/home/home-mobile-img-1.png';
+    secondAboutImg.src = 'img/home/home-mobile-img-2.png';
+    villas.src = 'img/home/home-mobile-villas-img.png';
+    outskirts.src = 'img/home/home-mobile-outskirts-img.png';
+    blocks.src = 'img/home/home-mobile-the-blocks-img.png';
+}
