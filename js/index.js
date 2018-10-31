9e29e32447011798645b672091cbd6b7e2a7353c
Dennis Orbison <orbison.dennis@gmail.com>
2018-10-30 19:00:57 -0700

    completed js for navigation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // JS goes here
+
+const ham = document.querySelector('.ham');
+const exit = document.querySelector('.exit');
+const nav = document.querySelector('nav');
+
+ham.addEventListener('click', () => {
+  nav.classList.toggle('nav-hidden');
+  nav.classList.toggle('nav-expanded');
+  ham.classList.toggle('ham');
+  ham.classList.toggle('exit');
+  exit.classList.toggle('ham');
+  exit.classList.toggle('exit');
+})
+
+exit.addEventListener('click', () => {
+  nav.classList.toggle('nav-hidden');
+  nav.classList.toggle('nav-expanded');
+  ham.classList.toggle('ham');
+  ham.classList.toggle('exit');
+  exit.classList.toggle('ham');
+  exit.classList.toggle('exit');
+})
