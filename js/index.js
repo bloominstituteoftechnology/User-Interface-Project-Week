16c40eb9be91cd6131d4321d6fa3b4e15f0060e3
David Situ <davidsitu626@gmail.com>
2018-10-31 14:49:43 -0700

    Home menu icon working, menu close pending, navigation destinations pending,

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// JS goes here
+let toggleMenu = () => {
+  //toggle menu
+  menu.classList.toggle('menu-selected');
+}
+
+
+//reference menu
+let menu = document.querySelector('.menu');
+
+//reference menu Button
+const menuOpen = document.querySelector('.menu-button');
+menuOpen.addEventListener('click', toggleMenu);
