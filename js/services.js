630a823438a7216487c2d004a3f48ca418a28a31
David Situ <davidsitu626@gmail.com>
2018-10-31 15:29:29 -0700

    Just triggered the tab's content after clicking tab button

--- js/services.js
+++ js/services.js
@@ -17,7 +17,6 @@ class TabLink {
     const links = document.querySelectorAll('.tab-button');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach( link => {
-
       link.classList.remove('tab-button-selected');
     });
 
@@ -38,7 +37,7 @@ class TabItem {
     // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll('.content');
     Array.from(items).forEach( item => {
-      console.log('content-selected?');
+
       item.classList.remove('content-selected');
     });
     // Remove the class "content-selected" from each element
