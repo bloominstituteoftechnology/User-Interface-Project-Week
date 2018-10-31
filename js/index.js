67202b99fa78aefd13cfbc767caad7ac96f83d0f
Trai Lynne <trailynne@outlook.com>
2018-10-31 12:39:40 -0600

    Set up JS file for TabsComponent Created class basic structure at top of page Initialized DOM reference to tabs

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // JS goes here
+
+class TabLink {
+    constructor(tab) {
+        this.tab = tab;
+    }
+
+    // Methods
+}
+
+class TabCard {
+    constructor(card) {
+        this.card = card;
+    }
+}
+
+
+// Initial Dom Reference
+
+let tabs = document.querySelectorAll('.tab');
+
+tabs = Array.from(tabs).map(tab => new TabLink(tab));
+
+console.log(tabs);
