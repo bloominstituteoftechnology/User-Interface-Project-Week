a669e84fcc5aea4a721b6d3f521e183103e2920f
cbtsao47 <cbtsao47@gmail.com>
2018-10-31 14:11:55 -0500

    expanded view html/less/js on homepage

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+const hamburger = document.querySelector('.menu__hamburger');
+const menuExpanded = document.querySelector('.menu-expanded');
+const hamburgerClose = document.querySelector('.menu__hamburger--close');
+
+hamburger.addEventListener('click', (e) => {
+    menuExpanded.style.display = 'block';
+    console.log('click1');
+});
+hamburgerClose.addEventListener('click',(e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    menuExpanded.style.display = 'none'
+    console.log('click2');
+});
