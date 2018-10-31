715e300b200a19b9d9a7bffc9396a375563410fb
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-10-31 13:07:14 -0400

    Changed footer and corrected overlay

--- js/index.js
+++ js/index.js
@@ -2,64 +2,41 @@ class TabLink {
     constructor(element) {
         this.element = element;
         this.data = this.element.dataset.tab;
-        this.itemElement = document.querySelector(`.tab-content[data-tab='${this.data}]`);
+        this.itemElement = document.querySelector(`.tab-content[data-tab='${this.data}']`);
         this.tabItem = new TabItem(this.itemElement);
-        this.element.addEventListener('click', () => {
-            this.selectTab()
-        });
+        this.element.addEventListener('click', () => this.select());
     }
 
-    selectTab() {
-            const tlinks = document.querySelectorAll('.tab');
+    select() {
+        const tlinks = document.querySelectorAll('.tab');
 
-            Array.from(tlinks).forEach(tlink => {
-                tlink.classList.remove('tab-selected')
-            });
+        Array.from(tlinks).forEach(tlink => {
+            tlink.classList.remove('tab-selected');
+        });
 
-            this.element.classList.add('tab-selected');
+        this.element.classList.toggle('tab-selected');
 
-            this.tabItem.select();
-        }
-        // deselect() {
-        //     this.link.classList.remove('tab-selected');
+        this.tabItem.select();
+    }
 
-    //     this.tabItem.deselect();
-    // }
 }
 
 class TabItem {
-    constructor(element) {
-        this.element = element;
+    constructor(tabContent) {
+        this.tabContent = tabContent;
     }
     select() {
-            let items = document.querySelectorAll('.tab-content');
+        let items = document.querySelectorAll('.tab-content');
 
-            Array.from(items).forEach(item => {
-                item.classList.remove('tab-selected')
-            });
+        Array.from(items).forEach(item => {
+            item.classList.add('tab-selected');
+        });
 
-            this.element.classList.add('tab-selected');
-        }
-        // deselect() {
-        //     this.link.classList.remove('tab-selected');
+        this.tabContent.classList.toggle('tab-selected');
+        console.log(this.element);
+    }
 
-    //     this.tabLink.deselect();
-    // }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 let tlinks = document.querySelectorAll('.tabs');
