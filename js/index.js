e7c3fddd323ba91fa90f380c2142da605bbd4b4f
Thomas-Ketterman <32739188+Thomas-Ketterman@users.noreply.github.com>
2018-10-31 19:03:07 -0400

    finished navigation JS.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // JS goes here
+const hamburger = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+const brand = document.querySelector(".brand");
+
+hamburger.addEventListener("click", function() {
+  nav.classList.toggle("nav-toggle");
+});
