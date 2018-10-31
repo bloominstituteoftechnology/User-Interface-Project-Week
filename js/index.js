cc24599f5729abc4da1a7b545e2fc5edf8ccd44e
Addison-Stavlo <AddieStavlo@gmail.com>
2018-10-30 20:00:50 -0400

    menu animation complete

--- js/index.js
+++ js/index.js
@@ -4,12 +4,35 @@
 const navButton = document.querySelector('.burger-button');
 const menu = document.querySelector('.expanding-menu');
 let buttonImgs = navButton.querySelectorAll('img');
+const aTags = Array.from(menu.querySelectorAll('a'));
 
 navButton.addEventListener('click',()=>{
-    menu.classList.toggle('hidden');
+    // menu.classList.toggle('hidden');
     buttonImgs[0].classList.toggle('current-btn');
     buttonImgs[1].classList.toggle('current-btn');
+
+    if(!menu.classList.contains('hidden')){
+        setTimeout(()=>aTags.forEach(tag => tag.style.display = 'none'),300);
+
+        TweenMax.to(menu,0.6,{height: 0, onComplete: ()=> {
+            menu.removeAttribute('style');
+            menu.classList.toggle('hidden');
+            buttonImgs[1].classList.remove('current-btn');
+            buttonImgs[0].classList.add('current-btn');
+            aTags.forEach(tag => tag.removeAttribute('style'));
+        }});
+    }
+
+    else{
+        menu.classList.toggle('hidden');
+        TweenMax.from(menu,0.6,{height: 0, ease: Bounce.easeOut, onComplete: () => {
+            menu.removeAttribute('style');
+            buttonImgs[0].classList.remove('current-btn');
+            buttonImgs[1].classList.add('current-btn');           
+        }});
+    }
 });
+
 //End of Nav Button
 class Tab {
     constructor(element){
