a929bf5ff63386a60410ff314a5e9d2dbc612e75
cbtsao47 <cbtsao47@gmail.com>
2018-10-31 14:34:10 -0500

    complete nav in services page

--- js/index.js
+++ js/index.js
@@ -4,12 +4,11 @@ const menuExpanded = document.querySelector('.menu-expanded');
 const hamburgerClose = document.querySelector('.menu__hamburger--close');
 
 hamburger.addEventListener('click', (e) => {
-    menuExpanded.style.display = 'block';
-    console.log('click1');
+    menuExpanded.classList.toggle('menu-open');
+    console.log(menuExpanded);
 });
 hamburgerClose.addEventListener('click',(e) => {
     e.stopPropagation();
-    e.preventDefault();
-    menuExpanded.style.display = 'none'
-    console.log('click2');
+    menuExpanded.classList.toggle('menu-open');
+    console.log(menuExpanded);
 });
