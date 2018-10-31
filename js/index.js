c89449366ef30e0a5e1f47e9cf098dd9cf32c3ff
Jasmine <jasmineanderson315@yahoo.com>
2018-10-31 13:15:53 -0400

    slightly tweaked nav toggle. fixed mobile issues. started services html

--- js/index.js
+++ js/index.js
@@ -2,27 +2,25 @@
 
 // navbar menu
 
-class Navbar {
-    constructor(element) {
-        this.element = element
-        this.button = this.element.querySelector('.navBurger')
-        this.content = this.element.querySelector('.dropdownNav')
+const navBurger = document.querySelector('.navBurger');
+const dropdownNav = document.querySelector('.dropdownNav');
+const nav = document.querySelector('nav');
+const navBar = document.querySelector('.navBar');
 
-        this.button.addEventListener("click", () => {
-            this.toggleContent(event);
-        })
+navBurger.addEventListener('click', () => {
+    // menu-open should be menu-closed. got it backwards. whoops
+    if (navBurger.classList.contains('navOpen')) {
+        // expand
+        navBurger.classList.remove('navOpen');
+        navBurger.src = '/img/nav-hamburger-close.png';
+        dropdownNav.style.display = 'flex';
+        dropdownNav.style['flex-direction'] = 'column';
+        navBar.style.display = 'block';
+        fixedNav.style['justify-content'] = 'space-around';
+    } else {
+        // hide menu
+        navBurger.classList.add('navOpen');
+        navBurger.src = '/img/nav-hamburger.png';
+        dropdownNav.style.display = 'none';
     }
-
-    toggleContent() {
-        this.content.classList.toggle("navHidden")
-    }
-}
-
-let navbar = document.querySelectorAll(".navbar")
-navbar = Array.from(navbar).map(navbar => new Navbar(navbar))
-
-let navBurger = document.querySelector(".navBurger")
-
-navBurger.addEventListener("click", () => {
-    navBurger.classList.toggle("dropdownNavToggle")
-})
+});
