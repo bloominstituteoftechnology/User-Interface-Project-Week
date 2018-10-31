b9d5e46b0ac6485b5daa4b119e5cc27314546215
tryingtokeepup <kl.hawaii@gmail.com>
2018-10-31 15:50:34 -0700

    lol, its complete crap, but the code for the navigation expand is done. mvp almost there

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // JS goes here
+
+const menu = document.querySelector('.navbar');
+const openBtn = document.querySelector('.navbar-hamburger');
+const closeBtn = document.querySelector('.navbar-close');
+const extNavBar = document.querySelector('.navbar-menu-content');
+
+openBtn.addEventListener('click', function(){
+  console.log('click worked!');
+  
+  openBtn.classList.toggle('navbar-toggle');
+  closeBtn.classList.toggle('navbar-toggle');
+  extNavBar.classList.toggle('menu-toggle');
+  
+})
+
+closeBtn.addEventListener('click', function() {
+    openBtn.classList.toggle('navbar-toggle');
+  closeBtn.classList.toggle('navbar-toggle');
+  extNavBar.classList.toggle('menu-toggle');
+})
