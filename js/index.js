79ef3e68f89b45b50d523a3ac869b19d3f6c5a8d
Trai Lynne <trailynne@outlook.com>
2018-10-31 12:43:36 -0600

    Tab Component: Connected tab to it's card used dataset.tab to get matching card turned card into new TabCard element

--- js/index.js
+++ js/index.js
@@ -3,6 +3,12 @@
 class TabLink {
     constructor(tab) {
         this.tab = tab;
+        this.data = this.tab.dataset.tab;
+        this.cardElement = document.querySelector(`.card[data-tab='${this.data}']`);
+
+        this.card = new TabCard(this.cardElement);
+
+        console.log(this.card);
     }
 
     // Methods
