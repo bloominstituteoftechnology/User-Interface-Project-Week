2d0e0d9f46971c242a8e73fbd86decbb5b7bb47b
Zayna C <zaynacrick@gmail.com>
2018-10-31 20:35:55 -0400

    navigation menu completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // JS goes here
+
+
+
+
+const full = document.querySelector('.fullmenu')
+const menubutton = document.querySelector('.menubtn');
+const menuclose =  document.querySelector('.menucontent');
+const menuclosebtn = document.querySelector('.menu-close');
+
+
+full.addEventListener('click', function(){
+    menubutton.classList.toggle('menu-toggle');
+    menuclose.classList.toggle('menu-toggle');
+    console.log('click is working');
+ 
+});
+
+menuclosebtn.addEventListener('click', function() {
+    display = none;
+});
