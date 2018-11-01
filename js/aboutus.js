23d7f3b5bef6f6841211f2e8e6fdd96ea997f850
Aman <amsingh714@gmail.com>
2018-11-01 12:43:23 -0400

    fixed transition for about us page

--- js/aboutus.js
+++ js/aboutus.js
@@ -2,31 +2,32 @@ class Question {
   constructor(element) {
     this.element = element;
     this.button = this.element.querySelector(".expand-button");
-    this.buttonText = document.querySelector(".expand-button").innerText;
+    this.button.textContent = "More Info:";
+    this.currentState = false;
 
     this.button.addEventListener("click", event => {
       this.expandFAQ();
-
     });
   }
 
-
   expandFAQ() {
-    event.currentTarget.parentNode.classList.toggle("expand");
-    // TweenMax.to(this.element, 2, {
-    //     ease: Power1.easeOut,
-    //     height: '100%'
-    //   });
+    if (this.currentState) {
+        TweenMax.to(this.element, 0.5, {
+            css:{className:'-=expand'},
+            ease: Power4.easeOut,
+        });
+        this.button.textContent = "More Info:";
+    } else {
+        TweenMax.to(this.element, 0.5, {
+            css:{className:'+=expand'},
+            ease: Power4.easeOut,
+        });
+        this.button.textContent = "Close";
+    }
+    this.currentState = !this.currentState;
   }
 }
 
 let questions = document.querySelectorAll(".faq");
 
 questions = Array.from(questions).map(question => new Question(question));
-
-let buttons = document.querySelectorAll(".expand-button");
-buttons = Array.from(buttons);
-
-function expandQuestion(arr) {}
-
-expandQuestion(buttons);
