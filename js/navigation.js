46ec04e5e800aac1e43922aa905a4682c0bb79bf
steelworx <eileencuevas62@gmail.com>
2018-11-01 12:00:39 -0400

    cleaned up less files a little bit by creating mixins.less

--- js/navigation.js
+++ js/navigation.js
@@ -3,12 +3,9 @@ let nav = document.querySelector('.navigation');
 let hamburger = document.querySelector('.hamburger-menu');
 let menuItems = document.querySelector('.navigation-links');
 
-let jumbotron = document.querySelector('.home-jumbotron');
-
 hamburger.addEventListener('click', () => {
         nav.classList.toggle('max-height');
         menuItems.classList.toggle('links-hidden');
-        //jumbotron.classList.toggle('hide-jumbo');
 
         if (menuItems.classList.contains('links-hidden')) {
             hamburger.src = 'img/nav-hamburger.png';
