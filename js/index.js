56878f1860003e4ecd17ab394dd22421abce0ddb
David Situ <davidsitu626@gmail.com>
2018-11-01 11:38:52 -0700

    pixel perfection pending

--- js/index.js
+++ js/index.js
@@ -10,3 +10,8 @@ let menu = document.querySelector('.menu');
 //reference menu Button
 const menuOpen = document.querySelector('.menu-button');
 menuOpen.addEventListener('click', toggleMenu);
+
+//reference menu Button close
+
+const menuClose = document.querySelector('.menu-button-close');
+menuClose.addEventListener('click', toggleMenu);
