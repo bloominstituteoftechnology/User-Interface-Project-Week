b564a4fdb3422d4fed42618b8ac163f1cb6de63c
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2018-11-01 10:41:30 -0700

    Add nav bar animation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // JS goes here
+const formButton = document.querySelector(".contact-form__form--submit");
+const formMain = document.querySelector(".contact-form__form");
+// const footerButton = document.querySelector(".contact__subscribe--form");
+// const footerForm = document.querySelector("contact__subscribe--form-top");
+
+formButton.addEventListener('click', function() {
+    formMain.reset();
+    window.alert("Thank you for your submition! We will be in contact within 24 hours");
+    
+})
+
+// footerForm.addEventListener('submit', function(){
+//     footerForm.reset();
+//     window.alert("Thank you for your submition! We will be in contact within 24 hours");
+// })
