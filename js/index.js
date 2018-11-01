7c823bf72e5d84cc71ea66234a24d4cd21229ac5
steelworx <eileencuevas62@gmail.com>
2018-11-01 16:00:43 -0400

    began making design a little more tablet-friendly

--- js/index.js
+++ js/index.js
@@ -5,11 +5,20 @@ let villas = document.querySelector('.villa-img');
 let outskirts = document.querySelector('.outskirts-img');
 let blocks = document.querySelector('.blocks-img');
 
-if (window.matchMedia('(max-width: 500px)').matches) {
-    jumbo.src = 'img/home/home-mobile-jumbotron.png';
-    firstAboutImg.src = 'img/home/home-mobile-img-1.png';
-    secondAboutImg.src = 'img/home/home-mobile-img-2.png';
-    villas.src = 'img/home/home-mobile-villas-img.png';
-    outskirts.src = 'img/home/home-mobile-outskirts-img.png';
-    blocks.src = 'img/home/home-mobile-the-blocks-img.png';
-}
+window.addEventListener('resize', () => {
+    if (window.matchMedia('(max-width: 600px)').matches) {
+        jumbo.src = 'img/home/home-mobile-jumbotron.png';
+        firstAboutImg.src = 'img/home/home-mobile-img-1.png';
+        secondAboutImg.src = 'img/home/home-mobile-img-2.png';
+        villas.src = 'img/home/home-mobile-villas-img.png';
+        outskirts.src = 'img/home/home-mobile-outskirts-img.png';
+        blocks.src = 'img/home/home-mobile-the-blocks-img.png';
+    } else {
+        jumbo.src = 'img/home/home-jumbotron.png';
+        firstAboutImg.src = 'img/home/home-img-1.png';
+        secondAboutImg.src = 'img/home/home-img-2.png';
+        villas.src = 'img/home/home-villas-img.png';
+        outskirts.src = 'img/home/home-outskirts-img.png';
+        blocks.src = 'img/home/home-the-blocks-img.png';
+    }
+})
