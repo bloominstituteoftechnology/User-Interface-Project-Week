0e2cc92ee605643466eb27fb017b8c2710243aa3
jamespagedev <jamespagedev@outlook.com>
2018-11-03 18:53:02 -0700

    (desktop) contact: html and css(left-side) complete

--- js/coming-soon-clock.js
+++ js/coming-soon-clock.js
@@ -2,7 +2,7 @@
 var countDownDate = new Date("Nov 5, 2018 8:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var clock = setInterval(function() {
 
   // Get todays date and time
   var now = new Date().getTime();
@@ -17,12 +17,12 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in an element with id="demo"
-  document.getElementById("countdown-clock").innerHTML = days + "d " + hours + "h "
+  document.querySelector(".countdown-clock").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text 
   if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown-clock").innerHTML = "EXPIRED";
+    clearInterval(clock);
+    document.querySelector(".countdown-clock").innerHTML = "EXPIRED";
   }
 }, 1000);
