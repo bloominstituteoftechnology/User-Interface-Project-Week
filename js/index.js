bd4418654ba113803331607e214a79859f4aebf5
1@$@1github <asaofdiamonds@gmail.com>
2018-11-06 16:46:16 -0700

    tabs on service page are functional, need to finish less for service page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,73 @@
 // JS goes here
+
+//Services page
+
+//Tabs
+class TabLink {
+    constructor(element) {
+        this.element = element;
+
+        // custom data attribute on the Link
+        this.data = this.element.dataset.tab;
+
+        // custom data attribute to get the associated Item element
+        this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+
+        //  a new instance of the TabItem class.
+        this.tabItem = new TabItem(this.itemElement);
+
+        //  event listener calling the select method on click
+        this.element.addEventListener('click', () => {
+            this.select();
+        });
+
+    }
+
+    select() {
+        // Get all of the elements with the tabs-link class
+        const links = document.querySelectorAll('.tabs-link');
+
+        // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+        links.forEach(link => {
+            link.classList.remove('tabs-link-selected');
+        })
+        // Array.from(links).forEach((link) => {
+        //     link.classList.remove('tabs-link-selected');        
+        // });
+
+        // Add a class named "tabs-link-selected" to this link
+        this.element.classList.add('tabs-link-selected');
+
+        // Call the select method on the item associated with this link
+        this.tabItem.select();
+        //   this.element.addEventListener('click', () => { this.select() });
+    }
+}
+
+class TabItem {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select() {
+        const items = document.querySelectorAll('.tabs-item');
+
+        items.forEach(item => {
+            item.classList.remove('tabs-item-selected');
+        });
+        // Select all ".tabs-item" elements from the DOM
+        // Remove the class "tabs-item-selected" from each element
+        // Add a class named "tabs-item-selected" to this element 
+        this.element.classList.add('tabs-item-selected');
+    }
+}
+
+
+// control flow starts here 
+let links = document.querySelectorAll('.tabs-link');
+
+// iterate through the array above creating a new instance of the TabLink class for each item. 
+links = Array.from(links).map(tabsLinks => new TabLink(tabsLinks));
+
+// DO THIS LAST: Once you have created an array of TabLink instances. call select() on the first item in the array
+links[0].select()
