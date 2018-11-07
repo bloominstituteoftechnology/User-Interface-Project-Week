8cd9a2c285769d2b568007e035e00e7049261c55
J A <jarata@gmail.com>
2018-11-07 15:10:23 -0800

    navigation menu tuning

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // JS goes here
+class Nav {
+  constructor(element) {
+    this.element = element;
+    this.button = this.element.querySelector('.menu-button');
+    this.content = this.element.querySelector('.menu-content');
+    this.closeButton = this.element.querySelector('.close-button');
+    this.button.addEventListener('click', event => this.toggleContent(event));
+    this.closeButton.addEventListener('click', event => this.toggleContent(event));
+  }
+  toggleContent() {
+    this.content.classList.toggle('menu-hidden');
+    this.button.classList.toggle('close-hidden');
+    this.closeButton.classList.toggle('close-hidden');
+  }
+}
+
+let links = document.querySelectorAll('.menu');
+links = Array.from(links).map(link => new Nav(link));
