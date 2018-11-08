2b81ed6eddd3f39d01a2cf912760706184936b41
Joseph Eastman <josephryan96@gmail.com>
2018-11-07 23:30:43 -0800

    navbar finally working functionally

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+const menu = document.querySelector('.menu-2');
+const hamburger = document.querySelector('.hamburger');
+const hamburgerOpen = document.querySelector('#menu-button-open');
+const hamburgerClose = document.querySelector('#menu-button-close');
+const navExpand = document.querySelector('nav');
+
+hamburger.addEventListener('click', function() {
+    menu.classList.toggle('menu-open')
+    menu.classList.toggle('hide')
+    menu.classList.toggle('menu');
+    navExpand.classList.toggle('nav-expand');
+    hamburgerOpen.classList.toggle('hide');
+    hamburgerClose.classList.toggle('hide');
+});
