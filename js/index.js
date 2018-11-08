bed0e23ee2f735334969b917bc20d9897dbaa058
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-08 15:50:11 -0500

    hamburger menu home page done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // JS goes here
+const navButtons = document.querySelector('.nav-buttons');
+const hamburger = document.querySelector('#hamburger');
+const hamburgerOpen = document.querySelector('#hamburger-open');
+const hamburgerClose = document.querySelector('#hamburger-close');
+const hideH1 = document.querySelector ('h1')
+
+hamburger.addEventListener('click', function() {
+  navButtons.classList.toggle('hidden');
+  hamburgerOpen.classList.toggle('hidden');
+  hamburgerClose.classList.toggle('hidden');
+  hideH1.classList.toggle('hidden');
+})
