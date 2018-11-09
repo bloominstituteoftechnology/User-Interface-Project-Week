2ab3d3dd6acdf6998c5afcbf5c76fb62805ce295
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-11-08 23:52:03 -0500

    services button color change selected

--- js/index.js
+++ js/index.js
@@ -12,10 +12,11 @@ hamburger.addEventListener('click', function() {
   hideH1.classList.toggle('hidden');
 })
 
-
+//Services tabs
 const buttons = document.querySelectorAll('.tab-link');
 const contents = document.querySelectorAll('.content');
-
+//dataset is an attribute of 'data-' on html and it is the bridge between the js and the html 
+//target is an event property and refers to the object that triggered the event.
 function buttonClick(event) {
   const dataBtn = event.target.dataset.tab;
   
@@ -32,4 +33,20 @@ function buttonClick(event) {
 
 for(let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', buttonClick);
+}
+
+
+
+
+const buttonsSelected = document.querySelectorAll('.tab-link');
+
+function buttonSelectedBlue(event){
+  const dataBtn = event.target.dataset.tab;
+  if (dataBtn===dataBtn){
+    this.style.backgroundColor = "#5E9FB9";
+  }
+}
+
+for(let i = 0; i < buttonsSelected.length; i++) {
+  buttonsSelected[i].addEventListener('click', buttonSelectedBlue);
 }
