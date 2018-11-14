c1a253f082af5bf45d6cbb1b30dddce15358efb8
Andrew <andrewryanogle@gmail.com>
2018-11-13 22:35:57 -0800

    finished navigation css and html, also worked out the logic for the menu. now I have to do the components for the menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // JS goes here
+let menu = document.querySelector('.menu');
+let menuButton = document.querySelector('.menu-button');
+let menuClose = document.querySelector('.menu-close')
+
+menuButton.addEventListener('click',(event)=>{
+    menu.classList.toggle('menu-open');
+    menuButton.classList.toggle('buttonClosed');
+    menuClose.classList.toggle('buttonClosed');
+});
+
+menuClose.addEventListener('click',(event)=>{
+    menu.classList.toggle('menu-open');
+    menuButton.classList.toggle('buttonClosed');
+    menuClose.classList.toggle('buttonClosed');
+});
