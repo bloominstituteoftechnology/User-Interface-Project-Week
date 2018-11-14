2baa6a3c2de41a49d25a844a93b4bd0c66e836be
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-11-13 22:47:51 -0500

    ADD MENU.JS FILE AND EXPANDABLE MODAL

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+
+const toggleMenu = () => {
+    // Toggle the "menu--open" class on your menu refence. 
+    menu.classList.toggle('menu--open');
+}
+
+// Short notation for document
+const doc = document;
+// Start Here: Create a reference to the ".menu" class
+const menu = doc.querySelector('.menu');
+// create a reference to the ".menu-button" class
+const menuButton = doc.querySelector('.menu-button');
+// Using your menuButton reference, add a click handler that calls toggleMenu
+// console.log(menuButton);
+
+menuButton.addEventListener('click', toggleMenu);
+  
+  
+  
