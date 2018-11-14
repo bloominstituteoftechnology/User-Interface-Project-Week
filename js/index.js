3fd034e4e3ab098a7e50d66ce84409ff3f0321db
Trip1701 <bj2336@email.vccs.edu>
2018-11-14 08:49:10 -0500

    Navigation -- Expanded Complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-// JS goes here
+
+//**********************Navigation Open and Close**********************
+
+const navButton = document.querySelector('.navContainer__top-bar img');
+
+navButton.addEventListener('click', () => document.querySelector('.menu').classList.toggle('open'));
