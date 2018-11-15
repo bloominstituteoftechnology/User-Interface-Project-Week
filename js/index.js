4b3906ab7fa414caa1a0fe1bba07ca5262ede803
Daniel Lim <danheelim@gmail.com>
2018-11-14 23:54:49 -0800

    finished all the CSS and finished the menu toggle

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // JS goes here
+
+//menu
+const toggleMenu = () => {
+	// console.log(menu)
+	menu.classList.toggle('nav--open')
+}
+
+const menu = document.querySelector(".nav_contain");
+// console.log(menu);
+const menuButton = document.querySelector('.main_nav_buttons');
+// console.log(menuButton);
+
+menuButton.addEventListener('click', toggleMenu)
