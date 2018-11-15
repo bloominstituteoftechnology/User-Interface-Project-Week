d990fcd6ac460c2d00a98c7ba88d9a15b0c28720
Bill Geist <wcgeist@gmail.com>
2018-11-14 23:03:29 -0500

    function services page

--- js/index.js
+++ js/index.js
@@ -1,64 +1,81 @@
 // JS goes here
 class NavButton {
     constructor(element) {
-      this.element = element;
-      this.data = this.element.dataset.tab;
-      this.itemElement = document.querySelector(`.nav-button-close[data-tab='${this.data}']`);
-      this.closeButton = new CloseButtonWhileOpen(this.itemElement);
-      this.element.addEventListener('click', () => {this.select() })
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.itemElement = document.querySelector(`.nav-button-close[data-tab='${this.data}']`);
+        this.closeButton = new CloseButtonWhileOpen(this.itemElement);
+        this.element.addEventListener('click', () => { this.select() })
     };
-    select() {    
-      this.element.classList.add('active');
-      this.closeButton.select();
-    }
-  }
-
-  class CloseButtonWhileOpen {
-    constructor(element) {
-      this.element = element;
-    }
-  
     select() {
-      this.element.classList.add('active');
-  
+        this.element.classList.add('active');
+        this.closeButton.select();
     }
-  }
-
-  class NavButtonClose {
+}
+class CloseButtonWhileOpen {
     constructor(element) {
-      this.element = element;
-      this.data = this.element.dataset.tab;
-      this.itemElement = document.querySelector(`.nav-button[data-tab='${this.data}']`);
-      this.openButton = new NavButtonOpen(this.itemElement);
-      this.element.addEventListener('click', () => {this.select() })
+        this.element = element;
+    }
+
+    select() {
+        this.element.classList.add('active');
+
+    }
+}
+
+class NavButtonClose {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.itemElement = document.querySelector(`.nav-button[data-tab='${this.data}']`);
+        this.openButton = new NavButtonOpen(this.itemElement);
+        this.element.addEventListener('click', () => { this.select() })
     };
-  
-    select() {    
-      this.element.classList.remove('active');
-      this.openButton.select();
-    }
-  }
-
-  class NavButtonOpen {
-    constructor(element) {
-      this.element = element;
-    }
-  
     select() {
-      this.element.classList.remove('active');
-  
+        this.element.classList.remove('active');
+        this.openButton.select();
     }
-  }
+}
 
+class NavButtonOpen {
+    constructor(element) {
+        this.element = element;
+    }
+    select() {
+        this.element.classList.remove('active');
 
+    }
+}
 
+navButton = new NavButton(document.querySelector('.nav-button'));
+navButtonClose = new NavButtonClose(document.querySelector('.nav-button-close'));
 
+class TabButton {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.itemElement = document.querySelector(`.services-body-section-container[data-tab='${this.data}']`);
+        this.tabItem = new TabItem(this.itemElement);
+        this.element.addEventListener('click', () => { this.select() })
+    };
+    select() {
+        const links = document.querySelectorAll('.services-button');
+        Array.from(links).forEach(link => link.classList.remove('button-selected'));
+        this.element.classList.add('button-selected');
+        this.tabItem.select();
+    }
+}
 
+class TabItem {
+    constructor(element) {
+        this.element = element;
+    }
+    select() {
+        const items = document.querySelectorAll('.services-body-section-container');
+        Array.from(items).forEach(item => item.classList.remove('container-selected'));
+        this.element.classList.add('container-selected');
+    }
+}
 
-
-
-
-
-
-navButton = new NavButton (document.querySelector('.nav-button'));
-navButtonClose = new NavButtonClose (document.querySelector('.nav-button-close'));
+buttons = document.querySelectorAll('.services-button')
+    .forEach(button => new TabButton(button));
