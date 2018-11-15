11fc4dd7d3d7f9d25c23b83ee6b01f6e114ba44d
VonKleistman <VonKleistman@gmail.com>
2018-11-15 14:20:07 -0600

    Built layout for Homepage Desktop

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // JS goes here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Nav hamburger recipe
+let burger = document.querySelector(`.burger`);
+let burgerButton = burger.querySelector(`#hamburger`);
+let menuToExpand = burger.querySelector(`nav`);
+burgerButton.addEventListener(`click`, function(){
+  burger.classList.toggle(`shrink-header-bar`);
+  menuToExpand.classList.toggle(`hide-nav`);
+  if (burgerButton.getAttribute(`src`) == `img/nav-hamburger.png`){
+    burgerButton.setAttribute(`src`, `img/nav-hamburger-close.png`);
+  } else {
+    burgerButton.setAttribute(`src`, `img/nav-hamburger.png`);
+  }
+})
