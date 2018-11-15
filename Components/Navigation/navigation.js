5da7afce4098ccf488fca6f8fa4737e9a65c8e34
Jonathan Dorety <kyullog@gmail.com>
2018-11-15 00:29:51 -0500

    Home page desktop layout almost finished, wth tweaking required

--- Components/Navigation/navigation.js
+++ Components/Navigation/navigation.js
@@ -3,6 +3,7 @@ class Navigation {
     this.element = element;
     this.button = this.element.querySelector('#menu-button');
     this.menu = this.element.querySelector('.links');
+    this.bar = this.element.querySelector('.main-nav-bar')
 
     this.button.addEventListener('click', (event) => {
       this.menu.classList.toggle('links-open')
