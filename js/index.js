d20babfe8fa05b11d4a294a9ac596af5270f6fdd
Andrew <andrewryanogle@gmail.com>
2018-11-14 19:14:00 -0800

    finished custom js module for navigation

--- js/index.js
+++ js/index.js
@@ -1,16 +1,48 @@
 // JS goes here
-let menu = document.querySelector('.menu');
-let menuButton = document.querySelector('.menu-button');
-let menuClose = document.querySelector('.menu-close')
+class Navigation{
+    constructor(navItem){
+        this.navItem = navItem;
+        this.menu = this.navItem.querySelector('.menu');
+        this.menuButton = this.navItem.querySelector('.menu-button');
+        this.menuClose = this.navItem.querySelector('.menu-close');
 
-menuButton.addEventListener('click',(event)=>{
-    menu.classList.toggle('menu-open');
-    menuButton.classList.toggle('buttonClosed');
-    menuClose.classList.toggle('buttonClosed');
-});
+        this.menuButton.addEventListener('click',()=>{
+            this.menuSlide();
+        });
+        this.menuClose.addEventListener('click',()=>{
+            this.menuSlide();
+        });
+        
 
-menuClose.addEventListener('click',(event)=>{
-    menu.classList.toggle('menu-open');
-    menuButton.classList.toggle('buttonClosed');
-    menuClose.classList.toggle('buttonClosed');
-});
+
+    }
+    menuSlide(){
+        this.menu.classList.toggle('menu-open');
+        this.menuButton.classList.toggle('buttonClosed');
+        this.menuClose.classList.toggle('buttonClosed');
+    };
+
+}
+
+let navigation = document.querySelectorAll('.navigation');
+navigation = Array.from(navigation);
+
+navigation.map((navItem)=>{
+    return new Navigation(navItem);
+})
+
+// let menu = document.querySelector('.menu');
+// let menuButton = document.querySelector('.menu-button');
+// let menuClose = document.querySelector('.menu-close')
+
+// menuButton.addEventListener('click',(event)=>{
+//     menu.classList.toggle('menu-open');
+//     menuButton.classList.toggle('buttonClosed');
+//     menuClose.classList.toggle('buttonClosed');
+// });
+
+// menuClose.addEventListener('click',(event)=>{
+//     menu.classList.toggle('menu-open');
+//     menuButton.classList.toggle('buttonClosed');
+//     menuClose.classList.toggle('buttonClosed');
+// });
