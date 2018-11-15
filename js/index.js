37f35a65635fb6258f6d78b3491660871197ae35
echopecho <pbsmithtbn@gmail.com>
2018-11-14 23:13:42 -0600

    Fix bug with toggle of the menu button
    I forgot to change my temporary solution in the code I committed before. The final version is in this commit.

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ let nav = document.querySelector('.nav-links');
 // Add Functionality to the Nav Menu
 menu.addEventListener('click', function() {
   nav.classList.toggle('nav-links-hidden');
-  if(menu.src == "file:///C:/Users/Philmanic%203000/Desktop/Lambda-School/Projects/User-Interface-Project-Week/img/nav-hamburger.png") {
+  if(menu.src.includes("img/nav-hamburger.png")) {
     menu.src = "img/nav-hamburger-close.png";
   } else {
     menu.src = "img/nav-hamburger.png"
