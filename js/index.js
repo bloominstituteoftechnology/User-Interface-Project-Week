cec4559a7158f129158ea859a720bba1fd7699fb
david-chua <dp_chua@hotmail.com>
2018-11-15 00:57:12 -0500

    completed home page -desktop-view

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // JS goes here
+
+const navBar = document.querySelector('.extra');
+const hamBurger = document.querySelector('.hamburger');
+const closeMenu = document.querySelector('.close-menu');
+
+
+hamBurger.addEventListener('click', function(){
+  navBar.classList.toggle("expanded");
+})
+
+closeMenu.addEventListener('click', function(){
+  navBar.classList.toggle("expanded");
+})
