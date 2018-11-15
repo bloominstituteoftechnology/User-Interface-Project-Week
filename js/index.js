935a0935a3f93a5e5521763b44f6233aff5d19c5
Phil Smith <pbsmithtbn@gmail.com>
2018-11-14 22:52:38 -0600

    Add functionality to menu button
    I completed the javascript required to make the menu function as stated in the ReadMe file.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // JS goes here
+
+let menu = document.querySelector('.menu img');
+let nav = document.querySelector('.nav-links');
+
+// Add Functionality to the Nav Menu
+menu.addEventListener('click', function() {
+  nav.classList.toggle('nav-links-hidden');
+  if(menu.src == "file:///C:/Users/Philmanic%203000/Desktop/Lambda-School/Projects/User-Interface-Project-Week/img/nav-hamburger.png") {
+    menu.src = "img/nav-hamburger-close.png";
+  } else {
+    menu.src = "img/nav-hamburger.png"
+  }
+})
