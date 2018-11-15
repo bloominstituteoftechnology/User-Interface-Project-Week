12243e650906c699b0a7e271fc38ca4460ce1eed
Jonathan Dorety <kyullog@gmail.com>
2018-11-14 23:30:39 -0500

    Very rough navigation component implemented

---
+++ Components/Navigation/navigation.js
@@ -0,0 +1,16 @@
+class Navigation {
+  constructor(element) {
+    this.element = element;
+    this.button = this.element.querySelector('#menu-button');
+    this.menu = this.element.querySelector('.links');
+
+    this.button.addEventListener('click', (event) => {
+      this.menu.classList.toggle('links-open')
+  })
+
+
+}
+}
+let navBar = document.querySelector('.site-navigation');
+navBar = new Navigation(navBar);
+console.log(navBar);
