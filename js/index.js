95ca7e83318d4dd5b1cacd932384a421cb00b304
Jamar Torres <Jamar.Torres@UNHQ.UN.ORG>
2018-11-16 17:34:56 -0500

    reconfigured toggle to add and remove classes in index.js file

--- js/index.js
+++ js/index.js
@@ -1,17 +1,28 @@
 // JS goes here
 
 const toggleMenu = () => {
-	menu.classList.toggle("menu--open");
+	menu.classList.add("menu--open");
 
+}
+
+const closeMenu = () => {
+	menu.classList.remove("menu--open");
 }
 
 const menu = document.querySelector('.menu-content');
 
 const menuButton = document.querySelector('.menu-button');
 
+const closeButton = document.querySelector('.close-button')
+
 menuButton.addEventListener('click', () => {
 	toggleMenu();
 	console.log("button was clicked")
+});
+
+closeButton.addEventListener('click', () => {
+	closeMenu();
+	console.log('menu closed')
 });
 
 
