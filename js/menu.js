4694ac5d80f6b5c14dcbda481c67796ac53c557d
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-11-15 21:24:31 -0500

    ADD MOBILE NAVBAR JS FUNCTIONALITY, ADJ FOOTER INPUT CSS

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,7 @@
 
 const toggleMenu = () => {
     // Toggle the "menu--open" class on your menu refence. 
-    console.log(menu.classList);
 
-    
     if (menu.classList.value !== 'menu menu--open') {
         menu.classList.toggle('menu--open');
         menuButton.setAttribute('src', './img/nav-hamburger-close.png');
@@ -18,15 +16,10 @@ const toggleMenu = () => {
 
 // Short notation for document
 const doc = document;
-// Start Here: Create a reference to the ".menu" class
 
 const navWrapper= doc.querySelector('.nav-wrapper');
-const body = doc.querySelector('body');
 const menu = doc.querySelector('.menu');
-// create a reference to the ".menu-button" class
 const menuButton = doc.querySelector('.menu-button');
-// Using your menuButton reference, add a click handler that calls toggleMenu
-// console.log(menuButton);
 
 menuButton.addEventListener('click', toggleMenu);
   
