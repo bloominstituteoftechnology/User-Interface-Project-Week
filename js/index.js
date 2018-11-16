4ede1e1dfaabfef505addc64d54101f0468a2751
VonKleistman <VonKleistman@gmail.com>
2018-11-15 19:01:38 -0600

    Built mobile version of the home page

--- js/index.js
+++ js/index.js
@@ -1,36 +1,32 @@
 // JS goes here
+const workArea = document.querySelector(`.content-wrapper`);
 
+// Replace images with ones appropriate for window size
+let ImageReplace = (element) => {
+  let tempString = element.getAttribute(`src`).split(`-`);
+  tempString.splice(1, 0, `mobile`);
+  tempString = tempString.join(`-`);
+  element.setAttribute(`src`, tempString);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (window.innerWidth <= 500) {
+  let temp = document.querySelector(`.jumbotron`);
+  ImageReplace(temp);
+  temp = workArea.getElementsByTagName(`img`);
+  temp = Array.from(temp);
+  for (let i in temp) {
+    ImageReplace(temp[i]);
+  }
+}
 
 // Nav hamburger recipe
 let burger = document.querySelector(`.burger`);
 let burgerButton = burger.querySelector(`#hamburger`);
 let menuToExpand = burger.querySelector(`nav`);
-burgerButton.addEventListener(`click`, function(){
+burgerButton.addEventListener(`click`, function () {
   burger.classList.toggle(`shrink-header-bar`);
   menuToExpand.classList.toggle(`hide-nav`);
-  if (burgerButton.getAttribute(`src`) == `img/nav-hamburger.png`){
+  if (burgerButton.getAttribute(`src`) == `img/nav-hamburger.png`) {
     burgerButton.setAttribute(`src`, `img/nav-hamburger-close.png`);
   } else {
     burgerButton.setAttribute(`src`, `img/nav-hamburger.png`);
