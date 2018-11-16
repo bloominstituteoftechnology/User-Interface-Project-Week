6df09cd1dcbf4e44b7911c5f8a4507ee84c82958
Jamar Torres <Jamar.Torres@UNHQ.UN.ORG>
2018-11-16 14:19:20 -0500

    created navigation toggle using an event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // JS goes here
+
+const toggleMenu = () => {
+	menu.classList.toggle("menu--open");
+
+}
+
+const menu = document.querySelector('.menu-content');
+
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', () => {
+	toggleMenu();
+	console.log("button was clicked")
+});
+
+
+
