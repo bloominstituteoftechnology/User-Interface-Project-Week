60c1fe2dac2426e9458b5fbacd32cf3f5c754c35
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-11-15 19:28:12 -0500

    ADD EXPANDING NAVBAR PANEL CSS &  HTML, ADJUST MAIN HEADER IMG TO MVP

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,15 @@
 
 const toggleMenu = () => {
     // Toggle the "menu--open" class on your menu refence. 
-
-    
-    if (menu.classList === 'menu-open') {
-        menuButton.setAttribute('src', './img/nav-hamburger-close.png');
-    }
-    else{
-        menu.classList.toggle('menu--open');
-        menuButton.setAttribute('src', './img/nav-hamburger.png');
-    }
-    console.log(menuButton.classList);
+    menu.classList.toggle('menu--open');
+    menuButton.setAttribute('src', './img/nav-hamburger-close.png');
 }
 
 // Short notation for document
 const doc = document;
 // Start Here: Create a reference to the ".menu" class
+
+
 const menu = doc.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = doc.querySelector('.menu-button');
