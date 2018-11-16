5d24a2503f4dadeddb01422d5cad2dbce76ae3a1
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-11-15 21:09:42 -0500

    ADD DESKTOP NAVBAR JS FUNCTIONALITY

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,27 @@
 
 const toggleMenu = () => {
     // Toggle the "menu--open" class on your menu refence. 
-    menu.classList.toggle('menu--open');
-    menuButton.setAttribute('src', './img/nav-hamburger-close.png');
+    console.log(menu.classList);
+
+    
+    if (menu.classList.value !== 'menu menu--open') {
+        menu.classList.toggle('menu--open');
+        menuButton.setAttribute('src', './img/nav-hamburger-close.png');
+        navWrapper.style.opacity = '0.96';
+    }
+    else if (menu.classList.value === 'menu menu--open') {
+        menu.classList.toggle('menu--open');
+        menuButton.setAttribute('src', './img/nav-hamburger.png');    
+        navWrapper.style.opacity = '1';
+    }
 }
 
 // Short notation for document
 const doc = document;
 // Start Here: Create a reference to the ".menu" class
 
-
+const navWrapper= doc.querySelector('.nav-wrapper');
+const body = doc.querySelector('body');
 const menu = doc.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = doc.querySelector('.menu-button');
