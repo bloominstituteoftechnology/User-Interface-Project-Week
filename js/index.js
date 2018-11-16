f31ea68dbdfe4a9cb162e63717fdc40f35770c82
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2018-11-15 20:17:51 -0700

    added functionality to menu button - menu appears and disappears on click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// JS goes here
+const menuButton = document.getElementById('hamb');
+const menu = document.getElementById('menu-items');
+
+
+
+class Dropdown {
+  constructor(menu, menuButton) {
+    this.menu = menu;
+    this.menuButton = menuButton;
+    this.menuButton.addEventListener('click', () => this.toggleMenu());
+  }
+  toggleMenu() {
+    this.menu.classList.toggle('menu-hidden');
+  }
+}
+
+new Dropdown(menu, menuButton);
