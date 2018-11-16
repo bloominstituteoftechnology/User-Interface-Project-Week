99fde1e0e1a0c0fb51496dba2a6ee9aee6a1e1bf
echopecho <pbsmithtbn@gmail.com>
2018-11-15 21:29:06 -0600

    Complete tab functionality on Services page
    I went through and add the code needed to make the tabs operate as specified in for the project. With that task done, I'm finished with the MVP part of the project. I will go through and clean up the Less files to make it more compact and uniform.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,45 @@
 // JS goes here
 
+class TabLink {
+  constructor(element) {
+    this.element = element;
+
+    this.data = this.element.dataset.tab;
+
+    this.item = document.querySelector(`.service-items .service-item[data-tab='${this.data}']`);
+
+    this.tabItem = new TabItem(this.item);
+
+    this.element.addEventListener('click', () => this.select());
+  }
+
+  select() {
+    const tabs = document.querySelectorAll('.service-tab');
+
+    Array.from(tabs).forEach(tab => tab.classList.remove('service-tab-selected'));
+
+    this.element.classList.add('service-tab-selected');
+
+    this.tabItem.select();
+  }
+}
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    const items = document.querySelectorAll('.service-item');
+    console.log(items);
+    Array.from(items).forEach(item => item.classList.remove('service-item-selected'));
+    this.element.classList.add('service-item-selected');
+  }
+}
+
+const tabs = document.querySelectorAll('.service-tabs .service-tab')
+                     .forEach(tab => new TabLink(tab));
+                     
 let menu = document.querySelector('.menu img');
 let nav = document.querySelector('.nav-links');
 
