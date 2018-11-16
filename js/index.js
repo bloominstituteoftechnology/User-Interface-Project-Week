eccbc688ff7508b856f1ba21c08262c78bf53aa4
Andrew Yurlov <yurlovandrew@gmail.com>
2018-11-15 18:43:08 -0800

    added nav toggle functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// JS goes here
+class Dropdown {
+  constructor(element) {
+    this.element = element;
+    this.button = this.element.querySelector('.dropdownButton');
+    this.nav = this.element.querySelector('nav');
+    this.button.addEventListener('click', this.toggle.bind(this));
+  }
+
+  toggle() {
+    const container = document.querySelector('header');
+
+    container.classList.toggle('headerExpanded');
+    this.nav.classList.toggle('navExpanded');
+    this.button.classList.toggle('closeNav');
+  }
+}
+
+let dropdowns = document.querySelectorAll('header')
+  .forEach(el => new Dropdown(el));
