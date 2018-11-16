4dd7c6b67214ea3b769b5e17505deb54d69b993b
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2018-11-16 01:08:32 -0600

    Working on Nav menu functionality.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// JS goes here
+const ham = document.querySelector('.open');
+const hamClose = document.querySelector('.close');
+const menuLinks = document.querySelector('.menu-links');
+
+ham.addEventListener("click", function(event) {
+    console.log('You opened a thing!');
+    event.stopPropagation();
+    menuLinks.classList.toggle('menu-links');
+    ham.classList.toggle('close');
+    hamClose.classList.toggle('close');
+});
+
+hamClose.addEventListener('click', function(event) {
+    console.log('You closed a thing');
+    event.stopPropagation();
+    menuLinks.classList.toggle('menu-links');
+    ham.classList.toggle('close');
+    hamClose.classList.toggle('close');
+});
