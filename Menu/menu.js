dd5e26f7f2fbc77ce3228d2e8d980e9dd1694c24
Arpita Sinha <sinha.arpita@gmail.com>
2018-11-17 11:08:13 -0800

    MVP

--- Menu/menu.js
+++ Menu/menu.js
@@ -2,17 +2,17 @@
 
 
 console.log("In Menu");
+
 const menu = document.querySelector(".menu");//Create a reference to the ".menu" class
 const menuButtonO=document.querySelector(".menu-buttonO");//create a reference to the
 // ".menu-button" class
+menuButtonO.classList.toggle("menu--open");
+//menuButtonO.style.display = "block";
 const toggleMenu = () => {
     console.log("Menu clicked...")
     menu.classList.toggle("menu--open");
     menuButtonO.classList.toggle("menu--open");
     menuButtonC.classList.toggle("menu--open");
-
-
-
 }
 menuButtonO.addEventListener("click",toggleMenu);
 //Using your menuButton reference, add a click handler that calls toggleMenu
