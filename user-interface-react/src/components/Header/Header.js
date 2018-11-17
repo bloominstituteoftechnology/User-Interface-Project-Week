40df321fd638f1b922903c496439330a1c06d13e
Andrew Yurlov <yurlovandrew@gmail.com>
2018-11-17 13:57:01 -0800

    created header component

---
+++ user-interface-react/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Aox from './../../hoc/Aox';
+
+const header = (props) => {
+  return (
+    <Aox>
+
+    </Aox>
+  );
+};
+
+export default header;
--- user-interface-react/src/components/Layout/Layout.js
+++ user-interface-react/src/components/Layout/Layout.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Aox from '../../hoc/Aox';
+import Header from './../Header/Header';
 
 const layout = (props) => (
   <Aox>
+    <Header />
     <main className="container">
       {props.children}
     </main>
