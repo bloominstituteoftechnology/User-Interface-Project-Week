b9cb1a4474232adaaf62683c63736faa93ccf2f6
jrizza88 <jetorres88@gmail.com>
2018-11-16 22:38:19 -0500

    fixed broken code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,42 @@
-// JS goes here
+// // JS goes here
 
-const toggleMenu = () => {
+// const openMenu = () => {
+// 	menu.classList.add("menu--open");
+// }
+
+// const closeMenu = () => {
+// 	menu.classList.remove("menu--open");
+// }
+
+// // const fadeAbsolutes = () => {
+// //    h1Home.classList.remove('.h1Home')
+// // }
+
+// // const addAbsolutes  = () => {
+// // 	h1Home.classList.add('')
+// // }
+
+
+// const menu = document.querySelector('.menu-content');
+
+// const menuButton = document.querySelector('.menu-button');
+// const closeButton = document.querySelector('.close-button');
+
+
+
+// menuButton.addEventListener('click', () => {
+// 	openMenu();
+// 	console.log("button was clicked")
+// });
+
+// closeButton.addEventListener('click', () => {
+// 	closeMenu();
+// 	console.log('menu closed')
+// });
+
+
+
+const openMenu = () => {
 	menu.classList.add("menu--open");
 
 }
@@ -16,7 +52,7 @@ const menuButton = document.querySelector('.menu-button');
 const closeButton = document.querySelector('.close-button')
 
 menuButton.addEventListener('click', () => {
-	toggleMenu();
+	openMenu();
 	console.log("button was clicked")
 });
 
