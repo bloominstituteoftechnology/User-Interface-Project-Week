9b342f24d0d3fda699afb37a12719336107de5d1
Leila <nb.leila10@gmail.com>
2018-11-17 13:22:42 -0600

    Add mobile view to services page and clean up
    Add mixins.less for flex
    clean up commented lines and add some comments
    Add mobile view to the services page
    Make home page a little more pixel perfect

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 
-// When the navigation button is clicked, the links are shown, taking the whole page view
+// When the navigation button is clicked, the links are shown.
 const navBtn = document.querySelector('.navBtn');
 const links  = document.querySelector('.linksArea');
 navBtn.addEventListener('click', () => {
+  //Show or hide the links
   links.classList.toggle('linksShow');
+  
+  // Change the button (either open or close)
   navBtn.classList.toggle('openBtn');
   navBtn.classList.toggle('closeBtn');
 })
