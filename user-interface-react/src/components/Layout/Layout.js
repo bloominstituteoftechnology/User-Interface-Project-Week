a07a434f58a4e4b24dbcb1523bb58b8275c29881
Andrew Yurlov <yurlovandrew@gmail.com>
2018-11-17 14:17:45 -0800

    created first section component

--- user-interface-react/src/components/Layout/Layout.js
+++ user-interface-react/src/components/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Aox from '../../hoc/Aox';
 import Banner from './../Banner/Banner';
+import Sec1 from './../Sec1/Sec1';
 import './Layout.css';
 
 const layout = (props) => (
@@ -9,6 +10,7 @@ const layout = (props) => (
       {props.children}
     </main>
     <Banner />
+    <Sec1 />
   </Aox>
 );
 
---
+++ user-interface-react/src/components/Sec1/Sec1.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Aox from './../../hoc/Aox';
+import './Sec1.css';
+
+const sec1 = (props) => (
+  <Aox>
+    <section className="sec1">
+      <div className="sec1Cont">
+        <h2>Smith & Jones Architects</h2>
+        <p>
+          Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea.
+					Eu timeam vidisse consectetuer sed. Duo etiam laboramus dissentiet in, nec no errem
+				</p>
+        <button>Learn More</button>
+      </div>
+      <div className="img"></div>
+    </section>
+  </Aox>
+);
+
+export default sec1;
