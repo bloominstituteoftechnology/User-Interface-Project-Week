cba80bd362a0fb40053e33154ca22c8fcb0638a3
Taylor Belk <belktaylor12@gmail.com>
2018-11-18 13:30:57 -0600

    pushing what I have for the menu drop down in index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // JS goes here
+
+
+//NAV TOGGLE
+class ShowMenu {
+    constructor(domElement) {
+        this.domElement = domElement;
+
+        this.toggleButton = this.domElement.querySelector('.menu-nav');
+        console.log(this.toggleButton)
+
+        this.toggleButton.addEventListener('click', this.expandNav());
+    
+    }
+
+    expandNav() {
+        this.domElement.classList.toggle('show');
+    }
+
+}
+
+const navs = document.querySelectorAll('.nav').forEach(function (navs) {
+    return new ShowMenu (navs)
+});
+
