5f6c346c54ca7ba453d4c0bb9be0a2847dac2670
Taylor Belk <belktaylor12@gmail.com>
2018-11-18 14:06:48 -0600

    so many things I want to say to this code. None of them nice. Finished toggle of overlay menu.

--- js/index.js
+++ js/index.js
@@ -3,21 +3,23 @@
 
 //NAV TOGGLE
 class ShowMenu {
-    constructor(domElement) { //.nav
-        this.domElement = domElement; //this/domElement = .nav
-        this.toggleButton = this.domElement.querySelector('.menu-nav'); //.nav.toggleButton =
+    constructor(domElement) { 
+        this.domElement = domElement; 
+        this.toggleButton = this.domElement.querySelector('.menu-nav'); 
 
-        this.toggleButton.addEventListener('click', this.expandNav());
-    
+        this.toggleButton.addEventListener('click', this.expandNav);
+
     }
 
     expandNav() {
-        this.domElement.classList.toggle('show');
+        const navs = document.querySelectorAll('.nav');
+
+        Array.from(navs).forEach(element => element.classList.toggle('show'));
     }
 
 }
 
 const navs = document.querySelectorAll('.nav').forEach(function (navs) {
-    return new ShowMenu (navs)
+    return new ShowMenu(navs)
 });
 
