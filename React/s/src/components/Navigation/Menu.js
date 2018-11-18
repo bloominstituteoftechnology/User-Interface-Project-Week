6a594b5f03cd06472c037f1ecb8cd50a182870b4
Trip1701 <bj2336@email.vccs.edu>
2018-11-18 11:13:20 -0500

    React Nav Animation Update

--- React/s/src/components/Navigation/Menu.js
+++ React/s/src/components/Navigation/Menu.js
@@ -1,18 +1,38 @@
 import React from 'react';
+let menuClass = "menu";
+let styles;
 
 const menu = props => {
+  function setZBack() {
+    const menu = document.querySelector('.menu');
+    if(menu == undefined) {
+      return
+    } else {
+      menu.style.zIndex = -1000;
+        console.log(menu);
+    }
+  }
 
-  let menuClass = "menu";
-  let styles;
+  function setZTop() {
+    const menu = document.querySelector('.menu');
+    if(menu == undefined) {
+      return
+    } else {
+      menu.style.zIndex = 1000;
+        console.log(menu);
+    }
+  }
 
   if(props.show) {
     menuClass = "menu open"
+    const zChange = setTimeout(setZTop, 0)
   } else {
     menuClass = "menu close"
+    const zChange = setTimeout(setZBack, 300)
   }
 
   return (
-    <div className={menuClass}>
+    <div className={menuClass} style={styles}>
       <div className="menu__links">
         <a href="#" onClick={props.toggle}>Home</a>
         <a href="#" onClick={props.toggle}>Services</a>
