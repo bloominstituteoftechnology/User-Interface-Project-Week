ab213c74b5ab710c33962d8bcda036f87d52a036
Jonathan Dorety <kyullog@gmail.com>
2018-11-19 16:04:40 -0500

    Finish JS for mobile layout of services.html

--- js/services.js
+++ js/services.js
@@ -2,14 +2,19 @@
 const images = document.querySelectorAll('img');
 console.log(images);
 
-// window.addEventListener('resize', function (event) {
-//   let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-//   if (viewportWidth <= 500) {
-//     images[1].setAttribute('src', 'img/home/home-mobile-img-1.png');
-//     images[2].setAttribute('src', 'img/home/home-mobile-img-2.png');
-//   }
-//   else if (viewportWidth > 500) {
-//     images[1].setAttribute('src', 'img/home/home-img-1.png');
-//     images[2].setAttribute('src', 'img/home/home-img-2.png');
-//   }
-// })
+window.addEventListener('resize', function (event) {
+  let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+  if (viewportWidth <= 500) {
+    images[1].setAttribute('src', 'img/services/services-tab-mobile-pre-construction-img.png');
+    images[2].setAttribute('src', 'img/services/services-tab-mobile-construction-img.png');
+    images[3].setAttribute('src', 'img/services/services-tab-mobile-design-build-img.png');
+    images[4].setAttribute('src', 'img/services/services-tab-mobile-sustainability-img.png');
+
+  }
+  else if (viewportWidth > 500) {
+    images[1].setAttribute('src', 'img/services/services-tab-pre-construction-img.png');
+    images[2].setAttribute('src', 'img/services/services-tab-construction-img.png');
+    images[3].setAttribute('src', 'img/services/services-tab-design-build-img.png');
+    images[4].setAttribute('src', 'img/services/services-tab-sustainability-img.png');
+  }
+})
