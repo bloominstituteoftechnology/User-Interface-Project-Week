7ffd0c8aa311519f23f1c6486e22666890fdd531
jrizza88 <jetorres88@gmail.com>
2018-11-18 20:07:44 -0500

    fixed the styling of the nav menu

--- js/index.js
+++ js/index.js
@@ -1,66 +1,27 @@
 // // JS goes here
 
-// const fadeAbsolutes = () => {
-//    h1Home.classList.remove('.h1Home')
-// }
-
-// const addAbsolutes  = () => {
-// 	h1Home.classList.add('')
-// }
-
-// const h1Home = document.querySelector(".")
-
-
-// menuButton.addEventListener('click', () => {
-// 	openMenu();
-// 	console.log("button was clicked")
-// });
-
-// closeButton.addEventListener('click', () => {
-// 	closeMenu();
-// 	console.log('menu closed')
-// });
-
-
 
 const openMenu = () => {
 	menu.classList.add("menu--open");
-	// document.getElementsByClassName("close-button").style.display = "block";
-	// displayCloser.classList.add("close-button")
-	// close.classList.add("closer--open");
-}
-
-const openCloser = () => {
-	close.classList.add("closer--open");
 }
 
 const closeMenu = () => {
 	menu.classList.remove("menu--open");
-	// close.classList.remove("closer--open");
-}
-
-const closeCloser = () => {
-	close.classList.remove("closer--open");
 }
 
 const menu = document.querySelector('.menu-content');
 
- const close = document.querySelector('.the-closure');
+//  const close = document.querySelector('.the-closure');
 
 const menuButton = document.querySelector('.menu-button');
 
- const closeButton = document.querySelector('.close-button')
+const closeButton = document.querySelector('.close-button')
 
 menuButton.addEventListener('click', () => {
 	openMenu();
 	document.querySelector(".close-button").style.display = "block";
 	document.querySelector(".menu-button").style.display = "none";
-	console.log("button was clicked")
-});
-
-menuButton.addEventListener('click', () => {
-	openCloser();
-	console.log("button was clicked")
+	console.log("menu button was clicked")
 });
 
 closeButton.addEventListener('click', () => {
@@ -70,10 +31,6 @@ closeButton.addEventListener('click', () => {
 	console.log('menu closed')
 });
 
-closeButton.addEventListener('click', () => {
-	closeCloser();
-	console.log('menu closed')
-});
 
 
 
