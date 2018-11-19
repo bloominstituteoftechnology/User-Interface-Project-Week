6cd7ddf9752b90e05dc6aa5e8cbae33e9e1d5724
Jonathan Dorety <kyullog@gmail.com>
2018-11-19 15:54:26 -0500

    Complete LESS styling for mobile layout in services.html

---
+++ js/services.js
@@ -0,0 +1,15 @@
+
+const images = document.querySelectorAll('img');
+console.log(images);
+
+// window.addEventListener('resize', function (event) {
+//   let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+//   if (viewportWidth <= 500) {
+//     images[1].setAttribute('src', 'img/home/home-mobile-img-1.png');
+//     images[2].setAttribute('src', 'img/home/home-mobile-img-2.png');
+//   }
+//   else if (viewportWidth > 500) {
+//     images[1].setAttribute('src', 'img/home/home-img-1.png');
+//     images[2].setAttribute('src', 'img/home/home-img-2.png');
+//   }
+// })
