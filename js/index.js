2d89714e0714ae47cd9d006268921e885c7dfe4c
Leila <nb.leila10@gmail.com>
2018-11-18 20:17:54 -0600

    Add code to make menu take the whole page when opened

--- js/index.js
+++ js/index.js
@@ -2,19 +2,25 @@
 // When the navigation button is clicked, the links are shown.
 const navBtn = document.querySelector('.navBtn');
 const links = document.querySelector('.linksShow');
+const nav = document.querySelector('nav');
 
 // Flag to keep track of menu open or close
 let flag = 0;
 
 navBtn.addEventListener('click', () => {
+
   //Show or hide the links
-  
   if(flag === 0) {
+    // make menu take the whole page
+    nav.style.height = '100%';
     // open menu
     TweenMax.fromTo(links, 1, {yPercent:-110, opacity:0}, {yPercent:0, opacity: 0.9});
     flag = 1;
   }
   else {
+    // Making menu small again
+    nav.style.height = '0';
+
     // close menu
     TweenMax.fromTo(links, 1, {yPercent:0, opacity:0.9}, {yPercent:-110, opacity: 0});
     flag = 0;
