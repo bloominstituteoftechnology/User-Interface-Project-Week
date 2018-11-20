0cf2d4858f8d5ff2a7753f6811d30915e9157cd7
CatsAreMLG <joshuadgon@gmail.com>
2018-11-20 17:45:31 -0600

    Added tab functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // JS goes here
+class Tabs {
+  constructor(element) {
+    this.element = element;
+    this.id = this.element.dataset.tab;
+    this.contentElement = document.querySelector(
+      `.services__option[data-content='${this.id}']`
+    );
+    console.log(this.contentElement);
+    this.elementContent = new Contents(this.contentElement);
+    this.element.addEventListener("click", _ => this.click());
+  }
+  click() {
+    const buttons = document.querySelectorAll(".services__options__btn");
+    buttons.forEach(e => e.classList.remove("active"));
+    this.element.classList.add("active");
+    this.elementContent.click();
+  }
+}
+class Contents {
+  constructor(content) {
+    this.content = content;
+  }
+  click() {
+    const contentList = document.querySelectorAll(".services__option");
+    contentList.forEach(e => e.classList.remove("shown"));
+    this.content.classList.add("shown");
+  }
+}
+const tabs = document
+  .querySelectorAll(".services__options__btn")
+  .forEach(tab => new Tabs(tab));
