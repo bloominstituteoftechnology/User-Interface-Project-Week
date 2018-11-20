d34e6fd30be3cc05de3c57f365d91059fbbebf81
Orlando Castillo <omaddoc@gmail.com>
2018-11-20 10:41:28 -0500

    mvp

--- js/index.js
+++ js/index.js
@@ -1,24 +1,33 @@
 // JS goes here
-class DropDown {
-    constructor(element){
-      this.element = element;
-    }
-  
-    speaks(){
-      console.log('Bark!');
-    }
-  }
-  
-  const lab = new Dog('Labrador');
-  const poodle = new Dog('Poodle');
+// class Dropdown {
+//   constructor(element) {
+//     // assign this.element to the dropdown element
+//     this.element = element;
+//     // assign the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
+//     this.button = this.element.querySelector('#dropdown');
 
-  const buttons = document.querySelectorAll('.button');
-  class Button {
-    constructor(element){
-      this.element = element;
-      this.element.addEventListener('click', () => { this.buttonClick() });
-    }
-    buttonClick(event){
-      console.log('button clicked!');
-    } 
-  }
+//     // assign the reference to the ".dropdown-content" class found in the dropdown element
+//     this.content = this.element.querySelector('.dropdown-content');
+//     // Add a click handler to the button reference and call the toggleContent method.
+//     this.button.addEventListener('click', () => {
+//       this.toggleContent();
+//     });
+//     // Instructor note, the reason we must wrap the toggleContent method in an anonymous function is that 'this' in toggleContent
+//     // would refer to the button, NOT the current instance of the class.
+//   }
+
+//   toggleContent() {
+//     // Toggle the ".dropdown-hidden" class off and on
+//     this.content.classList.toggle('dropdown-hidden');
+//   }
+// }
+// // Nothing to do here, just study what the code is doing and move on to the Dropdown class
+// let dropdowns = document.querySelectorAll('.dropdown');
+// dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
+
+function openNav() {
+  document.querySelector('.dropdown').style.width = '0%';
+}
+function closeNav() {
+  document.querySelector('.dropdown').style.width = '100%';
+}
