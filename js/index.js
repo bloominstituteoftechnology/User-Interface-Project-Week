8c42cf3c18c638b6c9e61dac47f22a70a984eeaa
Leila <nb.leila10@gmail.com>
2018-11-20 15:08:14 -0600

    Redesign nav bar to fix some issues
    was overlaping on the clickable button when open
    made it display none when closed

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 
 // When the navigation button is clicked, the links are shown.
 const navBtn = document.querySelector('.navBtn');
-const links = document.querySelector('.linksShow');
-const nav = document.querySelector('nav');
+const links  = document.querySelector('.linksShow');
+const nav    = document.querySelector('nav');
 
 // Flag to keep track of menu open or close
 let flag = 0;
@@ -11,21 +11,25 @@ navBtn.addEventListener('click', () => {
 
   //Show or hide the links
   if(flag === 0) {
-    // make menu take the whole page
-    nav.style.height = '100%';
     // open menu
+    console.log('open');
     TweenMax.fromTo(links, 1, {yPercent:-110, opacity:0}, {yPercent:0, opacity: 0.9});
     flag = 1;
   }
   else {
-    // Making menu small again
-    nav.style.height = '0';
+    console.log('close');
 
     // close menu
-    TweenMax.fromTo(links, 1, {yPercent:0, opacity:0.9}, {yPercent:-110, opacity: 0});
+    TweenMax.fromTo(links, 2, {yPercent:0, opacity:0.9}, {yPercent:-110, opacity: 0});
     flag = 0;
   }
-  
+ 
+  // Display/hide links
+  setTimeout(() => {
+    nav.classList.toggle('showFull');
+    links.classList.toggle('hide');
+  }, 200);
+    
   // Change the button (either open or close)
   TweenMax.fromTo(navBtn, 1, {rotationX:180}, {rotationX:-180});  
   navBtn.classList.toggle('openBtn');
