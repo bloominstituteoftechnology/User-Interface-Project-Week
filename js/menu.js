cc7a8ed9bed35c5af1fa7ab7ed87da5f3fb98e48
brettmadrid <brettmadrid9@gmail.com>
2018-11-21 14:03:03 -0800

    completed tablet media queries at 700px

--- js/menu.js
+++ js/menu.js
@@ -2,24 +2,28 @@
 const menuBtn = document.querySelector(".menu-button");
 const menu = document.querySelector(".menu");
 const menuBtnClose = document.querySelector(".menu-button-close");
+const headerH1 = document.querySelector('.topContainer .header h1')
 
 // hide the menu
 window.addEventListener('load', () => {
-  TweenMax.set('.menu', {x: -2560});
+  //TweenMax.set('.menu', {x: -2560});
+  TweenMax.set('.menu', {y: -2560});
 });
 
 const menuToggleOpen = () => {
   //menu.classList.toggle("menu--open");
   menuBtn.classList.toggle("hamburger-hide");
   menuBtnClose.classList.toggle("menu-btn-close-hide");
-  TweenMax.to('.menu', 1, {x: 1});
+  //TweenMax.to('.menu', 1, {x: 0});
+  TweenMax.to('.menu', 1, {y: 0});
 };
 
 const menuToggleClose = () => {
   //menu.classList.toggle("menu--open");
   menuBtn.classList.toggle("hamburger-hide");
   menuBtnClose.classList.toggle("menu-btn-close-hide");
-  TweenMax.fromTo('.menu', 2, {x:1}, {x: -2560});
+  //TweenMax.fromTo('.menu', 2, {x:1}, {x: -2560});
+  TweenMax.fromTo('.menu', 2, {y:1}, {y: -2560});
 };
 
 menuBtn.addEventListener("click", event => {
@@ -29,3 +33,6 @@ menuBtn.addEventListener("click", event => {
 menuBtnClose.addEventListener("click", event => {
   menuToggleClose();
 }); // end Menu code
+
+
+
