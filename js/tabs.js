68df5598fd9d4a9b946ecf6f1bdfe499701ced28
Kseniya Platonava <ksu2008@gmail.com>
2018-11-21 01:48:29 -0500

    Add Animation To The Tabs

--- js/tabs.js
+++ js/tabs.js
@@ -11,11 +11,13 @@ class TabLink {
     }
     selectTab() {
         document.querySelectorAll(".tab").forEach(x => x.classList.remove("active"));
-        document.querySelectorAll(".tab-content").forEach(x => x.classList.remove("active"));
-        this.element.classList.add("active");
-        this.tabContent.classList.add("active");
-    
         
+        this.element.classList.add("active");
+        TweenMax.to(this.tabContainer, 1, {scale:.05, opacity:0, rotation: 360, ease:Sine.easeIn, onComplete: () => {
+     document.querySelectorAll(".tab-content").forEach(x => x.classList.remove("active"));
+        this.tabContent.classList.add("active");
+        TweenMax.to(this.tabContainer, 1, {scale:1, opacity:1, rotation: 360, ease:Sine.easeOut});
+    }});
     }
 }
  let tabs = document.querySelectorAll(".tab");
