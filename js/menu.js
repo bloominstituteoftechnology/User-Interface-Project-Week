5ace521df14b19cf66e07194a369224136b99473
brettmadrid <brettmadrid9@gmail.com>
2018-11-21 00:32:37 -0800

    updated greensock margins on hidden menu

--- js/menu.js
+++ js/menu.js
@@ -5,21 +5,21 @@ const menuBtnClose = document.querySelector(".menu-button-close");
 
 // hide the menu
 window.addEventListener('load', () => {
-  TweenMax.set('.menu', {x: -1037});
+  TweenMax.set('.menu', {x: -2560});
 });
 
 const menuToggleOpen = () => {
   //menu.classList.toggle("menu--open");
   menuBtn.classList.toggle("hamburger-hide");
   menuBtnClose.classList.toggle("menu-btn-close-hide");
-  TweenMax.to('.menu', 2, {x: 1});
+  TweenMax.to('.menu', 1, {x: 1});
 };
 
 const menuToggleClose = () => {
   //menu.classList.toggle("menu--open");
   menuBtn.classList.toggle("hamburger-hide");
   menuBtnClose.classList.toggle("menu-btn-close-hide");
-  TweenMax.fromTo('.menu', 2, {x:1}, {x: -1037});
+  TweenMax.fromTo('.menu', 2, {x:1}, {x: -2560});
 };
 
 menuBtn.addEventListener("click", event => {
