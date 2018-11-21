de509c1ed76db24ea5072c5363fd9def16566985
seanwu20 <seanwu20@gmail.com>
2018-11-21 11:20:22 -0500

    Added comments for MVP

--- js/components.js
+++ js/components.js
@@ -1,21 +1,30 @@
 class Button {
     constructor(buttonElement) {
+        //Sets up a button object
         this.buttonElement = buttonElement;
+        //Grabs the data-tab associated with current button element
         this.data = this.buttonElement.dataset.tab;
+        //Grabs the tab from the document associated with the data-tab
         this.tab = document.querySelector(`.services-tab-components[data-tab ='${this.data}']`);
+        //creates a new tab element
         this.tab = new Tab(this.tab);
+        //Adds a click listener to button. Bind binds this to the current button
         this.buttonElement.addEventListener('click', this.buttonSelect.bind(this))
     }
 
     buttonSelect() {
+        //Selects all buttons from document
         const buttons = document.querySelectorAll('.services-btn');
+        //For each button, removes the btn-selected class
         buttons.forEach(button => {
             button.classList.remove('btn-selected');
         });
+        //Adds the btn-selected to the current button only (The button that was clicked)
         this.buttonElement.classList.add('btn-selected');
         console.log(this.buttonElement);
+        //this.tab is a Tab Object. This grabs Tab's method of tabSelect.
+        //Shows the tab associated with the button's data-tab
         this.tab.tabSelect()
-
     }
 }
 
@@ -23,23 +32,27 @@ class Button {
 class Tab {
     constructor(tabElement) {
         this.tabElement = tabElement;
-
     }
 
     tabSelect() {
+        //Selects all tabs
         const tabs = document.querySelectorAll('.services-tab-components');
+        //removes show-tab from all tabs
         tabs.forEach(tab => {
             tab.classList.remove('show-tab');
         });
+        //Adds show-tab to current tab
         this.tabElement.classList.add('show-tab');
         console.log(this.tabElement);
     }
 }
 
-
+//Selects all the buttons
 let buttons = document.querySelectorAll('.services-btn');
+//creates a new button object for all buttons
 buttons.forEach(button => new Button(button));
 
+//Shows the first tab
 showFirstTab = new Button(buttons[0]);
 showFirstTab.buttonSelect();
 
--- js/nav.js
+++ js/nav.js
@@ -1,28 +1,22 @@
 // JS goes here
 class MenuLogic {
     constructor(domElements) {
+        //Sets up navbar object
         this.domElements = domElements;
+        //Goes into the navbar to find hamburger picture
         this.menuButton = this.domElements.querySelector(".hamburger");
+        //Goes into the navbar to find the hidden menu
         this.menu = this.domElements.querySelector('.navbar-menu');
-        this.toggleVariable = false;
+        //Checks if menu is open (true) or closed(false)
+        this.menuActiveVariable = false;
+        //Adds event listener to hamburger menu
         this.menuButton.addEventListener('click', this.toggleMenu.bind(this))
     }
 
     toggleMenu() {
-        if (this.toggleVariable === false) {
-            this.domElements.style.opacity = '0.95';
-            this.domElements.style.zIndex = '1';
-
-
-            this.domElements.style.position = 'fixed';
-            this.domElements.style.left = '0';
-            this.domElements.style.right = '0';
-            this.domElements.style.height = '100vh';
-
-            this.menu.style.display = "block";
-            this.menuButton.src = "img/nav-hamburger-close.png"
-        }
-        else if (this.toggleVariable) {
+        //Menu closed
+        if (this.menuActiveVariable === false) {
+            //resets to default style of navbar
             this.domElements.style.height = 'auto';
             this.menuButton.src = "img/nav-hamburger.png";
             this.domElements.style.position = 'sticky';
@@ -32,11 +26,30 @@ class MenuLogic {
             this.menu.style.display = 'none';
             this.domElements.style.opacity = '1';
         }
-        this.toggleVariable = !this.toggleVariable;
+        //menu open
+        else if (this.menuActiveVariable) {
+            //see through effect
+            this.domElements.style.opacity = '0.95';
+            //overlays the menu on top of the page 
+            this.domElements.style.zIndex = '1';
+
+            //Menu stays on screen even if user scrolls
+            this.domElements.style.position = 'fixed';
+            this.domElements.style.left = '0';
+            this.domElements.style.right = '0';
+            //take up all of the viewport
+            this.domElements.style.height = '100vh';
+            //Changes the display:none to display:block
+            this.menu.style.display = "block";
+            //Changes the hamburger menu to an X
+            this.menuButton.src = "img/nav-hamburger-close.png"
+        }
+        this.menuActiveVariable = !this.menuActiveVariable;
     }
 
 }
-
+//Finds the navbar
 const nav = document.querySelector('.navbar');
+//passes the navbar into MenuLogic
 new MenuLogic(nav);
 
