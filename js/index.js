85ecf194dceb30af358ce6d7d7618d27dd45e2e5
Kseniya Platonava <ksu2008@gmail.com>
2018-11-21 01:01:59 -0500

    Do MVP
    Add Tabs Functionality, and Tabs.js
    Add Menu Functionality, and Menu.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 //navigation
 const hamburger = document.querySelector('.menu-button');
-const nav = document.querySelector('.nav');
-const header = document.querySelector('header');
+const nav = document.querySelector('.navigation a');
+const header = document.querySelector('.navigation');
 let open = hamburger.addEventListener('click', () => {
     nav.classList.toggle('nav-show');
 
    header.classList.toggle('nav-show');
     if (header.classList.contains('nav-show')) {
     hamburger.src='img/nav-hamburger-close.png';
-    hamburger.alt='Close Menu';
+    hamburger.alt='closed';
     } else {
     hamburger.src='img/nav-hamburger.png';
-    hamburger.alt='Open Menu';
+    hamburger.alt='open';
     }	    
  });
--- js/tabs.js
+++ js/tabs.js
@@ -1,1 +1,23 @@
-// JS goes here
+
+ //tabs
+
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.tabContainer = document.querySelector('.tabs-content');
+        this.element.addEventListener('click', () => this.selectTab());
+        this.tabContent = document.querySelector(`.tab-content[data-tab="${this.data}"]`);
+    }
+    selectTab() {
+        document.querySelectorAll(".tab").forEach(x => x.classList.remove("active"));
+        document.querySelectorAll(".tab-content").forEach(x => x.classList.remove("active"));
+        this.element.classList.add("active");
+        this.tabContent.classList.add("active");
+    
+        
+    }
+}
+ let tabs = document.querySelectorAll(".tab");
+tabs = Array.from(tabs).map(tab => new TabLink(tab));
+tabs[0].selectTab(); 
