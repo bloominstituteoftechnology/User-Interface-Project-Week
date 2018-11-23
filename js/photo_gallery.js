da7f9c62c236016baec680c6e36c16263c3dea1f
seanwu20 <seanwu20@gmail.com>
2018-11-22 21:02:18 -0500

    Photo gallery functionality complete

--- js/photo_gallery.js
+++ js/photo_gallery.js
@@ -4,7 +4,7 @@ class OpenPreviewImages {
         this.galleryContainer = galleryContainer;
         //View design button
         this.designButton = this.galleryContainer.querySelector('.design-button');
-        
+
         //Grabs all images, preview and single images
         this.imagesContainer = this.galleryContainer.querySelector('.image-div-container');
         //Grabs the preview image div
@@ -15,7 +15,7 @@ class OpenPreviewImages {
         this.designButton.addEventListener('click', this.displayImages.bind(this));
         this.closeButton.addEventListener('click', this.closeImages.bind(this));
         //Passes all images to EnlargeSelectedImage 
-        this.EnlargeSelectedImageObject = new EnlargeSelectedImage(this.imagesContainer)
+        this.EnlargeSelectedImageObject = new EnlargeSelectedImage(this.galleryContainer)
     }
 
     displayImages() {
@@ -34,106 +34,84 @@ class OpenPreviewImages {
 }
 
 class EnlargeSelectedImage {
-    constructor(imagesContainer) {
-        this.imagesContainer = imagesContainer;
-        
-        this.previewImagesDiv = this.imagesContainer.querySelector('.image-preview-div');
-        this.allPreviewImages = this.imagesContainer.querySelectorAll('.preview-img');
-        this.allPreviewImages.forEach(image=> image.addEventListener('click', this.enlargeImage.bind(this)))
-        
-        
-        this.selectedImage = document.querySelector(`.carousel-img[data-image ='${this.data}']`);
+    constructor(galleryContainer) {
+        this.galleryContainer = galleryContainer;
 
-        this.carouselImagesDiv = this.imagesContainer.querySelector('.carousel-div');
-        
-        this.backButton = this.imagesContainer.querySelector('.back-button');
+        this.previewImagesDiv = this.galleryContainer.querySelector('.image-preview-div');
+        this.allPreviewImages = this.galleryContainer.querySelectorAll('.preview-img');
+        this.allPreviewImages.forEach(image => image.addEventListener('click', this.enlargeImage.bind(this)))
+
+
+        this.carouselImagesDiv = this.galleryContainer.querySelector('.carousel-div');
+
+        this.closeButton = this.galleryContainer.querySelector('.close-button');
+
+        this.backButton = this.galleryContainer.querySelector('.back-button');
         this.backButton.addEventListener('click', this.backToPreview.bind(this));
+        
+        this.previousButton = this.galleryContainer.querySelector('.previous-button');
+        this.nextButton = this.galleryContainer.querySelector('.next-button');
+        
+        this.previousButton.addEventListener('click', this.cycleLeft.bind(this));
+        this.nextButton.addEventListener('click', this.cycleRight.bind(this));
     }
+
     enlargeImage() {
         this.previewImagesDiv.classList.remove('show-preview-div');
         this.carouselImagesDiv.classList.add('show-carousel-div');
         this.data = event.target.dataset.image;
-        this.selectedCarouselImage = this.imagesContainer.querySelector(`.carousel-img[data-image ='${this.data}']`);
-        
+        this.selectedCarouselImage = this.galleryContainer.querySelector(`.carousel-img[data-image ='${this.data}']`);
         this.selectedCarouselImage.style.display = 'block';
         this.selectedCarouselImage.style.margin = '0 auto';
+
+        this.closeButton.style.display = 'none';
     }
-    
+
     backToPreview() {
         this.selectedCarouselImage.style.display = 'none';
         this.carouselImagesDiv.classList.remove('show-carousel-div');
         this.previewImagesDiv.classList.add('show-preview-div');
-        
+        this.closeButton.style.display = 'block';
     }
     
+    cycleLeft() {
+        this.selectedCarouselImage.style.display = 'none';
+        if (this.data === "0"){
+            this.data = this.allPreviewImages.length;
+        }
+        this.dataDecrementer = parseInt(this.data) - 1;
+        this.data = this.dataDecrementer.toString();
+        console.log(this.data);
+        this.selectedCarouselImage = this.galleryContainer.querySelector(`.carousel-img[data-image ='${this.data}']`);
+        console.log(this.selectedCarouselImage);
+        this.selectedCarouselImage.style.display = 'block';
+        this.selectedCarouselImage.style.margin = '0 auto';
+
+
+    }
+    
+    cycleRight() {
+        this.selectedCarouselImage.style.display = 'none';
+        if(this.data === "5") {
+            this.data = "-1";
+        }
+        console.log(this.data);
+
+        this.dataIncrementer = parseInt(this.data) + 1;
+        this.data = this.dataIncrementer.toString();
+        this.selectedCarouselImage = this.galleryContainer.querySelector(`.carousel-img[data-image ='${this.data}']`);
+        console.log(this.selectedCarouselImage);
+        this.selectedCarouselImage.style.display = 'block';
+        this.selectedCarouselImage.style.margin = '0 auto';
+
+    }
 }
+
 
 
 galleryContainer = document.querySelector('.gallery-container');
 galleryContainer = new OpenPreviewImages(galleryContainer);
 
-/*
-
-class Carousel {
-    constructor(carouselDiv, data) {
-        //passes in the carousel node element
-        this.carouselDiv = carouselDiv;
-        this.data = data;
-        
-
-        this.previousButton = this.carouselDiv.querySelector('.previous-button');
-        this.nextButton = this.carouselDiv.querySelector('.next-button');
-
-        this.images = this.carouselDiv.querySelectorAll('.carousel-img');
-        this.imageLength = this.images.length;
-
-
-        this.index = 2;
-        this.previousButton.addEventListener('click', this.cycleLeft.bind(this));
-        this.nextButton.addEventListener('click', this.cycleRight.bind(this))
-    }
-
-    cycleLeft() {
-        //checks if index is greater than 0. If index is zero, the else statement will reset index to the last image
-        if (this.index > 0) {
-            console.log(this.index);
-            //Hides the current image
-            this.images[this.index].style.display = "none";
-            //subtracts index to get the previous image
-            this.index--;
-            console.log(this.index);
-            //displays previous image
-            //displays the previous image
-            this.images[this.index].style.display = "block";
-        }
-    }
-
-    cycleRight() {
-        //checks if index is less than the largest possible index. If index is, the else statement will reset index to 0
-
-        if (this.index < this.imageLength - 1) {
-            //hides the current display
-            this.images[this.index].style.display = "none";
-            //Adds 1 to index
-            this.index++;
-            //displays the next image
-            this.images[this.index].style.display = "block";
-        }
-        else if(this.index === this.imageLength-1) {
-            this.index--
-        }
-
-    }
-}
-
-
-
-let previewImagesDiv = document.querySelector('.image-preview-div');
-let previewImages = document.querySelectorAll('.preview-img');
-let carouselImageDiv = document.querySelector('.carousel-div-container');
-let backButton = document.querySelector('.back-button');
-previewImages.forEach(currentImage => new EnlargeImageSelected(previewImagesDiv, currentImage, carouselImageDiv, backButton));
-*/
 
 
 
