adae7ed357baf4768b8d1b34cb3024e514be425a
rberriz <rberriz@icloud.com>
2018-11-24 14:36:35 -0500

    another push for lauren

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // JS goes here
+
+const button = document.getElementById("menuOpen").addEventListener("click", unhide());
+
+
+function unhide(id) {
+    const selector = document.getElementsById('navbox')
+    if (selector.style.display === 'none') {
+        selector.style.display = 'block';
+    } else {
+        selector.style.display ='none';
+    }
+}
