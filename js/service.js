e93f02b9752946cff325634e59472edcc8853c9f
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2018-11-24 15:53:00 -0600

    finished services layout and CSS

---
+++ js/service.js
@@ -0,0 +1,11 @@
+const navButtons = document.querySelector('.menu-links');
+const hamburger = document.querySelector('#hamburger');
+const hamburgerOpen = document.querySelector('#hamburger-open');
+const hamburgerClose = document.querySelector('#hamburger-close');
+
+ hamburger.addEventListener('click', function() {
+  navButtons.classList.toggle('hidden');
+  hamburgerOpen.classList.toggle('hidden');
+  hamburgerClose.classList.toggle('hidden');
+  
+})
