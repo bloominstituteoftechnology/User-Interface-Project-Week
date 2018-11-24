21b94c451bc1ef4c18968ea487341fc46ce2655c
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2018-11-24 17:17:01 -0600

    Finished Tab fuctionality.

--- js/services.js
+++ js/services.js
@@ -1,3 +1,5 @@
+
+/////Menu //////////////
 const navButtons = document.querySelector('.menu-links');
 const hamburger = document.querySelector('#hamburger');
 const hamburgerOpen = document.querySelector('#hamburger-open');
@@ -7,5 +9,22 @@ const hamburgerClose = document.querySelector('#hamburger-close');
   navButtons.classList.toggle('hidden');
   hamburgerOpen.classList.toggle('hidden');
   hamburgerClose.classList.toggle('hidden');
-  
 })
+
+/////Tab component//////////////
+const buttons = document.querySelectorAll('.tab-btn')
+const contents = document.querySelectorAll('.tab')
+ function buttonClick(event) {
+  const btnTab = event.target.dataset.tab;
+   for(let i = 0; i < contents.length; i++) {
+    const dataContent = contents[i].dataset.tab;
+     if(btnTab === dataContent) {
+      contents[i].classList.remove('hidden');
+    } else {
+      contents[i].classList.add('hidden');
+    }
+  }
+}
+ for(let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', buttonClick);
+}
