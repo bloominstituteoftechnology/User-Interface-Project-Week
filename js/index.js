5969784b49472a259be9bf9dfe949e1afe557787
Eiosx <eiosxenzo@gmail.com>
2018-11-26 15:42:44 -0500

    Almost done with draft html/less/javascript for home and services pages. Going to fix details to make more accurate to design doc, then will start tab functionality.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // JS goes here
+const navButton = document.querySelector(".navContainer img");
+const navContainer = document.querySelector(".navContainer")
+const links = document.querySelector(".links");
+navButton.addEventListener("click", ()=>{
+    navContainer.classList.toggle("navExpand");
+    links.classList.toggle("linksAppear");
+
+})
