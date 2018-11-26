0c08673e71e24e44a01935cb5b122542cba7e889
James Wigdor <jpwigdor@gmail.com>
2018-11-26 10:20:59 -0500

    Added CSS and JS to Services Page
    getting a weird glitch where the first button shows as a column but the rest show flexed as they should

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // JS goes here
+class TabButton {
+	constructor(element){
+		this.element = element;
+    this.data = this.element.dataset.tab;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+		this.entry = new TabDisplay(this.itemElement);
+		this.element.addEventListener('click', () => this.displayChange());
+  }
+  
+	displayChange(){
+    const links = document.querySelectorAll('.tabs-link');
+    links.forEach(x => x.classList.remove('tabs-link-selected'));
+    this.element.classList.add('tabs-link-selected');
+    this.entry.selectTab();
+	}
+}
+
+class TabDisplay {
+	constructor(element){
+		this.element = element;
+	}
+	selectTab(){
+    const items = document.querySelectorAll('.tabs-item');
+    items.forEach(x => x.classList.remove('tabs-item-selected'));
+    this.element.classList.add('tabs-item-selected');
+	}
+}
+
+let links = document.querySelectorAll('.tabs-link').forEach(x => new TabButton(x));
