c5418d9d38b7aa6c82f3f3eef9474ba4a42aa5a8
Melvin Merlos <melvinamerlos@gmail.com>
2018-11-26 01:11:24 -0500

    update 7: functionality basic level"

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // JS goes here
+
+const toggleNav = () => {
+    i++;
+
+    if ((i%1) === 0 ) {
+        navBar.style.height = '575px';
+        integrity.style.display = 'none';
+        navLinks.style.display = 'inline-block';
+    } else if ((i%2) === 0) {
+        navBar.style.height = '100%';
+        integrity.style.display = 'inline-block';
+        navLinks.style.display = 'none';
+    }
+
+    console.log(i);
+  }
+  
+  // identify the button
+  const menu = document.querySelector('.nav-button');
+
+  // identify item to turn "off"/change
+  const navBar = document.querySelector('.nav-section');
+  const navLinks = document.querySelector('.nav-expanded-links');
+  const integrity = document.querySelector('.integrity');
+  let i = 1;
+
+
+  menu.addEventListener('click', () => { toggleNav()})
+  
+
