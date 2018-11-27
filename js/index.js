23f0eba9e84b87cc524b8fc9c4c4d9a28510dbd9
Eiosx <eiosxenzo@gmail.com>
2018-11-26 22:37:07 -0500

    Completed tab component.

--- js/index.js
+++ js/index.js
@@ -6,5 +6,51 @@ navButton.addEventListener("click", ()=>{
     navContainer.classList.toggle("navExpand");
     links.classList.toggle("linksAppear");
     navButton.classList.toggle("hamburgerClose");
-
 })
+
+
+
+class Tab{
+    constructor(tabElement){
+        this.tabElement = tabElement;
+        this.tabData = this.tabElement.dataset.tab;
+        this.tabContent = document.querySelector(`.tabContent[data-tab='${this.tabData}']`);
+        
+        this.tabContent = new tabContent(this.tabContent);
+        this.tabElement.addEventListener("click", ()=> {
+            this.selectTab();
+        })
+   
+    }
+
+    selectTab(){
+        const tabs = document.querySelectorAll(".servicesButtons");
+        const tabsContent = document.querySelectorAll(".tabContent");
+
+        tabs.forEach(tab => {
+            tab.classList.remove("activeButton");
+        })
+        
+        tabsContent.forEach(content => {
+            content.style.display = "none";
+        })
+
+        this.tabElement.classList.add("activeButton");
+
+        this.tabContent.selectContent();
+    }
+
+
+}
+
+class tabContent{
+    constructor(contentElement){
+        this.contentElement = contentElement;
+    }
+
+    selectContent(){
+        this.contentElement.style.display = "flex";
+    }
+}
+
+let tabs = document.querySelectorAll('.servicesButtons').forEach(button => new Tab(button));
