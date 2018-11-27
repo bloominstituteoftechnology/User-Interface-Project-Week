0c2c66f1f572420105a4e037592c6ccddeb41ad1
mustafa inaya <moose.inaya@icloud.com>
2018-11-26 16:58:16 -0800

    FINISH,Finished Contact Page For Mobile And Desktop Design.

--- js/navigation.js
+++ js/navigation.js
@@ -25,3 +25,35 @@ class Dropdown {
 let dropdowns = document.querySelectorAll('.menu-content').forEach((mustafa) => new Dropdown(mustafa));
 
 // Tackle
+
+class Close {
+	constructor(term) {
+		this.term = term;
+	}
+	selectClose() {
+		this.content.classList.toggle('button-hidden');
+		document.querySelector('.menu-x').style.display = 'none';
+		document.querySelector('.menu-bar').style.display = 'block';
+	}
+}
+// const content = document.querySelector('.button-content');
+// const dropdown = document.querySelector('.menu-bar');
+// const closeDropdown = document.querySelector('.menu-x');
+// const open = () => {
+// 	content.classList.add('open');
+// };
+// const close = () => {
+// 	content.classList.remove('open');
+// };
+
+// dropdown.addEventListener('click', () => {
+// 	open();
+// 	document.querySelector('.menu-x').style.display = 'block';
+// 	document.querySelector('.menu-bar').style.display = 'none';
+// });
+
+// closeDropdown.addEventListener('click', () => {
+// 	close();
+// 	document.querySelector('.menu-x').style.display = 'none';
+// 	document.querySelector('.menu-bar').style.display = 'block';
+// });
