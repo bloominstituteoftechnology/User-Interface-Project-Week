e6fb2ae82653fb1921e0016ae3a26aabb44369df
rberriz <rberriz@icloud.com>
2018-11-26 20:38:02 -0500

    submission = (

--- js/index.js
+++ js/index.js
@@ -1,13 +1,39 @@
 // JS goes here
 
-const button = document.getElementById("menuOpen").addEventListener("click", unhide());
+// const toggleMenu = () => {
+//     menu.classList.toggle('shown')
+// }
 
 
-function unhide(id) {
-    const selector = document.getElementsById('navbox')
-    if (selector.style.display === 'none') {
-        selector.style.display = 'block';
-    } else {
-        selector.style.display ='none';
-    }
-}
+// const menu = ducument.querySelector('.navbox');
+// const menuButton = document.querySelector('.menuOpen')
+
+// menuButton.addEventListener('click', toggleMenu)
+
+const navButtons = document.querySelector('.navbox');
+const threeLines = document.querySelector('#threeLines');
+const opener = document.querySelector('#menuOpen');
+const closer = document.querySelector('#menuClose');
+
+threeLines.addEventListener('click', function() {
+    navButtons.classList.toggle('hidden');
+    opener.classList.toggle('hidden');
+    closer.classList.toggle('hidden');
+})
+
+
+
+
+
+
+
+
+
+// function unhide(id) {
+//     const selector = document.getElementsById('navbox')
+//     if (selector.style.display === 'none') {
+//         selector.style.display = 'block';
+//     } else {
+//         selector.style.display ='none';
+//     }
+// }
