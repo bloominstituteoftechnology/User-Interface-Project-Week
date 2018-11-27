a0e27fd862e4e340bd12ce5addd9b7efda7aedf5
Eiosx <eiosxenzo@gmail.com>
2018-11-26 19:16:33 -0500

    Worked on making html/less for home and services as accurate to design doc. Need to finish tab component and make links appear on menu expand.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 // JS goes here
-const navButton = document.querySelector(".navContainer img");
+const navButton = document.querySelector(".navContainer .hamburger");
 const navContainer = document.querySelector(".navContainer")
 const links = document.querySelector(".links");
 navButton.addEventListener("click", ()=>{
     navContainer.classList.toggle("navExpand");
     links.classList.toggle("linksAppear");
+    navButton.classList.toggle("hamburgerClose");
 
 })
