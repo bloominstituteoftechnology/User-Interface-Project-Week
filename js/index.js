b069e3513f17b602b1dc00a057c5c5a378805614
Melvin Merlos <melvinamerlos@gmail.com>
2018-11-27 20:39:17 -0500

    update 9: javascript functionality 95% complete

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // JS goes here
 
+// Navigation Button
 const toggleNav = () => {
     i++;
 
@@ -12,22 +13,53 @@ const toggleNav = () => {
         integrity.style.display = 'inline-block';
         navLinks.style.display = 'none';
     }
-
     console.log(i);
   }
   
-
   // identify the button
   const menu = document.querySelector('.nav-button');
-  
+
   // identify item to turn "off"/change
   const navBar = document.querySelector('.nav-section');
   const navLinks = document.querySelector('.nav-expanded-links');
   const integrity = document.querySelector('.integrity');
 
-  
   let i = 1;
 
   menu.addEventListener('click', () => { toggleNav()})
   
+  
+  // Tab filtering  
 
+  class TabLink {
+    constructor(itemOption){
+      this.itemOption = itemOption;
+      
+      this.tabData = this.itemOption.dataset.tab; 
+      
+      this.items = document.querySelectorAll(`.item[data-tab='${this.tabData}']`);
+
+      this.itemOption.addEventListener('click', () => { this.selectTab() });
+    }
+  
+    selectTab(){
+  
+      const itemContents = document.querySelectorAll('.item');
+      
+      itemContents.forEach(function(itemContents) {
+        itemContents.classList.remove('active') 
+      });
+      
+      const itemsSelected = document.querySelectorAll(`.item[data-tab='${this.tabData}']`);
+
+      itemsSelected.forEach(function(itemsSelected) {
+        itemsSelected.classList.add('active') 
+      });
+    }
+  }
+  
+
+  let items = document.querySelectorAll('.item-option').forEach(function(item) {
+    return new TabLink(item)
+  });
+  
