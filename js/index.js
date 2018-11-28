f5f80b2b90fa11717644ad841631a4ee347eaac4
Andrew <acarmstrong98@gmail.com>
2018-11-27 22:14:03 -0500

    finished setting up html and started on styling

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // JS goes here
+const toggleMenu = () => {
+    
+      menu.classList.toggle('menu--open');
+    };
+    
+    let menu = document.querySelector('.menu');
+    
+    let menuButton = document.querySelector('.menu-button');
+    
+    menuButton.addEventListener('click', toggleMenu, false);
