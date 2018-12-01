95f9063e24de76a9a1b01ebec7ec2f28bd73b55d
ltann <leeroyleeroy18@gmail.com>
2018-12-01 10:54:11 -0800

    Finished MVP on navigation system design

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
 const toggleMenu = () => {
-    overlay.classList.toggle('overlay--open')
+    overlay.classList.toggle('overlay--open');
+}
+
+const hideMenu = () => {
+    hiddenNav.classList.toggle('nav--close');
 }
 
 const overlay = document.querySelector('.overlay');
 const hamburgerButton = document.querySelector('.hamburger-button');
+const closedHamburgerButton = document.querySelector('.hamburger-close-button');
+const hiddenNav = document.querySelector('.hidden-nav');
+
+closedHamburgerButton.addEventListener('click', toggleMenu);
+closedHamburgerButton.addEventListener('click', hideMenu);
 
 hamburgerButton.addEventListener('click', toggleMenu);
+hamburgerButton.addEventListener('click', hideMenu);
