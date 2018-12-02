b1279c97b274139795ecf78ca7b2a9adda1d865e
ltann <leeroyleeroy18@gmail.com>
2018-12-01 22:12:46 -0800

    Finished MVP for service page tab navigator design

--- js/index.js
+++ js/index.js
@@ -16,3 +16,34 @@ closedHamburgerButton.addEventListener('click', hideMenu);
 
 hamburgerButton.addEventListener('click', toggleMenu);
 hamburgerButton.addEventListener('click', hideMenu);
+
+// Tabs
+class Tablink {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+        this.tabItem = new TabItem(this.itemElement);
+        this.element.addEventListener('click', () => this.select());
+    }
+    select() {
+        const links = document.querySelectorAll('.tabs-link');
+        Array.from(links).forEach(element => element.classList.remove('tabs-link-selected'));
+        this.element.classList.add('tabs-link-selected');
+        this.tabItem.select();
+    }
+}
+
+class TabItem {
+    constructor(element) {
+        this.element = element;
+    }
+    select() {
+        const items = document.querySelectorAll('.tabs-item');
+        Array.from(items).forEach(element => element.classList.remove('tabs-item-selected'));
+        this.element.classList.add('tabs-item-selected');
+    }
+}
+
+
+const links = document.querySelectorAll('.tabs-link').forEach(link => new Tablink(link));
