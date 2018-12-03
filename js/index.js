eb900cf81b531ff0bd803982f86707336d1a0465
Pwolfe39 <peterwolfe35@gmail.com>
2018-12-03 05:39:33 -0800

    Added the navigation system for desktop but not for mobile

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // JS goes here
+//toggles the hamB icon
+const toggleMenu = () => {
+   menu.classList.toggle('menu--open');   
+   let closeBtn = document.querySelector('#menu__close');
+   closeBtn.addEventListener('click', () => {
+        menu.classList.toggle('menu--open');
+    }, false);
+};
+
+
+
+const menu = document.querySelector('.menu');
+const hamBimg = document.querySelector('.hamB-img');
+hamBimg.addEventListener('click', toggleMenu, false);
+
