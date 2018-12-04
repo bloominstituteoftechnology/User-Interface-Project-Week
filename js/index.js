8254985461cbff207313d9b3402fc5d5cf05f5cc
Pwolfe39 <peterwolfe35@gmail.com>
2018-12-04 08:15:35 -0800

    Fixed the hamburger open and close function

--- js/index.js
+++ js/index.js
@@ -2,16 +2,17 @@
 //toggles the hamB icon
 const toggleMenu = () => {
    menu.classList.toggle('menu--open');   
-   let closeBtn = document.querySelector('#menu__close');
-   closeBtn.addEventListener('click', () => {
-        menu.classList.toggle('menu--open');
- }, false);
+   
 };
 
 const menu = document.querySelector('.menu');
 const hamBimg = document.querySelector('.hamB-img');
+const closeBtn = document.querySelector('#menu__close');
 hamBimg.addEventListener('click', toggleMenu, false);
 
+closeBtn.addEventListener('click', () => {
+  toggleMenu();
+}, false);
 
 
 //Tabs
