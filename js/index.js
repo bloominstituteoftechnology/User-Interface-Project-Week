3e9b8e98d69c65f55e255b6d2df684ef85146bdb
amadison17 <angelmadison2015@gmail.com>
2018-12-04 00:04:28 -0500

    recent javascript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // JS goes here
+// Tabs
+function servicesHome(evt, servicesTab) {
+    
+    var i, tabcontent, tablinks;
+
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" lightgray", "");
+    }
+
+    
+    document.getElementById(servicesTab).style.display = "flex";
+    evt.currentTarget.className += " lightgray";
+}
+
+// Navigation
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
