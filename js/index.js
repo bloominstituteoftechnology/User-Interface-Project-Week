e2e72cad02e48306af46f97aee4614b5b3e76675
derekjones-io <42226382+derekjones-io@users.noreply.github.com>
2018-12-03 20:23:43 -0500

    Complete navigation overlay
    Finished the JavaScript to toggle the menu open and closed
    when clicking on the hamburger icon on the pages. Used a class
    in order to do it so I could practice with it.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // JS goes here
+
+// nav constructor and nav toggle function
+class Menu {
+  constructor(menuName, menuButtonName) {
+    this.menu = document.querySelector(menuName);
+    this.menuButton = document.querySelector(menuButtonName);
+    this.menuButton.addEventListener("click", () => { this.toggleMenu() });
+    this.isOpen = false;
+  }
+
+  toggleMenu() {
+    this.menu.classList.toggle("menu--open");
+    this.menu.parentElement.classList.toggle("nav-opactiy");
+    if (this.isOpen) {
+      this.menuButton.src = "img/nav-hamburger.png";
+      this.isOpen = false;
+    } else {
+      this.menuButton.src = "img/nav-hamburger-close.png";
+      this.isOpen = true;
+    }
+
+  }
+}
+
+// creates the navigation menu and the tab navigation
+const menu = new Menu(".menu", ".menu-button");
