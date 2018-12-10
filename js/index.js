c42a96a96fc5bee1ad3b3b8a2fce3c733e18780c
KROHNICK <business.kanekperez@gmail.com>
2018-12-10 12:02:20 -0800

    Added header dropdown - index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// JS goes here
+class Dropdown {
+    constructor(el) {
+        this.element = el;
+        this.button = el.querySelector(".dropdown-button");
+        this.content = el.querySelector(".dropdown-content");
+        this.el.addEventListener("click", () => this.toggleContent());
+    }
+    toggleContent() {
+        this.content.classList.toggle("dropdown-hidden");
+    }
+}
+let dropdowns = document.querySelectorAll(".dropdown").forEach(dropdown => new Dropdown(dropdown));
