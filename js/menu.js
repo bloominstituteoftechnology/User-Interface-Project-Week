afb19f95121962b87b571d9e7a71f36b65aa2848
linkel <ironcarbide@gmail.com>
2018-12-10 15:14:38 -0800

    menu buttons, open and close functionality

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,18 @@
-const toggleMenu = () => {
+const openMenu = () => {
     // Toggle the "menu--open" class on your menu refence. 
-    menu.classList.toggle("menu--open");
+    menu.style.display = "block"
   }
   
-  const offMenu = () => {
-    if (menu.classList.contains("menu--open")) {
-      menu.classList.remove("menu--open");
-    }
+const closeMenu = () => {
+  if (menu.style.display == "block") {
+    menu.style.display = "none";
   }
+}
   
-  // Start Here: Create a reference to the ".menu" class
   const menu = document.querySelector(".menu");
-  // create a reference to the ".menu-button" class
   const menuButton = document.querySelector(".menu-button");
-  // Using your menuButton reference, add a click handler that calls toggleMenu
-  menuButton.addEventListener("click", toggleMenu);
-  
-  //const articlesset = document.querySelector(".articles");
-  
-  // if an area in the articles is clicked while the menu is open, close the menu
-  //articlesset.addEventListener("click", offMenu);
+  const closeButton = document.querySelector(".close-button");
+
+  // click handlers that call menu functions
+  menuButton.addEventListener("click", openMenu);
+  closeButton.addEventListener("click", closeMenu);
