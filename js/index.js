a585bc7eb7ec6dd825456e6ac1f32815323d4c8c
Hamza Elkhoudiri <elkhoudh@gmail.com>
2018-12-10 17:14:12 -0500

    Added Overlay Nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// JS goes here
+/* Open */
+const overlay = document.querySelector(".overlay");
+
+function on() {
+  overlay.style.display = "block";
+}
+
+function off() {
+  overlay.style.display = "none";
+}
