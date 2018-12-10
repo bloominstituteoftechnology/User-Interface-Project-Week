27aa2f789fe442a716fc45f1e08666b708ec4590
KieranVieira <kieranvieira@live.com>
2018-12-10 15:19:43 -0500

    Completed navigation style and functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-// JS goes here
+//Nav Toggle
+
+const navButtonOn = document.querySelector('.nav-toggle-on');
+const navButtonOff = document.querySelector('.nav-toggle-off')
+const navMenu = document.querySelector('.nav-menu');
+
+navButtonOn.addEventListener('click', () => {
+    navMenu.classList.toggle('nav-toggle', true);
+    console.log('clicked nav on')
+})
+
+navButtonOff.addEventListener('click', () => {
+    navMenu.classList.toggle('nav-toggle', false)
+    console.log('clicked nav off')
+})
