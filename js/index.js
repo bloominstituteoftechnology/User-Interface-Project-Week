545083ec7164e5cdb41790f05658879b39c485ab
byronholmes2018 <lshopeful2018@mgmail.com>
2018-12-10 16:55:26 -0600

    Added event listener on hamburger button which upon click, switches hamburger image to close img and shows hidden menu, and upon clicking close img, changes img back to hamburger and closes hidden menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // JS goes here
+document.querySelector('.nav-btn').addEventListener('click', function(e){
+  if(e.target.getAttribute("src")==="img/nav-hamburger-close.png"){
+    document.querySelector('.menu-layover').style.display="none";
+    e.target.setAttribute("src", "img/nav-hamburger.png");
+  }else{
+    e.target.setAttribute("src", "img/nav-hamburger-close.png");
+    document.querySelector('.menu-layover').style.display = "flex";
+  }
+})
