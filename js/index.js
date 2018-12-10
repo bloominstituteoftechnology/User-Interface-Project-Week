28445ebc487713897c1de68be4c17a8c23c6bb28
Brandon Lent <blent1050@yahoo.com>
2018-12-10 18:49:36 -0500

    Navbar functionality completed for mobile and desktop

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// JS goes here
+
+
+// Nav Bar responsiveness
+const navbar = document.querySelector("nav");
+const hamburger = document.querySelector(".hamburger");
+const header = document.querySelector("header");
+hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('show-nav');
+    header.classList.toggle('transparent-bg');
+})
