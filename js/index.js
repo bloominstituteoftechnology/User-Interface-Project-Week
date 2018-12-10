4c6b66f1befabe2194c2d0d9e46c2a86c574df37
Matt Basile <matthewbasile2@gmail.com>
2018-12-10 16:16:43 -0500

    Added navbar drop down functionality. Need to tweek a little bit. LINES: index.js 2-7.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // JS goes here
+const hamburger = document.querySelector('.hb-btn');
+const navLinks = document.querySelector('nav');
+hamburger.addEventListener('click', ()=>{
+    navLinks.classList.toggle('show-nav');
+    navLinks.classList.toggle('hidden');
+});
