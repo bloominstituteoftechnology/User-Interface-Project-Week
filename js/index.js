fe11f4ab8c494da627ae511fd0df721cebf32f43
Matt Basile <matthewbasile2@gmail.com>
2018-12-10 16:26:48 -0500

    Added full functionality, and proper styles to full page nav.

--- js/index.js
+++ js/index.js
@@ -4,4 +4,9 @@ const navLinks = document.querySelector('nav');
 hamburger.addEventListener('click', ()=>{
     navLinks.classList.toggle('show-nav');
     navLinks.classList.toggle('hidden');
+    if(navLinks.classList.contains('show-nav')){
+        hamburger.src="img/nav-hamburger-close.png"
+    } else{
+        hamburger.src="img/nav-hamburger.png"
+    }
 });
