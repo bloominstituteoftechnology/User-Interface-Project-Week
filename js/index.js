1422f23743715adbdac40e997cc2562c1c1a7590
mcferrenm <mcferrenmax@gmail.com>
2018-12-10 18:57:26 -0500

    Navigation expanded styles and functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// JS goes here
+const navMenuBtn = document.querySelector('.nav-menu-btn');
+const navContainer = document.querySelector('.nav-container');
+const navCloseBtn = document.querySelector('.close-menu-btn');
+
+navMenuBtn.addEventListener('click', () => {
+  navContainer.classList.add('expanded');
+})
+
+navCloseBtn.addEventListener('click', () => {
+  navContainer.classList.remove('expanded');
+})
