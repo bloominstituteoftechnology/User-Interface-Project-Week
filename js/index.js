a1719b3a6b2e155bdb50f26b884d9b73c338b0ff
Samsoucy <newf930@outlook.com>
2018-12-11 13:26:20 -0500

    added JS for nav system

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 // JS goes here
+class Menu {
+    constructor(menuName, menuButtonName){
+        this.menu = document.querySelector(menuName);
+        this.menuButton = document.querySelector(menuButtonName);
+        this.menuButton.addEventListener("click", () => {
+            this.toggleMenu()
+        });
+        this.isOpen = false;
+    }
 
+  toggleMenu() {
+      this.menu.classList.toggle("menu-open");
+      this.menu.parentElement.classList.toggle("nav-opacity");
+      if(this.isOpen) {
+          this.menuButton.src = "img/nav-hamburger.png";
+          this.isOpen = "false";
+      }else{
+          this.menuButton.src = "img/nav-hamburger-close.png";
+          this.isOpen = true;
+      }
+  }  
+}
 
+const menu = new Menu(".menu", ".menu-button");
