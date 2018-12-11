3ac43a5729b296899aba3a9e13499b944b9334f7
Nathan Thomas <nwthomas@me.com>
2018-12-11 10:19:55 -0800

    Finish navbar and limit scroll function while open

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-// JS goes here
+const navbar = document.querySelector(".navbar__container");
+const navbarText = document.querySelector(".nav__menu");
+const burger = document.querySelector(".burger");
+const body = document.querySelector("body");
+
+burger.addEventListener("click", () => {
+  navbar.classList.toggle("navbar--open");
+  navbarText.classList.toggle("nav__menu--open");
+  body.classList.toggle("stop__scrolling");
+  if (body.classList.contains("stop__scrolling")) {
+    body.bind("touchmove", e => e.preventDefault());
+  } else {
+    body.unbind("touchmove");
+  }
+});
