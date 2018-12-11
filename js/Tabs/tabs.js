1f3f87d7427c07445662ebd7226820e7ed933c44
leiannel <leilouis112@gmail.com>
2018-12-11 14:15:49 -0500

    completed services tab section

---
+++ js/Tabs/tabs.js
@@ -0,0 +1,38 @@
+
+class TabLink {
+    constructor(domElement){
+        this.domElement = domElement;
+        this.data = this.domElement.dataset.tab;
+        this.itemElement = document.querySelector(`.tab-item[data-tab="${this.data}"]`)
+        this.itemElementImg = document.querySelector(`.tab-img[data-tab="${this.data}"]`)
+        this.domElement.addEventListener('click', () => {this.select()});
+        this.items = new TabItems(this.itemElement, this.itemElementImg);
+    }
+
+    select() {
+        const links = document.querySelectorAll('.tabs-link');
+        links.forEach(e => e.classList.remove('tabs-link--selected'))
+        this.domElement.classList.add('tabs-link--selected')
+        this.items.select()
+    }
+}
+
+class TabItems {
+    constructor(item, img){
+        this.item = item;
+        this.img = img;
+        console.log(this.img, this.item)
+    }
+
+    select(){
+        const items = document.querySelectorAll('.tab-item');
+        items.forEach(e => e.classList.remove('tab-item--selected'));
+        const images = document.querySelectorAll('.tab-img');
+        images.forEach(e => e.classList.remove('tab-img--selected'));
+        this.item.classList.add('tab-item--selected')
+        this.img.classList.add('tab-img--selected')
+    }
+}
+
+const tabs = document.querySelectorAll('.tabs-link').forEach(e => new TabLink(e));
+console.log(tabs);
