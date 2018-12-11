4cc5241a873715edd4e87d77b39837e625e3a6b4
BrannanC <brannanconrad@gmail.com>
2018-12-11 15:16:09 -0600

    Navigation functionality finished, styles need to be updated

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 const navigation = document.querySelector('.nav-open');
 const navButton = document.querySelector('.nav-button');
+const bigX = document.querySelector('.big-x');
+
 navButton.addEventListener('click', function(){
     navigation.classList.toggle('display-hidden');
-})
+    navButton.classList.toggle('display-hidden');
+    bigX.classList.toggle('display-hidden');
+    console.log('navB');
+});
+
+bigX.addEventListener('click', function(){
+    navigation.classList.toggle('display-hidden');
+    navButton.classList.toggle('display-hidden');
+    bigX.classList.toggle('display-hidden');
+
+});
