2e1c8e1c5485f70e441755ac8dac242106c3aea7
cecil john tantay <cjbtantay@gmail.com>
2018-12-11 07:56:50 -0800

    added mobile view style to services.html

--- js/test.js
+++ js/test.js
@@ -1,21 +1,24 @@
 class TabLink {
   constructor(element) {
     this.element = element;
+    
     this.data = this.element.dataset.tab;
-    this.tabItem = document.querySelector(
-      `.tabs-item[data-tab="${this.data}"]`
-    );
+    
+    this.tabItem = document.querySelector(`.tabs-item[data-tab="${this.data}"]`)
     this.tabItem = new TabItem(this.tabItem);
 
-    this.element.addEventListener("click", () => this.select());
+    this.element.addEventListener("click", ()=> this.select())
   }
   //method
   select() {
     const links = document.querySelectorAll(".tabs-link");
+    
     links.forEach(link => link.classList.remove("tabs-link-selected"));
     // Array.from(links).forEach(link => link.classList.remove("tabs-link-selected"));
     this.element.classList.add("tabs-link-selected");
+    
     this.tabItem.select();
+
   }
 }
 
@@ -24,13 +27,9 @@ class TabItem {
     this.element = element;
   }
   select() {
-    // secelect all tabs item content
-    const items = document.querySelectorAll(".tabs-item");
-    items.forEach(item => item.classList.remove("tabs-item-selected"));
-    this.element.classList.add("tabs-item-selected");
+   
   }
 }
 
-let tabslink = document
-  .querySelectorAll(".tabs-link")
-  .forEach(item => new TabLink(item));
+
+let tabslink = document.querySelectorAll(".tabs-link").forEach(item=>new TabLink(item));
