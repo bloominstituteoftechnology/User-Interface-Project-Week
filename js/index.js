1d7aab0be0e02a5818b0211f8be27b97ae4ecdba
leiannel <leilouis112@gmail.com>
2018-12-11 12:37:40 -0500

    completed services page styling

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // JS goes here
+
+function openModal() {
+    header.classList.toggle('modal--open')
+    if(header.classList.contains('modal--open')){
+        modal.style.display = 'flex'
+        menuBtn.src = "/img/nav-hamburger-close.png"
+        body.style.overflow = 'hidden'
+    } else {
+        modal.style.display = 'none'
+        menuBtn.src = "/img/nav-hamburger.png"
+        body.style.overflow = ''
+
+    }
+}
+
+const header = document.querySelector('.header');
+const body = document.querySelector('body')
+const modal = document.querySelector('.nav-modal');
+const menuBtn = document.getElementById('menuBtn');
+
+menuBtn.addEventListener('click', openModal)
+
