0a526d0e85445725a90c3f84e283b64e81ee2202
amberm <nekosenpai@gmail.com>
2018-12-11 15:43:24 -0700

    mvp done, though in a very simplistic way, gonna do a loop later

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 // JS goes here
 
-
-
 function openNav() {
     document.getElementById("myNav").style.height = "100%";
 }
@@ -10,3 +8,70 @@ function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
 
+
+
+
+
+function precon() {
+    document.getElementById("precon").style.display = "flex";
+    document.getElementById("cons").style.display ="none";
+    document.getElementById("desi").style.display ="none";
+    document.getElementById("sust").style.display ="none";
+
+    document.getElementById("yo1").style.color="white";
+    document.getElementById("yo1").style.backgroundColor="#5E9FB9"
+    document.getElementById("yo2").style.color="black";
+    document.getElementById("yo2").style.backgroundColor="white"
+    document.getElementById("yo3").style.color="black";
+    document.getElementById("yo3").style.backgroundColor="white"
+    document.getElementById("yo4").style.color="black";
+    document.getElementById("yo4").style.backgroundColor="white"
+}
+
+function cons() {
+    document.getElementById("precon").style.display = "none";
+    document.getElementById("cons").style.display ="flex";
+    document.getElementById("desi").style.display ="none";
+    document.getElementById("sust").style.display ="none";
+
+    document.getElementById("yo2").style.color="white";
+    document.getElementById("yo2").style.backgroundColor="#5E9FB9"
+    document.getElementById("yo1").style.color="black";
+    document.getElementById("yo1").style.backgroundColor="white"
+    document.getElementById("yo3").style.color="black";
+    document.getElementById("yo3").style.backgroundColor="white"
+    document.getElementById("yo4").style.color="black";
+    document.getElementById("yo4").style.backgroundColor="white"
+}
+
+function desi() {
+    document.getElementById("precon").style.display = "none";
+    document.getElementById("cons").style.display ="none";
+    document.getElementById("desi").style.display ="flex";
+    document.getElementById("sust").style.display ="none";
+
+    document.getElementById("yo3").style.color="white";
+    document.getElementById("yo3").style.backgroundColor="#5E9FB9"
+    document.getElementById("yo1").style.color="black";
+    document.getElementById("yo1").style.backgroundColor="white"
+    document.getElementById("yo2").style.color="black";
+    document.getElementById("yo2").style.backgroundColor="white"
+    document.getElementById("yo4").style.color="black";
+    document.getElementById("yo4").style.backgroundColor="white"
+}
+
+function sust() {
+    document.getElementById("precon").style.display = "none";
+    document.getElementById("cons").style.display ="none";
+    document.getElementById("desi").style.display ="none";
+    document.getElementById("sust").style.display ="flex";
+
+    document.getElementById("yo4").style.color="white";
+    document.getElementById("yo4").style.backgroundColor="#5E9FB9"
+    document.getElementById("yo1").style.color="black";
+    document.getElementById("yo1").style.backgroundColor="white"
+    document.getElementById("yo2").style.color="black";
+    document.getElementById("yo2").style.backgroundColor="white"
+    document.getElementById("yo3").style.color="black";
+    document.getElementById("yo3").style.backgroundColor="white"
+}
