c3230904c6b1f9fdb62f23fcecaa32e83c5072f6
Nathan Thomas <nwthomas@me.com>
2018-12-11 13:23:21 -0800

    Update contact.html with HTML structure

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -34,24 +34,3 @@ class TabItem {
 const tabs = document
   .querySelectorAll(".tab")
   .forEach(link => new TabLink(link));
-
-//   select() {
-//     // Get all of the elements with the tabs-link class
-//     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-//     this.deselect();
-//     TweenMax.to(this.element, 1, { className: "+=tabs-link-selected" }); // Add a class named "tabs-link-selected" to this link
-
-//     // Call the select method on the item associated with this link
-//     this.itemElement.select();
-//   }
-
-//   deselect() {
-//     const links = document.querySelectorAll(".tabs-link");
-//     links.forEach(function(link) {
-//       if (link.classList.contains("tabs-link-selected")) {
-//         TweenMax.to(link, 1, { className: "-=tabs-link-selected" });
-//       } else {
-//       }
-//     });
-//   }
-// }
