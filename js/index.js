ef414bc1824718ef0b9e91d6200fb56d06b45233
Brandon Lent <blent1050@yahoo.com>
2018-12-10 19:33:03 -0500

    Services tabs functionality added

--- js/index.js
+++ js/index.js
@@ -8,3 +8,39 @@ hamburger.addEventListener('click', () => {
     navbar.classList.toggle('show-nav');
     header.classList.toggle('transparent-bg');
 })
+
+//Service -> Tabs
+class TabLink {
+    constructor(element) {
+      this.element = element;
+      this.data = this.element.dataset.tab; 
+      this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+      this.itemElement = new TabItem(this.itemElement);
+      this.element.addEventListener('click', () => {this.select(element)});
+    };
+  
+    select() {
+      const links = document.querySelectorAll('.tabs-link');
+      links.forEach(link => {
+        link.classList.remove('tabs-link-selected');
+      });
+      this.element.classList.add('tabs-link-selected'); 
+      this.itemElement.select();   
+    }
+  }
+  
+  class TabItem {
+    constructor(element) {
+      this.tabItem = element;
+    }
+  
+    select() {
+      const items = document.querySelectorAll('.tabs-item');
+      items.forEach(item => {
+        item.classList.remove('tabs-item-selected');
+      });
+      this.tabItem.classList.add('tabs-item-selected');
+    }
+  }
+  
+  const links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link));
