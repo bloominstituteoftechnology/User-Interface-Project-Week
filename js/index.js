e2755b0764f25168069b8ec22847b09aebd30547
Angel Torres <alas4224@gmail.com>
2018-12-11 08:50:51 -0600

    finished MVP

--- js/index.js
+++ js/index.js
@@ -11,14 +11,25 @@ document.querySelector(".hamburger-close").addEventListener("click", function() 
 class Tab {
     constructor(element) {
         this.element = element;
+
         this.data = this.element.dataset.tab;
+
         this.content = document.querySelector(`.tab-content[data-tab="${this.data}"]`);
-        console.log(this.content);
-        this.element.addEventListener("click", () => this.select())
+
+        this.element.addEventListener("click", () => this.select());
+
+        document.querySelectorAll(".tab-content").forEach((content) => content.style.display = "none");
+
     }
 
     select() {
-        
+        document.querySelectorAll(".tab-content").forEach((content) => content.style.display = "none");
+
+        this.content.style.display = "flex";
+
+        document.querySelectorAll(".tab").forEach((tab) => tab.classList.remove("selected-tab"))
+
+        this.element.classList.add("selected-tab");
     }
 }
 
