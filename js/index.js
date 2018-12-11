c7694416ddbc05281a473b72c51504f7796a6ccf
Nate Boyette <nateboyette@icloud.com>
2018-12-11 15:00:47 -0800

    Added expanded menu feature.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // JS goes here
+
+
+//===== NAV Menu 
+
+const toggleMenu = () => {
+  menu.classList.toggle('menu--open');
+  menuBtnClose.classList.toggle("menu-btn-close");
+  menuBtn.classList.toggle('menu-btn-close');
+}
+
+const menu = document.querySelector('.menu');
+
+const menuBtn = document.querySelector('.menu-btn');
+const menuBtnClose = document.querySelector('.menu-btn-close');
+
+
+menuBtn.addEventListener('click', () => {
+  toggleMenu();
+  
+})
+
+menuBtnClose.addEventListener('click', () => {
+  toggleMenu();
+
+})
