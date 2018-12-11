4c8824d8eb25906b542d738e12e47ece6d1339ce
Imon Ovbude <imonovbude@gmail.com>
2018-12-11 14:58:53 -0800

    changed nav menu items to dropdown button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // JS goes here
+class TabLink {
+    constructor(elem) {
+        // Individual Tabs
+        this.elem = elem;
+
+        // Data Attribute
+        this.data = this.elem.dataset.tab;
+
+        // Item Selector
+        this.itemElem = document.querySelector(`.tabs-item[data-tab = '${this.data}']`);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const links = document.querySelectorAll('.tabs-link').forEach(tab => new TabLink(tab));
+
