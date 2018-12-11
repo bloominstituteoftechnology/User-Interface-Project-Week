859462cb2ab129325b06a5156e5b3c8efd19ae47
ajfrierson <alvinfrierson@fuller.edu>
2018-12-11 14:58:58 -0800

    pushing for first time today

--- js/index.js
+++ js/index.js
@@ -1,103 +1,65 @@
 // JS goes here
-//Home JS
-class Dropdown {
+/* Open */
+function openNav() {
+  document.querySelector(".dropdown").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.querySelector(".dropdown").style.height = "0%";
+}
+class TabItem {
   constructor(element) {
-    // assign this.element to the dropdown element
     this.element = element;
-    // assign the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector('.toggle-overlay')
-    this.closebutton = this.element.querySelector('.closebutton')
-    // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.element.querySelector('.aside');
-    this.closebuttoncontent = document.querySelector('.aside');
-    
-    // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', (event) => { this.toggleContent(event) })
-    this.closebutton.addEventListener('click', (event) => { this.closeContent(event) })
-    // Instructor note, the reason we must wrap the toggleContent method in an anonymous function is that 'this' in toggleContent
-    // would refer to the button, NOT the current instance of the class. 
   }
 
-  toggleContent(event) {
-    // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle('open');
-  }
+  select() {
+    // Select all items elements from the DOM
+    let tabItems = document.querySelectorAll('.service-detail');
 
-  closeContent(event) {
-    // Toggle the ".dropdown-hidden" class off and on
-    this.closebuttoncontent.classList.toggle('open');
+    // Remove the class "service-detail-selected" from each element
+    Array.from(tabItems).forEach( el => {
+      el.classList.remove('service-detail-selected');
+    });
+    // Add a class named "service-detail-selected" to this element
+    this.element.classList.add('service-detail-selected'); 
   }
 }
-// Nothing to do here, just study what the code is doing and move on to the Dropdown class
-let dropdowns = document.querySelectorAll('header');
-dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
 
-//Tabs JS
 class TabLink {
   constructor(element) {
-    // assign this.element to the element reference
     this.element = element;
-    // Get the tab data attribute and save the value here
-    this.tabData = this.element.dataset.tab;
-    // Find all elements with the .card class in index.html that correspond to the tab data attribute. If the data is 'all' then select all cards regardless of their data attribute
-    if (this.tabData === 'all') {
-      this.cards = document.querySelectorAll(`.card`);
-    } else {
-      this.cards = this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
-    }
-    // Map over the cards array and convert each card element into a new instance of the TabCard class. Pass in the card object to the TabCard class.
-    this.cards = Array.from(this.cards).map(card => new TabCard(card));
+    
+    // Get the custom data attribute on the Link
+    this.data = element.dataset.tab;
+    
+    // Using the custom data attribute get the associated Item element
+    this.itemElement = document.querySelector(`.service-detail[data-tab="${this.data}"]`);
+    
+    // Using the Item element, create a new instance of the TabItem class
+    this.tabItem = new TabItem(this.itemElement);
+    
+    // Add a click event listener on this instance, calling the select method on click
+    this.element.addEventListener('click', () => { this.select() });
+  };
 
-    // Add a click event that invokes selectTab
-    this.element.addEventListener('click', (event) => {
-      // Call the select method you define below
-      this.selectTab(event);
-    });
-  }
+  select() {
+    // Get all of the elements with the tabs class
+    const links = document.querySelectorAll('.tab');
 
-  selectTab(event) {
-
-    // Select all elements with the .tab class on them
-    const tabs = document.querySelectorAll('.tab');
-    // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach(link => {
-      link.classList.remove('active-tab')
+    // Using a loop or the forEach method remove the 'tabs-selected' class from all of the links
+    Array.from(links).forEach( el => {
+      el.classList.remove('tab-selected');
     });
 
-    // Add a class of ".active-tab" to this.element
-    this.element.classList.add('active-tab');
-    //console.log(this.element);
-
-    // Select all of the elements with the .card class on them
-    const cards = document.querySelectorAll('.card');
-    // Iterate through the NodeList setting the display style each one to 'none'
-    cards.forEach(card => {
-      card.style.display = 'none';
-    });
-    // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class, nothing to update here
-    this.cards.forEach(card => card.selectCard());
+    // Add a class named "tabs-selected" to this link
+    this.element.classList.add('tab-selected');
+    
+    // Call the select method on the item associated with this link
+    this.tabItem.select();
   }
 }
 
-class TabCard {
-  constructor(element) {
-    // Assign this.element to the passed in element.
-    this.element = element;
-  }
-  selectCard() {
-    // Update the style of this.element to display = null
-    this.element.style.display = 'flex';
-  }
-}
-
-// Create a reference to all ".tab" classes
-let tabs = document.querySelectorAll('.tab');
-// Map over the array and convert each tab reference into a new TabLink object.  Pass in the tab object to the Tabs class.
-tabs = Array.from(tabs).map(link => new TabLink(link));
-
-//Once you are complete, call the .select method on the first tab
-tabs[0].selectTab();
-
-
-
-
+let links = document.querySelectorAll('.tab');
+links = Array.from(links).map( (tab) => new TabLink(tab));
+links[0].select();
