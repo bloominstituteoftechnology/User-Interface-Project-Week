ae5bb3ddade8903c03917fa926705ab6070551d6
Henry Neal <henry.neal0@gmail.com>
2018-12-11 13:03:41 -0500

    working on sticky nav bar before mobile

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementsByClassName(".top-bar");
+
+var sticky = topbar.offsetTop;
+
+function myFunction(){
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
