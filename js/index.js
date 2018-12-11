0ed88b2e59dde0bbdb9e85be12ca75837a11281d
McNultyPT <mcnultypt@gmail.com>
2018-12-11 15:53:31 -0800

    navbar in progress

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// JS goes here
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open');
+    menuButton.style.display = 'none';
+    menuX.style.display = 'block';
+}
+
+const toggleX = () => {
+    menu.classList.toggle('menu--closed');
+    menuButton.style.display = 'block';
+    menuX.style.display = 'none';
+}
+
+const menu = document.querySelector('.menu');
+
+const menuButton = document.querySelector('.menu-button');
+
+const menuX = document.querySelector('.menu-x');
+
+menuButton.addEventListener('click', () => toggleMenu());
+
+menuX.addEventListener('click', () => toggleX());
