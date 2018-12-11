40a4256a83f4a0984eca454980692cf4ee738e54
yannafaith <yannafaith97@gmail.com>
2018-12-11 15:46:55 -0700

    added toggle header button img

--- js/index.js
+++ js/index.js
@@ -4,11 +4,19 @@ const headerButton = document.querySelector('#header-button');
 
 const dropMenu = document.querySelector('.drop-menu');
 
+const buttonImg = document.querySelectorAll('.menu button img');
+console.log(buttonImg);
+
 
 onClick = () => {
-    dropMenu.classList.toggle('inactive')
+    dropMenu.classList.toggle('inactive');
+     buttonImg[0].classList.toggle('hidden');
+     buttonImg[1].classList.toggle('hidden');
 }
 
 
+
 headerButton.addEventListener('click', onClick);
+
+
 
