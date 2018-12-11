75c012fc52b03c184a4d0c2357aef9f42f09499c
amberm <nekosenpai@gmail.com>
2018-12-11 13:34:21 -0700

    nav bar fully working with popup stuff too :)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS goes here
+
+
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+  
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
