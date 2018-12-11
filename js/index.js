638a5a4977c2edccc7edc25cf25d72271c71b351
Orlando Lopez <orlandolopez@Orlandos-MBP.fios-router.home>
2018-12-10 19:48:05 -0500

    added navigation styling and logic

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+// Navigation Toggle Logic 
+const togglerOpen = document.querySelector('.nav-toggle-open');
+const togglerClose = document.querySelector('.nav-toggle-close');
+const nav = document.querySelector('.navigation');
+
+const toggleNav = () => {
+    togglerOpen.classList.toggle('hide');
+    togglerClose.classList.toggle('hide');
+    nav.classList.toggle('navigation--open');
+}
+console.log(nav)
+
+togglerOpen.onclick = () => toggleNav();
+togglerClose.onclick = () => toggleNav();
