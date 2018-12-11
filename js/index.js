d249e9db5e5daee065bf49a9287f1f564ef7b34f
BrannanC <brannanconrad@gmail.com>
2018-12-11 14:54:09 -0600

    Made nav bar functional

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-// JS goes here
+const navigation = document.querySelector('.nav-open');
+const navButton = document.querySelector('.nav-button');
+navButton.addEventListener('click', function(){
+    navigation.classList.toggle('display-hidden');
+})
