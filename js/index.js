ce0eb669be582e86ce2ab6564a1ebfa54676ddd1
atbenedict <atbenedict@gmail.com>
2018-12-11 11:57:49 -0500

    finished desktop nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // JS goes here
+
+function openNav() {
+  document.getElementById("menuItems").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("menuItems").style.width = "0%";
+}
