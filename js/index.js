6a16a0de2fd2d209f62cdebb0d2a914bf49d1e1d
john2796 <benedictmiranda27@yahoo.com>
2018-12-10 18:50:24 -0800

    finished services page

--- js/index.js
+++ js/index.js
@@ -1,13 +1,9 @@
-
-
-
-
-
 class TabLink {
   constructor(element) {
     this.element = element;
     this.data = this.element.dataset.tab;
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`)
+    //console.log(this.itemElement);
     this.tabItem = new TabItem(this.itemElement);
 
     // get links and item 
@@ -19,12 +15,11 @@ class TabLink {
   };
 
   select(links) {
-    console.log('selected');
-    // Array.from(links).forEach(tab =>
-    //   tab.classList.remove("tabs-link-selected")
-    // );
-    // this.element.classList.add("tabs-link-selected");
-    // this.tabItem.select(this.tabItems);
+    Array.from(links).forEach(tab =>
+      tab.classList.remove("tabs-link-selected")
+    );
+    this.element.classList.add("tabs-link-selected");
+    this.tabItem.select(this.tabItems);
   }
 }
 
@@ -35,25 +30,25 @@ class TabItem {
     this.element = element;
   }
   select(tabItems) {
-    console.log('selected tab')
-    //   // const tabItems = document.querySelectorAll('.tabs-item');
-    //   tabItems.forEach(item => {
-    //     item.classList.remove('tabs-item-selected')
-    //   })
-    //   this.element.classList.add('tabs-item-selected')
-    // }
+    // const tabItems = document.querySelectorAll('.tabs-item');
+    tabItems.forEach(item => {
+      item.classList.remove('tabs-item-selected')
+    })
+    this.element.classList.add('tabs-item-selected')
   }
-  let links = document.querySelectorAll('.tabs-link');
-links.forEach((link) => new TabLink(link));
+}
 
 
+// class SingleTab {
+//   constructor(element, tabItem) {
+//     this.element = element;
+//     this.tabItems = tabItem
+//     console.log(this.element);
+//     console.log(this.tabItems);
+//   }
+// }
 
-
-
-
-
-
-
+links = document.querySelectorAll('.tabs-link').forEach((link) => new TabLink(link))
 
 
 
@@ -69,3 +64,5 @@ hamburger.addEventListener('click', function () {
     hamburger.src = "./img/nav-hamburger-close.png"
   }
 })
+
+
