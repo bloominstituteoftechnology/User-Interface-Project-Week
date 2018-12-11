cb24a014e6a08c66759b130bf148df23c230fc86
john2796 <benedictmiranda27@yahoo.com>
2018-12-11 11:51:54 -0800

    build a costume carousel done

--- js/index.js
+++ js/index.js
@@ -114,16 +114,19 @@ for (let i = 0; i < bubblyButtons.length; i++) {
 // carousel
 
 const SHOWING_CLASS = "showing";
-const firstSlide = document.querySelector('.slider__item:first-of-type');
-const lastChild = document.querySelector('.slider__item:last-of-type');
-const jumbotron = document.querySelector('.contact__jumbotron');
-// btn
-const next = document.querySelector('.right-button');
-const previous = document.querySelector('.left-button')
+const firstSlide = document.querySelector('.slider__item:first-child');
+const lastChild = document.querySelector('.slider__item:last-child');
+const next = document.querySelector('.slider_right_btn');
+const previous = document.querySelector('.slider_left_btn')
+
+// slide function 
 function slide() {
   const currentSlide = document.querySelector(`.${SHOWING_CLASS}`)
+  // if currentSlide is true and it has showing class then remove it and passed it to the next element
   if (currentSlide) {
+    //console.log(currentSlide);
     currentSlide.classList.remove(SHOWING_CLASS);
+    // if current slide is true continue with if statement get the next slide and check if next slide has the ".class" showing if true add the class and keep continue with the loop if not add the class to the first you want to set else to the first slide so if you keep clicking next button it would go back to the first item so it makes a full rotation same thing with the prev button
     const nextSlide = currentSlide.nextElementSibling;
     if (nextSlide) {
       nextSlide.classList.add(SHOWING_CLASS);
@@ -131,23 +134,28 @@ function slide() {
       firstSlide.classList.add(SHOWING_CLASS);
     }
   } else {
-
+    // if currentSlide false don't have showing class then add it to the first child element
     firstSlide.classList.add('showing');
   }
-  console.log('testing')
 }
-
+// need this so you don't have to wait 3seconds before first image shows
 slide();
 
+//next button
+//console.log(next);
 next.addEventListener('click', () => {
   slide();
-  console.log('clicked next')
 })
-previous.addEventListener('click', () => {
 
+//previous button
+//console.log(previous);
+previous.addEventListener('click', () => {
   const currentSlide = document.querySelector(`.${SHOWING_CLASS}`)
+  // if currentSlide is true and it has ".showing" class then remove it 
   if (currentSlide) {
+    //console.log(currentSlide);
     currentSlide.classList.remove(SHOWING_CLASS);
+    // if current slide is true continue with if statement get the next slide and check if next slide has the class showing if true add the class and keep continue with the loop if not add the class to the first ...you want to set else to the last slide so if you keep clicking prev button it would go back to the last item so it makes a full rotation same thing with the next button
     const prevSlide = currentSlide.previousElementSibling;
     if (prevSlide) {
       prevSlide.classList.add(SHOWING_CLASS);
@@ -155,11 +163,11 @@ previous.addEventListener('click', () => {
       lastChild.classList.add(SHOWING_CLASS);
     }
   } else {
+    // if currentSlide false don't have showing class then add it to the first child element
     firstSlide.classList.add('showing');
   }
-  console.log('left clicked')
-
 })
 
 
-setInterval(slide, 4000);
+// every 3 seconds change the image ; 
+console.log(setInterval(slide, 4000));
