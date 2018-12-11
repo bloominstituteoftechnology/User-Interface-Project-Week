2632dcb98cac9a778031c2ffafdbfe45ed7cd7da
Raine Wallace <rainebeaudubs@gmail.com>
2018-12-11 18:43:59 -0500

    Completed navigation.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// JS goes here
+const toggleMenu = () => {
+  menu.classList.toggle('menu--open');
+};
+
+const menu = 
+document.querySelector('.menu');
+
+const menuButton = 
+document.querySelector('.menu-button');
+
+const menuCloseButton = 
+document.querySelector('.menu-close-button');
+
+
+menuButton.addEventListener('click', function(e) {
+  e.target = toggleMenu();
+});
+
+menuCloseButton.addEventListener('click', function(e) {
+  e.target = toggleMenu();
+});
