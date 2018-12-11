472152366e75815d4fa7d88b839a9dd29c708ff8
Matt Basile <matthewbasile2@gmail.com>
2018-12-11 13:35:14 -0500

    Basic image resizing based on screen size for tab images. Could run into an issue on resizing but working for a solution on that

--- components/Tabs/tabs.js
+++ components/Tabs/tabs.js
@@ -17,14 +17,24 @@ class TabLink{
      }
     // Methods
     select(){
-        this.deselect();
+       this.deselect();
+       let mini = window.matchMedia("(max-width: 500px)");
        this.title.textContent = this.header;
        this.element.classList.add('selected')
+       if(mini.matches){
+        if(this.header === "Design Build"){
+            this.image.src=`img/services/services-tab-mobile-design-build-img.png`;
+           } else{
+           this.image.src=`img/services/services-tab-mobile-${this.header.toLowerCase()}-img.png`;
+           }
+       }
+       else{
        if(this.header === "Design Build"){
         this.image.src=`img/services/services-tab-design-build-img.png`;
        } else{
        this.image.src=`img/services/services-tab-${this.header.toLowerCase()}-img.png`;
        }
+        }
     }
     deselect(){
         const links = document.querySelectorAll('.link');
