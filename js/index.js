55019782c2d77a032ab2e32fae648178cad5fd7a
yannafaith <yannafaith97@gmail.com>
2018-12-11 15:49:12 -0700

    cleaned up header button js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,13 @@
-// JS goes here
-
 const headerButton = document.querySelector('#header-button');
-
 const dropMenu = document.querySelector('.drop-menu');
-
 const buttonImg = document.querySelectorAll('.menu button img');
-console.log(buttonImg);
-
 
 onClick = () => {
     dropMenu.classList.toggle('inactive');
-     buttonImg[0].classList.toggle('hidden');
-     buttonImg[1].classList.toggle('hidden');
+    buttonImg[0].classList.toggle('hidden');
+    buttonImg[1].classList.toggle('hidden');
 }
-
-
 
 headerButton.addEventListener('click', onClick);
 
-
-
+//=============== nav button scripts
