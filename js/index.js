698a7c4438e2999f1f091398083238539b7b3386
Hamza Elkhoudiri <elkhoudh@gmail.com>
2018-12-11 18:38:32 -0500

    mobile

--- js/index.js
+++ js/index.js
@@ -1,4 +1,41 @@
 /* Open */
+class Carousel {
+  constructor(ele) {
+    this.ele = ele;
+    this.leftBtn = this.ele.querySelector(".left-button");
+    this.rightBtn = this.ele.querySelector(".right-button");
+    this.images = this.ele.querySelectorAll("img");
+    this.currentIndex = this.images[0];
+    this.leftBtn.addEventListener("click", () => this.left());
+    this.rightBtn.addEventListener("click", () => this.right());
+    this.currentIndex.style.display = "block";
+    this.i = 0;
+    //setInterval(() => this.right(), 5000);
+  }
+
+  left() {
+    this.images.forEach(img => (img.style.display = "none"));
+    if (this.i > 0) {
+      this.images[this.i].style.display = "block";
+      this.i -= 1;
+    } else {
+      this.images[this.images.length - 1].style.display = "block";
+      this.i = this.images.length - 1;
+    }
+  }
+
+  right() {
+    this.images.forEach(img => (img.style.display = "none"));
+    //this.currentIndex = this.currentIndex.nextElementSibling;
+    if (this.i < this.images.length) {
+      this.images[this.i].style.display = "block";
+      this.i += 1;
+    } else {
+      this.images[0].style.display = "block";
+      this.i = 0;
+    }
+  }
+}
 
 class TabLink {
   constructor(element) {
@@ -52,3 +89,6 @@ function off() {
 links = document
   .querySelectorAll(".tabs-link")
   .forEach(link => new TabLink(link));
+
+let carousel = document.querySelector(".carousel");
+new Carousel(carousel);
