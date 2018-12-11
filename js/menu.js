5b0791ec49237e30fb0c47af1f87efa76796a221
croy <roy.christie95@gmail.com>
2018-12-10 19:35:44 -0600

    Achieved full screen functionality with nav menu although it is offkilter

--- js/menu.js
+++ js/menu.js
@@ -1,33 +1,18 @@
-class MenuItem {
-  constructor(menuitem){
-    this.menuitem = menuitem;
-    this.menuClick = document.querySelector('.menu-button');
-    this.menuContent = document.querySelector('.menu');
-    this.menuClick.addEventListener('click', () => this.toggleMenu());
-  }
-  // Methods
-  toggleMenu(){
-    this.menuContent.classList.toggle('menu--open');
-    // if (this.menuContent.classList.contains('menu--open')) {
-    //   TweenMax.to(menu, 1, {
-    //     left: 0,
-    //     ease: Power0.easeOut,
-    //   });
-    // } 
-    // else {
-    //   TweenMax.to(menu, 3, {
-    //     left: -150,
-    //     ease: Power0.easeOut,
-    //   });
-    // }
-  }
+const menuOpen = () => {
+  menu.style.display = 'block';
+  menuButton.style.display = 'none';
 }
-// Start Here: Create a reference to the ".menu" class
-const menu = document.querySelectorAll('.menu');
-// create a reference to the ".menu-button" class
-const menuButton = document.querySelectorAll('.menu-button');
-menuButton.forEach( menuitem => {
-  return new MenuItem(menuitem);
-});
-// Using your menuButton reference, add a click handler that calls toggleMenu
-// menuButton.addEventListener('click', () =>  toggleMenu());
+
+const menuClose = () => {
+ if (menu.style.display === 'block'){
+   menu.style.display = 'none';
+   menuButton.style.display = 'block';
+ }
+}
+
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.menu-button');
+const closeButton = document.querySelector('.close-button');
+
+menuButton.addEventListener('click', menuOpen);
+closeButton.addEventListener('click', menuClose);
