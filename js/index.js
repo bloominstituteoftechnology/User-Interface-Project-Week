e07dd64b0795cd78bb4e88735753fc3bc2b04c70
ChelseaTolnai <ChelseaTolnai@gmail.com>
2018-12-11 11:04:11 -0500

    Built navigation expandable menu.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// JS goes here
+// Home Page
+
+// Navigation
+
+class NavLink {
+    constructor (link) {
+        this.link = link;
+        this.link.addEventListener('click', toggleNav)
+    }
+}
+
+const toggleNav = () => {
+    navOpen.classList.toggle('nav-open-toggle');
+    navClose.classList.toggle('nav-close-toggle');
+    nav.classList.toggle('nav-toggle');
+}
+  
+const navOpen = document.querySelector('.nav-open');
+const navClose = document.querySelector('.nav-close');
+const nav = document.querySelector('.nav');
+const navLink = document.querySelectorAll('.nav-link');
+
+navOpen.addEventListener('click', toggleNav);
+navClose.addEventListener('click', toggleNav);
+navLink.forEach( navLink => new NavLink(navLink));
+
