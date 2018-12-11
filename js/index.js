bb9892095a18b76268ea86c6bbc4c98b0b2ee014
byronholmes2018 <lshopeful2018@mgmail.com>
2018-12-10 18:08:33 -0600

    Finished most of HTML/LESS for services.html. Not pixel perfect, but pixel-good-enough. Finished Javascript for tab navigation. Will clean up tab nav display.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // JS goes here
+
+//JS for expanded nav
+
 document.querySelector('.nav-btn').addEventListener('click', function(e){
   if(e.target.getAttribute("src")==="img/nav-hamburger-close.png"){
     document.querySelector('.menu-layover').style.display="none";
@@ -7,4 +10,40 @@ document.querySelector('.nav-btn').addEventListener('click', function(e){
     e.target.setAttribute("src", "img/nav-hamburger-close.png");
     document.querySelector('.menu-layover').style.display = "flex";
   }
-})
+});
+
+
+//JS for Services Tab navigatorTabs
+class Tab{
+  constructor(tab){
+    this.tab = tab;
+    this.data = this.tab.dataset.tab;
+    console.log(this.data);
+    this.content = new TabContent(document.querySelector(`.navContent[data-tab="${this.data}"]`));
+    this.tab.addEventListener('click', () => this.select());
+  }
+  select(){
+    Array.from(tabs).forEach(tab=>{
+      tab.classList.remove('selected');
+    });
+    this.tab.classList.add('selected');
+    this.content.showContent();
+  }
+}
+
+class TabContent{
+  constructor(tabContent){
+    this.tabContent = tabContent;
+  }
+
+  showContent(){
+    const tabContent = document.querySelectorAll('.navContent');
+    Array.from(tabContent).forEach(tabContent => tabContent.style.display = "none");
+    this.tabContent.style.display = "flex";
+  }
+}
+
+
+const tabs = document.querySelectorAll('.tab');
+console.log(tabs);
+tabs.forEach(tab => new Tab(tab));
