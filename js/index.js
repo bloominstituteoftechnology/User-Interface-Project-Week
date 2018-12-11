3cab67f75cbc6f864db91620790bb17ada80b408
Orlando Lopez <orlandolopez@Orlandos-MBP.fios-router.home>
2018-12-11 10:57:24 -0500

    added logic for services tab. Completed MVP specifications

--- js/index.js
+++ js/index.js
@@ -9,7 +9,42 @@ const toggleNav = () => {
     togglerClose.classList.toggle('hide');
     nav.classList.toggle('navigation--open');
 }
-console.log(nav)
 
 togglerOpen.onclick = () => toggleNav();
 togglerClose.onclick = () => toggleNav();
+
+// TABS LOGIC
+class initTab {
+    constructor(element) {
+        this.element = element;
+        this.service = JSON.stringify(this.element.getAttribute('service-tab'));
+        this.correspondingPanel = document.querySelector(`.services-section-main[service-tab=${this.service}]`);
+        this.panel = new ServicePanel(this.correspondingPanel);
+        this.element.addEventListener('click', () => this.tabSelected())
+    }
+    tabSelected() {
+        const serviceTabs = document.querySelectorAll('.tabs-holder__button');
+        serviceTabs.forEach(serTab => {
+            serTab.classList.remove('button--active');
+        })
+        this.element.classList.add('button--active');
+        this.panel.selected();
+    }
+}
+
+class ServicePanel {
+    constructor(element) {
+        this.panel = element;
+    }
+    selected() {
+        const panels = document.querySelectorAll('.services-section-main');
+        panels.forEach(panel => {
+            panel.classList.add('hideService');
+        })
+        this.panel.classList.remove('hideService');
+    }
+}
+
+
+const serviceTabs = document.querySelectorAll('.tabs-holder__button');
+serviceTabs.forEach(serTab => new initTab(serTab));
