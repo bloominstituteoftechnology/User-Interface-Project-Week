e028febb7ea64b048ea996667c14efa8b274bd44
ajfrierson <alvinfrierson@fuller.edu>
2018-12-11 12:18:37 -0800

    finished with CSS.

--- js/index.js
+++ js/index.js
@@ -1,149 +1,145 @@
 // JS goes here
 
-//project tab
 class TabItem {
-    constructor(element) {
-      this.element = element;
-    }
-  
-    select() {
-      // Select all items elements from the DOM
-      let tabItems = document.querySelectorAll('.project-detail');
-  
-      // Remove the class "project-detail-selected" from each element
-      Array.from(tabItems).forEach( el => {
-        el.classList.remove('project-detail-selected');
-      });
-      // Add a class named "project-detail-selected" to this element
-      this.element.classList.add('project-detail-selected'); 
-    }
+  constructor(element) {
+    this.element = element;
   }
 
-  
+  select() {
+    // Select all items elements from the DOM
+    let tabItems = document.querySelectorAll('.project-detail');
+
+    // Remove the class "project-detail-selected" from each element
+    Array.from(tabItems).forEach( el => {
+      el.classList.remove('project-detail-selected');
+    });
+    // Add a class named "project-detail-selected" to this element
+    this.element.classList.add('project-detail-selected'); 
+  }
+}
+
 class TabLink {
-    constructor(element) {
-      this.element = element;
-      
-      // Get the custom data attribute on the Link
-      this.data = element.dataset.tab;
-      
-      // Using the custom data attribute get the associated Item element
-      this.itemElement = document.querySelector(`.project-detail[data-tab="${this.data}"]`);
-      
-      // Using the Item element, create a new instance of the TabItem class
-      this.tabItem = new TabItem(this.itemElement);
-      
-      // Add a click event listener on this instance, calling the select method on click
-      this.element.addEventListener('click', () => { this.select() });
-    };
-  
-    select() {
-      // Get all of the elements with the tab class
-      const links = document.querySelectorAll('.tab');
-  
-      // Using a loop or the forEach method remove the 'tab-selected' class from all of the links
-      Array.from(links).forEach( el => {
-        el.classList.remove('tab-selected');
-      });
-  
-      // Add a class named "tab-selected" to this link
-      this.element.classList.add('tab-selected');
-      
-      // Call the select method on the item associated with this link
-      this.tabItem.select();
-    }
-  }
-  
-  let links = document.querySelectorAll('.tab');
-  links = Array.from(links).map( (tab) => new TabLink(tab));
-  links[0].select();
+  constructor(element) {
+    this.element = element;
+    
+    // Get the custom data attribute on the Link
+    this.data = element.dataset.tab;
+    
+    // Using the custom data attribute get the associated Item element
+    this.itemElement = document.querySelector(`.project-detail[data-tab="${this.data}"]`);
+    
+    // Using the Item element, create a new instance of the TabItem class
+    this.tabItem = new TabItem(this.itemElement);
+    
+    // Add a click event listener on this instance, calling the select method on click
+    this.element.addEventListener('click', () => { this.select() });
+  };
 
-  new Vue({
-    el: '#editor',
-    data: {
-      input: '*Hi!* You can write in **Markdown** 👇🏼 and it will be turned into styled text over there 👉🏼'
-    },
-    computed: {
-      compiledMarkdown: function () {
-        return marked(this.input, { sanitize: true })
-      }
-    },
-    methods: {
-      update: _.debounce(function (e) {
-        this.input = e.target.value
-      }, 300)
-    }
-  })
-  
-  /* Open */
-  function openNav() {
-    document.querySelector(".dropdown").style.height = "100%";
-  }
-  
-  /* Close */
-  function closeNav() {
-    document.querySelector(".dropdown").style.height = "0%";
-  }
-  // service tab
-  class TabItem {
-    constructor(element) {
-      this.element = element;
-    }
-  
-    select() {
-      // Select all items elements from the DOM
-      let tabItems = document.querySelectorAll('.service-detail');
-  
-      // Remove the class "service-detail-selected" from each element
-      Array.from(tabItems).forEach( el => {
-        el.classList.remove('service-detail-selected');
-      });
-      // Add a class named "service-detail-selected" to this element
-      this.element.classList.add('service-detail-selected'); 
-    }
-  }
-  
-  class TabLink {
-    constructor(element) {
-      this.element = element;
-      
-      // Get the custom data attribute on the Link
-      this.data = element.dataset.tab;
-      
-      // Using the custom data attribute get the associated Item element
-      this.itemElement = document.querySelector(`.service-detail[data-tab="${this.data}"]`);
-      
-      // Using the Item element, create a new instance of the TabItem class
-      this.tabItem = new TabItem(this.itemElement);
-      
-      // Add a click event listener on this instance, calling the select method on click
-      this.element.addEventListener('click', () => { this.select() });
-    };
-  
-    select() {
-      // Get all of the elements with the tabs class
-      const links = document.querySelectorAll('.tab');
-  
-      // Using a loop or the forEach method remove the 'tabs-selected' class from all of the links
-      Array.from(links).forEach( el => {
-        el.classList.remove('tab-selected');
-      });
-  
-      // Add a class named "tabs-selected" to this link
-      this.element.classList.add('tab-selected');
-      
-      // Call the select method on the item associated with this link
-      this.tabItem.select();
-    }
-  }
-  
-  let links = document.querySelectorAll('.tab');
-  links = Array.from(links).map( (tab) => new TabLink(tab));
-  links[0].select();
-  
-  //menu dropdown
+  select() {
+    // Get all of the elements with the tab class
+    const links = document.querySelectorAll('.tab');
 
-  
+    // Using a loop or the forEach method remove the 'tab-selected' class from all of the links
+    Array.from(links).forEach( el => {
+      el.classList.remove('tab-selected');
+    });
+
+    // Add a class named "tab-selected" to this link
+    this.element.classList.add('tab-selected');
+    
+    // Call the select method on the item associated with this link
+    this.tabItem.select();
+  }
+}
+
+let links = document.querySelectorAll('.tab');
+links = Array.from(links).map( (tab) => new TabLink(tab));
+links[0].select();
+
+new Vue({
+  el: '#editor',
+  data: {
+    input: '*Hi!* You can write in **Markdown** 👇🏼 and it will be turned into styled text over there 👉🏼'
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input, { sanitize: true })
+    }
+  },
+  methods: {
+    update: _.debounce(function (e) {
+      this.input = e.target.value
+    }, 300)
+  }
+})
+
+/* Open */
+function openNav() {
+  document.querySelector(".dropdown").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.querySelector(".dropdown").style.height = "0%";
+}
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    // Select all items elements from the DOM
+    let tabItems = document.querySelectorAll('.service-detail');
+
+    // Remove the class "service-detail-selected" from each element
+    Array.from(tabItems).forEach( el => {
+      el.classList.remove('service-detail-selected');
+    });
+    // Add a class named "service-detail-selected" to this element
+    this.element.classList.add('service-detail-selected'); 
+  }
+}
+
+class TabLink {
+  constructor(element) {
+    this.element = element;
+    
+    // Get the custom data attribute on the Link
+    this.data = element.dataset.tab;
+    
+    // Using the custom data attribute get the associated Item element
+    this.itemElement = document.querySelector(`.service-detail[data-tab="${this.data}"]`);
+    
+    // Using the Item element, create a new instance of the TabItem class
+    this.tabItem = new TabItem(this.itemElement);
+    
+    // Add a click event listener on this instance, calling the select method on click
+    this.element.addEventListener('click', () => { this.select() });
+  };
+
+  select() {
+    // Get all of the elements with the tabs class
+    const links = document.querySelectorAll('.tab');
+
+    // Using a loop or the forEach method remove the 'tabs-selected' class from all of the links
+    Array.from(links).forEach( el => {
+      el.classList.remove('tab-selected');
+    });
+
+    // Add a class named "tabs-selected" to this link
+    this.element.classList.add('tab-selected');
+    
+    // Call the select method on the item associated with this link
+    this.tabItem.select();
+  }
+}
+
+let links = document.querySelectorAll('.tab');
+links = Array.from(links).map( (tab) => new TabLink(tab));
+links[0].select();
+
+
 class Dropdown {
     constructor(element) {
       // assign this.element to the dropdown element
@@ -176,5 +172,3 @@ class Dropdown {
   let dropdowns = document.querySelectorAll('header');
   dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
 
-  
-  
