4ccbff4c6fa5d5778b24615c907234ec22404218
MichaelNey <ney.quentin.michael@gmail.com>
2018-12-11 13:37:31 -0800

    Carousel constructor created

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,18 @@
 class Carousel {
     constructor() {
+        //Get our carousel element
         this.carousel = document.querySelector('.carousel');
+        //Get our carousel left and right buttons
+        this.leftBtn = this.carousel.querySelector('.left');
+        this.rightBtn = this.carousel.querySelector('.right');
+        //Add event listeners to our left and right buttons
+        this.leftBtn.addEventListener('click', () => this.moveLeft());
+        this.rightBtn.addEventListener('click', () => this.moveRight());
+        //Retrieve all of our images/slides for desktop and mobile view. As we want the carousel to use images based on size to keep it mobile responsive.
+        this.desktopSlides = this.carousel.querySelectorAll('.desktop-slides .slide');
+        this.mobileSlides = this.carousel.querySelectorAll('.mobile-slides .slide');
     }
 }
+
+//Create instance of Carousel
+let pageCarousel = new Carousel();
