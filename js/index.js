1aff6e60d59d891adc1756e8141688e535ff2dc8
BrannanC <brannanconrad@gmail.com>
2018-12-11 15:46:39 -0600

    Technically done. Need to tweek styles and pixels before working on stretch

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+class TabLink {
+    constructor(element){
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.content = document.querySelector(`.tab-content[data-tab="${this.data}"`);
+        this.element.addEventListener('click', () => this.select());
+    }
+    select(){
+        const links = document.querySelectorAll('.tab-link');
+        links.forEach(el => el.classList.remove('tab-link-selected'));
+        this.element.classList.add('tab-link-selected');
+
+        const allContent = document.querySelectorAll('.tab-content');
+        allContent.forEach(el => el.classList.add('display-none'));
+        this.content.classList.remove('display-none');
+    }
+}
+
 const navigation = document.querySelector('.nav-open');
 const navButton = document.querySelector('.nav-button');
 const bigX = document.querySelector('.big-x');
@@ -6,12 +24,13 @@ navButton.addEventListener('click', function(){
     navigation.classList.toggle('display-hidden');
     navButton.classList.toggle('display-hidden');
     bigX.classList.toggle('display-hidden');
-    console.log('navB');
 });
 
 bigX.addEventListener('click', function(){
     navigation.classList.toggle('display-hidden');
     navButton.classList.toggle('display-hidden');
     bigX.classList.toggle('display-hidden');
-
 });
+
+tabLink = document.querySelectorAll('.tab-link');
+tabLink.forEach(x => new TabLink(x));
