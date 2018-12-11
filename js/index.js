15ec7c755e2716a8b3299fdd40b8a6a34110aee6
Angel Torres <alas4224@gmail.com>
2018-12-11 07:24:06 -0600

    Finished working on nav bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // JS goes here
+
+document.querySelector(".hamburger").addEventListener("click", function() {
+    document.querySelector(".navbar").classList.toggle("expanded-navbar")
+})
+
+document.querySelector(".hamburger-close").addEventListener("click", function() {
+    document.querySelector(".navbar").classList.toggle("expanded-navbar")
+})
+
+class Tab {
+    constructor(element) {
+        this.element = element;
+        console.log(this.element)
+
+    }
+}
+
+document.querySelectorAll(".tab").forEach((element) => new Tab(element));
