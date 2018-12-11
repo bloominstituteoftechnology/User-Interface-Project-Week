e6c665f70e5d920c6ee6890dceb9e572c32e72f4
yannafaith <yannafaith97@gmail.com>
2018-12-11 14:28:28 -0700

    toggled navbar display

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 // JS goes here
 
+const headerButton = document.querySelector('#header-button');
+
+const dropMenu = document.querySelector('.drop-menu');
+
+
+onClick = () => {
+    dropMenu.classList.toggle('inactive')
+}
+
+
+headerButton.addEventListener('click', onClick);
+
