b020d30cbcf0527c5b93126331c473109c1ef8a4
joepound <14357797+joepound@users.noreply.github.com>
2018-12-12 09:24:32 -0700

    Added expanded nav menu (with tablet & mobile views; with fade in/out animations)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
-// JS goes here
+// QUERY SELECTORS FOR BUTTONS
+// ===========================
+
+const navTopBarMenuButton = document.querySelector(".nav__top-bar__menu-button");
+
+const navMenuCloseButton = document.querySelector(".nav__menu__top-area__close-button");
+const navMenuLinks = document.querySelectorAll(".nav__menu__links__item");
+
+
+// FUNCTIONS TO BE USED WITH EVENT LISTENERS
+// =========================================
+
+function openNavMenu(e) {
+    const navMenuArea = document.querySelector(".nav__menu");
+    navMenuArea.classList.remove("nav__menu--closed");
+
+    TweenMax.fromTo(navMenuArea, 0.5, {
+        opacity: 0
+    }, {
+        opacity: 0.85
+    });
+}
+
+function closeNavMenu(e) {
+    const navMenuArea = document.querySelector(".nav__menu");
+    TweenMax.fromTo(navMenuArea, 0.5, {
+        opacity: 0.85
+    }, {
+        opacity: 0,
+        onComplete: () => navMenuArea.classList.add("nav__menu--closed")
+    });
+}
+
+
+// EVENT LISTENERS
+// ===============
+
+// Open menu with top nav menu button
+navTopBarMenuButton.addEventListener("click", e => openNavMenu(e));
+
+// Close menu with top area menu button or nav menu links
+navMenuCloseButton.addEventListener("click", e => closeNavMenu(e));
+navMenuLinks.forEach(link => link.addEventListener("click", e => closeNavMenu(e)))
