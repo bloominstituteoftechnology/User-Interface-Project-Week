1f64445730626ae3a001bfa80b34d9b8001aa237
mcferrenm <mcferrenmax@gmail.com>
2018-12-12 13:27:03 -0500

    add Recent Projects component

--- react.js
+++ react.js
@@ -18,7 +18,6 @@ var IntroSection = function (_React$Component) {
   _createClass(IntroSection, [{
     key: "render",
     value: function render() {
-
       return this.props.data.map(function (item) {
         return React.createElement(
           "section",
@@ -56,35 +55,123 @@ var IntroSection = function (_React$Component) {
   return IntroSection;
 }(React.Component);
 
-var App = function (_React$Component2) {
-  _inherits(App, _React$Component2);
+;
+
+var RecentProject = function (_React$Component2) {
+  _inherits(RecentProject, _React$Component2);
+
+  function RecentProject(props) {
+    _classCallCheck(this, RecentProject);
+
+    return _possibleConstructorReturn(this, (RecentProject.__proto__ || Object.getPrototypeOf(RecentProject)).call(this, props));
+  }
+
+  _createClass(RecentProject, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "section",
+        { className: "recent-projects" },
+        this.props.data.map(function (item) {
+          return React.createElement(
+            "div",
+            { className: "project-item" },
+            React.createElement(
+              "div",
+              { className: "project-header " + item.style },
+              React.createElement("img", { className: "desktop", src: item.desktopImgSrc }),
+              React.createElement("img", { className: "mobile", src: item.mobileImgSrc }),
+              React.createElement(
+                "h3",
+                null,
+                item.name
+              )
+            ),
+            React.createElement(
+              "div",
+              { className: "project-text" },
+              React.createElement(
+                "p",
+                null,
+                item.content
+              ),
+              React.createElement(
+                "p",
+                null,
+                "Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line."
+              )
+            )
+          );
+        })
+      );
+    }
+  }]);
+
+  return RecentProject;
+}(React.Component);
+
+;
+
+var App = function (_React$Component3) {
+  _inherits(App, _React$Component3);
 
   function App(props) {
     _classCallCheck(this, App);
 
-    var _this2 = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+    var _this3 = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
-    _this2.state = [{
-      name: 'Smith & Jones Architects',
-      content: 'Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea. Eu timeam vidisse consectetuer sed. Duo etiam laboramus dissentiet in, nec no errem',
-      desktopImgSrc: 'img/home/home-img-1.png',
-      mobileImgSrc: 'img/home/home-mobile-img-1.png',
-      style: null
-    }, {
-      name: 'Futuristic Designs',
-      content: 'Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea. Eu timeam vidisse consectetuer sed. Duo etiam laboramus dissentiet in, nec no errem',
-      desktopImgSrc: 'img/home/home-img-2.png',
-      mobileImgSrc: 'img/home/home-mobile-img-2.png',
-      style: 'designs'
-    }];
-
-    return _this2;
+    _this3.state = {
+      introData: [{
+        name: 'Smith & Jones Architects',
+        content: 'Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea. Eu timeam vidisse consectetuer sed. Duo etiam laboramus dissentiet in, nec no errem',
+        desktopImgSrc: 'img/home/home-img-1.png',
+        mobileImgSrc: 'img/home/home-mobile-img-1.png',
+        style: null
+      }, {
+        name: 'Futuristic Designs',
+        content: 'Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea. Eu timeam vidisse consectetuer sed. Duo etiam laboramus dissentiet in, nec no errem',
+        desktopImgSrc: 'img/home/home-img-2.png',
+        mobileImgSrc: 'img/home/home-mobile-img-2.png',
+        style: 'designs'
+      }, {
+        name: 'Critical Acclaim',
+        content: 'Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea. Eu timeam vidisse consectetuer sed. Duo etiam laboramus dissentiet in, nec no errem',
+        desktopImgSrc: 'img/home/home-img-2.png',
+        mobileImgSrc: 'img/home/home-mobile-img-2.png',
+        style: null
+      }],
+      recentProjectsData: [{
+        name: 'THE VILLAS',
+        content: 'The Villas bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution.',
+        desktopImgSrc: 'img/home/home-villas-img.png',
+        mobileImgSrc: 'img/home/home-mobile-villas-img.png',
+        style: 'right'
+      }, {
+        name: 'OUTSKIRTS',
+        content: 'The Outskirts are amazing to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution.',
+        desktopImgSrc: 'img/home/home-outskirts-img.png',
+        mobileImgSrc: 'img/home/home-mobile-outskirts-img.png',
+        style: 'left'
+      }, {
+        name: 'THE BLOCKS',
+        content: 'The Blocks are amazing to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution.',
+        desktopImgSrc: 'img/home/home-the-blocks-img.png',
+        mobileImgSrc: 'img/home/home-mobile-the-blocks-img.png',
+        style: 'right'
+      }]
+    };
+    return _this3;
   }
 
   _createClass(App, [{
     key: "render",
     value: function render() {
-      return React.createElement(IntroSection, { data: this.state });
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(IntroSection, { data: this.state.introData }),
+        React.createElement(RecentProject, { data: this.state.recentProjectsData })
+      );
     }
   }]);
 
