ba1288d8811f1a8d1785b888861c80811b6634f8
Nathan Thomas <nwthomas@me.com>
2018-12-12 13:38:37 -0800

    Create aboutus.html projects.html and carousel

--- components/Navigation/navigation.js
+++ components/Navigation/navigation.js
@@ -1,3 +1,4 @@
+const navContainer = document.querySelector(".navbar__container");
 const navMenu = document.querySelector(".nav__menu");
 const navText = document.querySelector(".menu__placement");
 const burger = document.querySelector(".burger");
@@ -7,12 +8,16 @@ burger.addEventListener("click", () => {
   if (navMenu.classList.contains("nav__menu--open")) {
     TweenMax.to(navMenu, 1, { className: "-=nav__menu--open" });
     navText.classList.remove("menu__placement--open");
+    navText.classList.add("menu__placement--closed");
     burger.src = "./img/nav-hamburger.png";
     body.classList.toggle("stop__scrolling");
+    navContainer.style.backgroundColor = "#8f9ba7";
   } else {
     TweenMax.to(navMenu, 1, { className: "+=nav__menu--open" });
     navText.classList.add("menu__placement--open");
+    navText.classList.remove("menu__placement--closed");
     burger.src = "./img/nav-hamburger-close.png";
     body.classList.toggle("stop__scrolling");
+    navContainer.style.backgroundColor = "transparent";
   }
 });
