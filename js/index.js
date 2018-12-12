e89a702f536192ce29c264cfa2f7400fbba8d319
rosilioroman <rosilioroman@gmail.com>
2018-12-12 14:40:42 -0700

    Added functionality to the navigation overlay. Changed HTML structure for overlay.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // JS goes here
+
+let overlay = document.querySelector('.overlay');
+let navBurger = document.querySelector('.menu-burger');
+let navCloseBtn = document.querySelector('.menu-close');
+
+function openNav() {
+    overlay.style.display="block";
+    console.log('burger clicked');
+}
+
+function closeNav() {
+    overlay.style.display="none";
+    console.log('x clicked');
+}
+
+navBurger.addEventListener('click', () => openNav());
+navCloseBtn.addEventListener('click', () => closeNav());
