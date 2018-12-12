a8a06342aae933aa4f03bb21e39a359ab587aab6
Nathan Thomas <nwthomas@me.com>
2018-12-12 12:15:44 -0800

    Make navbar menu slide open on hamburger click

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-const navbar = document.querySelector(".navbar__container");
-const navbarText = document.querySelector(".nav__menu");
+const navMenu = document.querySelector(".nav__menu");
+const navText = document.querySelector(".menu__placement");
 const burger = document.querySelector(".burger");
 const body = document.querySelector("body");
 
 burger.addEventListener("click", () => {
-  if (navbar.classList.contains("navbar--open")) {
-    TweenMax.to(navbar, 1, { className: "-=navbar--open" });
-    TweenMax.to(navbarText, 1, { className: "-=nav__menu--open" });
+  if (navMenu.classList.contains("nav__menu--open")) {
+    TweenMax.to(navMenu, 1, { className: "-=nav__menu--open" });
+    navText.classList.remove("menu__placement--open");
     burger.src = "./img/nav-hamburger.png";
     body.classList.toggle("stop__scrolling");
   } else {
-    TweenMax.to(navbar, 1, { className: "+=navbar--open" });
-    TweenMax.to(navbarText, 1, { className: "+=nav__menu--open" });
+    TweenMax.to(navMenu, 1, { className: "+=nav__menu--open" });
+    navText.classList.add("menu__placement--open");
     burger.src = "./img/nav-hamburger-close.png";
     body.classList.toggle("stop__scrolling");
   }
