12393db6da3e701fa82dc2991edf95dc055a9b21
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2018-12-12 13:38:13 -0600

    starting on nav toggle

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // JS goes here
+// Nav
+
+class NavLink {
+    constructor(link) {
+        this.link = link;
+        this.link.addEventListener('click', toggleNav);
+    }
+}
+
+const toggleNav = () => {
+    navOpen.classList.toggle('nav-open-toggle');
+    navClose.classList.toggle('nav-close-toggle');
+    nav.classList.toggle('nav-toggle');
+}
+
+
+const navOpen = document.querySelector('.nav-open');
+const navClose = document.querySelector('.nav-close');
+const nav = document.querySelector('.nav');
+const navLink = document.querySelectorAll('.nav-link');
+
+navOpen.addEventListener('click', toggleNav);
+navClose.addEventListener('click', toggleNav);
+
+
+navLink.forEach( navLink => newNavLink(navLink));
