229300b3a5c1df9f88d53c5b0068606e2cef960c
KieranVieira <kieranvieira@live.com>
2018-12-12 11:30:15 -0500

    Added custom headers for about us and projects, implemented carousel, removed visual scrollmagic triggers

--- js/index.js
+++ js/index.js
@@ -45,3 +45,37 @@ class TabItem {
 }
 
 const tabLinks = document.querySelectorAll('.tablink').forEach( link => new TabLink(link));
+
+//Carousel Projects
+
+class Carousel {
+    constructor(carousel){
+        this.element = carousel;
+        this.currentIndex = 1;
+        this.imageAmount = this.element.querySelectorAll('.slider').length;
+        this.leftBTN = this.element.querySelector('.left-button');
+        this.rightBTN = this.element.querySelector('.right-button');
+        this.leftBTN.addEventListener('click', () => this.left())
+        this.rightBTN.addEventListener('click', () => this.right())
+    }
+    right(){
+        this.currentIndex +=1;
+        if(this.currentIndex >= this.imageAmount+1){
+            this.currentIndex = 1
+        }
+        this.element.querySelectorAll('.slider').forEach((cv)=>{cv.classList.toggle('active-slider',false)})
+        this.element.querySelector(`.slider[data-position='${this.currentIndex}']`).classList.toggle('active-slider',true)
+    }
+    left(){
+        this.currentIndex -=1;
+        if(this.currentIndex <= 0){
+            this.currentIndex = this.imageAmount;
+        }
+        this.element.querySelectorAll('.slider').forEach((cv)=>{cv.classList.toggle('active-slider',false)})
+        this.element.querySelector(`.slider[data-position='${this.currentIndex}']`).classList.toggle('active-slider',true)
+    }
+}
+
+let carousel = document.querySelector('.carousel');
+
+new Carousel(carousel)
--- js/scroll.js
+++ js/scroll.js
@@ -4,26 +4,22 @@ const firstItem = new ScrollMagic.Scene({
     triggerElement: "#item1",
 })
 .setClassToggle('.item', 'fade-in')
-.addIndicators({name: "first trigger"})
 .addTo(controller);
 
 const secondItem = new ScrollMagic.Scene({
     triggerElement: "#item2",
 })
 .setClassToggle('#item2', 'fade-in')
-.addIndicators({name: "seccond trigger"})
 .addTo(controller);
 
 const thirdItem = new ScrollMagic.Scene({
     triggerElement: "#item3",
 })
 .setClassToggle('#item3', 'fade-in')
-.addIndicators({name: "third trigger"})
 .addTo(controller);
 
 const fourthItem = new ScrollMagic.Scene({
     triggerElement: "#item4",
 })
 .setClassToggle('#item4', 'fade-in')
-.addIndicators({name: "fourth trigger"})
 .addTo(controller);
