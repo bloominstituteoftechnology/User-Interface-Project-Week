1d4205cb5dea19f8dce6ea150f37e39c0f157882
ltrii <lelandtr@gmail.com>
2018-12-12 15:21:05 -0800

    Nav work

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,16 @@
+class Navbar {
+    constructor() {
+      this.expandButton = document.querySelector('.menubutton');
+      this.expandButton.addEventListener('click', (e) => this.expandNav(e));
+    }
+  
+    expandNav() {
+      navbar.classList.toggle('nav-open');
+    }
+  }
+  
+const navbar = document.querySelectorAll('.mainnav');
 
-window.onload = function() {
-    document.querySelector('.menu-button').addEventListener('click', () => {
-    toggleClass(document.getElementById('.mainnav'), 'nav-open');
-});
-}
+navbar.forEach( () => {
+  return new Navbar();
+})
