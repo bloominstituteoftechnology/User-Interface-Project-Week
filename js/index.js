87797c08a42e6299fed7d60a0d97169f6726524d
EJaona <ejaona@gmail.com>
2018-12-12 10:00:39 -0600

    linked services page to the menu, started styling services page

--- js/index.js
+++ js/index.js
@@ -7,3 +7,14 @@ menuBtn.addEventListener("click", () => {
   menu.forEach(link => link.classList.toggle("close"));
   menuBtn.classList.toggle("close");
 });
+
+// ==================== Services ==========================
+class Tab {
+  constructor(tab) {
+    this.tab = tab;
+    this.tabData = this.tab.dataSet;
+    console.log(this.tabData);
+  }
+}
+
+const tabs = document.querySelectorAll(".tab").forEach(tab => new Tab(tab));
