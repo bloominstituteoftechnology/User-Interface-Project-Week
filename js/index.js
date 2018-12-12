44dea4f28e74db9ed6d6652d8e8046c8f13caace
Jeff15113 <itsmejeffery@gmail.com>
2018-12-12 15:30:37 -0500

    created header script

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-// JS goes here
+let toggled = 0;
+
+$(document).ready(() => {
+	$("header").load("../navbar.html", () => {
+		$("nav").hide();
+		document.querySelectorAll("nav a").forEach(data => {
+			if (data.lastChild.textContent === document.title) {
+				data.classList.add("active");
+				data.removeAttribute("href");
+			}
+		});
+		document.querySelector(".menu").addEventListener("click", event => {
+			$("nav").slideToggle();
+
+			if (toggled) {
+				event.target.setAttribute("src", "./img/nav-hamburger.png");
+				toggled = 0;
+			} else {
+				event.target.setAttribute("src", "./img/nav-hamburger-close.png");
+				toggled = 1;
+			}
+			event.preventDefault();
+		});
+	});
+});
