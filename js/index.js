a89b269a34a921ee96c07df15a8785ce15dd54b1
john2796 <benedictmiranda27@yahoo.com>
2018-12-11 20:44:40 -0800

    added animation on home page🔥

--- js/index.js
+++ js/index.js
@@ -3,14 +3,9 @@ class TabLink {
     this.element = element;
     this.data = this.element.dataset.tab;
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`)
-    //console.log(this.itemElement);
     this.tabItem = new TabItem(this.itemElement);
-
-    // get links and item 
     this.links = document.querySelectorAll(".tabs-link");
     this.tabItems = document.querySelectorAll('.tabs-item');
-
-    // add click event to all links
     this.element.addEventListener('click', () => this.select(this.links));
   };
 
@@ -23,14 +18,11 @@ class TabLink {
   }
 }
 
-
-
 class TabItem {
   constructor(element) {
     this.element = element;
   }
   select(tabItems) {
-    // const tabItems = document.querySelectorAll('.tabs-item');
     tabItems.forEach(item => {
       item.classList.remove('tabs-item-selected')
     })
@@ -40,10 +32,6 @@ class TabItem {
 
 
 links = document.querySelectorAll('.tabs-link').forEach((link) => new TabLink(link))
-
-
-
-// JS goes here
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('nav');
 hamburger.addEventListener('click', function () {
@@ -56,9 +44,6 @@ hamburger.addEventListener('click', function () {
     disableScroll();
   }
 })
-
-
-
 
 function preventDefault(e) {
   e = e || window.event;
@@ -75,11 +60,11 @@ function preventDefaultForScrollKeys(e) {
 }
 
 function disableScroll() {
-  if (window.addEventListener) // older FF
+  if (window.addEventListener)
     window.addEventListener('DOMMouseScroll', preventDefault, false);
-  window.onwheel = preventDefault; // modern standard
-  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-  window.ontouchmove = preventDefault; // mobile
+  window.onwheel = preventDefault;
+  window.onmousewheel = document.onmousewheel = preventDefault; browsers, IE
+  window.ontouchmove = preventDefault;
   document.onkeydown = preventDefaultForScrollKeys;
 }
 
@@ -93,10 +78,9 @@ function enableScroll() {
 }
 
 
-// animtaed button 
 let animateButton = function (e) {
   e.preventDefault;
-  //reset animation
+
   e.target.classList.remove("animate");
 
   e.target.classList.add("animate");
@@ -114,7 +98,6 @@ for (let i = 0; i < bubblyButtons.length; i++) {
 
 
 
-// carousel
 
 const SHOWING_CLASS = "showing";
 const firstSlide = document.querySelector('.slider__item:first-child');
@@ -122,14 +105,10 @@ const lastChild = document.querySelector('.slider__item:last-child');
 const next = document.querySelector('.slider_right_btn');
 const previous = document.querySelector('.slider_left_btn')
 
-// slide function 
 function slide() {
   const currentSlide = document.querySelector(`.${SHOWING_CLASS}`)
-  // if currentSlide is true and it has showing class then remove it and passed it to the next element
   if (currentSlide) {
-    //console.log(currentSlide);
     currentSlide.classList.remove(SHOWING_CLASS);
-    // if current slide is true continue with if statement get the next slide and check if next slide has the ".class" showing if true add the class and keep continue with the loop if not add the class to the first you want to set else to the first slide so if you keep clicking next button it would go back to the first item so it makes a full rotation same thing with the prev button
     const nextSlide = currentSlide.nextElementSibling;
     if (nextSlide) {
       nextSlide.classList.add(SHOWING_CLASS);
@@ -137,28 +116,22 @@ function slide() {
       firstSlide.classList.add(SHOWING_CLASS);
     }
   } else {
-    // if currentSlide false don't have showing class then add it to the first child element
+
     firstSlide.classList.add('showing');
   }
 }
-// need this so you don't have to wait 3seconds before first image shows
 slide();
 
-//next button
-//console.log(next);
 next.addEventListener('click', () => {
   slide();
 })
 
-//previous button
-//console.log(previous);
 previous.addEventListener('click', () => {
   const currentSlide = document.querySelector(`.${SHOWING_CLASS}`)
-  // if currentSlide is true and it has ".showing" class then remove it 
+
   if (currentSlide) {
-    //console.log(currentSlide);
+
     currentSlide.classList.remove(SHOWING_CLASS);
-    // if current slide is true continue with if statement get the next slide and check if next slide has the class showing if true add the class and keep continue with the loop if not add the class to the first ...you want to set else to the last slide so if you keep clicking prev button it would go back to the last item so it makes a full rotation same thing with the next button
     const prevSlide = currentSlide.previousElementSibling;
     if (prevSlide) {
       prevSlide.classList.add(SHOWING_CLASS);
@@ -166,26 +139,13 @@ previous.addEventListener('click', () => {
       lastChild.classList.add(SHOWING_CLASS);
     }
   } else {
-    // if currentSlide false don't have showing class then add it to the first child element
     firstSlide.classList.add('showing');
   }
 })
 
 
-// every 3 seconds change the image ; 
-console.log(setInterval(slide, 4000));
+setInterval(slide, 10000);
 
 
-
-// let controller = new ScrollMagic.Controller();
-
-
-
-// let scene = new ScrollMagic.Scene({
-//   triggerElement: "#trigger1"
-// })
-//   .setTween("#animate1", 0.5, { backgroundColor: "green", scale: 2.5 }) // trigger a TweenMax.to tween
-//   .addIndicators({ name: "1 (duration: 0)" }) // add indicators (requires plugin)
-//   .addTo(controller);
 
 
