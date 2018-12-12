b645e5a78f5d8f8a970e1406d0271d79ede8063b
RachelKolk <rach.m.obrien@gmail.com>
2018-12-11 19:51:49 -0500

    double checked output against style guide

--- js/nav-menu-dropdown.js
+++ js/nav-menu-dropdown.js
@@ -1,33 +1,33 @@
-// class DropdownMenu {
-//     constructor(element) {
+class DropdownMenu {
+    constructor(element) {
       
-//       this.element = element;
+      this.element = element;
       
-//       this.button = this.element.querySelector('.dropdown-button');      
+      this.button = this.element.querySelector('.dropdown-button');      
       
-//       this.content = this.element.querySelector('.dropdown-content');
+      this.content = this.element.querySelector('.dropdown-content');
   
       
-//       this.button.addEventListener('click', () => {this.expandMenu()});
+      this.button.addEventListener('click', () => {this.expandMenu()});
   
-//     }
+    }
   
-//     expandMenu() {
+    expandMenu() {
        
-//         console.log('Expand nav menu button clicked.');
-//         this.element.classList.toggle('dropdown-hidden');
+        console.log('Expand nav menu button clicked.');
+        this.element.classList.toggle('dropdown-hidden');
         
-//         this.closeButton = this.domElement.querySelector('.close-dropdown');
+        this.closeButton = this.domElement.querySelector('.close-dropdown');
         
-//         this.closeButton.addEventListener('click', () => this.closeArticle());
+        this.closeButton.addEventListener('click', () => this.closeArticle());
         
-//     }
+    }
 
-//     closeMenu() {
-//         console.log('Nav menu closed.');
+    closeMenu() {
+        console.log('Nav menu closed.');
         
-//         this.domElement.classList.toggle('.nav-bar');
-//       }
+        this.domElement.classList.toggle('.nav-bar');
+      }
   
   
-//   let dropdowns = document.querySelectorAll('.nav-dropdown').forEach( dropdown => new DropdownMenu(dropdown));
+  let dropdowns = document.querySelectorAll('.nav-dropdown').forEach( dropdown => new DropdownMenu(dropdown));
