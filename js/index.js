3b78d0e91058cb49d51589595cb5b7a2aa6f5f1f
stellarluminant <stellarluminant@gmail.com>
2018-12-12 13:30:03 -0800

    Add tab navigator functionality
    Wow, formatting really is my weak point. This part was a breeze.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // JS goes here
+
+class TabsLink {
+    constructor (element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.content = document.querySelector(`.tabs-content-box[data-tab='${this.data}']`);
+        this.element.addEventListener('click', _ => this.switchTab());
+    }
+
+    switchTab () {
+        document.querySelectorAll('.tabs-link').forEach(elem => elem.classList.remove('tabs-link-selected'));
+        this.element.classList.add('tabs-link-selected');
+        document.querySelectorAll('.tabs-content-box').forEach(elem => elem.classList.remove('tabs-content-selected'));
+        this.content.classList.add('tabs-content-selected');
+    }
+}
+
+class TabsComponent {
+    constructor (element) {
+        this.element = element;
+        this.tabsLinks = element.querySelectorAll('.tabs-link');
+        this.tabsLinks.forEach(elem => new TabsLink(elem));
+    }
+}
+
+document.querySelectorAll('.tabs-component').forEach(elem => new TabsComponent(elem));
