5fa8fff8d738404c5968e84200c87eded8e4a884
rmejia-tajc <rmejia.tajc@gmail.com>
2018-12-12 16:52:55 -0700

    add js to tabs protion of services page

--- js/index.js
+++ js/index.js
@@ -14,3 +14,38 @@ navClose.addEventListener("click", function() {
     bigNav.classList.toggle("hide");
     smallNav.classList.toggle("hide");    
 })
+
+
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+        this.tabItem = new TabItem(this.itemElement);
+        this.element.addEventListener("click", () => {this.select()});
+    };
+
+    select() {
+        const links = document.querySelectorAll(".tabs-link");
+        Array.from(links).forEach(link => link.classList.remove("tabs-link-selected"));
+        this.element.classList.add("tabs-link-selected");
+        this.tabItem.select();
+    }
+}
+
+class TabItem {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select() {
+        const items = document.querySelectorAll(".tabs-item");
+        Array.from(items).forEach(item => item.classList.remove("tabs-item-selected"));
+        this.element.classList.add("tabs-item-selected");
+    }
+}
+
+
+
+
+const links = document.querySelectorAll(".tabs-link").forEach(link => new TabLink(link));
