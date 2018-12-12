ec051d5e6fe6f541e63e501063345e5ab8839872
McNultyPT <mcnultypt@gmail.com>
2018-12-12 12:07:29 -0800

    MVP tasks complete

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// ============================== Navbar
+
 const toggleMenu = () => {
     menu.style.display = 'block';
     menuButton.style.display = 'none';
@@ -19,3 +21,37 @@ const menuX = document.querySelector('.menu-x');
 menuButton.addEventListener('click', () => toggleMenu());
 
 menuX.addEventListener('click', () => toggleMenuX());
+
+// ============================== Services Tabs
+
+class TabLink {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.cards = document.querySelector(`.card[data-tab='${this.data}']`);
+        this.cards = new TabCard(this.cards);
+        this.element.addEventListener('click', () => {this.select()});
+    }
+
+    select() {
+        const tab = document.querySelectorAll('.tab');
+        Array.from(tab).forEach( tabs => tabs.classList.remove('active-tab'));
+        this.element.classList.add('active-tab');
+        this.cards.selectCard();
+    }
+}
+
+class TabCard {
+    constructor(element) {
+        this.element = element;
+    }
+    selectCard() {
+        const card = document.querySelectorAll('.card');
+        Array.from(card).forEach( card => card.classList.remove('active-card'));
+        this.element.classList.add('active-card');
+    }
+}
+
+tabs = document.querySelectorAll('.tab');
+
+tabs.forEach( element => new TabLink(element));
