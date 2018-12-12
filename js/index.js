54687d621b91a84014bfc7f8d896072d53227c6e
Imon Ovbude <imonovbude@gmail.com>
2018-12-12 08:49:08 -0800

    modified header nav and created toggle function for menu drop down content

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
 // JS goes here
+const toggleMenu = () => {
+    if (!dropDownContent.classList.contains('dropdownOpen')) {
+        dropDownContent.classList.toggle('dropdownOpen');
+        dropDownContent.style.display = 'block';
+        console.log('click')
+    } else {
+        dropdownContent.classList.toggle('dropdownOpen');
+        dropdownContent.style.display = 'none';
+        console.log('unclick');
+    }
+}
 
+// reference to drop down content element
+const dropDownContent = document.querySelector('.dropdownContent');
 
+// reference to drop down button element
+const dropDownButton = document.querySelector('.dropdownButton');
 
-
-
-
+// drop down button event listener
+dropDownButton.addEventListener('click', (e) => {
+    toggleMenu();
+})
 
 
 
@@ -16,20 +32,20 @@
 
 
 // Tab Section For Services Page
-class TabLink {
-    constructor(elem) {
-        // Individual Tabs
-        this.elem = elem;
+// class TabLink {
+//     constructor(elem) {
+//         // Individual Tabs
+//         this.elem = elem;
 
-        // Data Attribute
-        this.data = this.elem.dataset.tab;
+//         // Data Attribute
+//         this.data = this.elem.dataset.tab;
 
-        // Item Selector
-        this.itemElem = document.querySelector(`.tabs-item[data-tab = '${this.data}']`);
+//         // Item Selector
+//         this.itemElem = document.querySelector(`.tabs-item[data-tab = '${this.data}']`);
 
-    }
-}
+//     }
+// }
 
 
-const links = document.querySelectorAll('.tabs-link').forEach(tab => new TabLink(tab));
+// const links = document.querySelectorAll('.tabs-link').forEach(tab => new TabLink(tab));
 
