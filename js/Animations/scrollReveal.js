da4e231e8944b00d85236ff1914d41c5f9714464
leiannel <leilouis112@gmail.com>
2018-12-12 16:57:31 -0500

    added animations using greensock and scrollReveal

---
+++ js/Animations/scrollReveal.js
@@ -0,0 +1,18 @@
+ScrollReveal({ reset: true,  duration: 900, }).reveal('.project');
+ScrollReveal({ reset: true,  duration: 900, }).reveal('.middle-content--1');
+ScrollReveal({ reset: true,  duration: 900, }).reveal('.middle-content--2');
+
+TweenLite.from('.main-content-heading', .5, {
+    opacity: 0,  
+    ease: Power4.easeIn
+  });
+
+TweenLite.from('.services-heading', .5, {
+    opacity: 0,  
+    ease: Power4.easeIn
+});
+
+TweenLite.from('.contact-heading', .5, {
+    opacity: 0,  
+    ease: Power4.easeIn
+});
