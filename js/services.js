aec77d9543e51c5235628f67127ccb98cd0be6d1
Jacob Bryan <jtb36688@gmail.com>
2018-12-12 11:50:35 -0500

    ServicesMobileDone

--- js/services.js
+++ js/services.js
@@ -54,6 +54,22 @@ class TabmobImg {
         this.mobImg.classList.add('SelectedContent')
     }
 }
+class Dropdown {
+    constructor(navbar) {
+        this.navbar = navbar;
+        this.hamburger = this.navbar.querySelector('.Hamburger');
+        this.closemenu = this.navbar.querySelector('.CloseMenu');
+        this.content = document.querySelector('.NavContainer');
+        this.hamburger.addEventListener('click', () => {this.togglecontent()});
+        this.closemenu.addEventListener('click', () => {this.togglecontent()});
+    }
+    togglecontent() {
+        this.hamburger.classList.toggle('ButtonToggle');
+        this.closemenu.classList.toggle('ButtonToggle');
+        this.content.classList.toggle('NavToggle');
+    }
+}
 
 let servicestabs = document.querySelectorAll('.TabsLink');
 servicestabs.forEach(selector => new TabLink(selector));
+let dropdown = new Dropdown(document.querySelector('.HeaderBar'));
