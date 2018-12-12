9aa63932725c5b5224011f53beb95a8ccea083a4
cori-peck <cori.peck@ymail.com>
2018-12-11 22:21:53 -0600

    Working on navigation-button won't display

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // JS goes here
+
+class Overlay {
+    constructor(overlay) {
+        this.overlay = overlay;
+        this.button = this.overlay.querySelector('.closed');
+        this.content = this.overlay.querySelector('.overlay-content');
+
+        this.button.addEventListener('click', () => {this.toggleContent()});
+    }
+
+    toggleContent() {
+        this.content.classList.toggle('overlay-hidden');
+    }
+}
+
+let overlays = document.querySelectorAll('.overlay').forEach( overlay => new Overlay(overlay));
