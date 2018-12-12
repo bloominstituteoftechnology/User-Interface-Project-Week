c5ae5847b36bd60c2ab88db45ed3b90f19e44122
RachelKolk <rach.m.obrien@gmail.com>
2018-12-11 19:11:55 -0500

    found the missing curly brace that messed up everything

---
+++ js/nav-menu-dropdown.js
@@ -0,0 +1,33 @@
+// class DropdownMenu {
+//     constructor(element) {
+      
+//       this.element = element;
+      
+//       this.button = this.element.querySelector('.dropdown-button');      
+      
+//       this.content = this.element.querySelector('.dropdown-content');
+  
+      
+//       this.button.addEventListener('click', () => {this.expandMenu()});
+  
+//     }
+  
+//     expandMenu() {
+       
+//         console.log('Expand nav menu button clicked.');
+//         this.element.classList.toggle('dropdown-hidden');
+        
+//         this.closeButton = this.domElement.querySelector('.close-dropdown');
+        
+//         this.closeButton.addEventListener('click', () => this.closeArticle());
+        
+//     }
+
+//     closeMenu() {
+//         console.log('Nav menu closed.');
+        
+//         this.domElement.classList.toggle('.nav-bar');
+//       }
+  
+  
+//   let dropdowns = document.querySelectorAll('.nav-dropdown').forEach( dropdown => new DropdownMenu(dropdown));
