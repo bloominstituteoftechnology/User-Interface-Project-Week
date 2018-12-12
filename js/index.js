69e3a35ec59cd153e92c5628a5d4f2afb449fccc
EJaona <ejaona@gmail.com>
2018-12-11 19:12:12 -0600

    click event on menu button now displays full page menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelectorAll(".menu-link");
+const header = document.querySelector("header");
 
-menuBtn.addEventListener("click", () => menuBtn.classList.toggle("close"));
+menuBtn.addEventListener("click", () => {
+  header.classList.toggle("close");
+  menu.forEach(link => link.classList.toggle("close"));
+  menuBtn.classList.toggle("close");
+});
