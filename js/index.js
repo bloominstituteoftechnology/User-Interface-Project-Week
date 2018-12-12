d91d7e18480859ac131601beda4e336cbdc36bd0
rmejia-tajc <rmejia.tajc@gmail.com>
2018-12-12 14:50:36 -0700

    add js to navigation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // JS goes here
+
+const navOpen = document.querySelector(".open-btn");
+const navClose = document.querySelector(".close-btn");
+const bigNav = document.querySelector(".nav-big");
+const smallNav = document.querySelector(".nav-small");
+
+navOpen.addEventListener("click", function() {    
+    bigNav.classList.toggle("hide");
+    smallNav.classList.toggle("hide");
+})
+
+navClose.addEventListener("click", function() {
+    bigNav.classList.toggle("hide");
+    smallNav.classList.toggle("hide");    
+})
