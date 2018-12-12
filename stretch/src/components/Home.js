11ab63285565e6729b9a82c8ad3e15a30cee6749
Brandon Lent <blent1050@yahoo.com>
2018-12-11 19:36:14 -0500

    index.html transferred to react component. Images still missing

--- stretch/src/components/Home.js
+++ stretch/src/components/Home.js
@@ -3,7 +3,96 @@ import React from "react";
 const Home = () => {
     return(
         <div>
-            <p>Home</p>
+            <div className="jumbotron">
+		<div className="image-text">
+			<h2>Integrity,</h2>
+			<h2>Excellence,</h2>
+			<h2>Progress.</h2>
+		</div>
+	</div>
+	<div className="container">
+		<div className="about-container">
+			<div className="about-card ">
+				<div className="image-1"></div>
+				<div className="content">
+					<h2>Smith & Jones Architects</h2>
+					<p>Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea. Eu timeam vidisse consectetuer
+						sed.Duo etiam laboramus dissentiet in, nec no errem</p>
+					<a href="" className="btn">Learn More</a>
+				</div>
+
+			</div>
+			<div className="about-card ">
+
+				<div className="content">
+					<h2>Futuristic Designs</h2>
+					<p>Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea. Eu timeam vidisse consectetuer
+						sed.Duo etiam laboramus dissentiet in, nec no errem</p>
+					<a href="" className="btn">View Designs</a>
+
+				</div>
+				<div className="image-2"></div>
+			</div>
+		</div>
+
+	
+		<div className="projects-container">
+			<h2>Recent Projects</h2>
+			<div className="project-card ">
+				<div className="image-1">
+					<h3 className="h3-style">THE VILLAS</h3>
+				</div>
+
+				<div className="content">
+
+					<p> The Villas bring to the table win-win survival strategies to ensure proactive domination. At the end of the
+						day,goingforward, a new normal that has evolved from generation X is on the runway heading towards a streamlined
+						cloudsolution.</p>
+					<p> Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the
+						digital
+						dividewith additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will
+						close
+						the loop on focusing solely on the bottom line.</p>
+				</div>
+
+			</div>
+			<div className="project-card ">
+				<div className="image-2">
+					<h3 className="h3-style">OUTSKIRTS</h3>
+				</div>
+
+				<div className="content">
+					<p> The Outskirts are amazing to the table win-win survival strategies to ensure proactive domination. At the end
+						of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards
+						astreamlined cloud solution.</p>
+
+					<p> Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the
+						digital
+						divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will
+						close the loop on focusing solely on the bottom line.</p>
+				</div>
+
+
+			</div>
+			<div className="project-card ">
+				<div className="image-3">
+					<h3 className="h3-style">THE BLOCKS</h3>
+				</div>
+
+				<div className="content">
+					<p> The Blocks are amazing to the table win-win survival strategies to ensure proactive domination. At the end of
+						the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a
+						streamlined cloud solution.</p>
+					<p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital
+						divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will
+						close
+						the loop on focusing solely on the bottom line.</p>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
         </div>
     );
 }
