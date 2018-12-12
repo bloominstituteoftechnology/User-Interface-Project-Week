4944d8d1713de67bb584e6aa9db4e6757640fab4
Imon Ovbude <imonovbude@gmail.com>
2018-12-12 12:46:42 -0800

    created component constructor in index.js

--- js/index.js
+++ js/index.js
@@ -25,22 +25,59 @@ class ToggleMenu {
 // reference to drop down content element
 let dropdowns = document.querySelectorAll('.dropdown').forEach(dropdown => new ToggleMenu(dropdown));
 
-
+// ---------------------------------------------------------------------------
 // Tab Section For Services Page
-// class TabLink {
-//     constructor(elem) {
-//         // Individual Tabs
-//         this.elem = elem;
+class TabLink {
+    constructor(elem) {
+        // Individual Tabs
+        this.elem = elem;
 
-//         // Data Attribute
-//         this.data = this.elem.dataset.tab;
+        // Data Attribute
+        this.data = this.elem.dataset.tab;
 
-//         // Item Selector
-//         this.itemElem = document.querySelector(`.tabs-item[data-tab = '${this.data}']`);
+        // Item Selector
+        this.itemElem = document.querySelector(`.tabs-item[data-tab = '${this.data}']`);
 
-//     }
-// }
+        // create new instance of TabItem class constructor
+        this.itemElem = new TabItem(this.itemElem);
+
+        // event linstener
+        this.elem.addEventListener('click', () => this.select())
+    }
+
+    select() {
+        // reference tabs-links class
+        const links = document.querySelectorAll('.tabs-link');
+
+        // removed tabs-link-selected class 
+        links.forEach(link => link.classList.remove('tabs-link-selected'));
+
+        // add tabs-link-selected class to selected link
+        this.elem.classList.add('tabs-link-selected');
+
+        // call select method from TabItem class construtor
+        this.itemElem.selected();
+    }
+}
+
+class TabItem {
+    constructor(itemElem) {
+        this.itemElem = itemElem;
+    }
+
+    selected() {
+        //reference tab items
+        const items = document.querySelectorAll('.tabs-item');
+
+        // remove tabs-item-selected class
+        items.forEach(item => item.classList.remove('tabs-item-selected'));
+
+        // add tabs-item-selected class to selected tab
+        this.itemElem.classList.add('tabs-item-selected');
+
+    }
+}
 
 
-// const links = document.querySelectorAll('.tabs-link').forEach(tab => new TabLink(tab));
+const links = document.querySelectorAll('.tabs-link').forEach(tab => new TabLink(tab));
 
