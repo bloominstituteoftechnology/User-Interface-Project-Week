8b721fb23a4bd4b8be3769144330bd872b55674c
Nathan Thomas <nwthomas@me.com>
2018-12-11 16:11:39 -0800

    Finish contact.index content section

--- js/index.js
+++ js/index.js
@@ -4,14 +4,15 @@ const burger = document.querySelector(".burger");
 const body = document.querySelector("body");
 
 burger.addEventListener("click", () => {
-  navbar.classList.toggle("navbar--open");
-  navbarText.classList.toggle("nav__menu--open");
-  body.classList.toggle("stop__scrolling");
-  if (body.classList.contains("stop__scrolling")) {
-    // body.bind("touchmove", e => e.preventDefault());
-    burger.src = "./img/nav-hamburger-close.png";
-  } else {
-    // body.unbind("touchmove");
+  if (navbar.classList.contains("navbar--open")) {
+    TweenMax.to(navbar, 1, { className: "-=navbar--open" });
+    TweenMax.to(navbarText, 1, { className: "-=nav__menu--open" });
     burger.src = "./img/nav-hamburger.png";
+    body.classList.toggle("stop__scrolling");
+  } else {
+    TweenMax.to(navbar, 1, { className: "+=navbar--open" });
+    TweenMax.to(navbarText, 1, { className: "+=nav__menu--open" });
+    burger.src = "./img/nav-hamburger-close.png";
+    body.classList.toggle("stop__scrolling");
   }
 });
