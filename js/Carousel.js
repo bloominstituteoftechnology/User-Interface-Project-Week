9d24e03a28a0f5ec54c5d54a77b231c71865a76e
Moonkaman <nickbatmanglidj@gmail.com>
2018-12-12 11:27:09 -0700

    Add functionality to carousel component using JS

---
+++ js/Carousel.js
@@ -0,0 +1,58 @@
+
+
+class Carousel {
+  constructor(cElement) {
+    this.cElement = cElement;
+    this.arrows = this.cElement.querySelectorAll('.arrow');
+    this.leftArrow = this.arrows[0];
+    this.rightArrow = this.arrows[1];
+    this.slides = this.cElement.querySelectorAll('.carousel-img');
+    this.currentSlide = 0;
+    this.canSlide = true;
+
+    this.rightArrow.addEventListener('click', _ => this.slideRight());
+    this.leftArrow.addEventListener('click', _ => this.slideLeft());
+    
+  }
+
+  slideRight() {
+    if(this.canSlide){
+      this.canSlide = false;
+      let pastSlide = this.currentSlide;
+      if(this.currentSlide < this.slides.length-1){
+        this.currentSlide++;
+      }else{
+        this.currentSlide = 0;
+      }
+      this.slides[this.currentSlide].style.left = '855px';
+      this.slides[this.currentSlide].classList.remove('hide');
+      TweenMax.to(this.slides[pastSlide], 1, {left: -855, onComplete: _ => this.slideHide(this.slides[pastSlide])});
+      TweenMax.to(this.slides[this.currentSlide], 1, {left: 0, onComplete: _ => this.canSlide = true});
+    }
+  }
+
+  slideLeft() {
+    if(this.canSlide){
+      this.canSlide = false;
+      let pastSlide = this.currentSlide;
+      if(this.currentSlide > 0){
+        this.currentSlide--;
+      }else{
+        this.currentSlide = this.slides.length -1;
+      }
+      this.slides[this.currentSlide].style.left = '-855px';
+      this.slides[this.currentSlide].classList.remove('hide');
+      TweenMax.to(this.slides[pastSlide], 1, {left: 855, onComplete: _ => this.slideHide(this.slides[pastSlide])});
+      TweenMax.to(this.slides[this.currentSlide], 1, {left: 0, onComplete: _ => this.canSlide = true});
+    }
+  }
+
+
+  slideHide(elem) {
+    elem.classList.add('hide');
+  }
+}
+
+let carousel = document.querySelector('.carousel');
+
+carousel = new Carousel(carousel);
