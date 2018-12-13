1f285cf07372d64d745161c78f4d9e505d157834
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2018-12-12 21:15:54 -0500

    Finished non-expanded view on nav

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 // // JS goes here
 
+const toggleMenu = () => {
+  menu.classList.toggle('menu--open');
+}
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.menu-img')
+menuButton.addEventListener('click', toggleMenu);
+
+
 
 
 
