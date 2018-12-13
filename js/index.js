8e0e51dc74ddde60ac6d1ec21873b632f8ff9242
samjeffcoat <samjeffcoat@gmail.com>
2018-12-13 09:24:34 -0600

    added button components

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-const menu
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open');
+};
+
+const menu = document.querySelector('.menu');
+
+const menuButton= document.querySelector('.menuButton');
+
+const menuCloseButton= document.querySelector('.menuCloseButton');
+
+menuButton.addEventListener('click', function(e) {
+    e.target= toggleMenu();
+});
+
+menuCloseButton.addEventListener('click', function(e) {
+    e.target= toggleMenu();
+});
