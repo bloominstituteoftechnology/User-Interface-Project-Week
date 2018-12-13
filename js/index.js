def5d08b7dc9162f32a3333f6ac1bd0c7a25f8c6
Scott Vojik <scott.vojik@gmail.com>
2018-12-13 08:20:56 -0800

    refactored code, cleaned it up

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ const buttonImg = document.querySelector(".menu-button");
 const buttonClose = document.querySelector(".menu-button-close");
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle('menu--open');
   headerText.classList.toggle('opacity');
   buttonImg.classList.toggle('button-close');
@@ -14,12 +13,10 @@ const toggleMenu = () => {
   buttonClose.classList.toggle('menu-button-close');
 }
 
-// Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
-// create a reference to the ".menu-button" class
 
 const menuButton = document.querySelector('.menu-button');
-// Using your menuButton reference, add a click handler that calls toggleMenu
+
 menuButton.addEventListener('click', (e) => {
   toggleMenu();
 });
