4b3d55f3d3af79dd8752c0e532bde73d8ff105dc
joepound <14357797+joepound@users.noreply.github.com>
2018-12-13 13:20:02 -0700

    Added scroll animations to home page

--- js/scrollfx.js
+++ js/scrollfx.js
@@ -1,25 +1,53 @@
 const controller = new ScrollMagic.Controller();
 
-const scrollGroup = new ScrollMagic.Scene({
-    triggerElement: "#scrollGroup",
-});
-scrollGroup.setClassToggle('.scrollGroup', 'fade-in');
-scrollGroup.addTo(controller);
 
-const scrollBlock1 = new ScrollMagic.Scene({
-    triggerElement: "#scrollBlock1",
+const scrollGroup1Animation = TweenMax.to(".scrollGroup1", 1, {
+    y: 0,
+    opacity: 1
 });
-scrollBlock1.setClassToggle('#scrollBlock1', 'fade-in');
-scrollBlock1.addTo(controller);
 
-const scrollBlock2 = new ScrollMagic.Scene({
-    triggerElement: "#scrollBlock2",
+const scrollGroup1 = new ScrollMagic.Scene({
+    triggerElement: ".scrollGroup1",
 });
-scrollBlock2.setClassToggle('#scrollBlock2', 'fade-in');
-scrollBlock2.addTo(controller);
+scrollGroup1.setClassToggle('.scrollGroup1', 'fade-in');
+scrollGroup1.setTween(scrollGroup1Animation);
+scrollGroup1.addTo(controller);
 
-const scrollBlock3 = new ScrollMagic.Scene({
-    triggerElement: "#scrollBlock3",
-})
-scrollBlock3.setClassToggle('#scrollBlock3', 'fade-in');
-scrollBlock3.addTo(controller);
+
+const scrollGroup2Animation = TweenMax.to(".scrollGroup2", 1, {
+    y: 0,
+    opacity: 1
+});
+
+const scrollGroup2 = new ScrollMagic.Scene({
+    triggerElement: ".scrollGroup2",
+});
+scrollGroup2.setClassToggle('.scrollGroup2', 'fade-in');
+scrollGroup2.setTween(scrollGroup2Animation);
+scrollGroup2.addTo(controller);
+
+
+const scrollGroup3Animation = TweenMax.to(".scrollGroup3", 1, {
+    y: 0,
+    opacity: 1
+});
+
+const scrollGroup3 = new ScrollMagic.Scene({
+    triggerElement: ".scrollGroup3",
+});
+scrollGroup3.setClassToggle('.scrollGroup3', 'fade-in');
+scrollGroup3.setTween(scrollGroup3Animation);
+scrollGroup3.addTo(controller);
+
+
+const scrollGroup4Animation = TweenMax.to(".scrollGroup4", 1, {
+    y: 0,
+    opacity: 1
+});
+
+const scrollGroup4 = new ScrollMagic.Scene({
+    triggerElement: ".scrollGroup4",
+});
+scrollGroup4.setClassToggle('.scrollGroup4', 'fade-in');
+scrollGroup4.setTween(scrollGroup4Animation);
+scrollGroup4.addTo(controller);
