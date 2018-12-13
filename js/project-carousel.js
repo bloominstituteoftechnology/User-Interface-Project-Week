42dc558d73e02f96b495a04081b3b605274fa38c
RachelKolk <rach.m.obrien@gmail.com>
2018-12-13 14:11:18 -0500

    carousel working

--- js/project-carousel.js
+++ js/project-carousel.js
@@ -7,10 +7,16 @@ class Carousel {
         this.rightButton = document.querySelector('.right-button');
        
         this.imageContent = document.querySelectorAll('.image');
+        this.textContent = document.querySelectorAll('.paragraph');
         
         this.imgArray = [];
         this.imageContent.forEach(image => this.imgArray.push(new Image(image)));
         console.log(this.imgArray[this.currentIndex]);
+
+        this.textArray = [];
+        this.textContent.forEach(paragraph => this.textArray.push(new Paragraph(paragraph)));
+        console.log(this.textArray[this.currentIndex]);
+
         this.leftButton.addEventListener('click', () => {this.leftClick()});
         this.rightButton.addEventListener('click', () => {this.rightClick()});
 
@@ -18,23 +24,32 @@ class Carousel {
 
     leftClick() {
         this.imageContent.forEach(picture => picture.classList.remove('active-img'));
+
+        this.textContent.forEach(paragraph => paragraph.classList.remove('active-paragraph'));
+
         console.log(this.currentIndex);
         this.currentIndex--;
         if (this.currentIndex < 0) {
             this.currentIndex = this.imgArray.length - 1;
+            this.currentIndex = this.textArray.length - 1;
         }
         this.imgArray[this.currentIndex].selectImage();
-        
+        this.textArray[this.currentIndex].selectParagraph();
     }  
 
     rightClick() {
         this.imageContent.forEach(picture => picture.classList.remove('active-img'));
+
+        this.textContent.forEach(paragraph => paragraph.classList.remove('active-paragraph'));
+
         console.log(this.currentIndex);
         this.currentIndex++;
         if (this.currentIndex >  this.imgArray.length - 1) {
             this.currentIndex = 0;
         }
+       
         this.imgArray[this.currentIndex].selectImage();
+        this.textArray[this.currentIndex].selectParagraph();
     }
 }
 
@@ -47,6 +62,17 @@ class Image {
 
     selectImage() {
         this.imageContent.classList.add('active-img');
+    }
+}
+
+class Paragraph {
+    constructor(textContent) {
+        this.textContent = textContent;
+        console.log('text created');
+    }
+
+    selectParagraph() {
+        this.textContent.classList.add('active-paragraph');
     }
 }
 
