dbda3b6c47fec05e85e922087d88d3f39e0d3125
Jacob Bryan <jtb36688@gmail.com>
2018-12-13 14:16:43 -0500

    NavbarGreensock

--- js/contact.js
+++ js/contact.js
@@ -3,15 +3,35 @@ class Dropdown {
         this.navbar = navbar;
         this.hamburger = this.navbar.querySelector('.Hamburger');
         this.closemenu = this.navbar.querySelector('.CloseMenu');
-        this.content = document.querySelector('.NavContainer');
         this.hamburger.addEventListener('click', () => {this.togglecontent()});
-        this.closemenu.addEventListener('click', () => {this.togglecontent()});
+        this.closemenu.addEventListener('click', () => {this.untogglecontent()});
     }
     togglecontent() {
         this.hamburger.classList.toggle('ButtonToggle');
         this.closemenu.classList.toggle('ButtonToggle');
-        this.content.classList.toggle('NavToggle');
+        t1.play();
+        t2.play();
+        t3.play();
+        t4.play();
+    }
+    untogglecontent() {
+        this.hamburger.classList.toggle('ButtonToggle');
+        this.closemenu.classList.toggle('ButtonToggle');
+        t1.reverse();
+        t2.reverse();
+        t3.reverse();
+        t4.reverse();
     }
 }
+
+var t1 = new TimelineMax({paused:true});
+var t2 = new TimelineMax({paused:true});
+var t3 = new TimelineMax({paused:true});
+var t4 = new TimelineMax({paused:true});
+
+t1.to('#dropdown', .55, {height:'100vh'});
+t2.to('#navanchor1', .35, {right:'0'});
+t3.to('#navanchor2', .4, {right:'0'});
+t4.to('#navanchor3', .45, {right:'0'});
 
 let dropdown = new Dropdown(document.querySelector('.HeaderBar'));
