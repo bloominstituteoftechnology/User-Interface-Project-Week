eceed05de5be8572fefdb6aefe0bd7bf3a089fab
joshua-disney <joshua.a.disney@gmail.com>
2018-12-13 10:52:01 -0700

    Added expanded navigation tab.

--- js/navigation.js
+++ js/navigation.js
@@ -1,1 +1,16 @@
-console.log('Code me, Papi!');
+const hamburger = document.querySelector('.nav-hamburger');
+const closed = document.querySelector('.nav-closed');
+const navTabs = document.querySelector('.nav-tabs');
+
+
+hamburger.addEventListener('click', () => {
+  hamburger.style.display = 'none';
+  closed.style.display = 'block';
+  navTabs.style.display = 'flex';
+});
+
+closed.addEventListener('click', () => {
+  closed.style.display = 'none';
+  hamburger.style.display = 'block';
+  navTabs.style.display = 'none';
+});
