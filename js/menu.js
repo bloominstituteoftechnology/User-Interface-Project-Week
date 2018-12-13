a23b231296554368b40073091b45e7aafb60f66c
croy <roy.christie95@gmail.com>
2018-12-13 13:10:09 -0600

    Got menu rollout functionality and adjusted close button positioning

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,13 @@
 const menuOpen = () => {
   menu.style.display = 'block';
   menuButton.style.display = 'none';
+  TweenMax.from(menu, 2, {
+    ease: Power4.easeOut,
+    height: 0,
+    width: 0,
+    top: 0,
+    fontSize: 0,
+});
 }
 
 const menuClose = () => {
@@ -8,6 +15,13 @@ const menuClose = () => {
    menu.style.display = 'none';
    menuButton.style.display = 'block';
  }
+ TweenMax.to(menu, 2, {
+  ease: Power4.easeOut,
+  // height: 100,
+  // width: 100,
+  // top: 0,
+});
+ 
 }
 
 const menu = document.querySelector('.menu');
