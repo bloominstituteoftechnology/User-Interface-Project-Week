e5cc6efcd27735d1691e4604c70be4d6549312c9
Randy Wilson <phoenixrr2113@gmail.com>
2018-12-13 01:13:04 -0500

    contact done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// JS goes here
+const toggleMenu = () => {
+	navButton.classList.toggle('change');
+};
+
+const menu = document.querySelector('.menu');
+
+const menuButton = document.querySelector('.menu-icon');
+
+const navButton = document.querySelector('.nav-menu');
+
+const page = document.querySelector('.container');
+
+menuButton.addEventListener('click', () => {
+	toggleMenu();
+});
+console.log(navButton);
