42916cf96c1e33a6ac72e3ccad6b58c64a12adf6
cecil john tantay <cjbtantay@gmail.com>
2018-12-13 10:47:34 -0800

    added new feature and finished with animations

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./index.css";
-import Home from './Home';
-import Services from './Services';
-import Contact from "./Contact";
-import Error from "./Error";
-import Nav from "./Nav";
-import Footer from "./Footer";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-class App extends Component {
+import burger from "./img/nav-hamburger.png";
+import burgerClose from "./img/nav-hamburger-close.png";
+
+class Nav extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      onLoad: false
-    }
   }
-  handleChange = ()=> {
-    this.setState(prevState => ({onLoad: !prevState.onLoad}))
-  }
-
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Nav onLoad={this.state.onLoad} handleChange={this.handleChange}/>
-          <Switch>
-            <Route path="/" component={Home} exact/>
-            <Route path="/services" component={Services}/>
-            <Route path="/contact" component={Contact}/>
-            <Route component={Error}/>
-          </Switch>
-          <Footer />
+      <div className="nav-bar">
+        <div
+          className={
+            this.props.onLoad ? "nav--clicked nav-container" : "nav-container"
+          }
+        >
+          <NavLink to="/" className="navh2">
+            <h2>S&J</h2>
+          </NavLink>
+          <nav className={this.props.onLoad ? "nav-open" : "nav-close"}>
+            <NavLink to="/" onClick={() => this.props.handleChange()}>
+              Home
+            </NavLink>
+            <NavLink to="/services" onClick={() => this.props.handleChange()}>
+              Services
+            </NavLink>
+            <NavLink to="/contact" onClick={() => this.props.handleChange()}>
+              Contact
+            </NavLink>
+          </nav>
+          <div className="burger" onClick={() => this.props.handleChange()}>
+            <img
+              src={burger}
+              alt="hamburger menu img"
+              className={this.props.onLoad ? "nav-close" : "nav-open"}
+            />
+            <img
+              src={burgerClose}
+              alt=""
+              className={this.props.onLoad ? "nav-open" : "nav-close"}
+            />
+          </div>
         </div>
-      </BrowserRouter>
+      </div>
     );
   }
 }
 
-export default App;
+export default Nav;
