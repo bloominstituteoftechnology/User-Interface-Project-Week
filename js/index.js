ca6e8cea5f33d67dcafa09f0d885925026b69be2
umbur <work1415@gmail.com>
2018-12-13 11:19:34 -0700

    contact page desktop design mainly completed. Working on to adjust design with PixelPerfect program

--- js/index.js
+++ js/index.js
@@ -1,30 +1,35 @@
 class TabLink {
+    
     constructor(element) {
       this.element = element;
+      //console.log(this.element);
       this.data = this.element.dataset.tab;
       this.itemElement = document.querySelector(`.tabs-box[data-tab="${this.data}"]` );
       this.itemElement = new TabItem(this.itemElement);
+      //console.log(this.itemElement);
       this.element.addEventListener("click", () => this.select());
+      
     }
   
     select() {
-      const tabs = document.querySelectorAll(".tab").forEach(link => link.classList.remove("tab--selected"));
+        
+      document.querySelectorAll(".tab").forEach(link => link.classList.remove("tab--selected"));
       this.element.classList.add("tab--selected");
       this.itemElement.select();
+    
     }
   }
   
   class TabItem {
     constructor(element) {
-      this.element = element;
+     this.element = element;
     }
   
     select() {
-      const items = document
-        .querySelectorAll(".tabs-box")
-        .forEach(item => item.classList.remove("tabs-box--view"));
+      document.querySelectorAll(".tabs-box").forEach(item => item.classList.remove("tabs-box--view"));
       this.element.classList.add("tabs-box--view");
     }
+    
   }
   
   const tabs = document.querySelectorAll(".tab").forEach(link => new TabLink(link));
