75793200d4a9dbafd9c2a0e27052e26d2e096ed8
cecil john tantay <cjbtantay@gmail.com>
2018-12-13 17:33:11 -0800

    added error page

--- src/Error.js
+++ src/Error.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Error from "./img/404bob.jpeg"
+import Error from "./img/404bob.jpeg";
 
-const Error = ()=> {
-    return (
-      <h1>hello</h1>
-      <img src={Error} alt=""/>
-    )
-}
+const Error = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <img src={Error} alt="" />
+      <p>
+        You must have been looking for something else; let me fix that for you!
+      </p>
+      <NavLink to="/">Home</NavLink>
+    </div>
+  );
+};
 
 export default Error;
