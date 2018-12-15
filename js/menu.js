a05d70fdd05522ded080bbd4069b6ea7705aad48
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-12-15 18:45:55 -0500

    refactored hamburger and nav menu less refactored home.less projects text-content

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,14 @@
+const toggleHamburger = e => {
+  hamburger.classList.toggle('hamburger--open');
+  closeHamburger.classList.toggle('hamburger--open');
+}
+
 const toggleMenu = (e) => {
   menu.classList.toggle('menu--open');
 }
 
 const menu = document.querySelector('.menu');
 const hamburger = document.querySelector('.nav-hamburger');
+const closeHamburger = document.querySelector('.nav-hamburger-close');
 
 hamburger.addEventListener('click', toggleMenu);
