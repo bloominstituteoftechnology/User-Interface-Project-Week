0060aa687f665b7d1c1cafeb498ef7c8e0ba3a9e
KieranVieira <kieranvieira@live.com>
2018-12-18 13:19:48 -0500

    Beautified code

--- js/index.js
+++ js/index.js
@@ -6,19 +6,25 @@ const navMenu = document.querySelector('.nav-menu');
 
 navButtonOn.addEventListener('click', () => {
     navMenu.classList.toggle('nav-toggle', true);
-    TweenMax.to('.nav-menu', .5 ,{top:'0vw'})
+    TweenMax.to('.nav-menu', .5, {
+        top: '0vw'
+    })
 })
 
 navButtonOff.addEventListener('click', () => {
-    TweenMax.to('.nav-menu', .2 ,{top:'-100vw'})
-    setTimeout(()=>{navMenu.classList.toggle('nav-toggle', false)}, 210)
-    
+    TweenMax.to('.nav-menu', .2, {
+        top: '-100vw'
+    })
+    setTimeout(() => {
+        navMenu.classList.toggle('nav-toggle', false)
+    }, 210)
+
 })
 
 //tabs toggle
 
 class TabLink {
-    constructor(tablink){
+    constructor(tablink) {
         this.link = tablink;
         this.data = this.link.dataset.tab;
         this.tabContent = document.querySelector(`.tab-item[data-tab='${this.data}']`);
@@ -27,29 +33,33 @@ class TabLink {
     }
     select() {
         const links = document.querySelectorAll('.tablink');
-        links.forEach(cv => {cv.classList.toggle('tab-link-active', false)});
+        links.forEach(cv => {
+            cv.classList.toggle('tab-link-active', false)
+        });
         this.link.classList.toggle('tab-link-active', true);
         this.tabContent.select();
     }
 }
 
 class TabItem {
-    constructor(element){
+    constructor(element) {
         this.tabItem = element;
     }
-    select(){
+    select() {
         const tabItems = document.querySelectorAll('.tab-item');
-        tabItems.forEach(cv => {cv.style.display = 'none'});
+        tabItems.forEach(cv => {
+            cv.style.display = 'none'
+        });
         this.tabItem.style.removeProperty('display');
     }
 }
 
-const tabLinks = document.querySelectorAll('.tablink').forEach( link => new TabLink(link));
+const tabLinks = document.querySelectorAll('.tablink').forEach(link => new TabLink(link));
 
 //Carousel Projects
 
 class Carousel {
-    constructor(carousel){
+    constructor(carousel) {
         this.element = carousel;
         this.currentIndex = 1;
         this.imageAmount = this.element.querySelectorAll('.slider').length;
@@ -58,21 +68,25 @@ class Carousel {
         this.leftBTN.addEventListener('click', () => this.left())
         this.rightBTN.addEventListener('click', () => this.right())
     }
-    right(){
-        this.currentIndex +=1;
-        if(this.currentIndex >= this.imageAmount+1){
+    right() {
+        this.currentIndex += 1;
+        if (this.currentIndex >= this.imageAmount + 1) {
             this.currentIndex = 1
         }
-        this.element.querySelectorAll('.slider').forEach((cv)=>{cv.classList.toggle('active-slider',false)})
-        this.element.querySelector(`.slider[data-position='${this.currentIndex}']`).classList.toggle('active-slider',true)
+        this.element.querySelectorAll('.slider').forEach((cv) => {
+            cv.classList.toggle('active-slider', false)
+        })
+        this.element.querySelector(`.slider[data-position='${this.currentIndex}']`).classList.toggle('active-slider', true)
     }
-    left(){
-        this.currentIndex -=1;
-        if(this.currentIndex <= 0){
+    left() {
+        this.currentIndex -= 1;
+        if (this.currentIndex <= 0) {
             this.currentIndex = this.imageAmount;
         }
-        this.element.querySelectorAll('.slider').forEach((cv)=>{cv.classList.toggle('active-slider',false)})
-        this.element.querySelector(`.slider[data-position='${this.currentIndex}']`).classList.toggle('active-slider',true)
+        this.element.querySelectorAll('.slider').forEach((cv) => {
+            cv.classList.toggle('active-slider', false)
+        })
+        this.element.querySelector(`.slider[data-position='${this.currentIndex}']`).classList.toggle('active-slider', true)
     }
 }
 
