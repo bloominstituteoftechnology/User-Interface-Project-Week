d2bcf0f00197a58dab3ef76121be3c6326fcf3be
Scott Vojik <scott.vojik@gmail.com>
2018-12-18 21:45:27 -0800

    added green sock animation to menu closing

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 
 //---------------NAV MENU---------------------
 
-const headerText = document.querySelector(".header h1");
-
 const hamburgerButton = document.querySelector(".hamburger-button");
 const xButton = document.querySelector(".x-button");
 const menu = document.querySelector('.menu');
+
+const headerText = document.querySelector(".header h1");
 
 const toggleMenu = () => {
 
@@ -13,20 +13,25 @@ const toggleMenu = () => {
   hamburgerButton.classList.toggle('button-close');
   xButton.classList.toggle('button-open');
 
-  TweenMax.to('.menu', 3, {
-    scaleY: 1,
-    ease: Power4.easeOut,
-  });
+  let string1 = 'menu--open';
+  if (menu.classList.contains(string1)) {
+      TweenMax.to('.menu--open', 1, {
+      scaleY: 1,
+      ease: Power4.easeOut,
+    });
+  } else {
+    TweenMax.to('.menu', .6, {
+      scaleY: 0,
+      ease: Power4.easeOut,
+    });
+  }
   
   headerText.classList.toggle('opacity');
-
 }
 
 
 hamburgerButton.addEventListener('click', (e) => {
-  
   toggleMenu();
-  
 });
 
 xButton.addEventListener('click', (e) => {
