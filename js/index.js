22134582b411c97233648596a970c6d64455ce33
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-12-20 14:14:06 -0600

    working on nav bar

--- js/index.js
+++ js/index.js
@@ -56,3 +56,13 @@ class TabLink {
   
   links = document.querySelectorAll(".tabs-link") 
                                      .forEach(link => new TabLink(link));
+
+  //=====NAV
+  
+  function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  
+  document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
