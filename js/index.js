1d17da3ba41c006664fca5087843cdbf5e49a33d
JamesB <jblazer1@yahoo.com>
2018-12-21 08:53:39 -0800

    Update nav bar

--- js/index.js
+++ js/index.js
@@ -74,6 +74,9 @@ class TabLink {
   // Nothing to do here, just study what the code is doing and move on to the Dropdown class
   let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
 
+// ==========================================================================================================================
+
+
 
 
 
