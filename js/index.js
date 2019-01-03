8f02aeaaf786dec43f50ea4c9b8f734a36018c01
unknown <jjamal95@gmail.com>
2019-01-03 11:35:54 -0600

    got functionality working for home nav menu, now working on design

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// JS goes here
+
+const showNav = () => {
+    // Toggle the "menu--open" class on your menu refence. 
+    menu.classList.toggle("menu--open");
+  }
+
+
+
+
+
+const menu = document.querySelector(".menu");
+
+
+const hamButton= document.querySelector(".nav-button");
+
+hamButton.addEventListener("click", showNav);
