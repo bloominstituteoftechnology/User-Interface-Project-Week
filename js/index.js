f952f0ce4b515ec9bda2ae042d2c482cbbee7368
JamesB <jblazer1@yahoo.com>
2019-01-03 15:21:47 -0800

    starting over add nav bar functionality

--- js/index.js
+++ js/index.js
@@ -1,78 +1,97 @@
 // JS goes here
 class TabLink {
-    constructor(element) {
-      this.element = element;
-      
-      // Get the custom data attribute on teh Link
-      this.data = this.element.dataset.tab;
-      // Using the custom data attribute ge tthe associated Item element
-      this.item = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
-      // Using the Item element, create a new instance of the TabItem class
-      this.tabItem = new TabItem(this.item);
-      
-      this.element.addEventListener('click', () => {
-        // Call the select method you define below
-        this.select();
-      });
-    };
-  
-    select() {
-      // Get all of the elements with the tabs-link class
-      const links = document.querySelectorAll('.tabs-link');
-  
-      // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-      links.forEach( link => {
-        link.classList.remove('tabs-link-selected')
-      });
-  
-      // Add a class named "tabs-link-selected" to this link
-      this.element.classList.add('tabs-link-selected');
-      
-      // Call the select method on the item associated with this link
-      this.tabItem.select();
-    }
+  constructor(element) {
+    this.element = element;
+
+    // Get the custom data attribute on teh Link
+    this.data = this.element.dataset.tab;
+    // Using the custom data attribute ge tthe associated Item element
+    this.item = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+    // Using the Item element, create a new instance of the TabItem class
+    this.tabItem = new TabItem(this.item);
+
+    this.element.addEventListener('click', () => {
+      // Call the select method you define below
+      this.select();
+    });
+  };
+
+  select() {
+    // Get all of the elements with the tabs-link class
+    const links = document.querySelectorAll('.tabs-link');
+
+    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+    links.forEach(link => {
+      link.classList.remove('tabs-link-selected')
+    });
+
+    // Add a class named "tabs-link-selected" to this link
+    this.element.classList.add('tabs-link-selected');
+
+    // Call the select method on the item associated with this link
+    this.tabItem.select();
   }
-  
-  class TabItem {
-    constructor(element) {
-      this.element = element;
-    }
-  
-    select() {
-      // Select all ".tabs-item" elements from the DOM
-      const items = document.querySelectorAll('.tabs-item');
-      // Remove the class "tabs-item-selected" from each element
-      items.forEach( item => {
-        item.classList.remove('tabs-item-selected');
-      })
-      // Add a class named "tabs-item-selected" to this element 
-      this.element.classList.add('tabs-item-selected');
-    }
+}
+
+class TabItem {
+  constructor(element) {
+    this.element = element;
   }
 
-  // ============================================     DROPDOWN    =============================================================
-
-  class Dropdown {
-    constructor(element) {
-      // assign this.element to the dropdown element
-      this.element = element;
-      // assign the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-      this.button = this.element.querySelector('.dropdown-button')
-      // assign the reference to the ".dropdown-content" class found in the dropdown element
-      this.content = this.element.querySelector('.dropdown-content');
-      // Add a click handler to the button reference and call the toggleContent method.
-      this.button.addEventListener('click', () => { this.toggleContent() })
-      // Instructor note, the reason we must wrap the toggleContent method in an anonymous function is that 'this' in toggleContent
-      // would refer to the button, NOT the current instance of the class. 
-    }
-  
-    toggleContent() {
-      // Toggle the ".dropdown-hidden" class off and on
-      this.content.classList.toggle('dropdown-hidden');
-    }
+  select() {
+    // Select all ".tabs-item" elements from the DOM
+    const items = document.querySelectorAll('.tabs-item');
+    // Remove the class "tabs-item-selected" from each element
+    items.forEach(item => {
+      item.classList.remove('tabs-item-selected');
+    })
+    // Add a class named "tabs-item-selected" to this element 
+    this.element.classList.add('tabs-item-selected');
   }
-  // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-  let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+}
+
+// ======================================================== OVERLAY START ===========================================================================
+const openMenu = document.querySelector(".open");
+const closeMenu = document.querySelector(".close");
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+  openMenu.classList.toggle("close-menu");
+  closeMenu.classList.toggle("close-menu");
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+  openMenu.classList.toggle("close-menu");
+  closeMenu.classList.toggle("close-menu");
+}
+
+// ======================================================= OVERLAY END ===============================================================================
+
+// ============================================     DROPDOWN    =============================================================
+
+// class Dropdown {
+//   constructor(element) {
+//     // assign this.element to the dropdown element
+//     this.element = element;
+//     // assign the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
+//     this.button = this.element.querySelector('.dropdown-button')
+//     // assign the reference to the ".dropdown-content" class found in the dropdown element
+//     this.content = this.element.querySelector('.dropdown-content');
+//     // Add a click handler to the button reference and call the toggleContent method.
+//     this.button.addEventListener('click', () => { this.toggleContent() })
+//     // Instructor note, the reason we must wrap the toggleContent method in an anonymous function is that 'this' in toggleContent
+//     // would refer to the button, NOT the current instance of the class. 
+//   }
+
+//   toggleContent() {
+//     // Toggle the ".dropdown-hidden" class off and on
+//     this.content.classList.toggle('dropdown-hidden');
+//   }
+// }
+// // Nothing to do here, just study what the code is doing and move on to the Dropdown class
+// let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
 
 // ==========================================================================================================================
 
@@ -102,4 +121,4 @@ class TabLink {
 
 
 
-let links = document.querySelectorAll('.tabs-link').forEach( link => new TabLink(link));
+let links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link));
