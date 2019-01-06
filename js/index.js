5556076823555a3f5ed4dc6b3cd0d9e2afba130b
JamesB <jblazer1@yahoo.com>
2019-01-06 13:36:51 -0800

    Add Contact page

--- js/index.js
+++ js/index.js
@@ -69,56 +69,6 @@ function closeNav() {
 
 // ======================================================= OVERLAY END ===============================================================================
 
-// ============================================     DROPDOWN    =============================================================
-
-// class Dropdown {
-//   constructor(element) {
-//     // assign this.element to the dropdown element
-//     this.element = element;
-//     // assign the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-//     this.button = this.element.querySelector('.dropdown-button')
-//     // assign the reference to the ".dropdown-content" class found in the dropdown element
-//     this.content = this.element.querySelector('.dropdown-content');
-//     // Add a click handler to the button reference and call the toggleContent method.
-//     this.button.addEventListener('click', () => { this.toggleContent() })
-//     // Instructor note, the reason we must wrap the toggleContent method in an anonymous function is that 'this' in toggleContent
-//     // would refer to the button, NOT the current instance of the class. 
-//   }
-
-//   toggleContent() {
-//     // Toggle the ".dropdown-hidden" class off and on
-//     this.content.classList.toggle('dropdown-hidden');
-//   }
-// }
-// // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-// let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
-
-// ==========================================================================================================================
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 let links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link));
