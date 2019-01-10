d9d6c56c6caf424cca6b5475a5ca1014d622385c
drkfogle <rodriguez.kevin.094@gmail.com>
2019-01-10 15:28:37 -0500

    just trying to get the close to work

--- js/nav.js
+++ js/nav.js
@@ -8,7 +8,9 @@ const menu = document.querySelector('.menu');
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
+const closeButton = document.querySelector('.close')
   // console.log(menuButton);
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click,', toggleMenu);
