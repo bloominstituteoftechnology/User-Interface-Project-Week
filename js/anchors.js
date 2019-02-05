c8dfda4fb0788ee515e783592199ad008668c243
mark-marshall <mark.marshallgp@gmail.com>
2019-02-05 20:48:08 +0000

    [MVP] Added smooth scroll on Learn more anchor button

--- js/anchors.js
+++ js/anchors.js
@@ -1,7 +1,18 @@
-// Event Handlers
-
+// Load page to the bottom
 function loadBottom() {
   window.scrollTo(0, document.querySelector('html').scrollHeight);
 }
 
 window.addEventListener('DOMContentLoaded', loadBottom);
+
+// Page scroll when Learn More button is clicked
+function learnMore() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+const learnMoreButton = document.querySelector('.learn-more');
+
+learnMoreButton.addEventListener('click', learnMore);
