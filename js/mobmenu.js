9bab34e8d70f508f1481bb62071d25dfbdd57050
mark-marshall <mark.marshallgp@gmail.com>
2019-02-06 11:48:04 +0000

    [MVP] Added mobile/tablet menu open/close functionality

--- js/mobmenu.js
+++ js/mobmenu.js
@@ -1,2 +1,12 @@
 //Mobile Menu
 
+function toggleMenu() {
+    menu.classList.toggle('menu-open');
+}
+
+const menu = document.querySelector('.mobile-menu');
+const menuButton = document.querySelector('.mobile-menu-extras');
+const close = document.querySelector('.close');
+
+menuButton.addEventListener('click', toggleMenu);
+close.addEventListener('click', toggleMenu);
