80bcbdbc882a353efe36f4e828d181e974a3a6a2
mark-marshall <mark.marshallgp@gmail.com>
2019-02-07 11:39:57 +0000

    [STRETCH] Added cross rotation animation on FAQ answer open

--- js/accordian.js
+++ js/accordian.js
@@ -5,6 +5,7 @@ class Panel {
     this.panel = panel;
     this.headers = this.panel.querySelector('.headering');
     this.answers = this.panel.querySelector('.answers');
+    this.cross = this.panel.querySelector('.headering p');
 
     this.headers.addEventListener('click', () => this.toggleAnswers());
   }
@@ -12,11 +13,23 @@ class Panel {
   toggleAnswers() {
     if (this.answers.classList.contains('show')) {
       this.answers.classList.toggle('show');
+      this.cross.classList.toggle('rotate');
     } else {
-      document.querySelectorAll('.answers').forEach(item => item.classList.remove('show'));
-      document.querySelectorAll('.answers').forEach(item => item.style.maxHeight ="0px");
+      document
+        .querySelectorAll('.answers')
+        .forEach(item => item.classList.remove('show'));
+      document
+        .querySelectorAll('.answers')
+        .forEach(item => (item.style.maxHeight = '0px'));
+
+      document
+        .querySelectorAll('.headering p')
+        .forEach(item => item.classList.remove('rotate'));
+
       this.answers.classList.toggle('show');
       this.answers.style.maxHeight = `${this.answers.scrollHeight}px`;
+
+      this.cross.classList.toggle('rotate');
     }
   }
 }
