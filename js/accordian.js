6978a5f01f76ad5bcc19153e3fa1689ac050063e
mark-marshall <mark.marshallgp@gmail.com>
2019-02-07 11:05:48 +0000

    [STRETCH] Added HTML, CSS, and adjusted JS for header section on accordion

--- js/accordian.js
+++ js/accordian.js
@@ -3,8 +3,8 @@
 class Panel {
   constructor(panel) {
     this.panel = panel;
-    this.headers = this.panel.querySelector('.panel h3');
-    this.answers = this.panel.querySelector('.panel p');
+    this.headers = this.panel.querySelector('.headering');
+    this.answers = this.panel.querySelector('.answers');
 
     this.headers.addEventListener('click', () => this.toggleAnswers());
   }
@@ -13,8 +13,8 @@ class Panel {
     if (this.answers.classList.contains('show')) {
       this.answers.classList.toggle('show');
     } else {
-      document.querySelectorAll('.panel p').forEach(item => item.classList.remove('show'));
-      document.querySelectorAll('.panel p').forEach(item => item.style.maxHeight ="0px");
+      document.querySelectorAll('.answers').forEach(item => item.classList.remove('show'));
+      document.querySelectorAll('.answers').forEach(item => item.style.maxHeight ="0px");
       this.answers.classList.toggle('show');
       this.answers.style.maxHeight = `${this.answers.scrollHeight}px`;
     }
