6fa0dc6bb557114c68c708b4e620d8652d3bed19
unknown <jjamal95@gmail.com>
2019-02-13 20:58:01 -0600

    Built the Navigation system design, and expanded view design for navigation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// JS goes here
+// NAV XXXXXXX
+
+const showNav = () => {
+    
+    menu.classList.toggle("menu--open");
+  }
+
+const menu = document.querySelector(".menu");
+
+const hamButton= document.querySelectorAll(".nav-button");
+for(let i =0; i < hamButton.length; i++) {
+      hamButton[i].addEventListener("click", showNav);
+
+}
