6337b23b4c04b5252ab3679a246e77356f8062a5
Dexter Lewis <dexter.lewis@gmail.com>
2019-02-15 16:47:36 -0500

    Add class for tab links and tab content

--- js/index.js
+++ js/index.js
@@ -1,14 +1,30 @@
 // JS goes here
 
-function toggle() {
-    menu.classList.toggle('menu-hidden');
+    const toggle = () => {
+        menu.classList.toggle('menu-hidden');
+    }
+    const menuBtn = document.querySelector('.menu-open');
+    const closeBtn = document.querySelector('.menu-close');
+    const menu = document.querySelector('.menu-hidden');
+    
+    menuBtn.addEventListener('click', toggle);
+    closeBtn.addEventListener('click', toggle);
+
+
+class TabLinks {
+    constructor(tabs){
+        this.tabs = tabs;
+
+        console.log(this.tabs);
+    }
+    
 }
 
 
+class TabContent {
+    constructor(content) {
+        this.content = content;
+    }
+}
 
-const menuBtn = document.querySelector('.menu-open');
-const closeBtn = document.querySelector('.menu-close');
-const menu = document.querySelector('.menu-hidden');
-
-menuBtn.addEventListener('click', toggle);
-closeBtn.addEventListener('click', toggle);
+document.querySelectorAll('.tab-link').forEach(tab => new TabLinks(tab));
