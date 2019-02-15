265c91b1aa8a2a8572f714c2906590eeb17cec8f
Dexter Lewis <dexter.lewis@gmail.com>
2019-02-14 19:54:56 -0500

    Add nav menu click functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // JS goes here
+
+function toggle() {
+    menu.classList.toggle('menu-hidden');
+}
+
+
+
+const menuBtn = document.querySelector('.menu-open');
+const closeBtn = document.querySelector('.menu-close');
+const menu = document.querySelector('.menu-hidden');
+
+menuBtn.addEventListener('click', toggle);
+closeBtn.addEventListener('click', toggle);
