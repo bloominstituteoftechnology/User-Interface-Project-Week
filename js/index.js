b2583ed74ef95e682b562911aeee6e54ef8f472e
Dexter Lewis <dexter.lewis@gmail.com>
2019-02-16 12:22:11 -0500

    Add tab functionality to Services page

--- js/index.js
+++ js/index.js
@@ -14,16 +14,28 @@
 class TabLinks {
     constructor(tabs){
         this.tabs = tabs;
-
-        
+        // Add click event listener to all the tabs
         this.tabs.addEventListener('click', this.select.bind(this));
+        // Store the number of the <div class="tab-link" data-tab="2">Construction</div> = 2
+        this.data = tabs.dataset.tab;
+        // Store the corresponding tab content
+        this.dataElement = document.querySelector(`.tab-item[data-tab="${this.data}"]`);
+        // Craete new tab content class with the corresponding tab content
+        this.tabContent = new TabContent(this.dataElement);
+        
+        
+        
     }
     select() {
+        // Remove the selected class from all tabs
         const links = document.querySelectorAll('.tab-link');
         links.forEach(link => {
             link.classList.remove('tab-link-selected');
-        })
+        });
+        // Add the selected class to the tab that was clicked
         this.tabs.classList.add('tab-link-selected');
+        // Call the 
+        this.tabContent.select();
     }
     
 }
@@ -32,6 +44,14 @@ class TabLinks {
 class TabContent {
     constructor(content) {
         this.content = content;
+    }
+    select() {
+        const items = document.querySelectorAll('.tab-item');
+        // console.log(items);
+        items.forEach(item => item.classList.remove('tab-item-selected'));
+        
+        this.content.classList.add('tab-item-selected');
+        
     }
 }
 
