c177ba10995a24b39172f4f2d190f226830c7823
raberin <rmvaberin@gmail.com>
2019-02-18 14:30:01 -0800

    Finished navigation overlay

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // JS goes here
+//Navigation Overlay
+//Open nav when clickimg on hamburger
+function openNav() {
+    document.querySelector(".nav-overlay").style.width = "100%";
+    document.querySelector(".menu-button-close").style.display = "block";
+    document.querySelector(".menu-button-expand").style.display = "none";
+
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.querySelector(".nav-overlay").style.width = "0%";
+    document.querySelector(".menu-button-close").style.display = "none";
+    document.querySelector(".menu-button-expand").style.display = "block";
+  }
