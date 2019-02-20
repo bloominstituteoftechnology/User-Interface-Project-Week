4c3e0dd5baac16594b92d09601eb69debfc04dd3
raberin <rmvaberin@gmail.com>
2019-02-19 20:23:36 -0800

    Prettier'd all my files // Completed MVP

--- js/index.js
+++ js/index.js
@@ -24,22 +24,24 @@ class Tab {
 
     // Using the custom data attribute get the associated Item element
     //selecting each tab using the [data-somthing] for specificty
-    this.contentElement = document.querySelector(`.content[data-tab='${this.data}']`);
+    this.contentElement = document.querySelector(
+      `.content[data-tab='${this.data}']`
+    );
 
     //Using the data attrs, create a new instance of the TabContent class
     this.contentElement = new TabContent(this.contentElement);
 
-    this.tab.addEventListener('click', () => this.select());
+    this.tab.addEventListener("click", () => this.select());
   }
   select() {
     // Get all of the elements with the tab class
-    const tabs = document.querySelectorAll('.tab');
+    const tabs = document.querySelectorAll(".tab");
     // Using a loop or the forEach method remove the 'active-tab' class from all of the links
-    //This removed 
-    Array.from(tabs).forEach((tab) => tab.classList.remove('active-tab'));
+    //This removed
+    Array.from(tabs).forEach(tab => tab.classList.remove("active-tab"));
 
     // Add a class named "tabs-link-selected" to this link
-    this.tab.classList.add('active-tab');
+    this.tab.classList.add("active-tab");
 
     // Call the select method on the item associated with this link
     this.contentElement.showContent();
@@ -48,17 +50,19 @@ class Tab {
 
 class TabContent {
   constructor(content) {
-      this.content = content;
+    this.content = content;
   }
 
   showContent() {
-      const contents = document.querySelectorAll('.content');
+    const contents = document.querySelectorAll(".content");
 
-      //Removing the class "active-content" from each element
-      Array.from(contents).forEach((content) => content.classList.remove('active-content'));
+    //Removing the class "active-content" from each element
+    Array.from(contents).forEach(content =>
+      content.classList.remove("active-content")
+    );
 
-      //Add a class name "active-content" to this element
-      this.content.classList.add('active-content');
+    //Add a class name "active-content" to this element
+    this.content.classList.add("active-content");
   }
 }
 
