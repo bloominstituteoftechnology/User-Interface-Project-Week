ce9a2ccf92734c7a8cbe176c3cfcaf0490dba121
Mariana <mhuh0176@gmail.com>
2019-03-04 18:13:49 -0800

    made modifications to muliple less files

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // JS goes here
+class Dropdown {
+  constructor(element) {
+    this.element = element;
+    this.button = this.element.querySelector('.hamburger');
+    this.closeButton = this.element.querySelector('.close');
+    this.content = this.element.querySelector('.site-nav');
+    this.closeBtnContent = document.querySelector('.site-nav');
+
+    this.button.addEventListner('click', (event) => { this.toggleContent(event) })
+    this.closeButton.addEventListner('click', (event) => { this.closeContent(event) })
+  }
+
+  toggleContent(event) {
+    this.content.classList.toggle('site-nav');
+  }
+  closeContent(event) {
+    this.closeBtnContent.classList.toggle('site-nav');
+  }
+}
+
+
+
+let dropdowns = document.querySelectorAll('header');
+dropdowns = Array.from(dropdowns).map(dropdown => dropdown(dropdown));
