dab0ac07e4ef02ad8b96f75e52ebc5d51f379ba3
Mariana <mhuh0176@gmail.com>
2019-03-06 19:02:05 -0800

    added js to navigation

--- js/index.js
+++ js/index.js
@@ -1,25 +1,51 @@
 // JS goes here
+// class Dropdown {
+//   constructor(dropdown) {
+//     this.dropdown = dropdown;
+//     this.button = this.dropdown.querySelector('.hamburger');
+//     this.closeButton = this.dropdown.querySelector('.close');
+//     this.content = this.dropdown.querySelector('.site-nav');
+//     this.closeBtnContent = document.querySelector('.close');
+
+//     this.button.addEventListner('click', (event) => { this.toggleContent(event) })
+//     this.closeButton.addEventListner('click', (event) => { this.closeContent(event) })
+//   }
+
+//   toggleContent(event) {
+//     this.content.classList.toggle('toggle-on');
+//   }
+//   closeContent(event) {
+//     this.closeBtnContent.classList.toggle('toggle-on');
+//   }
+// }
+
+
+
+// let dropdowns = document.querySelectorAll('.menucontainer');
+// dropdowns.Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
+
 class Dropdown {
   constructor(dropdown) {
-    this.dropdown = dropdown;
-    this.button = this.dropdown.querySelector('.hamburger');
-    this.closeButton = this.dropdown.querySelector('.close');
-    this.content = this.dropdown.querySelector('.site-nav');
-    this.closeBtnContent = document.querySelector('.site-nav');
+    this.button = dropdown.querySelector('.hamburger');
+    this.closeButton = dropdown.querySelector('.close');
+    this.content = dropdown.querySelector('.site-nav');
+    this.closeBtnContent = dropdown.querySelector('.close');
 
-    this.button.addEventListner('click', (event) => { this.toggleContent(event) })
-    this.closeButton.addEventListner('click', (event) => { this.closeContent(event) })
+    this.button.addEventListner('click', () => this.toggleContent());
+    this.closeBtnContent.addEventListner('click', () => this.closeContent());
   }
 
-  toggleContent(event) {
+  toggleContent() {
+    this.button.classList.toggle('toggle-on');
     this.content.classList.toggle('toggle-on');
   }
-  closeContent(event) {
+
+  closeBtnContent() {
     this.closeBtnContent.classList.toggle('toggle-on');
   }
 }
 
 
 
-let dropdowns = document.querySelectorAll('header');
-dropdowns.Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
+const dropdowns = document.querySelectorAll('.menucontainer');
+dropdowns.forEach(dropdown => new Dropdown(dropdown));
