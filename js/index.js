3edca9af4305f41f7a306895ddbf79c51226c661
JacobLayton <jlayjlay3@gmail.com>
2019-03-14 21:28:40 -0700

    Navbar complete

--- js/index.js
+++ js/index.js
@@ -65,11 +65,59 @@ class TabsItem {
     }
 }
 
+function debounce(callback, wait = 20, immediate = true) {
+    let timeout = null 
+    
+    return function() {
+      const callNow = immediate && !timeout
+      const next = () => callback.apply(this, arguments)
+      
+      clearTimeout(timeout)
+      timeout = setTimeout(next, wait)
+  
+      if (callNow) {
+        next()
+      };
+    };
+};
+
 
 let tabs = document.querySelectorAll('.tabs');
 tabs = Array.from(tabs).map(tab => new Tabs(tab));
 
+//-------------------------NavBar------------------//
 
+
+
+
+let navBar = document.querySelector('.navigation');
+
+function checkSlide(e) {
+    console.log(scrollY);
+    console.log(navBar);
+    if (scrollY > 340 ) {
+        navBar.style.background = "#828282";
+        navBar.style.opacity = "0.85"
+    } else {
+        navBar.style.background = "";
+        navBar.style.opacity = ""
+    }
+    
+}
+// var checkSlide = (e) => console.log(e);
+
+
+window.addEventListener('scroll', checkSlide);
+
+
+
+
+
+
+// document.addEventListener("scroll", (e) => {
+//     navBar.style.background = "#828282";
+//     navBar.style.opacity = "0.9";
+// });
 
 
 
