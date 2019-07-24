72d6d516c5b200592844f6515f5d7f7ba29bb801
Luisan <luisangelsc@gmail.com>
2019-07-24 16:53:17 -0700

    fix carousel, clean up animations

--- js/greensock-nav.js
+++ js/greensock-nav.js
@@ -9,17 +9,17 @@
   hamburger.addEventListener('click', () => {
     var tlLinks = new TimelineMax();
     if (nav.classList.length === 2) {
-      TweenMax.fromTo(nav, 1, {opacity: 0, height: "auto"}, {opacity:.92, height: window.innerHeight})
+      TweenMax.to(nav, .8,  {opacity:.92, height: window.innerHeight})
 
       tlLinks.add('start')
-      .from(links[0], 1.6, {opacity: 0, x:-400}, {opacity: 1})
-      .from(links[1], 1.3, {opacity: 0, x:-400}, {opacity: 1}, 'start')
-      .from(links[2], 1, {opacity: 0, x:-400}, {opacity: 1}, 'start')
-      .from(links[3], .7, {opacity: 0, x:-400}, {opacity: 1}, 'start')
+      .from(links[0], 1.3, {opacity: 0, x:-400}, {opacity: 1})
+      .from(links[1], 1, {opacity: 0, x:-400}, {opacity: 1}, 'start')
+      .from(links[2], .8, {opacity: 0, x:-400}, {opacity: 1}, 'start')
+      // .from(links[3], .6, {opacity: 0, x:-400}, {opacity: 1}, 'start')
     } else {
-      TweenMax.to(nav, 1, {height: 50})
-      TweenMax.fromTo(hamburger, 1, {opacity: 0}, {opacity: 1})
-      TweenMax.fromTo(logo, 1, {opacity: 0}, {opacity: 1})
+      TweenMax.to(nav, .8, {height: 50})
+      TweenMax.fromTo(hamburger, .8, {opacity: 0}, {opacity: 1})
+      // TweenMax.fromTo(logo, .8, {opacity: 0}, {opacity: 1})
     }
 
 
