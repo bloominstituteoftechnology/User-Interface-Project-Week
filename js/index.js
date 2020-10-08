8f5843e3b5d5f70566327163c2b1d09d3322155a
Justin A <jarata@gmail.com>
2020-10-08 10:30:51 -0700

    Project Page updated carousel to gallery, also updated images for "projects", as well as desktop styling

--- js/index.js
+++ js/index.js
@@ -40,3 +40,36 @@ function buttonClick(event) {
 for(let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', buttonClick);
 }
+
+// Projects Carousel for Mobile
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("project-display");
+  const dots = document.getElementsByClassName("demo");
+  const captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
